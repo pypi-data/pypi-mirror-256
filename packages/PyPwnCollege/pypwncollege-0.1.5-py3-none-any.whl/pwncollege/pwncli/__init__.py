f8263839bd2f9cfbf7d6371891e7632e9b2d323e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:36:32 +0000

    Add package PyPwnCollege/0.1.5/pypwncollege-0.1.5-py3-none-any.whl

---
+++ packages/PyPwnCollege/pypwncollege-0.1.5-py3-none-any.whl/pwncollege/pwncli/__init__.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+import argparse
+from random import choice
+import pwncollege
+import os
+from .challenge import challenge
+from .get import get
+from pwncollege.pwncollege import colors
+
+def get_parser():
+    # Begin original commands - mostly related to authentication
+    parser = argparse.ArgumentParser(
+        description="Interact with pwncollege from the command line.",
+        usage="pwncli [-h] [-c CACHE] [-v] {login,get,challenge} ..."
+        )
+    parser.add_argument('-c', '--cache', type=str, required=False, help='filesystem location for cache. Default is ~/.pwncli.json', default="~/.pwncli.json")
+    subparsers = parser.add_subparsers(title='subcommands', dest='subcommand')
+
+    # Begin login subcommand
+    parser_login = subparsers.add_parser('login', help="Login to pwncollege. Stores session cookie in cache ")
+    parser_login.add_argument('-u', '--username', required=False, help='Username/Email for pwncollege.')
+    parser_login.add_argument('-p', '--password', required=False, help='Password for pwncollege. If not provided, will prompt.')
+
+    # Begin get subcommand
+    parser_get = subparsers.add_parser('get', help="Get dojos/modules and their scoreboards.")
+    parser_get.add_argument('-d', '--dojos', required=False, action="store_true", help='Get listing of all available dojos')
+    parser_get.add_argument('-m', '--modules', required=False, help='Get listing of all available modules in a dojo')
+    parser_get.add_argument('-c', '--challenges', required=False, help='Get listing of all available challenges in a module inside a dojo')
+    parser_get.add_argument('-dr', '--dojo-ranking', required=False, help='Get ranking of top users in a dojo')
+    parser_get.add_argument('-mr', '--module-ranking', required=False, help='Get ranking of top users in a module inside a dojo')
+    parser_get.add_argument('-b', '--belt', required=False, help='Get JSON data of all belted users')
+    parser_get.add_argument('-i', '--info', required=False, nargs='?', const='0', help='Get information about your user(default) or a specific user by id(int).')
+
+
+    # Begin challenge subcommand
+    parser_chall = subparsers.add_parser('challenge', help="Interact with challenges. You need a dojo+module+challenge_id to start a challenge.")
+    parser_chall.add_argument('-d', '--dojo', required=False, help='Name of the dojo')
+    parser_chall.add_argument('-m', '--module', required=False, help='Name of the module')
+    parser_chall.add_argument('-c', '--challenge', required=False, help='Name of the challenge')
+    parser_chall.add_argument('-r', '--practice', required=False, help='Start in practice mode. Default is False.', default=False, action='store_true')
+    parser_chall.add_argument('-s', '--start-docker', action="store_true", help='Start Docker instance.')
+
+    parser_chall.add_argument('-p', '--path', nargs='?', const=os.getcwd(), type=str, help='Download challenge files to the specified path.')
+    parser_chall.add_argument('-f', '--flag', type=str, help='Submit flag.')
+    parser_chall.add_argument('-e', '--execute', type=str, help='Run a command in container.')
+    parser_chall.add_argument('-i', '--interactive', action="store_true", help='Run an interactive shell in container.')
+
+    return parser
+
+class PWNCLI:
+    def __init__(self) -> None:
+        self.parser = get_parser()
+        self.args, _ = self.parser.parse_known_args()
+        self.client = None
+        self.myChall = None
+
+        if self.args == argparse.Namespace(cache="~/.pwncli.json", subcommand=None):
+            self.parser.print_help()
+            exit()
+
+        self.subcommand = self.args.subcommand
+
+    def login(self):
+        """Login to pwncollege"""
+        cache = os.path.expanduser(self.args.cache)
+
+        if not os.path.exists(cache):
+            if self.subcommand != 'login':
+                print(colors.red + "You must login first." + colors.reset)
+            elif self.subcommand == 'login' and not self.args.username:
+                print(colors.red + "Username is required." + colors.reset)
+            else:
+                self.client = pwncollege.PWNClient(
+                    email=self.args.username, password=self.args.password,
+                    cache=cache)
+        elif self.subcommand == 'login' and self.args.username:
+            self.client = pwncollege.PWNClient(
+                    email=self.args.username, password=self.args.password)
+            self.client.dump_to_cache(cache)
+        elif self.subcommand == 'login':
+            self.client = pwncollege.PWNClient(cache=cache, notif=True)
+        else:
+            self.client = pwncollege.PWNClient(cache=cache, notif=False)
+
+    def run(self):
+        """Executes the specified subcommand"""
+        self.login()
+
+        if self.subcommand == 'challenge':
+            challenge(self)
+        elif self.subcommand == 'get':
+            """if no flags, print help message"""
+            get(self)
+        elif self.subcommand != 'login':
+            print(colors.yellow + "Use the -h/--help flag for basic help information." + colors.reset)
+            
+
+def main():
+    flavortext = [
+        "Skill issue.",
+        "when new challenges? SoonTM",
+        ":feelsyanman:",
+        "dojo broken 504 error :c",
+        "check the man page",
+        "man chmod",
+        "it's too hard/ctfy/easy.That's your attitude to everything",
+        "how get belts?!?!?",
+        "If you're using Windows, don't.",
+        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA...",
+        "Hello Hackers!!!",
+        "zeeshan probably cheesed it :frowning:",
+    ]
+
+    print(f'\n\033[92mpwncli - v0.1.0 | {choice(flavortext)}\033[0m')
+    print('\033[35mauthor: @Zeeshan12340 (www.zeeshan1234.tech)\033[0m\n')
+    
+    obj = PWNCLI()
+    try:
+        obj.run()
+    except KeyboardInterrupt:
+        print("\nExiting...")
---
+++ packages/PyPwnCollege/pypwncollege-0.1.5-py3-none-any.whl/pwncollege/pwncli/get.py
@@ -0,0 +1,95 @@
+from pwncollege.pwncollege import colors
+import json
+import argparse
+
+def get(self):
+    """if no flags, print help message"""
+    if not self.args.dojos and not self.args.modules and not self.args.challenges and not self.args.dojo_ranking and not self.args.module_ranking and not self.args.belt and not self.args.info:
+        subparsers_actions = [
+            action for action in self.parser._actions 
+            if isinstance(action, argparse._SubParsersAction)]
+        for subparsers_action in subparsers_actions:
+            for choice, subparser in subparsers_action.choices.items():
+                if choice == "get":
+                    print(subparser.format_help())
+        exit()
+        
+    if self.args.dojos and not self.args.modules and not self.args.challenges:
+        dojos = self.client.get_dojos()
+        print("-"*12 + "Dojos" + "-"*13)
+        for dojo in dojos:
+            print("| " + dojo + " "*(27-len(dojo)) + "|")
+        print("-" * 30)
+    
+    if self.args.modules and not self.args.challenges:
+        dojo = self.args.modules
+        modules = self.client.get_modules(dojo)
+        if modules == []:
+            exit()
+        print("-"*11 + "Modules" + "-"*12)
+        for module in modules:
+            print("| " + module + " "*(27-len(module)) + "|")
+        print("-" * 30)
+    
+    if self.args.challenges:
+        if not self.args.modules:
+            print(colors.red + "You must specify a dojo using the -m/--modules flag." + colors.reset)
+            exit()
+        dojo = self.args.modules
+        module = self.args.challenges
+        challenges = self.client.get_challenges(dojo, module)
+        if challenges == []:
+            exit()
+        print("-"*10 + "Challenges" + "-"*10)
+        for challenge in challenges:
+            print("| " + challenge.id + " "*(27-len(challenge.id)) + "|")
+        print("-" * 30)
+    
+    if self.args.dojo_ranking and not self.args.module_ranking:
+        dojo = self.args.dojo_ranking
+        ranking = self.client.get_dojo_ranking(dojo)
+        print("-"*9 + "Dojo Ranking" + "-"*9)
+        for index, user in enumerate(ranking):
+            print("| " + f"{index+1}. " + user + " "*(25-len(user)-len(str(index+1))) + "|")
+        print("-" * 30)
+
+    if self.args.module_ranking:
+        if not self.args.dojo_ranking:
+            print(colors.red + "You must specify a dojo using the -dr/--dojo-ranking flag." + colors.reset)
+            exit()
+        dojo = self.args.dojo_ranking
+        module = self.args.module_ranking
+        ranking = self.client.get_module_ranking(dojo, module)
+        print("-"*8 + "Module Ranking" + "-"*8)
+        for index, user in enumerate(ranking):
+            print("| " + f"{index+1}. " + user + " "*(25-len(user)-len(str(index+1))) + "|")
+        print("-" * 30)
+    
+    if self.args.belt:
+        belt_color = self.args.belt
+        valid_belts = ["orange", "yellow", "green", "blue", "black"]
+        if belt_color not in valid_belts:
+            print(colors.red + "Invalid belt color." + colors.reset)
+            print(f"Valid belt colors: {', '.join(valid_belts)}")
+            exit()
+        belts = self.client.get_belts()["users"]
+        for user in belts.values():
+            if user["color"] == belt_color:
+                print(json.dumps(user, indent=2))
+    
+    if self.args.info:
+        if self.args.info == '0':
+            user = self.client.user
+        else:
+            user = self.client.get_user(int(self.args.info))
+        print("-"*10 + "User Info" + "-"*10)
+        print("| " + f"Username: {user.name}" + " "*(25-len(user.name)-9) + "|")
+        print("| " + f"Ranking: {user.ranking}" + " "*(25-len(user.ranking)-8) + "|")
+        print("| " + f"Point: {user.points}" + " "*(25-len(user.points)-6) + "|")
+        print("| " + f"Belt: {user.belt}" + " "*(25-len(user.belt)-5) + "|")
+        if user.website:
+            print("| " + f"Website: " + " "*(25-len("Website:")) + "|")
+            print(f"|   {user.website} " + " "*(23-len(user.website)) + "|")
+        if user.country_name:
+            print("| " + f"Country: {user.country_name}" + " "*(25-len(user.country_name)-9) + "|")
+        print("-" * 30)
