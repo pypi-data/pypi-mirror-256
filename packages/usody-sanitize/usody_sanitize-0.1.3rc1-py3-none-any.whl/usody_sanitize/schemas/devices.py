c5632f1b034aae46db8ffa39c966e719aaf8e9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:33 +0000

    Add package usody-sanitize/0.1.3rc1/usody_sanitize-0.1.3rc1-py3-none-any.whl

---
+++ packages/usody-sanitize/usody_sanitize-0.1.3rc1-py3-none-any.whl/usody_sanitize/schemas/devices.py
@@ -0,0 +1,27 @@
+"""
+Devices Schema
+==============
+
+This module contains the schema of the disk data, `Device.export_data`
+contains the output of each command when extracting data. The other
+values are duplicated from this `export_data` variable.
+"""
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from .export_data import ExportData
+
+
+class Device(BaseModel):
+    """Information of the device."""
+    manufacturer: Optional[str] = Field(default=None)
+    model: Optional[str] = Field(default=None)
+    serial_number: Optional[str] = Field(default=None)
+    connector: Optional[str] = Field(default=None,
+                                     description="IDE/SATA/SCSI/SAS/M.2/U.2")
+    size: Optional[str] = Field(default=None, description="Disk size in bites")
+    storage_medium: Optional[str] = Field(default=None,
+                                          description="HDD/SSD/SSDHD")
+
+    export_data: Optional[ExportData] = Field(default=None)
