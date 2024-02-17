31a08a0c3b5b2422ae0a47f3af64d3ad649d0384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:31 +0000

    Add package bino-dag-prob/1.0/bino_dag_prob-1.0.tar.gz

---
+++ packages/bino-dag-prob/bino_dag_prob-1.0.tar.gz/bino_dag_prob-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bino_dag_prob',
+      version='1.0',
+      description='Binomial and Gaussian distributions',
+      packages=['bino_dag_prob'],
+      zip_safe=False)
