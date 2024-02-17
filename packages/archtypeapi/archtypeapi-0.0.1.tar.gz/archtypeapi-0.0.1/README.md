b9950f92df871488f46bb3dcb2331f51bcf22a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:49 +0000

    Add package archtypeapi/0.0.1/archtypeapi-0.0.1.tar.gz

---
+++ packages/archtypeapi/archtypeapi-0.0.1.tar.gz/archtypeapi-0.0.1/README.md
@@ -0,0 +1,7 @@
+# ArchTypeAPI
+
+A simple API for a clothing brand in the works.
+
+[A tutorial on how to use this API](https://youtu.be/2NWXMy-g-ZM)
+
+Docs soon!
---
+++ packages/archtypeapi/archtypeapi-0.0.1.tar.gz/archtypeapi-0.0.1/src/archtypeapi/__init__.py
@@ -0,0 +1,12 @@
+class API:
+    def listProductsAvailable(self):
+        return """i feel like everything is coming to an end.
+everything will at one point, thats inevitable..
+
+but yeah, i feel like shit, im going insane, parents arent
+a fan that im like this, explaining the time my dad slapped
+the shit outta me when i told him im suicidal.. yiipee
+
+call client.pricesForProducts()"""
+    def pricesForProducts(self):
+        return "http://sndup.net/jm2k"
