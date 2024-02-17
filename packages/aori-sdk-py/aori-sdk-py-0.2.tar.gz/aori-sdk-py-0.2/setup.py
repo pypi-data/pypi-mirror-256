d05336bf4f08236686f3e95f3331aa229040b373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:06 +0000

    Add package aori-sdk-py/0.2/aori-sdk-py-0.2.tar.gz

---
+++ packages/aori-sdk-py/aori-sdk-py-0.2.tar.gz/aori-sdk-py-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='aori-sdk-py',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'python-dotenv',
+        'websocket-client'
+    ],
+    author='Joshua Baker',
+    author_email='joshua@aori.io',
+    description='aori.io python sdk',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important to ensure markdown is rendered correctly on PyPI
+    keywords='aori',
+    url='https://github.com/aori-io/aori-sdk-py'
+)
