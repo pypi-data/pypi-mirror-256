73fdbee4a4b183b7a20f741df02c6afb1af89a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:59 +0000

    Add package projectjoki-steampy/1.1.2/projectjoki-steampy-1.1.2.tar.gz

---
+++ packages/projectjoki-steampy/projectjoki-steampy-1.1.2.tar.gz/projectjoki-steampy-1.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import sys
+
+if not sys.version_info[0] == 3 and sys.version_info[1] < 8:
+    sys.exit('Python < 3.8 is not supported')
+
+version = '1.1.2'
+
+setup(
+    name='projectjoki-steampy',
+    packages=['steampy', 'test', 'examples', ],
+    version=version,
+    description='A Steam lib for trade automation',
+    long_description='A Steam lib for trade automation',
+    author='Michał Bukowski',
+    author_email='gigibukson@gmail.com',
+    license='MIT',
+    url='https://github.com/bukson/steampy',
+    download_url='https://github.com/bukson/steampy/tarball/' + version,
+    keywords=['steam', 'trade', ],
+    classifiers=[],
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "rsa"
+    ],
+)
