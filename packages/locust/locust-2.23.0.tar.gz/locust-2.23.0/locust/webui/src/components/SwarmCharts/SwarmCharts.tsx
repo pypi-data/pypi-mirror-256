56fe569448c1a65695b64619fa09484a0d187756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:13 +0000

    Add package locust/2.23.0/locust-2.23.0.tar.gz

---
+++ packages/locust/locust-2.23.0.tar.gz/locust-2.23.0/locust/webui/src/components/SwarmCharts/SwarmCharts.tsx
@@ -0,0 +1,51 @@
+import { connect } from 'react-redux';
+
+import LineChart, { ILineChartProps } from 'components/LineChart/LineChart';
+import { swarmTemplateArgs } from 'constants/swarm';
+import { IRootState } from 'redux/store';
+import { ICharts } from 'types/ui.types';
+
+const percentilesToChartLines = swarmTemplateArgs.percentilesToChart
+  ? swarmTemplateArgs.percentilesToChart.map(percentile => ({
+      name: `${percentile * 100}th percentile`,
+      key: `responseTimePercentile${percentile}` as keyof ICharts,
+    }))
+  : [];
+
+const availableSwarmCharts: ILineChartProps[] = [
+  {
+    title: 'Total Requests per Second',
+    lines: [
+      { name: 'RPS', key: 'currentRps' },
+      { name: 'Failures/s', key: 'currentFailPerSec' },
+    ],
+    colors: ['#00ca5a', '#ff6d6d'],
+  },
+  {
+    title: 'Response Times (ms)',
+    lines: [
+      ...percentilesToChartLines,
+      { name: 'Average Response Time', key: 'totalAvgResponseTime' },
+    ],
+    colors: ['#00ffee', '#eeff00'],
+  },
+  {
+    title: 'Number of Users',
+    lines: [{ name: 'Number of Users', key: 'userCount' }],
+    colors: ['#0099ff'],
+  },
+];
+
+export function SwarmCharts({ charts }: { charts: ICharts }) {
+  return (
+    <div>
+      {availableSwarmCharts.map((lineChartProps, index) => (
+        <LineChart key={`swarm-chart-${index}`} {...lineChartProps} charts={charts} />
+      ))}
+    </div>
+  );
+}
+
+const storeConnector = ({ ui: { charts } }: IRootState) => ({ charts });
+
+export default connect(storeConnector)(SwarmCharts);
