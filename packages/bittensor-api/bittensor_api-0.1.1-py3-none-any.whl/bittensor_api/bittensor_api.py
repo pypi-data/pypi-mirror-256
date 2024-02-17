154d0ea7de8b2dd3d94b002fc8b8a202d4492a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:52 +0000

    Add package bittensor-api/0.1.1/bittensor_api-0.1.1-py3-none-any.whl

---
+++ packages/bittensor-api/bittensor_api-0.1.1-py3-none-any.whl/bittensor_api/bittensor_api.py
@@ -0,0 +1,15 @@
+# Example PyPI (Python Package Index) Package
+
+
+class bittensorapi(object):
+    def __init__(self):
+        self.intialized = True
+
+    def __str__(self):
+        return "bittensorapi"
+
+    def __repr__(self):
+        return "bittensorapi"
+
+    def __call__(self):
+        return "bittensorapi"
---
+++ packages/bittensor-api/bittensor_api-0.1.1-py3-none-any.whl/bittensor_api/subnets/__init__.py
@@ -0,0 +1,1 @@
+from .SN2 import *
