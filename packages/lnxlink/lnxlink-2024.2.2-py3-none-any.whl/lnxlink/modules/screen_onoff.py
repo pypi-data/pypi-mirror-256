1203f00253e362b33d1440cfbb2dbfcea48ecdec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:26 +0000

    Add package lnxlink/2024.2.2/lnxlink-2024.2.2-py3-none-any.whl

---
+++ packages/lnxlink/lnxlink-2024.2.2-py3-none-any.whl/lnxlink/modules/screen_onoff.py
@@ -0,0 +1,42 @@
+"""Turns on or off the screen"""
+import re
+from shutil import which
+from .scripts.helpers import syscommand
+
+
+class Addon:
+    """Addon module"""
+
+    def __init__(self, lnxlink):
+        """Setup addon"""
+        self.name = "Screen OnOff"
+        self.lnxlink = lnxlink
+        if which("xset") is None:
+            raise SystemError("System command 'xset' not found")
+
+    def exposed_controls(self):
+        """Exposes to home assistant"""
+        return {
+            "Screen OnOff": {
+                "type": "switch",
+                "icon": "mdi:monitor",
+            }
+        }
+
+    def get_info(self):
+        """Gather information from the system"""
+        status = "ON"
+        if self.lnxlink.display is not None:
+            stdout, _, _ = syscommand(f"xset -display {self.lnxlink.display} q")
+            match = re.findall(r"Monitor is (\w*)", stdout)
+            if match:
+                status = match[0].upper()
+
+        return status
+
+    def start_control(self, topic, data):
+        """Control system"""
+        if data.lower() == "off":
+            syscommand(f"xset -display {self.lnxlink.display} dpms force off")
+        elif data.lower() == "on":
+            syscommand(f"xset -display {self.lnxlink.display} dpms force on")
