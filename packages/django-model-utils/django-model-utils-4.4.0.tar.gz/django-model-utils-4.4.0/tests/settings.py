35d8485d8d06170ec2dc916c11de767e5aa982ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:06 +0000

    Add package django-model-utils/4.4.0/django-model-utils-4.4.0.tar.gz

---
+++ packages/django-model-utils/django-model-utils-4.4.0.tar.gz/django-model-utils-4.4.0/tests/settings.py
@@ -0,0 +1,27 @@
+import os
+
+INSTALLED_APPS = (
+    'model_utils',
+    'tests',
+)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME", "modelutils"),
+        "USER": os.environ.get("DB_USER", 'postgres'),
+        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", 5432)
+    },
+}
+SECRET_KEY = 'dummy'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+USE_TZ = False
