1b3a2d16bdcdd257dda39824ea6c9208db9fa78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:40 +0000

    Add package plateFinderOne/1.0.0/plateFinderOne-1.0.0.tar.gz

---
+++ packages/plateFinderOne/plateFinderOne-1.0.0.tar.gz/plateFinderOne-1.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+# Setting up
+setup(
+    name="plateFinderOne",
+    version='1.0.0',
+    author="Guido Xhindoli",
+    author_email="<mail@gmail.com>",
+    description='A package find the plate',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["openpyxl", "pandas", "pyarrow"],
+    keywords=[ 'plateFinder', 'pythonsdaal', 'plate', 'sda', 'SDA'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
+
+
+
+
+
+
+
+
+
+
+
