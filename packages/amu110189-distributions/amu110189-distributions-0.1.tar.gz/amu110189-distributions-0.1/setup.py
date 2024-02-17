b9637292d928b327ba66482346b67b904f5766b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:51:17 +0000

    Add package amu110189-distributions/0.1/amu110189-distributions-0.1.tar.gz

---
+++ packages/amu110189-distributions/amu110189-distributions-0.1.tar.gz/amu110189-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='amu110189-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['amu110189-distributions'],
+      zip_safe=False)
