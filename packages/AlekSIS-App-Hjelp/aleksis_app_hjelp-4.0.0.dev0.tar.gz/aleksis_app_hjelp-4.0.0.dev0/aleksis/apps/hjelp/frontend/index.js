d49e06939b1cd3750e03a26eb199259724e7b772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:51 +0000

    Add package AlekSIS-App-Hjelp/4.0.0.dev0/aleksis_app_hjelp-4.0.0.dev0.tar.gz

---
+++ packages/AlekSIS-App-Hjelp/aleksis_app_hjelp-4.0.0.dev0.tar.gz/aleksis_app_hjelp-4.0.0.dev0/aleksis/apps/hjelp/frontend/index.js
@@ -0,0 +1,116 @@
+export default {
+  meta: {
+    inMenu: true,
+    titleKey: "hjelp.menu_title",
+    icon: "mdi-help-circle-outline",
+  },
+  props: {
+    byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+  },
+  children: [
+    {
+      path: "issues/report/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.reportIssue",
+      meta: {
+        inMenu: true,
+        titleKey: "hjelp.issues.menu_title",
+        icon: "mdi-bug-outline",
+        permission: "hjelp.report_issue_rule",
+      },
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "feedback/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.feedback",
+      meta: {
+        inMenu: true,
+        titleKey: "hjelp.feedback.menu_title",
+        icon: "mdi-message-text-outline",
+        permission: "hjelp.send_feedback_rule",
+      },
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "faq/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.faq",
+      meta: {
+        inMenu: true,
+        titleKey: "hjelp.faq.menu_title_list",
+        icon: "mdi-forum-outline",
+        permission: "hjelp.view_faq_rule",
+      },
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "faq/ask/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.askFaq",
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "faq/order/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.orderFaq",
+      meta: {
+        inMenu: true,
+        titleKey: "hjelp.faq.menu_title_manage",
+        icon: "mdi-priority-low",
+        permission: "hjelp.change_faq_rule",
+      },
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "faq/section/create/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.createFaqSection",
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "faq/section/:pk/delete/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.deleteFaqSection",
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "faq/question/create/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.createFaqQuestion",
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "faq/question/:pk/update/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.updateFaqQuestion",
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "faq/question/:pk/delete/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "hjelp.deleteFaqQuestion",
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+  ],
+};
