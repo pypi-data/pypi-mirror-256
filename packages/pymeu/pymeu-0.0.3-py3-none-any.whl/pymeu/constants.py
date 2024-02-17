95590efa84d76752fe52fd3a22261c95a450686c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:08 +0000

    Add package pymeu/0.0.3/pymeu-0.0.3-py3-none-any.whl

---
+++ packages/pymeu/pymeu-0.0.3-py3-none-any.whl/pymeu/constants.py
@@ -0,0 +1,32 @@
+CHUNK_SIZE = 1984
+
+HELPER_VERSIONS = {
+    '11.00.00'
+}
+
+ME_VERSIONS = {
+    '11.00.25.230'
+}
+
+PRODUCT_CODES = {
+    '102',
+    '51'
+}
+
+PRODUCT_TYPES = {
+    '24'
+}
+
+CREATE_DIR_SUCCESS = 183 # 'b\xb7'
+
+GET_UNK1_VALUES = {
+    b'\x02\x00'
+}
+
+GET_UNK2_VALUES = {
+    b'\x01\x60'
+}
+
+GET_UNK3_VALUES = {
+    b'\x64\x00'
+}
