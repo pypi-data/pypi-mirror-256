5c862f2fef90579456d97678e31775696272591e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:32 +0000

    Add package bplot/0.0.7/bplot-0.0.7-py3-none-any.whl

---
+++ packages/bplot/bplot-0.0.7-py3-none-any.whl/bplot/plot1d.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--grid', type=bool, default=True, help='Add grid to the plot')
+    args = parser.parse_args()
+
+
+
+    grid = args.grid
+
+
+
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+
+    ax.plot(np.sin(np.linspace(-np.pi, np.pi, 1000)))
+
+
+    if grid: ax.grid()
+
+    plt.show()
