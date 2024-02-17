1bc516f24217648c92d45232fd7393c4639117b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:31 +0000

    Add package Flask-JSONRPC/3.0.1/Flask_JSONRPC-3.0.1-py2.py3-none-any.whl

---
+++ packages/Flask-JSONRPC/Flask_JSONRPC-3.0.1-py2.py3-none-any.whl/flask_jsonrpc/helpers.py
@@ -0,0 +1,140 @@
+# Copyright (c) 2020-2022, Cenobit Technologies, Inc. http://cenobit.es/
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# * Neither the name of the Cenobit Technologies nor the names of
+#    its contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import typing as t
+from operator import getitem
+import itertools
+
+from .types import Types, Object
+
+if t.TYPE_CHECKING:
+    from .types import JSONRPCNewType
+
+
+def urn(name: str, *args: t.Any) -> str:  # noqa: ANN401
+    """Return the URN name.
+
+    >>> urn('python')
+    'urn:python'
+    >>> urn('python', 'Flask', 'JsonRPC')
+    'urn:python:flask:jsonrpc'
+    >>> urn('python', '/api/browse')
+    'urn:python:api:browse'
+    >>> urn(None)
+    Traceback (most recent call last):
+        ...
+    ValueError: name is required
+    >>> urn('')
+    Traceback (most recent call last):
+        ...
+    ValueError: name is required
+    """
+    if not name:
+        raise ValueError('name is required')
+    splitted_args = [arg.split('/') for arg in args]
+    st = ':'.join(list(itertools.chain(*splitted_args)))
+    st = st.rstrip(':').lstrip(':')
+    sep = ':' if len(args) > 0 else ''
+    return f"urn:{name}{sep}{st.replace('::', ':')}".lower()
+
+
+def from_python_type(tp: t.Any) -> 'JSONRPCNewType':  # noqa: ANN401
+    """Convert Python type to JSONRPCNewType.
+
+    >>> str(from_python_type(str))
+    'String'
+    >>> str(from_python_type(int))
+    'Number'
+    >>> str(from_python_type(dict))
+    'Object'
+    >>> str(from_python_type(list))
+    'Array'
+    >>> str(from_python_type(bool))
+    'Boolean'
+    >>> str(from_python_type(None))
+    'Null'
+    >>> str(from_python_type(t.NoReturn))
+    'Null'
+    """
+    for typ in Types:
+        if typ.check_type(tp):
+            return typ
+    return Object
+
+
+def get(obj: t.Dict[str, t.Any], path: str, default: t.Any = None) -> t.Any:  # noqa: ANN401
+    """Get the value at any depth of a nested object based on the path
+    described by `path`. If path doesn't exist, `default` is returned.
+    Args:
+        obj (dict): Object to process.
+        path (str): List or ``.`` delimited string of path describing
+            path.
+    Keyword Arguments:
+        default (mixed): Default value to return if path doesn't exist.
+            Defaults to ``None``.
+    Returns:
+        mixed: Value of `obj` at path.
+
+    Example:
+
+    >>> get(None, 'a')
+
+    >>> get(None, 'a', 'default')
+    'default'
+    >>> get({'a': 1}, 'a')
+    1
+    >>> get({'a': 1}, 'b')
+
+    >>> get({'a': 1}, 'b', 'default')
+    'default'
+    >>> get({'a': {'b': {'c': 1}}}, 'a.b.c')
+    1
+    >>> get({}, 'a.b.c')
+
+    >>> get([], 'a.b.c')
+
+    >>> get([], 'a.b.c', None)
+
+    """
+    if obj is None:
+        return default
+    if not isinstance(obj, dict):
+        return default
+
+    obj_val = obj
+    keys = path.split('.')
+    for key in keys:
+        try:
+            obj_val = getitem(obj_val, key)
+        except KeyError:
+            return default
+    return obj_val
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
---
+++ packages/Flask-JSONRPC/Flask_JSONRPC-3.0.1-py2.py3-none-any.whl/flask_jsonrpc/views.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2020-2022, Cenobit Technologies, Inc. http://cenobit.es/
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# * Neither the name of the Cenobit Technologies nor the names of
+#    its contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import typing as t
+
+from flask import typing as ft, jsonify, current_app, make_response
+from flask.views import MethodView
+
+from .site import JSONRPC_VERSION_DEFAULT, JSONRPC_DEFAULT_HTTP_HEADERS
+from .exceptions import JSONRPCError
+
+# Python 3.10+
+try:
+    from typing import Self
+except ImportError:  # pragma: no cover
+    from typing_extensions import Self
+
+if t.TYPE_CHECKING:
+    from .site import JSONRPCSite
+
+
+class JSONRPCView(MethodView):
+    def __init__(self: Self, jsonrpc_site: 'JSONRPCSite') -> None:
+        self.jsonrpc_site = jsonrpc_site
+
+    def post(self: Self) -> ft.ResponseReturnValue:
+        try:
+            response, status_code, headers = self.jsonrpc_site.dispatch_request()
+            if status_code == 204:
+                return make_response('', status_code, headers)
+            return make_response(jsonify(response), status_code, headers)
+        except JSONRPCError as e:
+            current_app.logger.exception('jsonrpc error')
+            response = {'id': None, 'jsonrpc': JSONRPC_VERSION_DEFAULT, 'error': e.jsonrpc_format}
+            return make_response(jsonify(response), e.status_code, JSONRPC_DEFAULT_HTTP_HEADERS)
