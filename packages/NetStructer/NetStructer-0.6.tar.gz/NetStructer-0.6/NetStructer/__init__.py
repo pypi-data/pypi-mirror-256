d9fdc6b858a64d5f9e2eb0e0f96856467b4cd428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:42:02 +0000

    Add package NetStructer/0.6/NetStructer-0.6.tar.gz

---
+++ packages/NetStructer/NetStructer-0.6.tar.gz/NetStructer-0.6/NetStructer/__init__.py
@@ -0,0 +1,6 @@
+from const import *
+
+try:
+	import cryptography 
+except ImportError:
+	raise ImportError('gryptography must br installed')
---
+++ packages/NetStructer/NetStructer-0.6.tar.gz/NetStructer-0.6/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+	name = 'NetStructer',
+	version = '0.6',
+	author='Haytam-Zakaria',
+	description="NetStructer is a python module for socket api.it's provides some function to send some type like list set str dict ...",
+	packages=setuptools.find_packages(),
+	long_description=open(r'E:\Secrit\description.txt','r').read(),
+	classifiers=[
+	'Programming Language :: Python :: 3',
+	'Operating System :: OS Independent',
+	'License :: OSI Approved :: MIT License'
+	]
+)
