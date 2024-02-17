f65c11f23f376326271e0617be14dda42ec18cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:36 +0000

    Add package tnreason/0.2.5/tnreason-0.2.5.tar.gz

---
+++ packages/tnreason/tnreason-0.2.5.tar.gz/tnreason-0.2.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="tnreason",
+    version="0.2.5",
+    author="Alex Goessmann",
+    author_email="alex.goessmann@web.de",
+    description="A package for reasoning with tensors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    install_requires=[
+        "numpy>=1.17", # was 1.23.4",
+        "pandas>=1.0.0",
+        "networkx",
+        "pyyaml",
+        "rdflib",
+        "matplotlib"
+    ],
+    python_requires=">=3",
+    license="AGPL-3.0",
+    url="https://github.com/EnexaProject/enexa-tensor-reasoning",
+    keywords="inductive reasoning, tensor networks, alternating least squares, markov logic networks"
+)
---
+++ packages/tnreason/tnreason-0.2.5.tar.gz/tnreason-0.2.5/tnreason/knowledge/__init__.py
@@ -0,0 +1,7 @@
+# knowledge/__init__.py
+
+from tnreason.knowledge.hard_kb import HardKnowledgeBase
+from tnreason.knowledge.soft_kb import SoftKnowledgeBase
+from tnreason.knowledge.hybrid_kb import HybridKnowledgeBase
+
+from tnreason.knowledge.hybrid_kb import from_yaml
