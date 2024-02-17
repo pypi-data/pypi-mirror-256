f9c927610765a4d685879a09799c6666ebf04e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:23 +0000

    Add package deepcallib/0.1/deepcallib-0.1.tar.gz

---
+++ packages/deepcallib/deepcallib-0.1.tar.gz/deepcallib-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='deepcallib',
+    version='0.1',
+    packages=['deepcallib'],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
