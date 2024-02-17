405f9fcd4c3a4cb3a2ad10faf51c7409a70c37cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:37 +0000

    Add package aplotly/1.1.7/aplotly-1.1.7.tar.gz

---
+++ packages/aplotly/aplotly-1.1.7.tar.gz/aplotly-1.1.7/examples/plot_gauge.py
@@ -0,0 +1,12 @@
+from aplotly.plots import plot_gauge
+
+value = 100
+
+fig = plot_gauge(
+    value=value,
+    threshold=50,
+    gauge_range=(-100, 200),
+    plot_title="Gauge",
+    color_palette="dark_mode",
+)
+fig.show()
