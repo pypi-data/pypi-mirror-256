35fc63470627b19fa9967377b217152adc8e0595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:20 +0000

    Add package distributions-classs/1.2/distributions_classs-1.2.tar.gz

---
+++ packages/distributions-classs/distributions_classs-1.2.tar.gz/distributions_classs-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_classs',
+      version='1.2',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_classs'],
+      author = 'Femi Adejumo',
+      author_email = 'sanyafemi21@gmail.com',
+      zip_safe=False)
