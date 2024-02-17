ad21786731e0d48bea4bf93391b0581a985df96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:38 +0000

    Add package lmoe/0.3.4/lmoe-0.3.4-py3-none-any.whl

---
+++ packages/lmoe/lmoe-0.3.4-py3-none-any.whl/lmoe/experts/project_initialization.py
@@ -0,0 +1,39 @@
+from lmoe.api.base_expert import BaseExpert
+from lmoe.api.lmoe_query import LmoeQuery
+from lmoe.framework.expert_registry import expert
+
+import ollama
+
+
+@expert
+class ProjectInitialization(BaseExpert):
+
+    @classmethod
+    def name(cls):
+        return "PROJECT_INITIALIZATION"
+
+    @classmethod
+    def has_modelfile(cls):
+        return True
+
+    def description(self):
+        return "A model designed to initialize programming projects. Given verbal and ascii art descriptions and context, initializes programming projects in a number of different languages. Does not answer questions about how to initialize, only commands to perform initialization."
+
+    def example_queries(self):
+        return [
+            "create a project for a java webserver using spring to serve the contents of the '/web/ directory",
+            "initialize a project for a new npm module",
+            "create a project like this",
+            "make a project like this one",
+        ]
+
+    def generate(self, lmoe_query: LmoeQuery):
+        stream = ollama.generate(
+            model="lmoe_project_initialization",
+            prompt=lmoe_query.render(),
+            stream=True,
+        )
+        for chunk in stream:
+            if chunk["response"] or not chunk["done"]:
+                print(chunk["response"], end="", flush=True)
+        print("")
