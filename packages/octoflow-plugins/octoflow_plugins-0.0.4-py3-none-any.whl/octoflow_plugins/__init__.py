323337dbc0e51a7f73c357e31005c18fa5189e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:00 +0000

    Add package octoflow-plugins/0.0.4/octoflow_plugins-0.0.4-py3-none-any.whl

---
+++ packages/octoflow-plugins/octoflow_plugins-0.0.4-py3-none-any.whl/octoflow_plugins/__init__.py
@@ -0,0 +1,21 @@
+from octoflow.plugin import Package
+
+__all__ = [
+    "package",
+]
+
+__version__ = "0.0.4"
+
+package = Package(
+    "plugins",
+    modules=[
+        {
+            "name": ".tracking",
+            "package": "octoflow_plugins",
+        },
+        {
+            "name": ".data",
+            "package": "octoflow_plugins",
+        },
+    ],
+)
