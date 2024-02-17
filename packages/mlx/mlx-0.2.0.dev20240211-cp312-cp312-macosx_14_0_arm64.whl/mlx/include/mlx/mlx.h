79664624b5f4367c3500ac8412cd32f663e99065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:31 +0000

    Add package mlx/0.2.0.dev20240211/mlx-0.2.0.dev20240211-cp312-cp312-macosx_14_0_arm64.whl

---
+++ packages/mlx/mlx-0.2.0.dev20240211-cp312-cp312-macosx_14_0_arm64.whl/mlx/include/mlx/mlx.h
@@ -0,0 +1,16 @@
+// Copyright © 2023 Apple Inc.
+
+#pragma once
+
+#include "mlx/array.h"
+#include "mlx/backend/metal/metal.h"
+#include "mlx/compile.h"
+#include "mlx/device.h"
+#include "mlx/fft.h"
+#include "mlx/io.h"
+#include "mlx/linalg.h"
+#include "mlx/ops.h"
+#include "mlx/random.h"
+#include "mlx/stream.h"
+#include "mlx/transforms.h"
+#include "mlx/utils.h"
