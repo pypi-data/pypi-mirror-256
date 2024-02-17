18c3ed340dcea606ceb054a6c2946e7e6c7528cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:21 +0000

    Add package evercycle-models/0.0.117/evercycle_models-0.0.117.tar.gz

---
+++ packages/evercycle-models/evercycle_models-0.0.117.tar.gz/evercycle_models-0.0.117/evercycle_models/models/address.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Address(models.Model):
+
+    street = models.CharField(max_length=50)
+    secondary = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return (f"{self.street} {self.secondary} {self.city} "
+                f"{self.state} {self.postal_code} {self.country}")
+
+    class Meta:
+        db_table = 'address'
