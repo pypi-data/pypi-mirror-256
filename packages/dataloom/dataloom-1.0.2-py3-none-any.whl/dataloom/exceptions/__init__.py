ac42fdc0b8fdd36886df64c27d050e19094b6981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:51:13 +0000

    Add package dataloom/1.0.2/dataloom-1.0.2-py3-none-any.whl

---
+++ packages/dataloom/dataloom-1.0.2-py3-none-any.whl/dataloom/exceptions/__init__.py
@@ -0,0 +1,34 @@
+class PkNotDefinedException(Exception):
+    pass
+
+
+class InvalidArgumentsException(Exception):
+    pass
+
+
+class TooManyPkException(Exception):
+    pass
+
+
+class UnsupportedDialectException(ValueError):
+    pass
+
+
+class UnsupportedTypeException(ValueError):
+    pass
+
+
+class InvalidFiltersForTableColumnException(ValueError):
+    pass
+
+
+class InvalidColumnValuesException(ValueError):
+    pass
+
+
+class UnknownColumnException(ValueError):
+    pass
+
+
+class InvalidOperatorException(ValueError):
+    pass
