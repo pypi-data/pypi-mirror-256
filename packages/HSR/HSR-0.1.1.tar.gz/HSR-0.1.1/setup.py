93f7bf5fdcee405c4c52e1623f8c46d083b51385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:43 +0000

    Add package HSR/0.1.1/HSR-0.1.1.tar.gz

---
+++ packages/HSR/HSR-0.1.1.tar.gz/HSR-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from version import __version__
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="HSR",
+    version=__version__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "rdkit"
+    ],
+    entry_points={
+        'console_scripts': [
+            'hsr = hsr.hsr_cli:main']
+    },
+)
