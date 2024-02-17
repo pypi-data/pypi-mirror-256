72d5037898c7e235fa5b3a4dc7cb4fe07c5540aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:51:20 +0000

    Add package erscipcard/1.6/erscipcard-1.6.tar.gz

---
+++ packages/erscipcard/erscipcard-1.6.tar.gz/erscipcard-1.6/erscipcard/yturls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import ytviews
+
+urlpatterns = [
+    path('', ytviews.helping ),
+    path('download/<str:url>/<str:link>/', ytviews.viddown ),
+    path('ytlink/', ytviews.vid ),
+    path('shqr/', ytviews.shqr ),
+    path('mp4/<str:link>/', ytviews.ytmp4 ),
+    path('nginx/', ytviews.ngin ),
+    path('<str:link>/', ytviews.ytdwn ),    
+]
