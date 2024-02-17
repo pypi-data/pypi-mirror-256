4ada1acdea5d51183ef9b43c1ac51c48dcbacb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:16 +0000

    Add package microvault/0.2.31/microvault-0.2.31.tar.gz

---
+++ packages/microvault/microvault-0.2.31.tar.gz/microvault-0.2.31/microvault/config_robot.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class ConfigRobot:
+    def __init__(
+        self,
+        *,
+        radius: Optional[float] = None,
+        backwards: Optional[bool] = None,
+        typeRobot: Optional[str] = None,
+        vel_linear: Optional[float] = None,
+        val_angular: Optional[float] = None
+    ):
+        self.radius = radius
+        self.backwards = backwards
+        self.typeRobot = typeRobot
+        self.vel_linear = vel_linear
+        self.val_angular = val_angular
