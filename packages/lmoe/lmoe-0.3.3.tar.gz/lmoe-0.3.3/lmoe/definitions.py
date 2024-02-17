9458ac8fc64dd6e3889a4f8c19b18278f09f8251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:34 +0000

    Add package lmoe/0.3.3/lmoe-0.3.3.tar.gz

---
+++ packages/lmoe/lmoe-0.3.3.tar.gz/lmoe-0.3.3/lmoe/definitions.py
@@ -0,0 +1,46 @@
+import importlib.resources
+import os
+import toml
+import pathlib
+
+
+def get_template_module():
+    """The module string within which all of the template resource files reside."""
+    return "lmoe.templates"
+
+
+# Cache data for the root pyproject.toml file.
+_PYPROJECT_DATA = None
+
+
+def get_project_data():
+    """Return the pyproject.toml file structured as a multi-layer dict.
+
+    Caches loaded data so that this is only retrieved from the filesystem once.
+    """
+    global _PYPROJECT_DATA
+    if _PYPROJECT_DATA is None:
+        pyproject_path = pathlib.Path(__file__).parent.parent / "pyproject.toml"
+        with open(pyproject_path, "r") as file:
+            _PYPROJECT_DATA = toml.load(file)
+    return _PYPROJECT_DATA
+
+
+def get_project_version():
+    """Returns the semantic versioning string associated with this project."""
+    return get_project_data()["tool"]["poetry"]["version"]
+
+
+# The absolute filenames of all resource template files
+_TEMPLATE_FILE_NAMES = None
+
+
+def get_template_file_names():
+    global _TEMPLATE_FILE_NAMES
+    if _TEMPLATE_FILE_NAMES is None:
+        template_dir_object = importlib.resources.files(get_template_module())
+        _TEMPLATE_FILE_NAMES = [
+            os.path.join(template_dir_object.name, file_object.name)
+            for file_object in template_dir_object.iterdir()
+        ]
+    return _TEMPLATE_FILE_NAMES
