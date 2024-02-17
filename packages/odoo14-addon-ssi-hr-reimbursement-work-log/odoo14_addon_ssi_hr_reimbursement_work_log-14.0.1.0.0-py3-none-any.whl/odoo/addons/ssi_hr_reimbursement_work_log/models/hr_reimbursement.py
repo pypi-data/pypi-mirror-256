602dfabcabb1807bf49ea54616b3978b68a1a03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:39 +0000

    Add package odoo14-addon-ssi-hr-reimbursement-work-log/14.0.1.0.0/odoo14_addon_ssi_hr_reimbursement_work_log-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-hr-reimbursement-work-log/odoo14_addon_ssi_hr_reimbursement_work_log-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_hr_reimbursement_work_log/models/hr_reimbursement.py
@@ -0,0 +1,15 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class HrReimbursement(models.Model):
+    _name = "hr.reimbursement"
+    _inherit = [
+        "hr.reimbursement",
+        "mixin.work_object",
+    ]
+
+    _work_log_create_page = True
