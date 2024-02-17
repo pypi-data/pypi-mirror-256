1b85a30d92c445f4217b4bfde043205fa13f5826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:29 +0000

    Add package gabi-distri/1.0/gabi_distri-1.0.tar.gz

---
+++ packages/gabi-distri/gabi_distri-1.0.tar.gz/gabi_distri-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gabi_distri',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Ope Banjo',
+      authoremail='theoseed@gmail.com',
+      zip_safe=False)
