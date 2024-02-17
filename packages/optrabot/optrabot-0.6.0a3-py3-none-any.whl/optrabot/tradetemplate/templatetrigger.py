79cc76d5780d8aa0e1badf56d3de6138cdf2d9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:47 +0000

    Add package optrabot/0.6.0a3/optrabot-0.6.0a3-py3-none-any.whl

---
+++ packages/optrabot/optrabot-0.6.0a3-py3-none-any.whl/optrabot/tradetemplate/templatetrigger.py
@@ -0,0 +1,13 @@
+from typing import OrderedDict
+
+class TriggerType:
+	External: str = 'external'
+	Time: str = 'time'
+
+class TemplateTrigger:
+	def __init__(self, triggerdata: OrderedDict) -> None:
+		type = triggerdata['type']
+		assert type in [TriggerType.External, TriggerType.Time]
+		self.type = type
+		self.value = triggerdata['value']
+		assert self.value != '' and self.value != None
