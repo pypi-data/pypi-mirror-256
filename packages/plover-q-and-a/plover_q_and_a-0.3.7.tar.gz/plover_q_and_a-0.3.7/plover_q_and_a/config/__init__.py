a89203768abcaedb040b55a35146e36b4edab5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:10 +0000

    Add package plover-q-and-a/0.3.7/plover_q_and_a-0.3.7.tar.gz

---
+++ packages/plover-q-and-a/plover_q_and_a-0.3.7.tar.gz/plover_q_and_a-0.3.7/plover_q_and_a/config/__init__.py
@@ -0,0 +1,12 @@
+"""
+Config - a package dealing with importing JSON data containing sign output
+customisations, munging it into app configuration, and then managing it.
+"""
+
+from .load import load
+from .reload import reload
+
+__all__ = [
+    "load",
+    "reload"
+]
