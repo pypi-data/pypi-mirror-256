22112c7c4c868713e47b2f36baf650a144ba2279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:36:32 +0000

    Add package gitea-act-tools/0.0.3/gitea-act-tools-0.0.3.tar.gz

---
+++ packages/gitea-act-tools/gitea-act-tools-0.0.3.tar.gz/gitea-act-tools-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='gitea-act-tools',
+    version='0.0.3',
+    description='Some tools for the gitea api.',
+    url='',
+    author='曾钦李',
+    author_email='1838696034@qq.com',
+    license='MIT',
+    keywords='',
+    packages=['gitea_act_tools'],
+    install_requires=['httpx'],
+    python_requires=''
+)
