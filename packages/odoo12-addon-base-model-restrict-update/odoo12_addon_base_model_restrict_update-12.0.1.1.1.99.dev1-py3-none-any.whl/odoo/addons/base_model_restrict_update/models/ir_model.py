023ea84ea1bebbdfb0c47091e301f4a16c5845b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:14 +0000

    Add package odoo12-addon-base-model-restrict-update/12.0.1.1.1.99.dev1/odoo12_addon_base_model_restrict_update-12.0.1.1.1.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-base-model-restrict-update/odoo12_addon_base_model_restrict_update-12.0.1.1.1.99.dev1-py3-none-any.whl/odoo/addons/base_model_restrict_update/models/ir_model.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class IrModel(models.Model):
+    _inherit = "ir.model"
+
+    restrict_update = fields.Boolean(
+        "Update Restrict Model",
+        help="When selected, the model is restricted to read-only unless the "
+        "user has the special permission.",
+    )
+    skip_check_for_readonly_users = fields.Boolean(
+        help="Allow readonly users to create/write/delete records of this model")
