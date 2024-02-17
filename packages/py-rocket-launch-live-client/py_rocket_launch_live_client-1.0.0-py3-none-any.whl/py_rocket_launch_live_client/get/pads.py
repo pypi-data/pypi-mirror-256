3be451c5f8d0a43a50562e896261752d6db23b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:07 +0000

    Add package py-rocket-launch-live-client/1.0.0/py_rocket_launch_live_client-1.0.0-py3-none-any.whl

---
+++ packages/py-rocket-launch-live-client/py_rocket_launch_live_client-1.0.0-py3-none-any.whl/py_rocket_launch_live_client/get/pads.py
@@ -0,0 +1,17 @@
+import requests
+import py_rocket_launch_live_client as prllc
+
+
+def get(key: str, headers: dict = {}, id: int | None = None, name: str | None = None, state: str | None = None, country: str = 'US',
+        limit: int | None = None, page_size: int = 25):
+    params = {}
+    prllc.auth.set_auth_header(key, headers)
+    if id is not None:
+        params['id'] = id
+    if name is not None:
+        params['name'] = name
+    if state is not None:
+        params['state_abbr'] = state
+    if country is not None:
+        params['country_code'] = country
+    return prllc.cursor.RocketLaunchliveCursor('pads', headers=headers, params=params, limit=limit,page_size=page_size)
