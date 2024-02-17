c05d1bc8e4f503b35fe7aec3c5266aea3a828a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:37 +0000

    Add package fake-bge-module/20240211/fake_bge_module-20240211-py3-none-any.whl

---
+++ packages/fake-bge-module/fake_bge_module-20240211-py3-none-any.whl/bge/__init__.py
@@ -0,0 +1,11 @@
+import sys
+import typing
+from . import types
+from . import texture
+from . import render
+from . import events
+from . import app
+from . import logic
+from . import constraints
+
+GenericType = typing.TypeVar("GenericType")
