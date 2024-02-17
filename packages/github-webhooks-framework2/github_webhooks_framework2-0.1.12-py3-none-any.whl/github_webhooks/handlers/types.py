931113a50f0c5abbe7859ae08fb55f61a1506943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:20 +0000

    Add package github-webhooks-framework2/0.1.12/github_webhooks_framework2-0.1.12-py3-none-any.whl

---
+++ packages/github-webhooks-framework2/github_webhooks_framework2-0.1.12-py3-none-any.whl/github_webhooks/handlers/types.py
@@ -0,0 +1,37 @@
+from typing import Any, Optional, Protocol, Union
+
+from pydantic import BaseModel
+from starlette.requests import QueryParams
+
+from github_webhooks.schemas import WebhookHeaders
+
+PayloadT = type[BaseModel]
+HandlerResult = Optional[str]
+
+
+class HandlerBasic(Protocol):
+    async def __call__(self, payload: Any, query_params: QueryParams) -> HandlerResult:
+        # actually payload will be parsed pydantic model
+        ...
+
+
+class HandlerWithHeaders(Protocol):
+    async def __call__(self, payload: Any, *, headers: WebhookHeaders, query_params: QueryParams) -> HandlerResult:
+        # actually payload will be parsed pydantic model
+        ...
+
+
+class DefaultHandlerBasic(Protocol):
+    async def __call__(self, event: str, payload: bytes, query_params: QueryParams) -> HandlerResult: ...
+
+
+class DefaultHandlerWithHeaders(Protocol):
+    async def __call__(
+        self, event: str, payload: bytes, *, headers: WebhookHeaders, query_params: QueryParams
+    ) -> HandlerResult: ...
+
+
+Handler = Union[HandlerBasic, HandlerWithHeaders]
+DefaultHandler = Union[DefaultHandlerBasic, DefaultHandlerWithHeaders]
+AnyHandlerWithHeaders = Union[HandlerWithHeaders, DefaultHandlerWithHeaders]
+AnyHandlerWithoutHeaders = Union[HandlerBasic, DefaultHandlerBasic]
