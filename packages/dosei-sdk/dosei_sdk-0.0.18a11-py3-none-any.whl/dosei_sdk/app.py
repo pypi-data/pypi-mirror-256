781f07065573c2caff5067d2cb1f748ff1832f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:45 +0000

    Add package dosei-sdk/0.0.18a11/dosei_sdk-0.0.18a11-py3-none-any.whl

---
+++ packages/dosei-sdk/dosei_sdk-0.0.18a11-py3-none-any.whl/dosei_sdk/app.py
@@ -0,0 +1,60 @@
+import asyncio
+import inspect
+import json
+import os
+from typing import List, Optional
+
+from croniter import croniter
+from pydantic import BaseModel, field_validator
+
+
+class FindDoseiInitError(Exception):
+    pass
+
+
+class CronJob(BaseModel):
+    schedule: str
+    entrypoint: str
+    is_async: bool
+
+    @field_validator("schedule")
+    @classmethod
+    def validate_schedule(cls, value):
+        if croniter.is_valid(value) is False:
+            raise ValueError("Invalid cron schedule format.")
+        return value
+
+
+class Dosei(BaseModel):
+    _app_export_path: str = ".dosei/app.json"
+
+    name: Optional[str] = None
+    run: Optional[str] = None
+    port: Optional[int] = None
+    cron_jobs: Optional[List[CronJob]] = []
+
+    def cron_job(self, schedule: str):
+
+        def decorator(func):
+            self.cron_jobs.append(CronJob(
+                schedule=schedule,
+                entrypoint=f"{func.__module__}:{func.__name__}",
+                is_async=Dosei.is_func_async(func)
+            ))
+            return func
+
+        return decorator
+
+    def export(self):
+        os.makedirs(os.path.dirname(self._app_export_path), exist_ok=True)
+        with open(self._app_export_path, 'w') as f:
+            json.dump(self.model_dump(), f, indent=2)
+
+    @staticmethod
+    def is_func_async(func) -> bool:
+        return inspect.iscoroutinefunction(func)
+
+    @staticmethod
+    def call_func(func):
+        is_async = Dosei.is_func_async(func)
+        return asyncio.run(func()) if is_async else func()
