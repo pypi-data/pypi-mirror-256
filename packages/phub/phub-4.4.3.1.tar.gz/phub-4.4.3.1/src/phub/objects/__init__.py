98da2210271b7190a3598180ba83f9cf32bb1782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:51:23 +0000

    Add package phub/4.4.3.1/phub-4.4.3.1.tar.gz

---
+++ packages/phub/phub-4.4.3.1.tar.gz/phub-4.4.3.1/src/phub/objects/__init__.py
@@ -0,0 +1,24 @@
+'''
+PHUB objects.
+'''
+
+__all__ = [
+    'Image', 'Param', 'NO_PARAM',
+    'Tag', 'Like', 'FeedItem', 'User',
+    'Feed', 'Video', 'Account',
+    '_BaseQuality', 'Query', 'queries'
+]
+
+# Dataclasses
+from .image import Image
+from .param import Param, NO_PARAM
+from .data import Tag, Like, FeedItem, _BaseQuality
+
+# Classes
+from .user import User
+from .feed import Feed
+from .video import Video
+from .account import Account
+from .query import Query, queries
+
+# EOF
