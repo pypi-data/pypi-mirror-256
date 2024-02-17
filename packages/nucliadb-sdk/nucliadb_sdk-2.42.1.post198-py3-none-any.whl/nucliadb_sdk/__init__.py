45d12c953e8e99c26271351cc8e4452189d732fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:30 +0000

    Add package nucliadb-sdk/2.42.1.post198/nucliadb_sdk-2.42.1.post198-py3-none-any.whl

---
+++ packages/nucliadb-sdk/nucliadb_sdk-2.42.1.post198-py3-none-any.whl/nucliadb_sdk/__init__.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Old SDK APIs that should eventually be removed
+from nucliadb_sdk.entities import Entity
+from nucliadb_sdk.file import File
+from nucliadb_sdk.knowledgebox import KnowledgeBox
+from nucliadb_sdk.labels import DEFAULT_LABELSET, Label, LabelSet, LabelType
+from nucliadb_sdk.utils import (
+    create_knowledge_box,
+    delete_kb,
+    get_kb,
+    get_or_create,
+    list_kbs,
+)
+from nucliadb_sdk.v2 import NucliaDB, NucliaDBAsync, Region, exceptions
+from nucliadb_sdk.vectors import Vector
+
+# backward compatibility
+NucliaSDK = NucliaDB
+NucliaSDKAsync = NucliaDBAsync
+
+__all__ = (
+    "NucliaSDK",
+    "NucliaSDKAsync",
+    "NucliaDB",
+    "NucliaDBAsync",
+    "Region",
+    "exceptions",
+    # OLD support APIs
+    # Plan on removing these in the future
+    "File",
+    "KnowledgeBox",
+    "Entity",
+    "LabelType",
+    "Label",
+    "LabelSet",
+    "DEFAULT_LABELSET",
+    "Vector",
+    "get_kb",
+    "get_or_create",
+    "create_knowledge_box",
+    "delete_kb",
+    "list_kbs",
+)
