a44afcbbf314b1c055e456ed6c18f374df9eaf0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:42 +0000

    Add package django-better-image/0.0.5/django-better-image-0.0.5.tar.gz

---
+++ packages/django-better-image/django-better-image-0.0.5.tar.gz/django-better-image-0.0.5/django_better_image/urls.py
@@ -0,0 +1,21 @@
+# 2019-08-14 : Created by Eric Lapouyade
+
+from django.urls import re_path
+
+from .views import (
+    BetterImageClearView,
+    BetterImageEditView,
+    BetterImageUploadView,
+)
+
+urlpatterns = [
+    re_path(r'^upload/$',
+        BetterImageUploadView.as_view(),
+        name='better_image_upload'),
+    re_path(r'^clear/$',
+        BetterImageClearView.as_view(),
+        name='better_image_clear'),
+    re_path(r'^edit/$',
+        BetterImageEditView.as_view(),
+        name='better_image_edit'),
+]
