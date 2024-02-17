3388cd0058c82eda38790e387180122cd5021032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:31 +0000

    Add package omnijp/1.2/omnijp-1.2-py3-none-any.whl

---
+++ packages/omnijp/omnijp-1.2-py3-none-any.whl/restq/http_cached_request.py
@@ -0,0 +1,17 @@
+import tempfile
+
+from restq.http_request import HttpRequest
+from restq.disk_cache import DiskCache
+
+
+class HttpCachedRequest(HttpRequest):
+    def __init__(self, url, headers, cache_dir):
+        super().__init__(url, headers)
+        self.cache = DiskCache(cache_dir)
+
+    def request_get(self, url, cache_name):
+        result = super().request_get(url)
+        if result.status_code == 200:
+            self.cache.save(result.content, cache_name)
+            return result.content
+        return self.cache.load(cache_name)
