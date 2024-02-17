b882e7395a95529ba43110153939f2c73e3cdb08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:30 +0000

    Add package lemmyreportmessenger/0.2.0a1/lemmyreportmessenger-0.2.0a1-py3-none-any.whl

---
+++ packages/lemmyreportmessenger/lemmyreportmessenger-0.2.0a1-py3-none-any.whl/data/lemmy_facade.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from typing import List
+
+from plemmy import LemmyHttp
+from plemmy.responses import ListPostReportsResponse, ListCommentReportsResponse
+
+from lemmyreportmessenger.data import ContentType, content_type
+
+
+@dataclass
+class Report:
+    report_id: int
+    content_id: int
+    content_type: ContentType
+    reason: str
+    resolved: bool
+
+    def get_url(self, instance: str) -> str:
+        return f"{instance}/{'post' if self.content_type == ContentType.POST else 'comment'}/{self.content_id}"
+
+
+class LemmyFacade:
+    lemmy: LemmyHttp
+
+    def __init__(self, lemmy: LemmyHttp):
+        self.lemmy = lemmy
+
+    def get_post_reports(self, community_id: int) -> List[Report]:
+        reports = ListPostReportsResponse(self.lemmy.list_post_reports(community_id)).post_reports
+        return [Report(
+            report_id=r.post_report.id,
+            content_id=r.post.id,
+            content_type=ContentType.POST,
+            reason=r.post_report.reason,
+            resolved=r.post_report.resolved
+        ) for r in reports]
+
+    def get_comment_reports(self, community_id: int) -> List[Report]:
+        reports = ListCommentReportsResponse(self.lemmy.list_comment_reports(community_id)).comment_reports
+        return [Report(
+            report_id=r.comment_report.id,
+            content_id=r.comment.id,
+            content_type=ContentType.COMMENT,
+            reason=r.comment_report.reason,
+            resolved=r.comment_report.resolved
+        ) for r in reports]
