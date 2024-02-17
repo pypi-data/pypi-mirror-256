32a5e5bf4cc4b3d4a9aaa88b8ae5255ea5fbd91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:10 +0000

    Add package timeseriesflattener/1.15.0/timeseriesflattener-1.15.0.tar.gz

---
+++ packages/timeseriesflattener/timeseriesflattener-1.15.0.tar.gz/timeseriesflattener-1.15.0/src/conftest.py
@@ -0,0 +1,73 @@
+import pytest
+from pandas import DataFrame
+from timeseriesflattener.feature_specs.group_specs import NamedDataframe
+from timeseriesflattener.testing.load_synth_data import (
+    load_synth_outcome,
+    load_synth_prediction_times,
+    load_synth_text,
+)
+from timeseriesflattener.testing.utils_for_testing import load_long_df_with_multiple_values
+
+
+def pytest_addoption(parser):  # noqa
+    parser.addoption(
+        "--skiphuggingface",
+        action="store_true",
+        default=False,
+        help="run tests that use huggingface models",
+    )
+
+
+def pytest_configure(config):  # noqa
+    config.addinivalue_line("markers", "huggingface: mark test as using huggingface models")
+
+
+def pytest_collection_modifyitems(config, items):  # noqa
+    if config.getoption("--skiphuggingface"):
+        # --skiphuggingface given in cli: skip huggingface tests
+        skip_hf = pytest.mark.skip(reason="remove --skiphuggingface option to run")
+        for item in items:
+            if "huggingface" in item.keywords:
+                item.add_marker(skip_hf)
+        return
+
+
+@pytest.fixture()
+def synth_prediction_times() -> DataFrame:
+    """Load the prediction times."""
+    return load_synth_prediction_times()
+
+
+@pytest.fixture()
+def synth_predictor() -> DataFrame:
+    """Load the synth outcome times."""
+    return load_synth_outcome(n_rows=1_000)
+
+
+@pytest.fixture()
+def synth_outcome() -> DataFrame:
+    """Load the synth outcome times."""
+    return load_synth_outcome()
+
+
+@pytest.fixture()
+def long_df_with_multiple_values() -> DataFrame:
+    """Load the long df."""
+    return load_long_df_with_multiple_values()
+
+
+@pytest.fixture()
+def synth_text_data() -> DataFrame:
+    """Load the synth text data."""
+    return load_synth_text()
+
+
+@pytest.fixture()
+def empty_df() -> DataFrame:
+    """Create an empty dataframe."""
+    return DataFrame()
+
+
+@pytest.fixture()
+def empty_named_df() -> NamedDataframe:
+    return NamedDataframe(df=DataFrame(), name="empty")
---
+++ packages/timeseriesflattener/timeseriesflattener-1.15.0.tar.gz/timeseriesflattener-1.15.0/src/timeseriesflattenerv2/feature_specs.py
@@ -0,0 +1,130 @@
+import datetime as dt
+from dataclasses import dataclass
+from typing import Protocol, Sequence, Union
+
+import pandas as pd
+import polars as pl
+from polars.lazyframe.group_by import LazyGroupBy
+
+ValueType = Union[int, float, str]
+LookDistance = dt.timedelta
+
+# TODO: Add validation that all entity_id and timestamp columns are the same
+
+default_entity_id_col_name = "entity_id"
+default_pred_time_uuid_col_name = "pred_time_uuid"
+default_pred_time_col_name = "pred_timestamp"
+
+
+@dataclass
+class PredictionTimeFrame:
+    df: pl.LazyFrame
+    entity_id_col_name: str = default_entity_id_col_name
+    timestamp_col_name: str = default_pred_time_col_name
+    pred_time_uuid_col_name: str = default_pred_time_uuid_col_name
+
+    def __post_init__(self):
+        self.df = self.df.with_columns(
+            pl.concat_str(
+                pl.col(self.entity_id_col_name), pl.lit("-"), pl.col(self.timestamp_col_name)
+            )
+            .str.strip_chars()
+            .alias(self.pred_time_uuid_col_name)
+        )
+
+    def to_lazyframe_with_uuid(self) -> pl.LazyFrame:
+        return self.df
+
+
+@dataclass
+class ValueFrame:
+    """A frame that contains the values of a time series."""
+
+    df: pl.LazyFrame | pd.DataFrame
+    value_type: str
+    entity_id_col_name: str = default_entity_id_col_name
+    value_timestamp_col_name: str = "value_timestamp"
+
+    @property
+    def lazyframe(self) -> pl.LazyFrame:
+        return self.df if isinstance(self.df, pl.LazyFrame) else pl.from_pandas(self.df).lazy()
+
+    @property
+    def eagerframe(self) -> pl.DataFrame:
+        return self.df.collect() if isinstance(self.df, pl.LazyFrame) else pl.from_pandas(self.df)
+
+
+@dataclass(frozen=True)
+class SlicedFrame:
+    """A frame that has been sliced by a lookdirection."""
+
+    df: pl.LazyFrame
+    pred_time_uuid_col_name: str = default_pred_time_uuid_col_name
+    value_col_name: str = "value"
+
+
+@dataclass(frozen=True)
+class AggregatedValueFrame:
+    df: pl.LazyFrame
+    value_col_name: str
+    pred_time_uuid_col_name: str = default_pred_time_uuid_col_name
+
+    def fill_nulls(self, fallback: ValueType) -> "AggregatedValueFrame":
+        filled = self.df.with_columns(
+            pl.col(self.value_col_name)
+            .fill_null(fallback)
+            .alias(f"{self.value_col_name}_fallback_{fallback}")
+        ).drop([self.value_col_name])
+
+        return AggregatedValueFrame(
+            df=filled,
+            pred_time_uuid_col_name=self.pred_time_uuid_col_name,
+            value_col_name=self.value_col_name,
+        )
+
+    def eagerframe(self) -> pl.DataFrame:
+        return self.df.collect()
+
+
+class Aggregator(Protocol):
+    def apply(self, value_frame: LazyGroupBy, column_name: str) -> AggregatedValueFrame:
+        ...
+
+
+@dataclass(frozen=True)
+class PredictorSpec:
+    value_frame: ValueFrame
+    lookbehind_distances: Sequence[LookDistance]
+    aggregators: Sequence[Aggregator]
+    fallback: ValueType
+    column_prefix: str = "pred"
+
+
+@dataclass(frozen=True)
+class OutcomeSpec:
+    value_frame: ValueFrame
+    lookahead_distances: Sequence[LookDistance]
+    aggregators: Sequence[Aggregator]
+    fallback: ValueType
+    column_prefix: str = "outc"
+
+
+@dataclass(frozen=True)
+class TimedeltaFrame:
+    df: pl.LazyFrame
+    pred_time_uuid_col_name: str = default_pred_time_uuid_col_name
+    timedelta_col_name: str = "time_from_prediction_to_value"
+    value_col_name: str = "value"
+
+    def get_timedeltas(self) -> Sequence[dt.datetime]:
+        return self.df.collect().get_column(self.timedelta_col_name).to_list()
+
+
+ValueSpecification = Union[PredictorSpec, OutcomeSpec]
+
+
+@dataclass(frozen=True)
+class AggregatedFrame:
+    df: pl.LazyFrame
+    pred_time_uuid_col_name: str
+    timestamp_col_name: str
