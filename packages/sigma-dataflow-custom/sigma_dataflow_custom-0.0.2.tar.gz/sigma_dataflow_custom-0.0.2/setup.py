4c25e60a4daa69d10d88840d22f96935e70fbd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:04 +0000

    Add package sigma-dataflow-custom/0.0.2/sigma_dataflow_custom-0.0.2.tar.gz

---
+++ packages/sigma-dataflow-custom/sigma_dataflow_custom-0.0.2.tar.gz/sigma_dataflow_custom-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+REQUIRED_PACKAGES = [
+    "google-cloud-storage==2.14.0",
+    "apache_beam[gcp]==2.48.0"
+]
+
+setuptools.setup(
+    name="sigma_dataflow_custom",
+    version="0.0.2",
+    author="sigma",
+    author_email="",
+    install_requires=REQUIRED_PACKAGES,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
