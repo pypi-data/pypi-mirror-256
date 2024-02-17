3b98f77d2e883c61e37929c84fc2c70edd53f496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:40:59 +0000

    Add package freshworks/0.1.0/freshworks-0.1.0.tar.gz

---
+++ packages/freshworks/freshworks-0.1.0.tar.gz/freshworks-0.1.0/README.md
@@ -0,0 +1,3 @@
+This project is abandoned. If you want the PyPI name, please contact me: [PyPI freshworks](https://pypi.org/project/freshworks/)
+
+The remnants of the package, for their Freshdesk components, has been moved to [pyfreshworks](https://pypi.org/project/pyfreshworks/)
---
+++ packages/freshworks/freshworks-0.1.0.tar.gz/freshworks-0.1.0/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+
+with open("README.md", "r") as fin:
+    long_description = fin.read()
+
+
+setuptools.setup(
+    name="freshworks",
+    version="0.1.0",
+    author="Kyle L. Davis",
+    author_email="AceofSpades5757.github@gmail.com",
+    url="https://github.com/AceofSpades5757/freshworks",
+    license="MIT",
+    description="Python client library for interacting with Freshworks products.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages("src"),
+    project_urls={
+        "Documentation": "https://fzflib.readthedocs.io/en/latest/",
+        "Author": "https://github.com/AceofSpades5757",
+    },
+    package_dir={
+        "": "src",
+        "freshdesk": "src/freshdesk",
+        "freshcaller": "src/freshcaller",
+    },
+    test_suite="tests",
+    python_requires=">=3.8",
+    install_requires=["requests", "python-dateutil"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
