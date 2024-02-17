74b61f2c9a0d09e099b11ed4fc0d827c86a3c51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:16 +0000

    Add package toolong/1.0.3/toolong-1.0.3-py3-none-any.whl

---
+++ packages/toolong/toolong-1.0.3-py3-none-any.whl/toolong/find_dialog.py
@@ -0,0 +1,94 @@
+from dataclasses import dataclass
+
+from textual import on
+from textual.app import ComposeResult
+from textual.binding import Binding
+from textual.message import Message
+from textual.suggester import Suggester
+from textual.widget import Widget
+from textual.widgets import Input, Checkbox
+
+
+class FindDialog(Widget, can_focus_children=True):
+    DEFAULT_CSS = """
+    FindDialog {
+        layout: horizontal;
+        dock: top; 
+        padding-top: 1;                       
+        width: 1fr;
+        height: auto;
+        max-height: 70%;
+        display: none;
+        & #find {
+            width: 1fr;
+        }
+        &.visible {
+            display: block;
+        }
+    }    
+    """
+    BINDINGS = [
+        Binding("escape", "dismiss_find", "Dismiss", key_display="esc", show=False),
+        Binding("down,j", "pointer_down", "Next", key_display="↓"),
+        Binding("up,k", "pointer_up", "Previous", key_display="↑"),
+        Binding("j", "pointer_down", "Next", key_display="↓", show=False),
+        Binding("k", "pointer_up", "Previous", key_display="↑", show=False),
+    ]
+    DEFAULT_CLASSES = "float"
+    BORDER_TITLE = "Find"
+
+    @dataclass
+    class Update(Message):
+        find: str
+        regex: bool
+        case_sensitive: bool
+
+    class Dismiss(Message):
+        pass
+
+    @dataclass
+    class MovePointer(Message):
+        direction: int = 1
+
+    class SelectLine(Message):
+        pass
+
+    def __init__(self, suggester: Suggester) -> None:
+        self.suggester = suggester
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        yield Input(placeholder="Find", id="find", suggester=self.suggester)
+        yield Checkbox("Case sensitive", id="case-sensitive")
+        yield Checkbox("Regex", id="regex")
+
+    @on(Input.Changed)
+    @on(Checkbox.Changed)
+    def input_change(self, event: Input.Changed) -> None:
+        event.stop()
+        self.post_update()
+
+    @on(Input.Submitted)
+    def input_submitted(self, event: Input.Changed) -> None:
+        event.stop()
+        self.post_message(self.SelectLine())
+
+    def post_update(self) -> None:
+        update = FindDialog.Update(
+            find=self.query_one("#find", Input).value,
+            regex=self.query_one("#regex", Checkbox).value,
+            case_sensitive=self.query_one("#case-sensitive", Checkbox).value,
+        )
+        self.post_message(update)
+
+    def allow_focus_children(self) -> bool:
+        return self.has_class("visible")
+
+    def action_dismiss_find(self) -> None:
+        self.post_message(FindDialog.Dismiss())
+
+    def action_pointer_down(self) -> None:
+        self.post_message(self.MovePointer(direction=+1))
+
+    def action_pointer_up(self) -> None:
+        self.post_message(self.MovePointer(direction=-1))
---
+++ packages/toolong/toolong-1.0.3-py3-none-any.whl/toolong/ui.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+import locale
+
+from pathlib import Path
+
+from textual.app import App, ComposeResult
+from textual.binding import Binding
+from textual.lazy import Lazy
+from textual.screen import Screen
+from textual.widgets import TabbedContent, TabPane
+
+from toolong.log_view import LogView
+from toolong.watcher import Watcher
+from toolong.help import HelpScreen
+
+
+locale.setlocale(locale.LC_ALL, "")
+
+
+class LogScreen(Screen):
+
+    BINDINGS = [
+        Binding("f1", "help", "Help"),
+    ]
+
+    CSS = """
+    LogScreen {
+        layers: overlay;
+        & TabPane {           
+            padding: 0;
+        }
+        & Tabs:focus Underline > .underline--bar {
+            color: $accent;
+        }        
+        Underline > .underline--bar {
+            color: $panel;
+        }
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        assert isinstance(self.app, UI)
+        with TabbedContent():
+            if self.app.merge and len(self.app.file_paths) > 1:
+                tab_name = " + ".join(Path(path).name for path in self.app.file_paths)
+                with TabPane(tab_name):
+                    yield Lazy(
+                        LogView(
+                            self.app.file_paths,
+                            self.app.watcher,
+                            can_tail=False,
+                        )
+                    )
+            else:
+                for path in self.app.file_paths:
+                    with TabPane(path):
+                        yield Lazy(
+                            LogView(
+                                [path],
+                                self.app.watcher,
+                                can_tail=True,
+                            )
+                        )
+
+    def on_mount(self) -> None:
+        assert isinstance(self.app, UI)
+        self.query("TabbedContent Tabs").set(display=len(self.query(TabPane)) > 1)
+        active_pane = self.query_one(TabbedContent).active_pane
+        if active_pane is not None:
+            active_pane.query("LogView > LogLines").focus()
+
+    def action_help(self) -> None:
+        self.app.push_screen(HelpScreen())
+
+
+class UI(App):
+    """The top level App object."""
+
+    @classmethod
+    def sort_paths(cls, paths: list[str]) -> list[str]:
+        def key(path) -> list:
+            return [
+                int(token) if token.isdigit() else token.lower()
+                for token in path.split("/")[-1].split(".")
+            ]
+
+        return sorted(paths, key=key)
+
+    def __init__(self, file_paths: list[str], merge: bool = False) -> None:
+        self.file_paths = self.sort_paths(file_paths)
+        self.merge = merge
+        self.watcher = Watcher()
+        super().__init__()
+
+    async def on_mount(self) -> None:
+        await self.push_screen(LogScreen())
+        self.screen.query("LogLines").focus()
+        self.watcher.start()
+
+    def on_unmount(self) -> None:
+        self.watcher.close()
