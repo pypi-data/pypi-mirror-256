59f560814dbd11bb6f41fc81557675236a1f76d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:13 +0000

    Add package bg-distributions-zs/0.1/bg_distributions_zs-0.1.tar.gz

---
+++ packages/bg-distributions-zs/bg_distributions_zs-0.1.tar.gz/bg_distributions_zs-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bg_distributions_zs',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bg_distributions_zs'],
+      zip_safe=False)
