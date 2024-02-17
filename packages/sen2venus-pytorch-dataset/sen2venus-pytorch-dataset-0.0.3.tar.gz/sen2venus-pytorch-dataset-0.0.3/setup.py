90052ef80f62fa5be2a0d9bdd7b0d875229b6417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:09 +0000

    Add package sen2venus-pytorch-dataset/0.0.3/sen2venus-pytorch-dataset-0.0.3.tar.gz

---
+++ packages/sen2venus-pytorch-dataset/sen2venus-pytorch-dataset-0.0.3.tar.gz/sen2venus-pytorch-dataset-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import subprocess
+import logging
+from setuptools import setup, find_packages
+import re
+
+
+def get_version():
+    VERSIONFILE = os.path.join('sen2venus', '__init__.py')
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r'^__version__ = [\"\']*([\d.]+)[\"\']'
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+log = logging.getLogger()
+
+setup(name='sen2venus-pytorch-dataset',
+      version=get_version(),
+      description="Unofficial pytorch dataloader for the Sen2Venµs Super-Resolution dataset.",
+      long_description=open("README.md").read(),
+      long_description_content_type="text/markdown",
+      classifiers=[],
+      author=u"Clément Peyrard",
+      author_email='clement.peyrard.phd@gmail.com',
+      url='https://github.com/piclem/sen2venus-pytorch-dataset',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=[
+        'torch', 'torchvision', 'rioxarray', 'py7zr', 'geopandas', 'fire', 'py7zr'
+      ]
+      )
