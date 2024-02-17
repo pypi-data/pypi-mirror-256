4e090ec4928279791a5b269ab0ea879a5dd90d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:20 +0000

    Add package moulti/1.1.0/moulti-1.1.0-py3-none-any.whl

---
+++ packages/moulti/moulti-1.1.0-py3-none-any.whl/moulti/app.py
@@ -0,0 +1,429 @@
+import os
+import asyncio
+import selectors
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any, cast, Iterator
+from socket import socket as Socket
+from time import time_ns, localtime, strftime
+from queue import Queue
+from threading import get_ident
+from textual import work
+from textual.app import App, ComposeResult
+from textual.dom import BadIdentifier
+from textual.widgets import Footer, Label
+from textual.worker import get_current_worker, NoActiveWorker
+from .protocol import PRINTABLE_MOULTI_SOCKET, clean_socket
+from .protocol import moulti_listen, get_unix_credentials, send_json_message
+from .protocol import MoultiConnectionClosedException, MoultiProtocolException, Message, FDs
+from .protocol import MoultiTLVReader, data_to_message, getraddr
+from .widgets import VertScroll, Step
+
+def timestamp() -> str:
+	timestamp_ns = time_ns()
+	timestamp_s = timestamp_ns//10**9
+	ns = timestamp_ns - timestamp_s * 10**9
+	ms = ns // 10**6
+	timestr = strftime('%FT%T', localtime(timestamp_s))
+	timestr = f'{timestr}.{ms:03d} '
+	return timestr
+
+class MoultiMessageException(Exception):
+	pass
+
+class Moulti(App):
+	"""
+	moulti is a Terminal User Interface (TUI) meant to display multiple outputs.
+	It is visually similar to https://github.com/dankilman/multiplex but unlike
+	it, moulti does not do anything until instructed to.
+	"""
+	BINDINGS = [
+		("c", "toggle_debug", "Toggle console"),
+		("d", "toggle_dark", "Toggle dark mode"),
+		("x", "expand_all", "Expand all"),
+		("o", "collapse_all", "Collapse all"),
+		("q", "quit", "Quit Moulti"),
+	]
+
+	def __init__(self, command: list[str]|None = None):
+		self.init_security()
+		self.init_widgets()
+		self.init_debug()
+		self.init_command = command
+		super().__init__()
+
+	def init_security(self) -> None:
+		def ids_from_env(env_var_name: str) -> list[int]:
+			try:
+				return [int(xid) for xid in os.environ.get(env_var_name, '').split(',')]
+			except Exception:
+				return []
+
+		# By default, allow only the current uid to connect to Moulti:
+		self.allowed_uids = [os.getuid()]
+		self.allowed_gids = []
+
+		# Make this behaviour configurable through environment variables:
+		self.allowed_uids.extend(ids_from_env('MOULTI_ALLOWED_UID'))
+		self.allowed_gids.extend(ids_from_env('MOULTI_ALLOWED_GID'))
+
+	def init_widgets(self) -> None:
+		self.title_label = Label('Moulti', id='header')
+		self.steps_container = VertScroll(id='steps_container')
+		self.footer = Footer()
+
+	def init_threads(self) -> None:
+		try:
+			max_pass = max(1, int(os.environ['MOULTI_PASS_CONCURRENCY']))
+		except (KeyError, ValueError):
+			max_pass = 20
+		default_threads = 2 # network_loop() + exec()
+		threads_per_pass = 2 # Each "moulti pass" command spawns 2 threads -- cf handle_pass_command()
+		thread_count = default_threads + threads_per_pass * max_pass
+		moulti_executor = ThreadPoolExecutor(max_workers=thread_count, thread_name_prefix='moulti')
+		asyncio.get_running_loop().set_default_executor(moulti_executor)
+		self.logdebug(f'allow up to {thread_count} threads for up to {max_pass} concurrent "moulti pass" commands')
+
+	def compose(self) -> ComposeResult:
+		"""Create child widgets for the app."""
+		yield self.title_label
+		yield self.steps_container
+		yield self.footer
+		yield self.debug_step
+
+	def on_ready(self) -> None:
+		self.init_threads()
+		self.network_loop()
+
+	def network_loop_is_ready(self) -> None:
+		if self.init_command is not None:
+			self.exec(self.init_command)
+
+	@work(thread=True)
+	def exec(self, command: list[str]) -> None:
+		import subprocess # pylint: disable=import-outside-toplevel
+		try:
+			environment = os.environ.copy()
+			environment['MOULTI_RUN'] = 'moulti'
+			environment['MOULTI_SOCKET_PATH'] = PRINTABLE_MOULTI_SOCKET
+			self.logdebug(f'exec: about to run {command}')
+			completed = subprocess.run(command, env=environment, stdin=subprocess.DEVNULL, check=False)
+			self.logdebug(f'exec: {command} exited with return code {completed.returncode}')
+		except Exception as exc:
+			self.logdebug(f'exec: error running {command}: {exc}')
+
+	def all_steps(self) -> Iterator[Step]:
+		return cast(Iterator[Step], self.query('#steps_container Step').results())
+
+	def init_debug(self) -> None:
+		self.debug_step = Step('__moulti_debug', title='Console', classes='debug', min_height=5, max_height=15)
+		self.debug_step.styles.display = 'none'
+		self.debug_step.collapsible.collapsed = False
+
+	def logdebug(self, line: str) -> None:
+		line = timestamp() + line
+		if self._thread_id == get_ident():
+			self.debug_step.append(line)
+		else:
+			try:
+				worker = get_current_worker()
+			except NoActiveWorker:
+				worker = None
+			if worker and not worker.is_cancelled:
+				self.call_from_thread(self.debug_step.append, line)
+
+	def action_toggle_debug(self) -> None:
+		"""Toggle the debug console."""
+		if self.debug_step.styles.display == 'none':
+			self.debug_step.collapsible.collapsed = False
+			self.debug_step.styles.display = 'block'
+		else:
+			self.debug_step.styles.display = 'none'
+
+	def action_toggle_dark(self) -> None:
+		"""Toggle dark mode."""
+		self.dark = not self.dark
+
+	def action_expand_all(self) -> None:
+		"""Expand all steps."""
+		for step in self.all_steps():
+			step.collapsible.collapsed = False
+
+	def action_collapse_all(self) -> None:
+		"""Collapse all steps."""
+		for step in self.all_steps():
+			step.collapsible.collapsed = True
+
+	def reply(self, connection: Socket, raddr: str, message: dict[str, Any], **kwargs: Any) -> None:
+		try:
+			# If the original message bears a message id, use it to send a
+			# smaller reply. Otherwise, send the original message back with
+			# extra data.
+			if msgid := message.get('msgid'):
+				message = {'msgid': msgid, **kwargs}
+			else:
+				message = {**message, **kwargs}
+			self.logdebug(f'{raddr}: <= message={message}')
+			send_json_message(connection, message)
+		except Exception as exc:
+			self.logdebug(f'{raddr}: reply: kwargs={kwargs}: {exc}')
+
+	@work(thread=True)
+	async def append_from_file_descriptor_to_queue(
+		self,
+		file_desc: int,
+		queue: Queue,
+		connection: Socket,
+		raddr: str,
+		message: Message
+	) -> None:
+		current_worker = get_current_worker()
+		error = None
+		try:
+			# Read lines from the given file descriptor:
+			with os.fdopen(file_desc, encoding='utf-8', errors='surrogateescape') as text_io:
+				# Syscall-wise, Python will read(fd, buffer, count) where count = max(8192, read_size) - read_bytes.
+				# But it will NOT return anything unless it has reached the hinted read size.
+				# Out of the box, Moulti should strive to display lines as soon as possible, hence the default value of
+				# 1. It remains possible to specify a larger value, e.g. when one knows there are going to be many
+				# lines over a short timespan, e.g. "find / -ls".
+				read_size = int(message.get('read_size', 1))
+				while data := text_io.read(read_size):
+					if current_worker.is_cancelled:
+						break
+					queue.put_nowait(data)
+				queue.put_nowait(None)
+		except Exception as exc:
+			error = str(exc)
+			self.logdebug(f'{raddr}: pass: {error}')
+		self.reply(connection, raddr, message, done=error is None, error=error)
+
+	@work(thread=True)
+	async def append_from_queue_to_step(self, queue: Queue, step: Step) -> None:
+		current_worker = get_current_worker()
+		step.prevent_deletion += 1
+		try:
+			buffer = []
+			while True:
+				if current_worker.is_cancelled:
+					break
+				data = queue.get()
+				if data is not None:
+					# Buffer data to avoid queuing partial lines as, down the
+					# line, RichLog.write() only handles complete lines:
+					# look for the position of \n from the end of the string :
+					eol = data.rfind('\n')
+					if eol == -1: # no \n found, buffer the whole string:
+						buffer.append(data)
+					else:
+						before = data[:eol]
+						after = data[eol+1:]
+						buffer.append(before)
+						self.call_from_thread(step.append, ''.join(buffer))
+						buffer.clear()
+						if after:
+							buffer.append(after)
+				else:
+					# Reached EOF: flush buffer and signal EOF:
+					if buffer:
+						self.call_from_thread(step.append, ''.join(buffer))
+					break
+		except Exception as exc:
+			self.logdebug(f'append_from_queue_to_step: {exc}')
+		finally:
+			step.prevent_deletion -= 1
+
+	def step_from_message(self, message: Message) -> None | Step:
+		try:
+			step = self.steps_container.query_one('#step_' + str(message['id']))
+			return cast(Step, step)
+		except Exception:
+			return None
+
+	def handle_pass_command(self, connection: Socket, raddr: str, message: Message, file_descriptors: FDs) -> None:
+		step = self.step_from_message(message)
+		if not step:
+			self.reply(connection, raddr, message, done=False, error=f'no such step: {message.get("id")}')
+			return
+		if not file_descriptors:
+			self.reply(connection, raddr, message, done=False, error='missing file descriptor for pass operation')
+			return
+		# Set up a queue between two workers:
+		# - one that reads data from the file descriptor and replies to the client;
+		# - one that appends lines to the step.
+		queue: Queue = Queue()
+		self.append_from_queue_to_step(queue, step)
+		self.append_from_file_descriptor_to_queue(file_descriptors[0], queue, connection, raddr, message)
+
+	def handle_message(self, connection: Socket, raddr: str, message: Message, file_descriptors: FDs) -> None:
+		command = message.get('command')
+		# Deal with special cases:
+		if command == 'pass':
+			self.handle_pass_command(connection, raddr, message, file_descriptors)
+			return
+		# Analyse the message to determine what to call or what error to return:
+		call: Any = ()
+		error = None
+		try:
+			if command == 'step':
+				step = self.step_from_message(message)
+				action = message.get('action')
+				if action == 'add':
+					if step:
+						raise MoultiMessageException(f'step {message.get("id")} already exists')
+					if 'id' not in message:
+						raise MoultiMessageException('missing step id')
+					call = (self.steps_container.mount, Step(**message))
+				else:
+					# All other actions require an existing step:
+					if not step:
+						raise MoultiMessageException(f'no such step: {message.get("id")}')
+					if action == 'append':
+						if 'text' not in message:
+							raise MoultiMessageException('missing text for append operation')
+						call = (step.append, '\n'.join(message['text']))
+					elif action == 'clear':
+						call = (step.clear,)
+					elif action == 'delete':
+						if step.prevent_deletion:
+							err = 'cannot delete this step as '
+							err += f'{step.prevent_deletion} ongoing operations depend upon it'
+							raise MoultiMessageException(err)
+						call = (step.remove,)
+					elif action == 'update':
+						call = (step.update_properties, message)
+					else:
+						raise MoultiMessageException('unknown action {action}')
+			elif command == 'set':
+				if 'title' in message:
+					call = (self.title_label.update, str(message['title']))
+			elif command == 'ping':
+				call = ()
+			else:
+				raise MoultiMessageException('unknown command {command}')
+			# At this stage, the analysis is complete; perform the required action and reply accordingly:
+			if call:
+				self.call_from_thread(*call)
+		except (BadIdentifier, MoultiMessageException) as exc:
+			error = str(exc)
+		finally:
+			self.reply(connection, raddr, message, done=error is None, error=error)
+
+	def check_unix_credentials(self, socket: Socket) -> tuple[bool, int, int]:
+		_, uid, gid = get_unix_credentials(socket)
+		allowed = uid in self.allowed_uids or gid in self.allowed_gids
+		return allowed, uid, gid
+
+	@work(thread=True)
+	async def network_loop(self) -> None:
+		current_worker = get_current_worker()
+
+		def read(tlv_reader: MoultiTLVReader) -> None:
+			raddr = tlv_reader.raddr
+			try:
+				try:
+					# Read whatever there is to read; if a complete TLV message is received, calls got_tlv, which calls
+					# handle_message, which calls reply().
+					tlv_reader.read()
+				except (MoultiProtocolException, MoultiConnectionClosedException, ConnectionResetError) as exc:
+					server_selector.unregister(tlv_reader.socket)
+					tlv_reader.socket.close()
+					self.logdebug(f'{raddr}: read: closed connection; cause: {exc}')
+					return
+			except Exception as exc:
+				self.logdebug(f'{raddr}: read: {exc}')
+
+		def got_tlv(socket: Socket, raddr: str, data_type: str, data: bytes, file_descriptors: FDs) -> None:
+			if data_type == 'JSON':
+				message = data_to_message(data)
+				self.logdebug(f'{raddr}: => {message=} {file_descriptors=}')
+				self.handle_message(socket, raddr, message, file_descriptors)
+			else:
+				raise MoultiProtocolException(f'cannot handle {data_type} {len(data)}-byte message')
+
+		def accept(socket: Socket) -> None:
+			raddr = None
+			try:
+				connection, _ = socket.accept()
+				raddr = getraddr(connection)
+				self.logdebug(f'{raddr}: accept: accepted new connection')
+				# Check Unix credentials (uid/gid) if and only if Moulti listens on an abstract socket.
+				# Regular sockets are protected by file permissions.
+				if server_socket_is_abstract:
+					allowed, uid, gid = self.check_unix_credentials(connection)
+					if not allowed:
+						connection.close()
+						self.logdebug(f'{raddr}: accept: closed connection: invalid Unix credentials {uid}:{gid}')
+						return
+				connection.setblocking(False)
+				tlv_reader = MoultiTLVReader(connection, raddr, got_tlv, self.logdebug)
+				server_selector.register(connection, selectors.EVENT_READ, tlv_reader)
+			except Exception as exc:
+				self.logdebug(f'{raddr}: accept: {exc}')
+
+		try:
+			server_socket, server_socket_is_abstract = moulti_listen()
+		except Exception as exc:
+			# A Moulti instance is useless if it cannot listen:
+			self.exit(f'Fatal: {exc}')
+			return
+		socket_type = "abstract socket" if server_socket_is_abstract else "socket"
+		self.logdebug(f'listening on {socket_type} {PRINTABLE_MOULTI_SOCKET}')
+
+		try:
+			server_selector = selectors.DefaultSelector()
+			server_selector.register(server_socket, selectors.EVENT_READ, accept)
+
+			self.network_loop_is_ready()
+			while not current_worker.is_cancelled:
+				events = server_selector.select(1)
+				for key, _ in events:
+					if isinstance(key.data, MoultiTLVReader):
+						read(key.data)
+					elif callable(key.data):
+						key.data(key.fileobj)
+		except Exception as exc:
+			self.logdebug(f'network loop: {exc}')
+		finally:
+			clean_socket(PRINTABLE_MOULTI_SOCKET)
+
+	DEFAULT_CSS = """
+	/* Styles inherited by all widgets: */
+	$scrollbar_background: #b2b2b2;
+	$scrollbar_inactive_bar: #686868;
+	$scrollbar_active_bar: $accent-darken-1;
+	Widget {
+		/* By default, in dark mode, scrollbars are not rendered clearly; enforce their colours: */
+		scrollbar-background: $scrollbar_background;
+		scrollbar-background-hover: $scrollbar_background;
+		scrollbar-background-active: $scrollbar_background;
+		scrollbar-color: $scrollbar_inactive_bar;
+		scrollbar-color-active: $scrollbar_active_bar;
+		scrollbar-color-hover: $scrollbar_active_bar;
+	}
+
+	/* One-line title at the top of the screen: */
+	#header {
+		text-align: center;
+		/* For the title to appear centered, the widget must occupy all available width: */
+		width: 100%;
+		background: $accent;
+		color: auto;
+	}
+
+	/* Leave a thin space between steps and their container's vertical scrollbar: */
+	#steps_container.vertical_scrollbar_visible > Widget {
+		margin-right: 1;
+	}
+	"""
+
+	# Allow end users to redefine Moulti's look and feel through an optional
+	# custom CSS set through an environment variable:
+	CSS_PATH = os.environ.get('MOULTI_CUSTOM_CSS')
+
+def main(command: list[str]|None = None) -> None:
+	reply = Moulti(command=command).run()
+	if reply is not None:
+		print(reply)
+
+if __name__ == '__main__':
+	main()
---
+++ packages/moulti/moulti-1.1.0-py3-none-any.whl/moulti/cli.py
@@ -0,0 +1,180 @@
+# ruff: noqa: E501 Line too long
+import sys
+from typing import Any, cast
+from argparse import ArgumentParser, BooleanOptionalAction, _SubParsersAction, ArgumentTypeError
+from .protocol import moulti_connect, send_to_moulti, send_json_message, recv_json_message
+from .protocol import Message, PRINTABLE_MOULTI_SOCKET
+
+Args = dict[str, Any]
+
+def pint(value: str) -> int:
+	integer_value = int(value)
+	if integer_value < 0:
+		raise ArgumentTypeError('expected a positive integer')
+	return integer_value
+
+def init(args: dict) -> None:
+	"""Start a new Moulti instance."""
+	from .app import main as init_moulti # pylint: disable=import-outside-toplevel
+	init_moulti(**args)
+
+def wait(verbose: bool = False, delay: int = 500, max_attempts: int = 0) -> None:
+	"""Wait until the Moulti instance is available.
+	Args:
+		verbose: if True, output the reason why each connection attempt failed
+		delay: number of milliseconds between two connection attempts
+		max_attempts: maximum number of attempts before giving up; 0 means "never give up"
+	"""
+	import time # pylint: disable=import-outside-toplevel
+	connected = False
+	attempts = 0
+	while not connected:
+		try:
+			attempts += 1
+			send_to_moulti({'command': 'ping'})
+			connected = True
+			break
+		except Exception as exc:
+			if verbose:
+				print(f'Connection #{attempts} to {PRINTABLE_MOULTI_SOCKET}: {exc}')
+			if max_attempts > 0 and attempts == max_attempts:
+				print('Giving up.')
+				break
+			time.sleep(delay / 1000.0)
+	sys.exit(0 if connected else 1)
+
+def handle_reply(reply: Message) -> None:
+	success = reply.get('done') is True
+	if not success:
+		fallback = 'alas, no error message was provided.'
+		print('Something went wrong: ' + reply.get('error', fallback))
+	sys.exit(0 if success else 1)
+
+def send_to_moulti_and_handle_reply(message: Message) -> None:
+	reply = cast(dict[str, Any], send_to_moulti(message))
+	handle_reply(reply)
+
+def pass_stdin(args: Args) -> None:
+	"""Pass the stdin file descriptor to Moulti and inject everything read from it into a given step."""
+	moulti_socket = moulti_connect()
+	step_id = args['id']
+	# Clear the target step unless told not to through --append or --no-clear:
+	if not args.get('append', False):
+		send_json_message(moulti_socket, {'command': 'step', 'action': 'clear', 'id': step_id})
+		reply, _ = recv_json_message(moulti_socket, 0)
+	pass_message = {'command': 'pass', 'id': step_id, 'read_size': args['read_size']}
+	send_json_message(moulti_socket, pass_message, [sys.stdin.fileno()])
+	# At this stage, the remote Moulti process has the file descriptor and is
+	# expected to read lines from it. On our side, we need to remain up &
+	# running, otherwise the line-emitting process gets a SIGPIPE.
+	# Specifically, we remain up and running until we get a message from the
+	# remote Moulti process:
+	reply, _ = recv_json_message(moulti_socket, 0)
+	moulti_socket.close()
+	handle_reply(reply)
+
+set_options = step_add = step_delete = step_clear = step_append = send_to_moulti_and_handle_reply
+
+def step_update(args: Args) -> None:
+	no_none_args = {k:v for (k,v) in args.items() if v is not None}
+	send_to_moulti_and_handle_reply(no_none_args)
+
+def add_main_commands(subparsers: _SubParsersAction) -> None:
+	# moulti init
+	init_parser = subparsers.add_parser('init', help='Start a new Moulti instance.')
+	init_parser.set_defaults(func=init)
+
+	# moulti run
+	run_parser = subparsers.add_parser('run', help='Start a new Moulti instance and run a command')
+	run_parser.set_defaults(func=init)
+	run_parser.add_argument('command', type=str, nargs='+', help='command to run along with its arguments')
+
+	# moulti wait
+	wait_parser = subparsers.add_parser('wait', help='Wait until the Moulti instance is available.')
+	wait_parser.set_defaults(func=wait)
+	wait_parser.add_argument('--verbose', '-v', action='store_true', help='if True, output the reason why each connection attempt failed')
+	wait_parser.add_argument('--delay', '-d', type=pint, default=500, help='number of milliseconds between two connection attempts')
+	wait_parser.add_argument('--max-attempts', '-m', type=pint, default=0, help='maximum number of attempts before giving up; 0 means "never give up"')
+
+	# moulti set
+	set_parser = subparsers.add_parser('set', help='Set Moulti options')
+	set_parser.set_defaults(func=set_options, command='set')
+	set_parser.add_argument('--title', '-t', type=str, help='title displayed at the top of the screen')
+
+def add_pass_command(subparsers: _SubParsersAction) -> None:
+	# moulti pass
+	pass_parser = subparsers.add_parser('pass', help='Pass standard input to an existing Moulti step')
+	pass_parser.set_defaults(func=pass_stdin)
+	pass_parser.add_argument('id', type=str, help='unique identifier')
+	pass_parser.add_argument('--append', '--no-clear', '-a', dest='append', action='store_true', help='do not clear the target step')
+	pass_parser.add_argument('--read-size', '-rs', dest='read_size', type=pint, default=1, help='read size')
+
+def add_step_options(parser: ArgumentParser, none: bool = False) -> None:
+	"""Options common to step add (with actual default values) and step update (with None default values)."""
+	parser.add_argument('--collapsed', action=BooleanOptionalAction, default=None if none else False, help='whether to collapse the step')
+	parser.add_argument('--title', type=str, default=None, help='step title, always visible')
+	parser.add_argument('--text', '-t', type=str, default=None if none else '', help='content')
+	parser.add_argument('--top-text', '-tt', type=str, default=None if none else '', help='line of text displayed above the content')
+	parser.add_argument('--bottom-text', '-bt', type=str, default=None if none else '', help='line of text displayed below the content')
+	parser.add_argument('--min-height', '-mh', type=pint, default=None if none else  1, help='minimum content height')
+	parser.add_argument('--max-height', '-Mh', type=pint, default=None if none else 25, help='maximum content height; 0 to disable')
+	parser.add_argument('--classes', '-c', type=str, default=None if none else 'standard', help='step class (color): standard, error, warning, success')
+
+def add_step_commands(step_subparsers: _SubParsersAction) -> None:
+	# moulti step add
+	step_add_parser = step_subparsers.add_parser('add', help='Add a new step to Moulti.')
+	step_add_parser.set_defaults(func=step_add, command='step', action='add')
+	step_add_parser.add_argument('id', type=str, help='unique identifier')
+	add_step_options(step_add_parser)
+
+	# moulti step update
+	step_update_parser = step_subparsers.add_parser('update', help='Update an existing Moulti step.')
+	step_update_parser.set_defaults(func=step_update, command='step', action='update')
+	step_update_parser.add_argument('id', type=str, help='unique identifier')
+	add_step_options(step_update_parser, none=True)
+
+	# moulti step delete
+	step_delete_parser = step_subparsers.add_parser('delete', help='Delete an existing Moulti step.')
+	step_delete_parser.set_defaults(func=step_delete, command='step', action='delete')
+	step_delete_parser.add_argument('id', type=str, help='unique identifier')
+
+	# moulti step clear
+	step_clear_parser = step_subparsers.add_parser('clear', help='Clear the contents of an existing Moulti step.')
+	step_clear_parser.set_defaults(func=step_clear, command='step', action='clear')
+	step_clear_parser.add_argument('id', type=str, help='unique identifier')
+
+	# moulti step append
+	step_append_parser = step_subparsers.add_parser('append', help='Append the given contents to an existing Moulti step.')
+	step_append_parser.set_defaults(func=step_append, command='step', action='append')
+	step_append_parser.add_argument('id', type=str, help='unique identifier')
+	step_append_parser.add_argument('text', type=str, nargs='+', help='strings to append')
+
+def build_arg_parser() -> ArgumentParser:
+	arg_parser = ArgumentParser(prog='moulti', description='step-by-step logs')
+	subparsers = arg_parser.add_subparsers(required=True)
+	# moulti init, moulti wait:
+	add_main_commands(subparsers)
+	# moulti step:
+	step_parser = subparsers.add_parser('step', help='Create and manage steps shown by Moulti.')
+	step_subparsers = step_parser.add_subparsers(required=True)
+	add_step_commands(step_subparsers)
+	# moulti pass:
+	add_pass_command(subparsers)
+	return arg_parser
+
+def main() -> None:
+	try:
+		arg_parser = build_arg_parser()
+		args = vars(arg_parser.parse_args())
+		func = args.pop('func')
+		# Subtlety: func and args are not always used the same way:
+		if func == wait: # pylint: disable=comparison-with-callable
+			wait(**args)
+		else:
+			func(args)
+	except KeyboardInterrupt:
+		print('')
+		sys.exit(1)
+
+if __name__ == '__main__':
+	main()
