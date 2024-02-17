cc3f4ab8933511fba05733b457d8a866b291a51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:44 +0000

    Add package jk-dist-package/1.3/jk-dist-package-1.3.tar.gz

---
+++ packages/jk-dist-package/jk-dist-package-1.3.tar.gz/jk-dist-package-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jk-dist-package',
+      version='1.3',
+      description='Gauss and Binomial distribution',
+      packages=['jk-dist-package'],
+      author = 'Junaid Khan',
+      author_email = 'junaidkhandev123@gmail.com',
+    zip_safe=False)
