75937f58da11ddece6c94f5f33699853785bcb8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:34 +0000

    Add package pocs-based-clustering/1.2.0/pocs_based_clustering-1.2.0.tar.gz

---
+++ packages/pocs-based-clustering/pocs_based_clustering-1.2.0.tar.gz/pocs_based_clustering-1.2.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from setuptools import setup
+
+
+setup(
+    name="pocs_based_clustering",
+    version="1.2.0",
+    author="Le-Anh Tran",
+    author_email="leanhtran@mju.ac.kr",
+    description="Official Implementation of POCS-based Clustering Algorithm",
+    url="https://github.com/tranleanh/pocs-based-clustering",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ),
+)
