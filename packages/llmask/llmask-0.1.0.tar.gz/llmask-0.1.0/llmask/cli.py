26e785d6d82e618f3bfca503966becd172a4d5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:00 +0000

    Add package llmask/0.1.0/llmask-0.1.0.tar.gz

---
+++ packages/llmask/llmask-0.1.0.tar.gz/llmask-0.1.0/llmask/cli.py
@@ -0,0 +1,61 @@
+"""Main entrypoint."""
+
+import logging
+import os
+from typing import Callable
+
+from typer import Option, Typer
+
+from llmask.transform import (
+    chain_apply_transformations,
+    parse_transformations_string,
+)
+
+logging.basicConfig(level=logging.WARN)
+os.environ["NO_COLOR"] = "1"  # deactivate color for rich/colorama (if installed)
+
+app = Typer(
+    name="llmask",
+    add_completion=False,
+    context_settings={"help_option_names": ["-h", "--help"]},
+)
+
+
+@app.command()
+def transform(
+    transformations: str = Option(
+        "ts",
+        "-t",
+        "--transformations",
+        help=(
+            "Compact representation of operations "
+            "(e.g. 'ts' for the steps 'thesaurus -> simplify')"
+        ),
+    ),
+    input: str = Option(
+        ...,
+        "-i",
+        "--input",
+        help="Input text that will be transformed.",
+    ),
+):
+    """Transform input text with chained transformations by a Large Language Model."""
+    print("\nUser-provided input:\n")
+    print(f"> {input}\n\n")
+
+    transformation_funcs: list[Callable] = parse_transformations_string(
+        transformations=transformations
+    )
+
+    transformed_texts = chain_apply_transformations(
+        input=input,
+        transformation_funcs=transformation_funcs,
+    )
+
+    for func, text in zip(transformation_funcs, transformed_texts):
+        print(f"Result after applying transformation '{func.__name__}':\n")
+        print(f"> {text}\n\n")
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/llmask/llmask-0.1.0.tar.gz/llmask-0.1.0/llmask/transform.py
@@ -0,0 +1,100 @@
+"""Module with functions to transform text."""
+
+from typing import Callable
+
+from llmask.model import get_api_client, query_llm
+
+
+# OPTIONAL: make functions retun system prompts only, and move queries LLM in model.py?
+def thesaurus(input: str) -> str:
+    """Change input by replacing words with synonyms."""
+    api_client = get_api_client()
+    instructions: str = """
+        change the user input by replacing each word with their synonyms.
+        replace every word. change all words.
+        find a word with similar meaning and use it in the original word's place.
+
+        think like a thesaurus, replacing each word.
+        but do not change the meaning of the paragraph.
+        Only output the changed input.
+        Do not start with 'understood' etc.
+    """
+    response = query_llm(
+        api_client=api_client,
+        instructions=instructions,
+        input=input,
+        temperature=1.5,
+        seed=42,
+    )
+    return response
+
+
+def simplify(input: str) -> str:
+    """Simplify language of input."""
+    api_client = get_api_client()
+
+    instructions: str = """
+        change the user input by simplifying the language.
+        replace uncommon words by common ones.
+        make the writing as simple as possible.
+        make sure that even children would know each word.
+
+        Do not change the input's meaning.
+        do not leave out aspects.
+        it's important that the output means the same as the input.
+
+        Only output the changed input.
+        Do not start with 'understood' etc.
+    """
+    response = query_llm(
+        api_client=api_client,
+        instructions=instructions,
+        input=input,
+        temperature=0.3,
+        seed=42,
+    )
+
+    return response
+
+
+TRANSFORMATION_MAPPING = {
+    "s": simplify,
+    "t": thesaurus,
+}
+
+
+def parse_transformations_string(transformations: str) -> list[Callable]:
+    """Parse CLI parameter 'transformations' to list of to-be-applied functions."""
+    # check input validity
+    for transformation in transformations:
+        try:
+            assert transformation in TRANSFORMATION_MAPPING.keys()
+        except AssertionError:
+            print(
+                f"Transformation key '{transformation}' is not valid! "
+                f"Valid options are: {list(TRANSFORMATION_MAPPING.keys())}. "
+                "Exiting."
+            )
+            exit(1)
+
+    return [TRANSFORMATION_MAPPING[char] for char in transformations]
+
+
+def chain_apply_transformations(
+    input: str,
+    transformation_funcs: list[Callable],
+) -> list[str]:
+    """Apply chain of transformations, passing each result as input to the next step.
+
+    Args:
+        input: user-provided text input.
+        transformation_funcs: list of transformation functions to be chained.
+    Return:
+        list of transformed text, for each step of transformation pipeline.
+    """
+    transformed_texts: list[str] = []  # list with each result of transformation chain
+    for transformation_func in transformation_funcs:
+        output = transformation_func(input=input)
+        transformed_texts.append(output)
+        input = output  # use this transformtion's output as next input
+    return transformed_texts
