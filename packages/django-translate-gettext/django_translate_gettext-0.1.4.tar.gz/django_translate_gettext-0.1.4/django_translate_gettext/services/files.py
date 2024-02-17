86bc4e98798c8e39e4add76ef1e9f5e25bece2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:50:39 +0000

    Add package django-translate-gettext/0.1.4/django_translate_gettext-0.1.4.tar.gz

---
+++ packages/django-translate-gettext/django_translate_gettext-0.1.4.tar.gz/django_translate_gettext-0.1.4/django_translate_gettext/services/files.py
@@ -0,0 +1,23 @@
+import ast
+import subprocess
+from pathlib import Path
+
+from loguru import logger
+
+from django_translate_gettext.services.transformers import ClassDefTransformer
+
+
+def update_py_file(*, file_path: str, formatted: bool = False) -> None:
+    model_file = Path(file_path).with_suffix(".py").absolute()
+    tree = ast.parse(model_file.read_text())
+
+    transformer = ClassDefTransformer()
+    new_tree = transformer.visit(tree)
+    new_tree = transformer.insert_getetxt_import(new_tree)
+
+    code = ast.unparse(new_tree)
+    model_file.write_text(code)
+
+    if formatted:
+        logger.info("Formatting the code...")
+        subprocess.run(["ruff", "format", str(model_file)], check=True)  # noqa: S603, S607
