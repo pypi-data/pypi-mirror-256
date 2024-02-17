37152d7b94ceae06863be32dba670975afba9ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:49 +0000

    Add package test-pack-kotek/0.0.1/test_pack_kotek-0.0.1.tar.gz

---
+++ packages/test-pack-kotek/test_pack_kotek-0.0.1.tar.gz/test_pack_kotek-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='test_pack_kotek',
+	version='0.0.1',
+	author='kotvpalto',
+	author_email='kotvpaltoof@ya.ru',
+	description='PYPI TESTING!!',
+	packages=['test_pack_kotek'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
