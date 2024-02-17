572037f2e972799960c611b78e4b0f87c75f5100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:51:55 +0000

    Add package pypsdm/0.0.1/pypsdm-0.0.1-py3-none-any.whl

---
+++ packages/pypsdm/pypsdm-0.0.1-py3-none-any.whl/pypsdm/processing/dataframe.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+import pandas as pd
+from pandas import DataFrame
+
+
+def divide_positive_negative(df: DataFrame):
+    positive = df.copy()
+    positive[positive < 0] = 0
+    negative = df.copy()
+    negative[negative > 0] = 0
+    return positive, negative
+
+
+def join_dataframes(dfs: list[DataFrame]):
+    return pd.concat(dfs)
+
+
+def filter_data_for_time_interval(
+    data: DataFrame, start: datetime, end: datetime
+) -> DataFrame:
+    if data.empty:
+        return data
+    if not data.index.is_monotonic_increasing:
+        data = data.sort_index()
+    # we want the first value to be start if present and if not the last value before start
+    all_after_start = (data.index > start).all()
+    all_after_end = (data.index >= end).all()
+    if all_after_start and all_after_end:
+        return data.drop(data.index)
+    elif all_after_start:
+        start_row = data.index[1]
+    else:
+        before_start = data.index[data.index <= start]
+        start_row = before_start[-1]
+    # since this is the start of the time interval
+    end_row = data.index[data.index <= end][-1]
+    filtered_data = data[start_row:end_row]
+    return filtered_data.rename(index={filtered_data.loc[start_row].name: start})
+
+
+def compare_dfs(a: DataFrame, b: DataFrame, check_like=True, **kwargs):
+    # compare columns
+    a_cols = set(a.columns)
+    b_cols = set(b.columns)
+
+    a_diff = a_cols - b_cols
+    if a_diff:
+        raise AssertionError(f"Columns: {a_diff} in left but not in right")
+    b_diff = b_cols - a_cols
+    if b_diff:
+        raise AssertionError(f"Columns: {b_diff} in right but not in left")
+
+    pd.testing.assert_frame_equal(
+        a,
+        b,
+        check_like=check_like,
+        check_column_type=False,
+        check_index_type=False,
+        **kwargs,
+    )
