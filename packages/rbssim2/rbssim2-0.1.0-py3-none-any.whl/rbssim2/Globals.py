cce8a46295cab9abde276163c741c3130d962c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:56 +0000

    Add package rbssim2/0.1.0/rbssim2-0.1.0-py3-none-any.whl

---
+++ packages/rbssim2/rbssim2-0.1.0-py3-none-any.whl/rbssim2/Globals.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+E_THRESHOLD = 200
+CS_FOLDER = PROJECT_DIR + "/resources/cross_section/"
+STOPPING_FOLDER = PROJECT_DIR + "/resources/stop/"
+SIGMACALC = True 
+MIN_YEILD_DISCRETIZATION = 256
+ABUNDANCE_THRESHOLD = 0.01
+STRAGGLING = 0
+ENERGY_STEP = float(2.1)
+
+
+def loadSettings(fname):
+
+    with open(fname) as settingsFile:
+        settings = json.load(settingsFile)
+        global E_THRESHOLD, CS_FOLDER, STOPPING_FOLDER, SIGMACALC, MIN_YEILD_DISCRETIZATION, ABUNDANCE_THRESHOLD, STRAGGLING, ENERGY_STEP
+        
+        E_THRESHOLD = settings['E_THRESHOLD']
+        CS_FOLDER = settings['CS_FOLDER']
+        STOPPING_FOLDER = settings['STOPPING_FOLDER']
+        SIGMACALC = settings['SIGMACALC']
+        MIN_YEILD_DISCRETIZATION = settings['MIN_YEILD_DISCRETIZATION']
+        ABUNDANCE_THRESHOLD = settings['ABUNDANCE_THRESHOLD']
