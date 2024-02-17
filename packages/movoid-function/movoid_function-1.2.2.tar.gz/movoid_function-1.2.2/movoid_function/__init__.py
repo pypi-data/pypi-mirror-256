a2840dbce4479ac7dd4b4c01a81642d4d4665105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:47 +0000

    Add package movoid-function/1.2.2/movoid_function-1.2.2.tar.gz

---
+++ packages/movoid-function/movoid_function-1.2.2.tar.gz/movoid_function-1.2.2/movoid_function/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/1/28 16:03
+# Description   : 
+"""
+from .decorator import recover_signature_from_function as wraps
+from .decorator import recover_signature_from_function_only_kwargs as wraps_kw
+from .decorator import recover_signature_from_function_func as wraps_func
