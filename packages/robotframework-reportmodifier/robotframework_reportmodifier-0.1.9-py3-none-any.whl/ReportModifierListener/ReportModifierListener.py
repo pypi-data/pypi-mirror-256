ed7cce74440a6313b1bbbca4155886094ec35856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:50:56 +0000

    Add package robotframework-reportmodifier/0.1.9/robotframework_reportmodifier-0.1.9-py3-none-any.whl

---
+++ packages/robotframework-reportmodifier/robotframework_reportmodifier-0.1.9-py3-none-any.whl/ReportModifierListener/ReportModifierListener.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import Optional
+
+from reportmodifier.ReportModifier import ReportModifier
+from robot.api.deco import not_keyword
+
+
+class ReportModifierListener:
+    """ Listener der verwendet werden kann, um Ergebnise nach Jira zu übertragen.
+    Voraussetzung: Der Robot-Testfallname entspricht dem Jira-Key.
+    """
+    ROBOT_LISTENER_API_VERSION = 3
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+
+    def __init__(self) -> None:
+        self.ROBOT_LIBRARY_LISTENER = self
+        self._output_file: Optional[str] = None
+
+    @not_keyword
+    def output_file(self, path: str) -> None:
+        self._output_file = Path(path)
+
+    def close(self) -> None:
+        ReportModifier(
+              self._output_file,
+              self._output_file.parent
+        ).write_report()
