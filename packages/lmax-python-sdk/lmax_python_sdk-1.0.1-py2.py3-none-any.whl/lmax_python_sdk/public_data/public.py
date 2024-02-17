9f21a27dfa853817b4d33f34e63134e9ae14ecce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:51:36 +0000

    Add package lmax-python-sdk/1.0.1/lmax_python_sdk-1.0.1-py2.py3-none-any.whl

---
+++ packages/lmax-python-sdk/lmax_python_sdk-1.0.1-py2.py3-none-any.whl/lmax_python_sdk/public_data/public.py
@@ -0,0 +1,8 @@
+from .. import client
+
+
+class LMAXPublicData(client.LMAXClient):
+    """This class is not implemented yet."""
+
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("This class is not implemented yet.")
