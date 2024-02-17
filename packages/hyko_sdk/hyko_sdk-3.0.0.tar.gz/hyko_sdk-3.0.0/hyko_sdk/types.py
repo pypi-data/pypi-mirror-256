fe29b5beddeb52c2d6dad692ea305747561bd5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:29 +0000

    Add package hyko_sdk/3.0.0/hyko_sdk-3.0.0.tar.gz

---
+++ packages/hyko_sdk/hyko_sdk-3.0.0.tar.gz/hyko_sdk-3.0.0/hyko_sdk/types.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Ext(str, Enum):
+    TXT = "txt"
+    CSV = "csv"
+    PDF = "pdf"
+    PNG = "png"
+    JPEG = "jpeg"
+    MPEG = "mpeg"
+    WEBM = "webm"
+    WAV = "wav"
+    MP4 = "mp4"
+    MP3 = "mp3"
