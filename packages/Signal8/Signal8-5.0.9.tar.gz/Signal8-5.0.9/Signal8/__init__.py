f64901fe0244cfdc6c956b0c27564a0c9b1befa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:37 +0000

    Add package Signal8/5.0.9/Signal8-5.0.9.tar.gz

---
+++ packages/Signal8/Signal8-5.0.9.tar.gz/Signal8-5.0.9/Signal8/__init__.py
@@ -0,0 +1,1 @@
+from .Signal8 import env
---
+++ packages/Signal8/Signal8-5.0.9.tar.gz/Signal8-5.0.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Signal8",
+    version="5.0.9",
+    packages=find_packages(),
+    author="Ethan Clark",
+    author_email="eclark715@gmail.com.com",
+    description="A multi-agent environment inspired by the Lewis Signaling Game, featuring eight unique problem configurations with both static and dynamic obstacles.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/ethanmclark1/signal8",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+)
