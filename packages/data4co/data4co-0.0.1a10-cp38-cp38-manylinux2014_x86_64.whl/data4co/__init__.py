cacdad4f9f95b2b100c249675def249b97a910f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:51:43 +0000

    Add package data4co/0.0.1a10/data4co-0.0.1a10-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/data4co/data4co-0.0.1a10-cp38-cp38-manylinux2014_x86_64.whl/data4co/__init__.py
@@ -0,0 +1,7 @@
+from .eva import TSPEvaluator
+from .generator import TSPDataGenerator, MISDataGenerator
+from .solver import TSPSolver, TSPLKHSolver, TSPConcordeSolver
+from .solver import MISSolver, KaMIS, MISGurobi
+
+__version__ = '0.0.1a10'
+__author__ = 'ThinkLab at SJTU'
