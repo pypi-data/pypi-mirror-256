d991d4e1e1cf09b5bc01fcd8c328e92a401f94d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:58 +0000

    Add package toolong/1.0.2/toolong-1.0.2.tar.gz

---
+++ packages/toolong/toolong-1.0.2.tar.gz/toolong-1.0.2/src/toolong/cli.py
@@ -0,0 +1,18 @@
+from importlib.metadata import version
+import click
+
+from toolong.ui import UI
+
+
+@click.command()
+@click.version_option(version("toolong"))
+@click.argument("files", metavar="FILE1 FILE2", nargs=-1)
+@click.option("-m", "--merge", is_flag=True, help="Merge files")
+def run(files: list[str], merge: bool):
+    """View / tail / search log files."""
+    if not files:
+        ctx = click.get_current_context()
+        click.echo(ctx.get_help())
+        ctx.exit()
+    ui = UI(files, merge=merge)
+    ui.run()
