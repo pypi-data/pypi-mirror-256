274ca4d4f979f17c8bbbe3ddbdae24f633d084a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:43 +0000

    Add package tcrutils/0.0.90/tcrutils-0.0.90-py3-none-any.whl

---
+++ packages/tcrutils/tcrutils-0.0.90-py3-none-any.whl/tcrutils/src/tcr_int.py
@@ -0,0 +1,25 @@
+from builtins import hex as sex  # nocom (actually yescom because this text is a comment dummy!!)
+from collections.abc import Iterable, Mapping
+
+
+def hex(number: int, leading_zeroes=2, *, upper=True) -> str:
+  """Slightly more advanced version of builtin `hex()`, offers ability to choose if uppercase and how many leading zeroes."""
+  hex_output = sex(number)
+  hex_value = (
+    hex_output[2:].zfill(leading_zeroes).upper() if upper else hex_output[2:].zfill(leading_zeroes)
+  )
+
+  formatted_output = f'0x{hex_value}'
+  if not upper:
+    formatted_output = formatted_output.lower()
+
+  return formatted_output
+
+
+def recursive_sum(it: Iterable):
+  s = 0
+
+  for item in it.values() if isinstance(it, Mapping) else it:
+    s = s + recursive_sum(item) if isinstance(item, Iterable) else s + item
+
+  return s
