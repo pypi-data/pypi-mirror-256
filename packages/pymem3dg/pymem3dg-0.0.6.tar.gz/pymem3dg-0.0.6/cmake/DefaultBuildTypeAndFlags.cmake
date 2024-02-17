39942b1c317094ece62ea8a6ed25e4635840dedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:39 +0000

    Add package pymem3dg/0.0.6/pymem3dg-0.0.6.tar.gz

---
+++ packages/pymem3dg/pymem3dg-0.0.6.tar.gz/pymem3dg-0.0.6/cmake/DefaultBuildTypeAndFlags.cmake
@@ -0,0 +1,62 @@
+# Membrane Dynamics in 3D using Discrete Differential Geometry (Mem3DG)
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2020:
+#     Laboratory for Computational Cellular Mechanobiology
+#     Cuncheng Zhu (cuzhu@eng.ucsd.edu)
+#     Christopher T. Lee (ctlee@ucsd.edu)
+#     Ravi Ramamoorthi (ravir@cs.ucsd.edu)
+#     Padmini Rangamani (prangamani@eng.ucsd.edu)
+#
+
+
+set(CMAKE_C_VISIBILITY_PRESET hidden)
+set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+
+# Set a default build type if none was specified
+set(default_build_type "Release")
+
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
+      STRING "Choose the type of build." FORCE)
+  # Set the possible values of build type for cmake-gui
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
+    "MinSizeRel" "RelWithDebInfo")
+endif()
+
+
+if(MSVC)
+    option(USE_MSVC_RUNTIME_LIBRARY_DLL "Use MSVC runtime library DLL" ON)
+
+    if(CMAKE_VERSION VERSION_LESS 3.15 AND NOT USE_MSVC_RUNTIME_LIBRARY_DLL)
+        foreach (flag
+            CMAKE_C_FLAGS
+            CMAKE_C_FLAGS_DEBUG
+            CMAKE_C_FLAGS_RELEASE
+            CMAKE_C_FLAGS_MINSIZEREL
+            CMAKE_C_FLAGS_RELWITHDEBINFO
+            CMAKE_CXX_FLAGS
+            CMAKE_CXX_FLAGS_DEBUG
+            CMAKE_CXX_FLAGS_RELEASE
+            CMAKE_CXX_FLAGS_MINSIZEREL
+            CMAKE_CXX_FLAGS_RELWITHDEBINFO
+        )
+            if (${flag} MATCHES "/MD")
+                string(REGEX REPLACE "/MD" "/MT" ${flag} "${${flag}}")
+            endif()
+            if (${flag} MATCHES "/MDd")
+                string(REGEX REPLACE "/MDd" "/MTd" ${flag} "${${flag}}")
+            endif()
+        endforeach()
+    else()
+        if(USE_MSVC_RUNTIME_LIBRARY_DLL)
+            set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+        else()
+            set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+        endif()
+    endif()
+endif(MSVC)
---
+++ packages/pymem3dg/pymem3dg-0.0.6.tar.gz/pymem3dg-0.0.6/include/mem3dg/solver/integrator/forward_euler.h
@@ -0,0 +1,87 @@
+// Membrane Dynamics in 3D using Discrete Differential Geometry (Mem3DG)
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2021:
+//     Laboratory for Computational Cellular Mechanobiology
+//     Cuncheng Zhu (cuzhu@eng.ucsd.edu)
+//     Christopher T. Lee (ctlee@ucsd.edu)
+//     Ravi Ramamoorthi (ravir@cs.ucsd.edu)
+//     Padmini Rangamani (prangamani@eng.ucsd.edu)
+//
+
+#pragma once
+
+#include "mem3dg/solver/integrator/integrator.h"
+#include "mem3dg/solver/system.h"
+
+namespace mem3dg {
+namespace solver {
+namespace integrator {
+/**
+ * @brief Euler (gradient descent) time Integration
+ * @param system_ System object to be integrated
+ * @param characteristicTimeStep_ time step, or the initial time step for
+ * backtracking algorithm
+ * @param totalTime_ total simulation time
+ * @param savePeriod_ period of saving output data (in the unit of
+ * characteristicTimeStep_)
+ * @param tolerance_ tolerance of force L2 norm for termination
+ * @param outputDirectory_ path to the output directory
+ * @param frame_ frame index, if nonzero, enable continuation mode
+ */
+class DLL_PUBLIC Euler : public Integrator {
+public:
+  Euler(System &system_, double characteristicTimeStep_, double totalTime_,
+        double savePeriod_, double tolerance_, std::string outputDirectory_,
+        std::size_t frame_ = 0)
+      : Integrator(system_, characteristicTimeStep_, totalTime_, savePeriod_,
+                   tolerance_, outputDirectory_, frame_) {
+    // check the validity of parameter
+    checkParameters();
+  }
+  Euler(System &system_, double characteristicTimeStep_, double tolerance_,
+        std::string outputDirectory_)
+      : Integrator(system_, characteristicTimeStep_, tolerance_,
+                   outputDirectory_) {
+    checkParameters();
+  }
+
+  /**
+   * @brief Forward Euler driver function
+   */
+  bool integrate() override;
+
+  /**
+   * @brief Forward Euler stepper
+   */
+  void march() override;
+
+  // std::tuple<EigenVectorX3dr, EigenVectorX1d>
+  // flowMap(EigenVectorX3dr &position, EigenVectorX1d &protein, double h);
+
+  /**
+   * @brief Forward Euler status computation and thresholding
+   */
+  void status() override;
+
+  /**
+   * @brief Check parameters for time integration
+   */
+  void checkParameters() override;
+
+  /**
+   * @brief step for n iterations
+   */
+  void step(std::size_t n) {
+    for (std::size_t i = 0; i < n; i++) {
+      status();
+      march();
+    }
+  }
+};
+} // namespace integrator
+} // namespace solver
+} // namespace mem3dg
