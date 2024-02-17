d983063946397cc41145cb595ec87f1a892a9cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:46 +0000

    Add package simple-pyper/0.0.1/simple-pyper-0.0.1.tar.gz

---
+++ packages/simple-pyper/simple-pyper-0.0.1.tar.gz/simple-pyper-0.0.1/setup.py
@@ -0,0 +1,28 @@
+
+#-*- encoding: UTF-8 -*-
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+setup(name='simple-pyper',
+      version=VERSION,
+      description="a tiny cli tool for using any python file esaily.",
+      long_description='just enjoy',
+      classifiers=[], 
+      author='chen',
+      author_email='c1556613010@outlook.com',
+      url='https://github.com/chenyiru3/simple-pyper',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'requests',
+      ],
+      entry_points={
+        'console_scripts':[
+            'pyper = simple_pyper.main:main'
+        ]
+      },
+)
+
