41bef5b598bdad5b151086a9ca1513725a5c8033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:39 +0000

    Add package lemmyreportmessenger/0.1.0a30/lemmyreportmessenger-0.1.0a30-py3-none-any.whl

---
+++ packages/lemmyreportmessenger/lemmyreportmessenger-0.1.0a30-py3-none-any.whl/data/matrix_facade.py
@@ -0,0 +1,39 @@
+import asyncio
+
+from nio import AsyncClient
+
+from lemmyreportmessenger.data import ContentType
+
+
+class MatrixFacade:
+    client: AsyncClient
+    room_id: str
+    lemmy_instance: str
+
+    def __init__(self, client: AsyncClient, password: str, room_id: str, instance_url: str):
+        self.client = client
+        self.room_id = room_id
+        self.lemmy_instance = instance_url
+        asyncio.run(self._setup(password))
+
+    async def _setup(self, password: str):
+        print(await self.client.login(password=password, device_name="lemmy-report-bot"))
+
+        if self.room_id in (await self.client.joined_rooms()).rooms:
+            return
+        await self.client.join(self.room_id)
+
+    async def send_report_message(self, content_id: int, content_type: ContentType, reason: str):
+        print("Sending report to matrix!")
+        url = f"{self.lemmy_instance}/{'post' if content_type == ContentType.POST else 'comment'}/{content_id}"
+
+        await self.client.room_send(
+            room_id=self.room_id,
+            message_type="m.room.message",
+            content={
+                "msgtype": "m.notice",
+                "format": "org.matrix.custom.html",
+                "body": f"The post at {url} has been reported for {reason}",
+                "formatted_body": f"The post at <a href='{url}'>{url}</a> has been reported for <i>{reason}</i>"
+            }
+        )
