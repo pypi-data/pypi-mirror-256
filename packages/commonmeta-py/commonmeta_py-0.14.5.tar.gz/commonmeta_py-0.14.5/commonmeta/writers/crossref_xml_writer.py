43175f5f48cc633a54d0a747a543d88a29d8d3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:33 +0000

    Add package commonmeta-py/0.14.5/commonmeta_py-0.14.5.tar.gz

---
+++ packages/commonmeta-py/commonmeta_py-0.14.5.tar.gz/commonmeta_py-0.14.5/commonmeta/writers/crossref_xml_writer.py
@@ -0,0 +1,9 @@
+"""Crossref XML writer for commonmeta-py"""
+from typing import Optional
+from ..constants import Commonmeta
+from ..crossref_utils import generate_crossref_xml
+
+
+def write_crossref_xml(metadata: Commonmeta) -> Optional[str]:
+    """Write Crossref XML"""
+    return generate_crossref_xml(metadata)
