651e4d0fcea60ab6b894230fe31bd264dc0c0519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:34 +0000

    Add package Njalla/0.4.1/Njalla-0.4.1.tar.gz

---
+++ packages/Njalla/Njalla-0.4.1.tar.gz/Njalla-0.4.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Njalla',
+    version='0.4.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+
+        ],
+    },
+)
