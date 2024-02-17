00c0bc318afe922b3f07bd7e05919b42324de8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:52 +0000

    Add package github-webhooks-framework2/0.1.13/github_webhooks_framework2-0.1.13-py3-none-any.whl

---
+++ packages/github-webhooks-framework2/github_webhooks_framework2-0.1.13-py3-none-any.whl/github_webhooks/handlers/ping.py
@@ -0,0 +1,16 @@
+import logging
+from typing import Optional
+
+from starlette.requests import QueryParams
+
+from github_webhooks.schemas import WebhookCommonPayload, WebhookHeaders
+
+
+class PingPayload(WebhookCommonPayload):
+    zen: str
+    hook_id: int
+
+
+async def handle_ping(payload: PingPayload, headers: WebhookHeaders, query_params: QueryParams) -> Optional[str]:
+    logging.debug('ping event, headers: %s, payload: %s, query_params: %s', headers, payload, query_params)
+    return 'OK'
