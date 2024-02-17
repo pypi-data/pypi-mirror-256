77cd8af538ecb773739f47dde72fbbe2d3222f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:41 +0000

    Add package FruceAPI/1.0.3/FruceAPI-1.0.3.tar.gz

---
+++ packages/FruceAPI/FruceAPI-1.0.3.tar.gz/FruceAPI-1.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open(r'M:\PyPl\FRAPI\README.md', 'r', encoding='utf-8') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='FruceAPI',
+	version='1.0.3',
+	author='kotvpalto',
+	author_email='kotvpaltoof@ya.ru',
+	description='',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/catvibriss/FruceAPI/tree/main',
+	packages=['FruceAPI'],
+	install_requires=["aiohttp"],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
