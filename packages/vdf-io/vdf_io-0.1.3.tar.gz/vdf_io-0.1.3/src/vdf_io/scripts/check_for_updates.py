3bd37114b6ae8b3d7b5a9177d3934bb783f4c8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:44 +0000

    Add package vdf-io/0.1.3/vdf_io-0.1.3.tar.gz

---
+++ packages/vdf-io/vdf_io-0.1.3.tar.gz/vdf_io-0.1.3/src/vdf_io/scripts/check_for_updates.py
@@ -0,0 +1,14 @@
+import requests
+
+import vdf_io
+
+def check_for_updates():
+    package_name = 'vdf-io'
+    response = requests.get(f'https://pypi.org/pypi/{package_name}/json')
+    if response.status_code == 200:
+        latest_version = response.json()['info']['version']
+        if latest_version != vdf_io.__version__:
+            print(f'Update available: {latest_version}. Run `pip install --upgrade {package_name}` to update.')
+
+if __name__ == "__main__":
+    check_for_updates()
