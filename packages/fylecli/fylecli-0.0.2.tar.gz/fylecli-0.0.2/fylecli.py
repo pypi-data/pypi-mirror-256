b66032a6073037a02d22770d81f073b52c22fbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:14 +0000

    Add package fylecli/0.0.2/fylecli-0.0.2.tar.gz

---
+++ packages/fylecli/fylecli-0.0.2.tar.gz/fylecli-0.0.2/fylecli.py
@@ -0,0 +1,9 @@
+from fyle_internals import bootstrap
+
+
+def main():
+    bootstrap.start()
+
+
+if __name__ == "__main__":
+    bootstrap.start()
---
+++ packages/fylecli/fylecli-0.0.2.tar.gz/fylecli-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='fylecli',
+    version='0.0.2',
+    description='File transfer',
+    packages=['fyle_internals'],
+    scripts=['fylecli.py'],
+    entry_points={
+        'console_scripts': [
+            'fylecli=fylecli.fylecli:main'
+        ]
+    },
+    include_package_data=True,
+    platforms=['any'],
+    author='Mark Smirnov',
+    author_email='mark@mark99.ru'
+)
