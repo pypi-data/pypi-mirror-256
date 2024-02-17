133a3b8d8a3cbb023ff120fa3cde83d91d5c1e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:48 +0000

    Add package dgood-gabinodist/0.3/dgood_gabinodist-0.3.tar.gz

---
+++ packages/dgood-gabinodist/dgood_gabinodist-0.3.tar.gz/dgood_gabinodist-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dgood_gabinodist',
+      version='0.3',
+      description='Gaussian and binomial distributions',
+      long_description='The dgood-gabinodist package has classes that will do the following for Gaussian and Binomial distributions: Read data from file, calculate the mean and standard deviation, find the probability distribution function, display plots, add two distributions. This was created as part of the Udacity lesson: introduction to object oriented programming with Python.',
+      packages=['dgood_gabinodist'],
+      zip_safe=False)
