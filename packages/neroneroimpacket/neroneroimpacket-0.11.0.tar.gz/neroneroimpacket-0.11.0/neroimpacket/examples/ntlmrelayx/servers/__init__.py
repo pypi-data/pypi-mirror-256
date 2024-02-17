1c7bb51a6ad6e56c679fc1dd6a2e8e97ef2f3a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:36:00 +0000

    Add package neroneroimpacket/0.11.0/neroneroimpacket-0.11.0.tar.gz

---
+++ packages/neroneroimpacket/neroneroimpacket-0.11.0.tar.gz/neroneroimpacket-0.11.0/neroimpacket/examples/ntlmrelayx/servers/__init__.py
@@ -0,0 +1,12 @@
+# Impacket - Collection of Python classes for working with network protocols.
+#
+# Copyright (C) 2023 Fortra. All rights reserved.
+#
+# This software is provided under a slightly modified version
+# of the Apache Software License. See the accompanying LICENSE file
+# for more information.
+#
+from neroimpacket.examples.ntlmrelayx.servers.httprelayserver import HTTPRelayServer
+from neroimpacket.examples.ntlmrelayx.servers.smbrelayserver import SMBRelayServer
+from neroimpacket.examples.ntlmrelayx.servers.wcfrelayserver import WCFRelayServer
+from neroimpacket.examples.ntlmrelayx.servers.rawrelayserver import RAWRelayServer
---
+++ packages/neroneroimpacket/neroneroimpacket-0.11.0.tar.gz/neroneroimpacket-0.11.0/setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# Impacket - Collection of Python classes for working with network protocols.
+#
+# Copyright (C) 2023 Fortra. All rights reserved.
+#
+# This software is provided under a slightly modified version
+# of the Apache Software License. See the accompanying LICENSE file
+# for more information.
+#
+# Description:
+#   Setup file
+#
+import glob
+import os
+import platform
+
+from setuptools import setup
+from subprocess import *
+
+PACKAGE_NAME = "neroneroimpacket"
+
+VER_MAJOR = 0
+VER_MINOR = 11
+VER_MAINT = 0
+VER_PREREL = ""
+try:
+    if call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w')) == 0:
+        p = Popen("git log -1 --format=%cd --date=format:%Y%m%d.%H%M%S", shell=True, stdin=PIPE, stderr=PIPE, stdout=PIPE)
+        (outstr, __) = p.communicate()
+        (VER_CDATE,VER_CTIME) = outstr.strip().decode("utf-8").split('.')
+
+        p = Popen("git rev-parse --short HEAD", shell=True, stdin=PIPE, stderr=PIPE, stdout=PIPE)
+        (outstr, __) = p.communicate()
+        VER_CHASH = outstr.strip().decode("utf-8")
+
+        VER_LOCAL = "+{}.{}.{}".format(VER_CDATE, VER_CTIME, VER_CHASH)
+    else:
+        VER_LOCAL = ""
+except Exception:
+    VER_LOCAL = ""
+
+if platform.system() != 'Darwin':
+    data_files = [(os.path.join('share', 'doc', PACKAGE_NAME), ['README.md', 'LICENSE']+glob.glob('doc/*'))]
+else:
+    data_files = []
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name=PACKAGE_NAME,
+    version="{}.{}.{}".format(VER_MAJOR, VER_MINOR, VER_MAINT),
+    description="Network protocols Constructors and Dissectors",
+    url="https://www.coresecurity.com",
+    author="SecureAuth Corporation",
+    maintainer="Fortra",
+    license="Apache modified",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    platforms=["Unix", "Windows"],
+    packages=['neroimpacket', 'neroimpacket.dcerpc', 'neroimpacket.examples', 'neroimpacket.dcerpc.v5', 'neroimpacket.dcerpc.v5.dcom',
+              'neroimpacket.krb5', 'neroimpacket.ldap', 'neroimpacket.examples.ntlmrelayx',
+              'neroimpacket.examples.ntlmrelayx.clients', 'neroimpacket.examples.ntlmrelayx.servers',
+              'neroimpacket.examples.ntlmrelayx.servers.socksplugins', 'neroimpacket.examples.ntlmrelayx.utils',
+              'neroimpacket.examples.ntlmrelayx.attacks', 'neroimpacket.examples.ntlmrelayx.attacks.httpattacks'],
+    scripts=glob.glob(os.path.join('examples', '*.py')),
+    data_files=data_files,
+    install_requires=['pyasn1>=0.2.3', 'pycryptodomex', 'pyOpenSSL>=21.0.0', 'six', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6',
+                      'ldapdomaindump>=0.9.0', 'flask>=1.0', 'future', 'charset_normalizer', 'dsinternals'],
+    extras_require={'pyreadline:sys_platform=="win32"': [],
+                    },
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
