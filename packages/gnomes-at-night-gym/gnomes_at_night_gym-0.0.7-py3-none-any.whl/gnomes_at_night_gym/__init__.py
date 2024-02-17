67967e372352ad266b373ff8b30b0079f13b8035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:45:16 +0000

    Add package gnomes-at-night-gym/0.0.7/gnomes_at_night_gym-0.0.7-py3-none-any.whl

---
+++ packages/gnomes-at-night-gym/gnomes_at_night_gym-0.0.7-py3-none-any.whl/gnomes_at_night_gym/__init__.py
@@ -0,0 +1,13 @@
+from gymnasium.envs.registration import register
+
+register(
+     id="Board9A-v0",
+     entry_point="gnomes_at_night_gym.envs:GnomesAtNightEnv9A",
+     max_episode_steps=300,
+)
+
+register(
+     id="Board5A-v0",
+     entry_point="gnomes_at_night_gym.envs:GnomesAtNightEnv5A",
+     max_episode_steps=300,
+)
