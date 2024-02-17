7609e3adbb4b5577b3f14852c0f01d93608329b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:43 +0000

    Add package validpeas/0.1/validpeas-0.1.tar.gz

---
+++ packages/validpeas/validpeas-0.1.tar.gz/validpeas-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="validpeas",
+    version="0.1",
+    packages=find_packages(),
+    description="Tools for structured validations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Juan Ezequiel Molina Riddell",
+    author_email="jmriddell@protonmain.ch",
+    url="https://github.com/jmriddell/validpeas",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="validation",
+    install_requires=["pydantic"],
+)
