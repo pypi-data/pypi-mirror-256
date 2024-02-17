bd7caa5eab755c7445a221ebdb0638f30ff61923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:36:32 +0000

    Add package lunnaris-pyinject/1.0.0/lunnaris-pyinject-1.0.0.tar.gz

---
+++ packages/lunnaris-pyinject/lunnaris-pyinject-1.0.0.tar.gz/lunnaris-pyinject-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+long_desc = ''
+with open("README.md", "r", encoding='utf-8') as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name="lunnaris-pyinject",
+    version='1.0.0',
+    description='Dependency injection package',
+    package_dir={"lunnaris_pyinject": "lunnaris_pyinject"},
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    author='Kenneth Ulloa',
+    author_email='ulloakenth@gmail.com',
+    url='https://github.com/KennethUlloa/pyinject',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Utilities"
+    ],
+    python_requires=">=3.10",
+    packages=["lunnaris_pyinject"],
+    include_package_data=True,
+    extras_require={
+        "dev": ["pytest"]
+    }
+)
