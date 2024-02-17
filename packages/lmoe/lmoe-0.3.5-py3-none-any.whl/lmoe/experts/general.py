fd18ae3d1abfc570e35a972a63d692fe91624183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:51 +0000

    Add package lmoe/0.3.5/lmoe-0.3.5-py3-none-any.whl

---
+++ packages/lmoe/lmoe-0.3.5-py3-none-any.whl/lmoe/experts/general.py
@@ -0,0 +1,37 @@
+from lmoe.api.base_expert import BaseExpert
+from lmoe.api.lmoe_query import LmoeQuery
+from lmoe.framework.expert_registry import expert
+
+import ollama
+
+
+@expert
+class General(BaseExpert):
+
+    @classmethod
+    def name(cls):
+        return "GENERAL"
+
+    @classmethod
+    def has_modelfile(cls):
+        return True
+
+    def description(self):
+        return "An all-purpose model, to be used if a question is asked which requires general knowledge, or if you cannot determine a more specific model that would be more appropriate."
+
+    def example_queries(self):
+        return [
+            "what is the last item in the list?",
+            "What is the distance between Earth and Mars?",
+        ]
+
+    def generate(self, lmoe_query: LmoeQuery):
+        stream = ollama.generate(
+            model="lmoe_general",
+            prompt=lmoe_query.render(),
+            stream=True,
+        )
+        for chunk in stream:
+            if chunk["response"] or not chunk["done"]:
+                print(chunk["response"], end="", flush=True)
+        print("")
