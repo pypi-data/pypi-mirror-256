0ccd80623bea2b5f0620e0a6583aa8cbaf42b048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:55 +0000

    Add package systemd-language-server/0.2.0/systemd_language_server-0.2.0.tar.gz

---
+++ packages/systemd-language-server/systemd_language_server-0.2.0.tar.gz/systemd_language_server-0.2.0/systemd_language_server/server.py
@@ -0,0 +1,132 @@
+import re
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+import logging
+import sys
+import os.path
+
+from pygls.server import LanguageServer
+from lsprotocol.types import (
+    INITIALIZE,
+    TEXT_DOCUMENT_COMPLETION,
+    TEXT_DOCUMENT_HOVER,
+    CompletionParams,
+    CompletionList,
+    CompletionItem,
+    CompletionItemKind,
+    CompletionOptions,
+    HoverParams,
+    InitializedParams,
+)
+from .unit import (
+    get_current_section,
+    get_directives,
+    UnitType,
+    UnitFileSection,
+    unit_type_to_unit_file_section,
+)
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="systemd_language_server.log")
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+server = LanguageServer("systemd-language-server", "v0.1")
+
+logging.basicConfig(
+    filename="systemd_language_server.log", filemode="w", level=logging.DEBUG
+)
+
+
+class SystemdLanguageServer(LanguageServer):
+    _has_pandoc: bool
+
+    def __init__(self):
+        _has_pandoc = os.path.exists("/bin/pandoc")
+
+
+@server.feature(INITIALIZE)
+def initialize(params: InitializedParams):
+    pass
+
+
+def complete_unit_file_section(params: CompletionParams, unit_type: UnitType):
+    possible_sections = [UnitFileSection.install, UnitFileSection.unit]
+    section = unit_type_to_unit_file_section(unit_type)
+    if section is not None:
+        possible_sections.append(section)
+    items = [
+        CompletionItem(
+            label=sec.value, insert_text=sec.value + "]", kind=CompletionItemKind.Struct
+        )
+        for sec in possible_sections
+    ]
+    return CompletionList(is_incomplete=False, items=items)
+
+
+def complete_directive_property(
+    params: CompletionParams,
+    unit_type: UnitType,
+    section: UnitFileSection | None,
+    current_line: str,
+):
+    directive = current_line.split("=")[0]
+    pass
+
+
+def complete_directive(
+    params: CompletionParams,
+    unit_type: UnitType,
+    section: UnitFileSection | None,
+    current_line: str,
+):
+    directives = get_directives(unit_type, section)
+    logger.debug(directives)
+    items = [
+        CompletionItem(label=s, insert_text=s + "=", kind=CompletionItemKind.Property)
+        for s in directives
+        if s.startswith(current_line)
+    ]
+    return CompletionList(is_incomplete=False, items=items)
+
+
+@server.feature(
+    TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=["[', '="])
+)
+def textDocument_completion(params: CompletionParams) -> CompletionList | None:
+    """Complete systemd unit properties. Determine the required completion type and
+    dispatch it."""
+    items = []
+    uri = params.text_document.uri
+    document = server.workspace.get_document(uri)
+    current_line = document.lines[params.position.line].strip()
+    unit_type = UnitType(Path(uri).suffix.strip("."))
+    section = get_current_section(document, params.position)
+    logger.debug(f"{unit_type=} {section=}")
+
+    if current_line == "[":
+        return complete_unit_file_section(params, unit_type)
+    elif "=" not in current_line:
+        return complete_directive(params, unit_type, section, current_line)
+    elif len(current_line.split("=")) == 2:
+        return complete_directive_property(params, unit_type, section, current_line)
+
+
+@server.feature(TEXT_DOCUMENT_HOVER)
+def textDocument_hover(params: HoverParams):
+    """Help for directives."""
+    document = server.workspace.get_document(params.text_document.uri)
+    current_line = document.lines[params.position.line].strip()
+
+
+def get_parser():
+    parser = ArgumentParser()
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args(sys.argv[1:])
+    server.start_io()
---
+++ packages/systemd-language-server/systemd_language_server-0.2.0.tar.gz/systemd_language_server-0.2.0/systemd_language_server/unit.py
@@ -0,0 +1,158 @@
+import re
+from enum import Enum, auto
+from itertools import islice
+import logging
+from glob import glob
+from pathlib import Path
+
+from lxml import etree  # type: ignore
+from pygls.workspace import TextDocument
+from lsprotocol.types import Position
+
+from .constants import (
+    systemd_unit_directives,
+    systemd_install_directives,
+    systemd_service_directives,
+    systemd_socket_directives,
+    systemd_mount_directives,
+    systemd_automount_directives,
+    systemd_timer_directives,
+    systemd_scope_directives,
+    systemd_swap_directives,
+    systemd_path_directives,
+    systemd_exec_directives,
+    systemd_kill_directives,
+)
+
+#  The ultimate source for information on unit files is the docbook files distributed with
+#  systemd. Therefore, the following data is managed by the language server:
+#  - unit type
+#  - unit file sections
+#  - directives
+#  - directive values
+#  - which docbook (.xml) directives are documented in
+#  Data is resolved at runtime, to the extent possible, therefore docbooks are bundled
+#  with systemd-language-server and parsed as required.
+
+SECTION_HEADER_PROG = re.compile(r"^\[(?P<name>\w+)\]$")
+
+
+class UnitType(Enum):
+    service = "service"
+    socket = "socket"
+    device = "device"
+    mount = "mount"
+    automount = "automount"
+    swap = "swap"
+    target = "target"
+    path = "path"
+    timer = "timer"
+    slice = "slice"
+    scope = "scope"
+
+
+class UnitFileSection(Enum):
+    unit = "Unit"
+    install = "Install"
+    service = "Service"
+    socket = "Socket"
+    mount = "Mount"
+    automount = "Automount"
+    swap = "Swap"
+    path = "Path"
+    timer = "Timer"
+
+
+_assets_dir = Path(__file__).absolute().parent / "assets"
+docbooks = glob("*.xml", root_dir=_assets_dir)
+
+#  dict mapping docbook documentation file to the list of  systemd unit directives
+#  documented within
+directives = dict()
+
+
+def initialize_directive():
+    for filename in docbooks:
+        docbook_file = _assets_dir / filename
+        tree = etree.parse(open(docbook_file).read())
+        directives[filename] = tree.xpath()
+        #  TODO 10/02/20 psacawa: finish this
+
+
+def init():
+    """docstring for init"""
+    pass
+
+
+def unit_type_to_unit_file_section(ut: UnitType) -> UnitFileSection | None:
+    try:
+        return UnitFileSection(ut.value.capitalize())
+    except Exception:
+        return None
+
+
+def unit_file_section_to_unit_type(ufs: UnitFileSection) -> UnitType | None:
+    try:
+        return UnitType(ufs.value.lower())
+    except Exception:
+        return None
+
+
+directive_dict = {
+    UnitFileSection.service: systemd_service_directives,
+    UnitFileSection.timer: systemd_timer_directives,
+    UnitFileSection.socket: systemd_socket_directives,
+    UnitFileSection.mount: systemd_mount_directives,
+    UnitFileSection.automount: systemd_automount_directives,
+    UnitFileSection.swap: systemd_swap_directives,
+    UnitFileSection.path: systemd_path_directives,
+}
+
+
+def get_directives(unit_type: UnitType, section: UnitFileSection | None) -> list[str]:
+    #  Two variants: i) the current unit file section is known, ii) it isn't (e.g. buffer
+    #  has no section headers yet). If it is, we supply completions value for the unit
+    #  type/section. Otherwise, we supply those valid for all sections.
+    if section == UnitFileSection.unit:
+        return systemd_unit_directives
+    if section == UnitFileSection.install:
+        return systemd_install_directives
+
+    directives: list[str] = []
+    if section is None:
+        #  if unit type has a corresponding unit file section, add it
+        section_from_type = unit_type_to_unit_file_section(unit_type)
+        if section_from_type is not None:
+            directives += directive_dict[section_from_type]
+        directives += systemd_unit_directives + systemd_install_directives
+    else:
+        directives = directive_dict[section]
+
+    if unit_type in [
+        UnitType.service,
+        UnitType.socket,
+        UnitType.mount,
+        UnitType.swap,
+    ]:
+        directives += systemd_exec_directives + systemd_kill_directives
+    return directives
+
+    return []
+
+
+def get_current_section(
+    document: TextDocument, position: Position
+) -> UnitFileSection | None:
+    """Determine section of cursor in current document"""
+
+    for i in reversed(range(0, position.line)):
+        line = document.lines[i].strip()
+        match = SECTION_HEADER_PROG.search(line)
+        logging.debug(f"{line=} {match=}")
+        if match is not None:
+            try:
+                section = UnitFileSection(match.group("name"))
+                return section
+            except ValueError:
+                pass
+    return None
