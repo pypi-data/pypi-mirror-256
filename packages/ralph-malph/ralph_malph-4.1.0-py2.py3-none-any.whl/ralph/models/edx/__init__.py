4fde2068acca21a804bcaec0bc247624f53a0c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:35 +0000

    Add package ralph-malph/4.1.0/ralph_malph-4.1.0-py2.py3-none-any.whl

---
+++ packages/ralph-malph/ralph_malph-4.1.0-py2.py3-none-any.whl/ralph/models/edx/__init__.py
@@ -0,0 +1,78 @@
+"""edX pydantic models."""
+
+# flake8: noqa
+
+from ralph.models.edx.navigational import statements
+
+from .enrollment.statements import (
+    EdxCourseEnrollmentActivated,
+    EdxCourseEnrollmentDeactivated,
+    EdxCourseEnrollmentModeChanged,
+    EdxCourseEnrollmentUpgradeSucceeded,
+    UIEdxCourseEnrollmentUpgradeClicked,
+)
+from .navigational.statements import UIPageClose, UISeqGoto, UISeqNext, UISeqPrev
+from .open_response_assessment.statements import (
+    ORACreateSubmission,
+    ORAGetPeerSubmission,
+    ORAGetSubmissionForStaffGrading,
+    ORAPeerAssess,
+    ORASaveSubmission,
+    ORASelfAssess,
+    ORAStaffAssess,
+    ORAStudentTrainingAssessExample,
+    ORASubmitFeedbackOnAssessments,
+    ORAUploadFile,
+)
+from .peer_instruction.statements import (
+    PeerInstructionAccessed,
+    PeerInstructionOriginalSubmitted,
+    PeerInstructionRevisedSubmitted,
+)
+from .problem_interaction.statements import (
+    EdxProblemHintDemandhintDisplayed,
+    EdxProblemHintFeedbackDisplayed,
+    ProblemCheck,
+    ProblemCheckFail,
+    ProblemRescore,
+    ProblemRescoreFail,
+    ResetProblem,
+    ResetProblemFail,
+    SaveProblemFail,
+    SaveProblemSuccess,
+    ShowAnswer,
+    UIProblemCheck,
+    UIProblemGraded,
+    UIProblemReset,
+    UIProblemSave,
+    UIProblemShow,
+)
+from .server import Server
+from .textbook_interaction.statements import (
+    UIBook,
+    UITextbookPdfChapterNavigated,
+    UITextbookPdfDisplayScaled,
+    UITextbookPdfOutlineToggled,
+    UITextbookPdfPageNavigated,
+    UITextbookPdfPageScrolled,
+    UITextbookPdfSearchCaseSensitivityToggled,
+    UITextbookPdfSearchExecuted,
+    UITextbookPdfSearchHighlightToggled,
+    UITextbookPdfSearchNavigatedNext,
+    UITextbookPdfThumbnailNavigated,
+    UITextbookPdfThumbnailsToggled,
+    UITextbookPdfZoomButtonsChanged,
+    UITextbookPdfZoomMenuChanged,
+)
+from .video.statements import (
+    UIHideTranscript,
+    UILoadVideo,
+    UIPauseVideo,
+    UIPlayVideo,
+    UISeekVideo,
+    UIShowTranscript,
+    UISpeedChangeVideo,
+    UIStopVideo,
+    UIVideoHideCCMenu,
+    UIVideoShowCCMenu,
+)
