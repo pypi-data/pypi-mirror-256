cc9fb48a0c5908c8e230e27715895d8988a4680f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:18 +0000

    Add package ctransformer-core/0.0.2/ctransformer_core-0.0.2.tar.gz

---
+++ packages/ctransformer-core/ctransformer_core-0.0.2.tar.gz/ctransformer_core-0.0.2/README.md
@@ -0,0 +1,13 @@
+### ctransformer-core
+
+[<img src="https://raw.githubusercontent.com/calcuis/ctransformer-core/master/milk.gif" width="128" height="128">](https://github.com/calcuis/ctransformer-core)
+[![Static Badge](https://img.shields.io/badge/core-0.0.2-yellow?logo=github)](https://github.com/calcuis/ctransformer-core/releases)
+
+Follow up the stopping updated project ctransformers; continue working on it.
+
+#### install it via pip/pip3:
+```
+pip install ctransformer-core
+```
+
+This is another solo connector core; same as llama-core; being able to work independently as well.
---
+++ packages/ctransformer-core/ctransformer_core-0.0.2.tar.gz/ctransformer_core-0.0.2/tests/test_model.py
@@ -0,0 +1,20 @@
+from ctransformer_core import AutoModelForCausalLM
+
+
+class TestModel:
+    def test_generate(self, lib):
+        llm = AutoModelForCausalLM.from_pretrained("marella/gpt-2-ggml", lib=lib)
+        response = llm("AI is going to", seed=5, max_new_tokens=3)
+        assert response == " be a big"
+
+        token = llm.sample()
+        logits = llm.logits
+        value = logits[token]
+        logits[token] -= 1
+        assert logits[token] == llm.logits[token] == value - 1
+        llm.logits[token] *= 2
+        assert logits[token] == llm.logits[token] == (value - 1) * 2
+
+        assert llm.eos_token_id == 50256
+        assert llm.vocab_size == 50257
+        assert llm.context_length == 1024
