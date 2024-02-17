579d194a0f842b9b8bb4c3d2414a27750906a54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:04 +0000

    Add package Rubik-Vault/5.4/Rubik_Vault-5.4.tar.gz

---
+++ packages/Rubik-Vault/Rubik_Vault-5.4.tar.gz/Rubik_Vault-5.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+    name='Rubik_Vault',
+    version='5.4',
+    description='¡Bienvenido a Rubik Vault, tu lugar para explorar y coleccionar imágenes de cubos Rubik!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  #Obligatorio especificar el tipo de archivo que es el README.md
+    author='Michael Esquivel',
+    author_email='michaelesquivel100@gmail.com',
+    url='https://github.com/Michael-Esquivel/Rubik-Vault',
+    license_files=['LICENSE'],
+
+    packages=['codex','codex.res'],
+    scripts=[],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Utilities',
+    ],
+)
