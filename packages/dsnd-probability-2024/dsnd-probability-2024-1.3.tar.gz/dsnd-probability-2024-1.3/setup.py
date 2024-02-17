4477fdb853773575d74e2a1a6f5eea29676924b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:06 +0000

    Add package dsnd-probability-2024/1.3/dsnd-probability-2024-1.3.tar.gz

---
+++ packages/dsnd-probability-2024/dsnd-probability-2024-1.3.tar.gz/dsnd-probability-2024-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd-probability-2024',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd-probability-2024'],
+      author = 'Marwan Alsharabbi',
+      author_email = 'marwanalsharabbi@gmail.com',
+   zip_safe=False)
