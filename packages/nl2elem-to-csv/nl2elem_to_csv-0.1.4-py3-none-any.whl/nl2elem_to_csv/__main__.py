ca74d79bf5216bb96d4f67bb948e1d59e5b1483b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:45 +0000

    Add package nl2elem-to-csv/0.1.4/nl2elem_to_csv-0.1.4-py3-none-any.whl

---
+++ packages/nl2elem-to-csv/nl2elem_to_csv-0.1.4-py3-none-any.whl/nl2elem_to_csv/__main__.py
@@ -0,0 +1,25 @@
+import argparse
+
+from nl2elem_to_csv import nl2elem_reader
+from nl2elem_to_csv import csv_writer
+from nl2elem_to_csv import data_processor
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', '-i', type=str, required=False, default='track.nl2elem')
+    parser.add_argument('--output', '-o', type=str, required=False, default='track.csv')
+    args = parser.parse_args()
+
+    data = None
+    with open(args.input, 'rb') as file:
+        data = nl2elem_reader.read(file)
+
+    processed_data = data_processor.process_data(data)
+    print(processed_data)
+
+    csv_writer.write_to_csv(processed_data, args.output)
+
+
+if __name__ == '__main__':
+    main()
