b484a9441c4b97323b616561b1dcd9dc12b9f775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:12 +0000

    Add package gmt-gerby/0.1.2/gmt-gerby-0.1.2.tar.gz

---
+++ packages/gmt-gerby/gmt-gerby-0.1.2.tar.gz/gmt-gerby-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gmt-gerby",
+    description="flask-based web application for managing a gerby-plastex site",
+    version="0.1.2",
+    author="Pieter Belmans",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "PyPDF2",
+        "Markdown",
+        "python-markdown-math",
+        "validators",
+        "peewee",
+        "flask_profiler",
+        # "mdx_bleach ",
+        # "pybtex",
+        "feedparser",
+        "networkx"])
