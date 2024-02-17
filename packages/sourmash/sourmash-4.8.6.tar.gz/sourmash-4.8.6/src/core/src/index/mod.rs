63768362db20212426c78a0599cd50c4e56b34a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:36:29 +0000

    Add package sourmash/4.8.6/sourmash-4.8.6.tar.gz

---
+++ packages/sourmash/sourmash-4.8.6.tar.gz/sourmash-4.8.6/src/core/src/index/mod.rs
@@ -0,0 +1,147 @@
+//! # Indexing structures for fast similarity search
+//!
+//! An index organizes signatures to allow for fast similarity search.
+//! Some indices also support containment searches.
+
+pub mod linear;
+
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(feature = "branchwater")]
+pub mod revindex;
+
+pub mod search;
+
+use std::path::Path;
+
+use getset::{CopyGetters, Getters, Setters};
+
+use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
+
+use crate::encodings::Idx;
+use crate::index::search::{search_minhashes, search_minhashes_containment};
+use crate::prelude::*;
+use crate::Result;
+
+#[derive(TypedBuilder, CopyGetters, Getters, Setters, Serialize, Deserialize, Debug, PartialEq)]
+pub struct GatherResult {
+    #[getset(get_copy = "pub")]
+    intersect_bp: usize,
+
+    #[getset(get_copy = "pub")]
+    f_orig_query: f64,
+
+    #[getset(get_copy = "pub")]
+    f_match: f64,
+
+    f_unique_to_query: f64,
+    f_unique_weighted: f64,
+    average_abund: usize,
+    median_abund: usize,
+    std_abund: usize,
+
+    #[getset(get = "pub")]
+    filename: String,
+
+    #[getset(get = "pub")]
+    name: String,
+
+    #[getset(get = "pub")]
+    md5: String,
+
+    #[serde(skip)]
+    match_: Signature,
+
+    f_match_orig: f64,
+    unique_intersect_bp: usize,
+    gather_result_rank: usize,
+    remaining_bp: usize,
+}
+
+impl GatherResult {
+    pub fn get_match(&self) -> Signature {
+        self.match_.clone()
+    }
+}
+
+type SigCounter = counter::Counter<Idx>;
+
+pub trait Index<'a> {
+    type Item: Comparable<Self::Item>;
+    //type SignatureIterator: Iterator<Item = Self::Item>;
+
+    fn find<F>(&self, search_fn: F, sig: &Self::Item, threshold: f64) -> Result<Vec<&Self::Item>>
+    where
+        F: Fn(&dyn Comparable<Self::Item>, &Self::Item, f64) -> bool,
+    {
+        Ok(self
+            .signature_refs()
+            .into_iter()
+            .flat_map(|node| {
+                if search_fn(&node, sig, threshold) {
+                    Some(node)
+                } else {
+                    None
+                }
+            })
+            .collect())
+    }
+
+    fn search(
+        &self,
+        sig: &Self::Item,
+        threshold: f64,
+        containment: bool,
+    ) -> Result<Vec<&Self::Item>> {
+        if containment {
+            self.find(search_minhashes_containment, sig, threshold)
+        } else {
+            self.find(search_minhashes, sig, threshold)
+        }
+    }
+
+    //fn gather(&self, sig: &Self::Item, threshold: f64) -> Result<Vec<&Self::Item>>;
+
+    fn insert(&mut self, node: Self::Item) -> Result<()>;
+
+    fn batch_insert(&mut self, nodes: Vec<Self::Item>) -> Result<()> {
+        for node in nodes {
+            self.insert(node)?;
+        }
+
+        Ok(())
+    }
+
+    fn save<P: AsRef<Path>>(&self, path: P) -> Result<()>;
+
+    fn load<P: AsRef<Path>>(path: P) -> Result<()>;
+
+    fn signatures(&self) -> Vec<Self::Item>;
+
+    fn signature_refs(&self) -> Vec<&Self::Item>;
+
+    fn len(&self) -> usize {
+        self.signature_refs().len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /*
+    fn iter_signatures(&self) -> Self::SignatureIterator;
+    */
+}
+
+impl<'a, N, L> Comparable<L> for &'a N
+where
+    N: Comparable<L>,
+{
+    fn similarity(&self, other: &L) -> f64 {
+        (*self).similarity(other)
+    }
+
+    fn containment(&self, other: &L) -> f64 {
+        (*self).containment(other)
+    }
+}
---
+++ packages/sourmash/sourmash-4.8.6.tar.gz/sourmash-4.8.6/src/core/src/manifest.rs
@@ -0,0 +1,390 @@
+use std::convert::TryInto;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::ops::Deref;
+
+use camino::Utf8PathBuf as PathBuf;
+use getset::{CopyGetters, Getters, Setters};
+#[cfg(feature = "parallel")]
+use rayon::prelude::*;
+use serde::de;
+use serde::{Deserialize, Serialize};
+
+use crate::encodings::HashFunctions;
+use crate::prelude::*;
+use crate::signature::{Signature, SigsTrait};
+use crate::sketch::Sketch;
+use crate::Result;
+
+#[derive(Debug, Serialize, Deserialize, Clone, CopyGetters, Getters, Setters, PartialEq)]
+pub struct Record {
+    #[getset(get = "pub", set = "pub")]
+    internal_location: PathBuf,
+
+    #[getset(get = "pub", set = "pub")]
+    md5: String,
+
+    md5short: String,
+
+    #[getset(get = "pub", set = "pub")]
+    ksize: u32,
+
+    moltype: String,
+
+    num: u32,
+    scaled: u64,
+    n_hashes: usize,
+
+    #[getset(get = "pub", set = "pub")]
+    #[serde(deserialize_with = "to_bool")]
+    with_abundance: bool,
+
+    #[getset(get = "pub", set = "pub")]
+    name: String,
+
+    filename: String,
+}
+
+fn to_bool<'de, D>(deserializer: D) -> std::result::Result<bool, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    match String::deserialize(deserializer)?
+        .to_ascii_lowercase()
+        .as_ref()
+    {
+        "0" | "false" => Ok(false),
+        "1" | "true" => Ok(true),
+        other => Err(de::Error::invalid_value(
+            de::Unexpected::Str(other),
+            &"0/1 or true/false are the only supported values",
+        )),
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone)]
+pub struct Manifest {
+    records: Vec<Record>,
+}
+
+impl Record {
+    pub fn from_sig(sig: &Signature, path: &str) -> Vec<Self> {
+        sig.iter()
+            .map(|sketch| {
+                let (ksize, md5, with_abundance, moltype, n_hashes, num, scaled) = match sketch {
+                    Sketch::MinHash(mh) => (
+                        mh.ksize() as u32,
+                        mh.md5sum(),
+                        mh.track_abundance(),
+                        mh.hash_function(),
+                        mh.size(),
+                        mh.num(),
+                        mh.scaled(),
+                    ),
+                    Sketch::LargeMinHash(mh) => (
+                        mh.ksize() as u32,
+                        mh.md5sum(),
+                        mh.track_abundance(),
+                        mh.hash_function(),
+                        mh.size(),
+                        mh.num(),
+                        mh.scaled(),
+                    ),
+                    _ => unimplemented!(),
+                };
+
+                let md5short = md5[0..8].into();
+
+                Self {
+                    internal_location: path.into(),
+                    moltype: moltype.to_string(),
+                    name: sig.name(),
+                    ksize,
+                    md5,
+                    md5short,
+                    with_abundance,
+                    filename: sig.filename(),
+                    n_hashes,
+                    num,
+                    scaled,
+                }
+            })
+            .collect()
+    }
+
+    pub fn moltype(&self) -> HashFunctions {
+        self.moltype.as_str().try_into().unwrap()
+    }
+
+    pub fn check_compatible(&self, other: &Record) -> Result<()> {
+        /*
+        if self.num != other.num {
+            return Err(Error::MismatchNum {
+                n1: self.num,
+                n2: other.num,
+            }
+            .into());
+        }
+        */
+        use crate::Error;
+
+        if self.ksize() != other.ksize() {
+            return Err(Error::MismatchKSizes);
+        }
+        if self.moltype() != other.moltype() {
+            // TODO: fix this error
+            return Err(Error::MismatchDNAProt);
+        }
+        /*
+        if self.scaled() < other.scaled() {
+            return Err(Error::MismatchScaled);
+        }
+        if self.seed() != other.seed() {
+            return Err(Error::MismatchSeed);
+        }
+        */
+        Ok(())
+    }
+}
+
+impl Manifest {
+    pub fn from_reader<R: Read>(rdr: R) -> Result<Self> {
+        let mut records = vec![];
+
+        let mut rdr = csv::ReaderBuilder::new()
+            .comment(Some(b'#'))
+            .from_reader(rdr);
+        for result in rdr.deserialize() {
+            let record: Record = result?;
+            records.push(record);
+        }
+        Ok(Manifest { records })
+    }
+
+    pub fn to_writer<W: Write>(&self, mut wtr: W) -> Result<()> {
+        wtr.write_all(b"# SOURMASH-MANIFEST-VERSION: 1.0\n")?;
+
+        let mut wtr = csv::Writer::from_writer(wtr);
+
+        for record in &self.records {
+            wtr.serialize(record)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn internal_locations(&self) -> impl Iterator<Item = &str> {
+        self.records.iter().map(|r| r.internal_location.as_str())
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &Record> {
+        self.records.iter()
+    }
+}
+
+impl Select for Manifest {
+    fn select(self, selection: &Selection) -> Result<Self> {
+        let rows = self.records.iter().filter(|row| {
+            let mut valid = true;
+            valid = if let Some(ksize) = selection.ksize() {
+                row.ksize == ksize
+            } else {
+                valid
+            };
+            valid = if let Some(abund) = selection.abund() {
+                valid && *row.with_abundance() == abund
+            } else {
+                valid
+            };
+            valid = if let Some(moltype) = selection.moltype() {
+                valid && row.moltype() == moltype
+            } else {
+                valid
+            };
+            valid = if let Some(scaled) = selection.scaled() {
+                // num sigs have row.scaled = 0, don't include them
+                valid && row.scaled != 0 && row.scaled <= scaled as u64
+            } else {
+                valid
+            };
+            valid = if let Some(num) = selection.num() {
+                valid && row.num == num
+            } else {
+                valid
+            };
+            valid
+        });
+
+        Ok(Manifest {
+            records: rows.cloned().collect(),
+        })
+
+        /*
+        matching_rows = self.rows
+        if ksize:
+            matching_rows = ( row for row in matching_rows
+                              if row['ksize'] == ksize )
+        if moltype:
+            matching_rows = ( row for row in matching_rows
+                              if row['moltype'] == moltype )
+        if scaled or containment:
+            if containment and not scaled:
+                raise ValueError("'containment' requires 'scaled' in Index.select'")
+
+            matching_rows = ( row for row in matching_rows
+                              if row['scaled'] and not row['num'] )
+        if num:
+            matching_rows = ( row for row in matching_rows
+                              if row['num'] and not row['scaled'] )
+
+        if abund:
+            # only need to concern ourselves if abundance is _required_
+            matching_rows = ( row for row in matching_rows
+                              if row['with_abundance'] )
+
+        if picklist:
+            matching_rows = ( row for row in matching_rows
+                              if picklist.matches_manifest_row(row) )
+
+        # return only the internal filenames!
+        for row in matching_rows:
+            yield row
+        */
+    }
+}
+
+impl From<Vec<Record>> for Manifest {
+    fn from(records: Vec<Record>) -> Self {
+        Manifest { records }
+    }
+}
+
+impl From<&[PathBuf]> for Manifest {
+    fn from(paths: &[PathBuf]) -> Self {
+        #[cfg(feature = "parallel")]
+        let iter = paths.par_iter();
+
+        #[cfg(not(feature = "parallel"))]
+        let iter = paths.iter();
+
+        let records: Vec<Record> = iter
+            .flat_map(|p| {
+                let recs: Vec<Record> = Signature::from_path(p)
+                    .unwrap_or_else(|_| panic!("Error processing {:?}", p))
+                    .into_iter()
+                    .flat_map(|v| Record::from_sig(&v, p.as_str()))
+                    .collect();
+                recs
+            })
+            .collect();
+
+        Manifest { records }
+    }
+}
+
+impl From<&PathBuf> for Manifest {
+    fn from(pathlist: &PathBuf) -> Self {
+        let file = File::open(pathlist).unwrap_or_else(|_| panic!("Failed to open {:?}", pathlist));
+        let reader = BufReader::new(file);
+
+        let paths: Vec<PathBuf> = reader
+            .lines()
+            .map(|line| line.unwrap_or_else(|_| panic!("Failed to read line from {:?}", pathlist)))
+            .map(PathBuf::from)
+            .collect();
+
+        paths.as_slice().into()
+    }
+}
+
+impl Deref for Manifest {
+    type Target = Vec<Record>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.records
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use camino::Utf8PathBuf as PathBuf;
+    use std::fs::File;
+    use std::io::Write;
+    use tempfile::TempDir;
+
+    use super::Manifest;
+
+    #[test]
+    fn manifest_from_pathlist() {
+        let temp_dir = TempDir::new().unwrap();
+        let utf8_output = PathBuf::from_path_buf(temp_dir.path().to_path_buf())
+            .expect("Path should be valid UTF-8");
+        let mut filename = utf8_output.join("sig-pathlist.txt");
+        //convert to camino utf8pathbuf
+        filename = PathBuf::from(filename);
+        // build sig filenames
+        let base_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let test_sigs = vec![
+            "../../tests/test-data/47.fa.sig",
+            "../../tests/test-data/63.fa.sig",
+        ];
+
+        let full_paths: Vec<_> = test_sigs
+            .into_iter()
+            .map(|sig| base_path.join(sig))
+            .collect();
+
+        // write a file in test directory with a filename on each line
+        let mut pathfile = File::create(&filename).unwrap();
+        for sigfile in &full_paths {
+            writeln!(pathfile, "{}", sigfile).unwrap();
+        }
+
+        // load into manifest
+        let manifest = Manifest::from(&filename);
+        assert_eq!(manifest.len(), 2);
+    }
+
+    #[test]
+    #[should_panic(expected = "Failed to open \"no-exist\"")]
+    fn manifest_from_pathlist_nonexistent_file() {
+        let filename = PathBuf::from("no-exist");
+        let _manifest = Manifest::from(&filename);
+    }
+
+    #[test]
+    #[should_panic]
+    fn manifest_from_pathlist_badfile() {
+        let temp_dir = TempDir::new().unwrap();
+        let utf8_output = PathBuf::from_path_buf(temp_dir.path().to_path_buf())
+            .expect("Path should be valid UTF-8");
+        let mut filename = utf8_output.join("sig-pathlist.txt");
+        //convert to camino utf8pathbuf
+        filename = PathBuf::from(filename);
+
+        let mut pathfile = File::create(&filename).unwrap();
+        write!(pathfile, "Valid line\n").unwrap();
+        pathfile.write_all(&[0xED, 0xA0, 0x80]).unwrap(); // invalid UTF-8
+
+        // load into manifest
+        let _manifest = Manifest::from(&filename);
+    }
+
+    #[test]
+    #[should_panic]
+    fn manifest_from_paths_badpath() {
+        let base_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let test_sigs = vec![
+            PathBuf::from("no-exist"),
+            PathBuf::from("../../tests/test-data/63.fa.sig"),
+        ];
+
+        let full_paths: Vec<PathBuf> = test_sigs
+            .into_iter()
+            .map(|sig| base_path.join(sig))
+            .collect();
+
+        // load into manifest
+        let _manifest = Manifest::from(&full_paths[..]); // pass full_paths as a slice
+    }
+}
