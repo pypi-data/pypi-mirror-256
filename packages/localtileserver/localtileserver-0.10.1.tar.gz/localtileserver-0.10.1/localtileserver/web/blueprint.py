48f809adfc4e1c1f03592db8492b427c9fbac531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:52 +0000

    Add package localtileserver/0.10.1/localtileserver-0.10.1.tar.gz

---
+++ packages/localtileserver/localtileserver-0.10.1.tar.gz/localtileserver-0.10.1/localtileserver/web/blueprint.py
@@ -0,0 +1,28 @@
+from flask import Blueprint
+from flask_caching import Cache
+
+tileserver = Blueprint(
+    "tileserver",
+    __name__,
+    static_folder="static",
+    static_url_path="/static/tileserver",
+    template_folder="templates",
+)
+
+
+def create_cache(url: str = None, username: str = None, password: str = None):
+    if url:
+        config = {"CACHE_MEMCACHED_SERVERS": url.split(",")}
+        if username and password:
+            config["CACHE_TYPE"] = "SASLMemcachedCache"
+            config["CACHE_MEMCACHED_USERNAME"] = username
+            config["CACHE_MEMCACHED_PASSWORD"] = password
+        else:
+            config["CACHE_TYPE"] = "MemcachedCache"
+
+    else:
+        config = {"CACHE_TYPE": "SimpleCache"}
+    return Cache(config=config)
+
+
+cache = create_cache()
