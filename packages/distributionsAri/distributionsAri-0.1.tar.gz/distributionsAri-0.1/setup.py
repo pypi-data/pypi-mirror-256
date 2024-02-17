a1586947d8e59c55f3439f0ec34316ab0f8656ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:59 +0000

    Add package distributionsAri/0.1/distributionsAri-0.1.tar.gz

---
+++ packages/distributionsAri/distributionsAri-0.1.tar.gz/distributionsAri-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsAri',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsAri'],
+      zip_safe=False)
