4ca3c3f254b378eb7aa3634417ab8be5d263c95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:13 +0000

    Add package discbase/0.3.0/discbase-0.3.0-py3-none-any.whl

---
+++ packages/discbase/discbase-0.3.0-py3-none-any.whl/test_e2e/test_client_state.py
@@ -0,0 +1,59 @@
+import asyncio
+import unittest
+
+import discord
+
+from discbase.Client import Client
+from test_e2e.util import TEST_BOT_TOKEN, TEST_CHANNEL_ID, get_client
+
+
+class TestClientState(unittest.TestCase):
+    def test_alive(self):
+        client = get_client()
+
+        async def main():
+            await client.start()
+            self.assertTrue(client.alive())
+            await client.stop()
+
+        asyncio.run(main())
+        self.assertFalse(client.alive())
+
+    def test_client_as_context_manager(self):
+        client = get_client()
+
+        async def main():
+            async with client as c:
+                self.assertTrue(c.alive())
+
+        asyncio.run(main())
+        self.assertFalse(client.alive())
+
+    def test_create_client_with_invalid_client_token(self):
+        # TODO: this passes but logs "Unclosed connector". Not sure why.
+        async def main():
+            client = Client(
+                discord_client_token="im bad",
+                discord_channel_id=TEST_CHANNEL_ID,
+                connection_timeout_seconds=3,
+            )
+            with self.assertRaises(discord.errors.LoginFailure):
+                await client.start()
+            self.assertFalse(client.alive())
+
+        asyncio.run(main())
+
+    def test_dump_with_invalid_channel_id(self):
+        async def main():
+            client = Client(
+                discord_client_token=TEST_BOT_TOKEN,
+                discord_channel_id=12345,
+                connection_timeout_seconds=3,
+            )
+
+            with self.assertRaises(Exception) as context:
+                await client.start()
+            self.assertFalse(client.alive())
+            self.assertEqual("COULD NOT RETRIEVE CHANNEL WITH ID 12345", str(context.exception))
+
+        asyncio.run(main())
---
+++ packages/discbase/discbase-0.3.0-py3-none-any.whl/test_e2e/test_retrieve.py
@@ -0,0 +1,65 @@
+import asyncio
+import os
+import unittest
+
+from test_e2e.util import get_client, run_async_test
+
+
+class TestRetrieve(unittest.TestCase):
+    RESOURCES_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "resources"))
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.client = get_client()
+        cls.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(cls.loop)
+        cls.loop.run_until_complete(cls.client.start())
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.loop.run_until_complete(cls.client.stop())
+        cls.loop.close()
+
+    def test_retrieve_text(self):
+        async def main():
+            stored_record = await self.client.retrieve(record_id=1204650562943979580)
+            self.assertEqual("retrieve me", stored_record.text_data)
+            self.assertIsNotNone(stored_record.record_id)
+            self.assertEqual([], stored_record.media_urls)
+            self.assertIsNotNone(stored_record.created_at_utc)
+            self.assertIsNotNone(stored_record.discord_message)
+
+        run_async_test(self, main())
+
+    def test_retrieve_image(self):
+        async def main():
+            stored_record = await self.client.retrieve(record_id=1204651320561238086)
+            self.assertEqual("", stored_record.text_data)
+            self.assertIsNotNone(stored_record.record_id)
+            self.assertEqual(1, len(stored_record.media_urls))
+            self.assertIsNotNone(stored_record.created_at_utc)
+            self.assertIsNotNone(stored_record.discord_message)
+
+        run_async_test(self, main())
+
+    def test_retrieve_multiple_images(self):
+        async def main():
+            stored_record = await self.client.retrieve(record_id=1205749135924404224)
+            self.assertEqual("", stored_record.text_data)
+            self.assertIsNotNone(stored_record.record_id)
+            self.assertEqual(3, len(stored_record.media_urls))
+            self.assertIsNotNone(stored_record.created_at_utc)
+            self.assertIsNotNone(stored_record.discord_message)
+
+        run_async_test(self, main())
+
+    def test_retrieve_text_and_image(self):
+        async def main():
+            stored_record = await self.client.retrieve(record_id=1204652081420443668)
+            self.assertEqual("image text", stored_record.text_data)
+            self.assertIsNotNone(stored_record.record_id)
+            self.assertEqual(1, len(stored_record.media_urls))
+            self.assertIsNotNone(stored_record.created_at_utc)
+            self.assertIsNotNone(stored_record.discord_message)
+
+        run_async_test(self, main())
