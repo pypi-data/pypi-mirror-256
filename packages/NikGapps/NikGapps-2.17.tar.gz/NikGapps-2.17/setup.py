33f976db07289773a83de5fa1333038a8984ab53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:33 +0000

    Add package NikGapps/2.17/NikGapps-2.17.tar.gz

---
+++ packages/NikGapps/NikGapps-2.17.tar.gz/NikGapps-2.17/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NikGapps",
+version="2.17",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['*.sh', '*.yml'],
+        'NikGapps.helper': ['assets/*'],
+        'NikGapps.helper.assets': ['*'],
+    },
+    author="Nikhil Menghani",
+    author_email="nikgapps@gmail.com",
+    description="A short description of your project",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/nikgapps/project",
+    install_requires=[
+        'colorama>=0.4.4',
+        'pytz>=2022.1',
+        'pysftp>=0.2.9',
+        'PyYAML>=6.0',
+        'psutil>=5.9.0',
+        'setuptools>=65.5.1',
+        'requests>=2.25.1',
+        'GitPython>=3.1.31',
+        'pexpect>=4.8.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'nikgapps=NikGapps.main:main',
+            'nikgapps_overlay=NikGapps.overlay_control:overlay_control',
+            'nikgapps_config_upload=NikGapps.config_upload:config_upload',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.10',
+)
