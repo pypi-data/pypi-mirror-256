4efd69feff03bd3ed4559e7bb9c1c16e86d025f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:54 +0000

    Add package LLMagic/0.0.1/LLMagic-0.0.1.tar.gz

---
+++ packages/LLMagic/LLMagic-0.0.1.tar.gz/LLMagic-0.0.1/src/llmagic.py
@@ -0,0 +1,5 @@
+def hello_llm():
+  """
+  A simple function that returns a greeting related to Large Language Models.
+  """
+  return "Hello from LLMagic! Let's explore building amazing applications with Large Language Models!"
