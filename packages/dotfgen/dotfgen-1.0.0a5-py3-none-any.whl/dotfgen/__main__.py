952548dc77161d42cd292ed34d416ae4ea9da93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:38 +0000

    Add package dotfgen/1.0.0a5/dotfgen-1.0.0a5-py3-none-any.whl

---
+++ packages/dotfgen/dotfgen-1.0.0a5-py3-none-any.whl/dotfgen/__main__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2019 Rémy Taymans <remytms@tsmail.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Executed when module is run as a script."""
+
+from dotfgen import cli
+
+cli.main()  # pylint: disable=no-value-for-parameter
