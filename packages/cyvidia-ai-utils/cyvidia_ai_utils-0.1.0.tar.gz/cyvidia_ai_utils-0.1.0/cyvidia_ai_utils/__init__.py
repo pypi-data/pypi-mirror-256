faddf17d98750344e582cad1a78db2c23e5e961b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:46 +0000

    Add package cyvidia-ai-utils/0.1.0/cyvidia_ai_utils-0.1.0.tar.gz

---
+++ packages/cyvidia-ai-utils/cyvidia_ai_utils-0.1.0.tar.gz/cyvidia_ai_utils-0.1.0/cyvidia_ai_utils/__init__.py
@@ -0,0 +1,3 @@
+from ai_utils.src.cross_validation.cross_validation import CrossValidationModel, EvaluationResult, cross_validate
+from ai_utils.src.cross_validation.cross_validation_model.cross_validation_model import EvalAverageType, CrossValidationModel
+from ai_utils.src.cross_validation.cross_validation_model.transformer_cross_validation_model import BertCrossValidationModel
---
+++ packages/cyvidia-ai-utils/cyvidia_ai_utils-0.1.0.tar.gz/cyvidia_ai_utils-0.1.0/cyvidia_ai_utils/src/cross_validation/cross_validation.py
@@ -0,0 +1,25 @@
+from typing import  Dict
+from datasets import Dataset,concatenate_datasets
+from ai_utils.src.cross_validation.cross_validation_model.cross_validation_model import CrossValidationModel, EvalAverageType, EvaluationResult
+   
+def cross_validate(
+    model: CrossValidationModel,
+    folds: list[Dataset],
+    target_id_column:str,
+    input_text_column:str,
+    average: EvalAverageType= EvalAverageType.WEIGHTED
+):
+    """Cross validate the model using the given folds and target column.
+    Returns a dictionary of validation fold index to evaluation result.
+    """
+    results: Dict[int, EvaluationResult] = {}
+    for i, val_ds in enumerate(folds):
+        train_folds = folds[:i] + folds[i+1:]
+        
+        train_ds= concatenate_datasets(train_folds)
+        trained_model= model.train(train_ds, val_ds)
+        result= trained_model.evaluate(val_ds, target_column=target_id_column, input_column=input_text_column, average=average)
+        
+        results[i] = result
+    return results
+        
