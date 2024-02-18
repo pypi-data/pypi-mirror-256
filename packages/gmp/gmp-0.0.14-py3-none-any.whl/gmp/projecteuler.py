f03af73e58d8b1c9abc683b79cf6e61f60545d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:45 +0000

    Add package gmp/0.0.14/gmp-0.0.14-py3-none-any.whl

---
+++ packages/gmp/gmp-0.0.14-py3-none-any.whl/gmp/projecteuler.py
@@ -0,0 +1,15 @@
+def solve(problem):
+  with open('projecteuler.txt', 'r') as file:
+      content = file.read()
+  
+  data = content.split("\n")
+  data_dict = {}
+  
+  for x in data:
+      split_data = x.split(". ")
+      if len(split_data) >= 2:
+          key = split_data[0]
+          value = split_data[1]
+          data_dict[key] = value
+  
+  return data_dict[str(problem)]
