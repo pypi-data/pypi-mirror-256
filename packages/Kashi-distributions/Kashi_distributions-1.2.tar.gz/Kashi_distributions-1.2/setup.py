79310ac1beaafffe7a286fa170002ccb35ddb168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:58 +0000

    Add package Kashi-distributions/1.2/Kashi_distributions-1.2.tar.gz

---
+++ packages/Kashi-distributions/Kashi_distributions-1.2.tar.gz/Kashi_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Kashi_distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['Kashi_distributions'],
+      author="Kashi Sankhla",
+      author_email="kashisankhla2105@gmail.com",
+      zip_safe=False)
