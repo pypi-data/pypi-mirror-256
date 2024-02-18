0d9c8762fa054d816af665a121bac7352e88aae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:17 +0000

    Add package sta-distributions11/0.1/sta_distributions11-0.1.tar.gz

---
+++ packages/sta-distributions11/sta_distributions11-0.1.tar.gz/sta_distributions11-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sta_distributions11',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sta_distributions11'],
+      author ='Chidume Jacob',
+      author_email ='chidumejacob@gmail.com',
+      zip_safe=False)
