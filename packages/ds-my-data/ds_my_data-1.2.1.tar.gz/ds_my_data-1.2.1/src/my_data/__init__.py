70c7cfa448500cece36311698f9e21dae5fadd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:05 +0000

    Add package ds-my-data/1.2.1/ds_my_data-1.2.1.tar.gz

---
+++ packages/ds-my-data/ds_my_data-1.2.1.tar.gz/ds_my_data-1.2.1/src/my_data/__init__.py
@@ -0,0 +1,10 @@
+"""My Data Package.
+
+This package contains the code to create, retrieve, update and delete data from
+a database containing the objects for the `my_model` package.
+```
+"""
+
+from .my_data import MyData  # noqa: F401
+
+__version__ = '1.2.1'
