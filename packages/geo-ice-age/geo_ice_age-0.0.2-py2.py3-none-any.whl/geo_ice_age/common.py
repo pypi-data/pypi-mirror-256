52fafd3488ee57643b905d2171e5d1f11667e539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:04:38 +0000

    Add package geo-ice-age/0.0.2/geo_ice_age-0.0.2-py2.py3-none-any.whl

---
+++ packages/geo-ice-age/geo_ice_age-0.0.2-py2.py3-none-any.whl/geo_ice_age/common.py
@@ -0,0 +1,12 @@
+"""The common module contains common functions and classes used by the other modules.
+"""
+
+def hello_world():
+    """Prints "Hello World!" to the console.
+    """
+    print("Hello World!")
+
+def ice_age_init():
+    """Prints welcome message to the console.
+    """
+    print("Hello Ice Age!")
