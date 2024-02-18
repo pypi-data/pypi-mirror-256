28dc3b2afefbd61735d3033b4913e1388b3c403c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:40 +0000

    Add package inox/0.4.6/inox-0.4.6.tar.gz

---
+++ packages/inox/inox-0.4.6.tar.gz/inox-0.4.6/tests/test_debug.py
@@ -0,0 +1,15 @@
+r"""Tests for the inox.debug module."""
+
+import jax
+
+from functools import partial
+from inox.debug import *
+
+
+def test_same_trace():
+    x = jax.numpy.zeros(1)
+    y = jax.numpy.ones(1)
+
+    assert same_trace(x, y)
+    assert jax.jit(same_trace)(x, y)
+    assert not jax.jit(partial(same_trace, x))(y)
