31a6906b8296535ddc3da7497812079de67b796e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:42 +0000

    Add package g2fl/0.0.19/g2fl-0.0.19-py3-none-any.whl

---
+++ packages/g2fl/g2fl-0.0.19-py3-none-any.whl/g2fl/dl.py
@@ -0,0 +1,278 @@
+import time
+from typing import Any, Callable
+
+import numpy as np
+import torch
+import torchvision
+from torch import nn
+from torch.utils import data
+from torchvision import transforms
+
+from g2fl.plotting import Animator, plot, show_images  # noqa
+
+
+def synthetic_data(w, b, num_examples):
+    """生成 y = Wx + b + 噪声。"""
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+
+def load_array(data_arrays, batch_size, is_train=True):
+    """Construct a PyTorch data iterator."""
+    dataset = torch.utils.data.TensorDataset(*data_arrays)
+    return torch.utils.data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
+class Timer:
+    """Record multiple running times."""
+
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
+
+def get_fashion_mnist_labels(labels):
+    """Return text labels for the Fashion-MNIST dataset."""
+
+    text_labels = [
+        "t-shirt",
+        "trouser",
+        "pullover",
+        "dress",
+        "coat",
+        "sandal",
+        "shirt",
+        "sneaker",
+        "bag",
+        "ankle boot",
+    ]
+    return [text_labels[int(i)] for i in labels]
+
+
+def get_dataloader_workers():
+    """Use 4 processes to read the data."""
+    return 4
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """下载Fashion-MNIST数据集，然后将其加载到内存中"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="../data", train=True, transform=trans, download=True
+    )
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="../data", train=False, transform=trans, download=True
+    )
+    return (
+        data.DataLoader(
+            mnist_train,
+            batch_size,
+            shuffle=True,
+            num_workers=get_dataloader_workers(),
+        ),
+        data.DataLoader(
+            mnist_test,
+            batch_size,
+            shuffle=False,
+            num_workers=get_dataloader_workers(),
+        ),
+    )
+
+
+def sgd(params, lr, batch_size):
+    """小批量随机梯度下降。"""
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = astype(y_hat, y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+def train_epoch_reg(net, train_iter, loss, updater):
+    """训练模型一个迭代周期"""
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l: object = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_reg(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(
+        xlabel="epoch",
+        xlim=[1, num_epochs],
+        ylim=[0.3, 0.9],
+        legend=["train loss", "train acc", "test acc"],
+    )
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_reg(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert 1 >= train_acc > 0.7, train_acc
+
+
+def corr2d(X: torch.tensor, K: torch.tensor):
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i : i + h, j : j + w] * K).sum()
+    return Y
+
+
+class Accumulator:
+    """在n个变量上累加"""
+
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def num_gpus():
+    """Get the number of available GPUs."""
+    return torch.cuda.device_count()
+
+
+def try_gpu(i=0):
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    if num_gpus() >= i + 1:
+        return gpu(i)
+    return cpu()
+
+
+def cpu():
+    """Get the CPU device."""
+
+    return torch.device("cpu")
+
+
+def gpu(i=0):
+    """Get a GPU device."""
+
+    return torch.device(f"cuda:{i}")
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    if isinstance(net, nn.Module):
+        net.eval()
+        if not device:
+            device = next(iter(net.parameters())).device
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_conv(net, train_iter, test_iter, num_epochs, lr, device):
+    def init_weights(m):
+        if type(m) is nn.Linear or type(m) is nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+
+    net.apply(init_weights)
+    print("training on ", device)
+    net.to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    animator = Animator(
+        xlabel="epoch",
+        xlim=[1, num_epochs],
+        legend=["train loss", "train acc", "test acc"],
+    )
+    timer, num_batches = Timer(), len(train_iter)
+    for epoch in range(num_epochs):
+        metric = Accumulator(3)
+        net.train()
+        for i, (X, y) in enumerate(train_iter):
+            timer.start()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            ls = loss(y_hat, y)
+            ls.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(ls * X.shape[0], accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches, (train_l, train_acc, None))
+        test_acc = evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch + 1, (None, None, test_acc))
+    print(f"loss {train_l:.3f}, train acc {train_acc:.3f}, " f"test acc {test_acc:.3f}")
+    print(
+        f"{metric[2] * num_epochs / timer.sum():.1f} examples/sec " f"on {str(device)}"
+    )
+
+
+astype: Callable[[Any, Any, Any], Any] = lambda x, *args, **kwargs: x.type(
+    *args, **kwargs
+)
