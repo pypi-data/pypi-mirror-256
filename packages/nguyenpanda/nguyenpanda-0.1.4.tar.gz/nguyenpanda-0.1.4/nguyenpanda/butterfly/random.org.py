60e09018f603ae27317f3a4c4acfe5993417311c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:15 +0000

    Add package nguyenpanda/0.1.4/nguyenpanda-0.1.4.tar.gz

---
+++ packages/nguyenpanda/nguyenpanda-0.1.4.tar.gz/nguyenpanda-0.1.4/nguyenpanda/butterfly/random.org.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+'''
+Int, IntSeq, Dec, Gauss, Str, 
+
+apiKey
+n
+
+min (Int
+max (Int
+length (Int, IntSeq, str
+decimalPlaces (DecimalFractions
+mean, standardDeviation, significantDigits (Gaussians
+characters (str
+size (Blobs
+
+replacement
+base
+pregeneratedRandomization
+format
+'''
+
+
+class RealRandom():
+    def __init__(self, api_key: str | None = None, *args, **kwargs):
+        pass
+
+
+if __name__ == '__main__':
+    a = {'a': 1,
+         'b': 2}
+
+    print(str(a))
