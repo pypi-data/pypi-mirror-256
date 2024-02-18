9d43a42208f4ff412147a3bf3a507bf4d71cc690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:41 +0000

    Add package gaussnomial314/0.1/gaussnomial314-0.1.tar.gz

---
+++ packages/gaussnomial314/gaussnomial314-0.1.tar.gz/gaussnomial314-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussnomial314',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussnomial314'],
+      zip_safe=False)
