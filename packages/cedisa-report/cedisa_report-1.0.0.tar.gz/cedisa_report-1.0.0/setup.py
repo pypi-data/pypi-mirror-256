6d4ac1a67afbd85d2bf7c8675ffc9faf4816a5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:47 +0000

    Add package cedisa-report/1.0.0/cedisa_report-1.0.0.tar.gz

---
+++ packages/cedisa-report/cedisa_report-1.0.0.tar.gz/cedisa_report-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'cedisa_report',
+    version = '1.0.0',
+    author = 'Gustavo Gomes Dias',
+    author_email = 'gustavo.dias@cedisa.com.br',
+    packages = ['src']
+)
