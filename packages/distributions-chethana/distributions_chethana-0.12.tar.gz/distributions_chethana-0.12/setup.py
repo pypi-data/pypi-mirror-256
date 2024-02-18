2fc7a204a9128214bff4f7526d2ff19145aff45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:09 +0000

    Add package distributions-chethana/0.12/distributions_chethana-0.12.tar.gz

---
+++ packages/distributions-chethana/distributions_chethana-0.12.tar.gz/distributions_chethana-0.12/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_chethana',
+      version='0.12',
+      description='Gaussian distributions',
+      packages=['distributions_chethana'],
+      zip_safe=False)
