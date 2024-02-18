d0ae8d56bdaf0e023b2c2a0bc73591c246780a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:16 +0000

    Add package barril/2.0.1/barril-2.0.1.tar.gz

---
+++ packages/barril/barril-2.0.1.tar.gz/barril-2.0.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.rst", encoding="UTF-8") as readme_file:
+    readme = readme_file.read()
+
+with open("CHANGELOG.rst", encoding="UTF-8") as changelog_file:
+    history = changelog_file.read()
+
+requirements = ["attrs>=18.1.0", "numpy>=1.11.0", "oop-ext>=1.1", "typing_extensions"]
+extras_require = {
+    "testing": [
+        "codecov",
+        "data-science-types",
+        "mypy",
+        "pre-commit",
+        "pytest",
+        "pytest-cov",
+        "pytest-mock",
+        "tox",
+    ],
+}
+
+setup(
+    author="ESSS",
+    author_email="foss@esss.co",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    description="Python package to manage units for physical quantities",
+    extras_require=extras_require,
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    python_requires=">=3.8",
+    keywords="barril",
+    name="barril",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    url="https://github.com/ESSS/barril",
+    zip_safe=False,
+)
