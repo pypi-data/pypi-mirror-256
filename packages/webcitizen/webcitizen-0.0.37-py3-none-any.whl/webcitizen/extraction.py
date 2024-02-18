16f28ef53f52eefadb1ad15a305101ab9f16d6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:46 +0000

    Add package webcitizen/0.0.37/webcitizen-0.0.37-py3-none-any.whl

---
+++ packages/webcitizen/webcitizen-0.0.37-py3-none-any.whl/webcitizen/extraction.py
@@ -0,0 +1,7 @@
+# import requests
+
+# obj = url(url="https://acquia.com", html=requests.get("https://acquia.com").text)
+# m = obj.json_nltk2()
+# print((m))
+
+# print(json_html2(requests.get("https://acquia.com").text))
