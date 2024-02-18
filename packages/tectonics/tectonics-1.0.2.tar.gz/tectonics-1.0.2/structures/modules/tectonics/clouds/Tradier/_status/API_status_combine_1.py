3721afcac437a0f50a362ca2429fc85c9718820c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:39 +0000

    Add package tectonics/1.0.2/tectonics-1.0.2.tar.gz

---
+++ packages/tectonics/tectonics-1.0.2.tar.gz/tectonics-1.0.2/structures/modules/tectonics/clouds/Tradier/_status/API_status_combine_1.py
@@ -0,0 +1,25 @@
+
+
+import tectonics.clouds.Tradier.procedures.options.combine as combine_options  
+import tectonics.climate as climate
+import tectonics.treasures.options.shapes.shape_1 as shares_shape_1 
+
+def check_1 ():
+	Tradier = climate.find ("Tradier")
+
+	options_chains = combine_options.presently ({
+		"symbol": "RUN",
+		"authorization": Tradier ["authorization"]
+	})	
+	
+	shares_shape_1.assertions (options_chains)
+	
+	
+	#print ("options_chains:", options_chains)
+
+	return;
+	
+	
+checks = {
+	'check 1': check_1
+}
