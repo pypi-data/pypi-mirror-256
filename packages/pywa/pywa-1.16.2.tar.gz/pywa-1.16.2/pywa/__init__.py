6a427388304eb4a44ff3c3bda04db6a6a5d6e4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:02 +0000

    Add package pywa/1.16.2/pywa-1.16.2.tar.gz

---
+++ packages/pywa/pywa-1.16.2.tar.gz/pywa-1.16.2/pywa/__init__.py
@@ -0,0 +1,12 @@
+"""
+Python wrapper for the WhatsApp Cloud API.
+    - GitHub: https://github.com/david-lev/pywa
+    - Docs: https://pywa.readthedocs.io/
+    - PyPI: https://pypi.org/project/pywa/
+"""
+
+from pywa.client import WhatsApp
+
+__version__ = "1.16.2"
+__author__ = "David Lev"
+__license__ = "MIT"
