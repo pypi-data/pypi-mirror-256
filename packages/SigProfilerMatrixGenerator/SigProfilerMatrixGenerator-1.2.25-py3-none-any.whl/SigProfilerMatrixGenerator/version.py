4da3e5644685a9d4563ea623ff46a73359cc705a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:05:19 +0000

    Add package SigProfilerMatrixGenerator/1.2.25/SigProfilerMatrixGenerator-1.2.25-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.25-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.25'
+version = '1.2.25'
+Update = 'v1.2.25: Add support for rn7 and fix custom install tsb_BED issue.'
+
+	
