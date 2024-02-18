407fc6929dbace8f6ebad336bb527fdd37619aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:38 +0000

    Add package distributions-esra/2.5/distributions_esra-2.5.tar.gz

---
+++ packages/distributions-esra/distributions_esra-2.5.tar.gz/distributions_esra-2.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_esra',
+      version='2.5',
+      description='Gaussian distributions',
+      packages=['distributions_esra'],
+      author = "esra ai",
+      author_email = "esra.al@gmail.com",
+      zip_safe=False)
