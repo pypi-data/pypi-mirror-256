1eb5c4ab0b3dda968e2d27c617889ce41b6c9a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:11 +0000

    Add package myrino/1.2/myrino-1.2.tar.gz

---
+++ packages/myrino/myrino-1.2.tar.gz/myrino-1.2/myrino/__init__.py
@@ -0,0 +1,6 @@
+from .client import Client
+
+__version__: int = 1.1
+__author__: str = 'amirali irvany'
+__github__: str = 'https://github.com/metect'
+__email__: str = 'dev.amirali.irvany@gmail.com'
