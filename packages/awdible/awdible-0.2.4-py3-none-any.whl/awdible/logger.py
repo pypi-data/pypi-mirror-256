3dcf168d4d13cacb9cbd5ecdee98f79ab818ed6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:09 +0000

    Add package awdible/0.2.4/awdible-0.2.4-py3-none-any.whl

---
+++ packages/awdible/awdible-0.2.4-py3-none-any.whl/awdible/logger.py
@@ -0,0 +1,20 @@
+"""
+Logger module to log the messages
+"""
+
+import logging
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.WARNING)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s  - %(filename)s - %(levelname)s - %(message)s"
+    )
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
+
+
+logger = get_logger(__name__)
