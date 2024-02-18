7374f1d38745dc4e3dac5cf13d0eff17fb358c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:45 +0000

    Add package bishajit-distributions/0.2/bishajit_distributions-0.2.tar.gz

---
+++ packages/bishajit-distributions/bishajit_distributions-0.2.tar.gz/bishajit_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bishajit_distributions',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['bishajit_distributions'],
+      author = 'Bishajit Lodh',
+      author_email = 'Bishajitklodh@gmail.com',
+    zip_safe=False)
