a59ea9f6e2e99b12f7f107a0034962d8c77fc93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:44 +0000

    Add package ez-probability/1.0/ez_probability-1.0.tar.gz

---
+++ packages/ez-probability/ez_probability-1.0.tar.gz/ez_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ez_probability',
+      version='1.0',
+      description='Binomial & Gaussian distributions from the course "AI Programming with Python" by Udacity',
+      packages=['ez_probability'],
+      authors='Andrew Paster & Blake Johnson',
+      zip_safe=False)
