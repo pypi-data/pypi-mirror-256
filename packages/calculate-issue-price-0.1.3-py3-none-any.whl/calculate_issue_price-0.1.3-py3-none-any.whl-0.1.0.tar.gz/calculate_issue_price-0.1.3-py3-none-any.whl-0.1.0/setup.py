6841d28bfbf86d655708dbf5c4be345922021c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:04 +0000

    Add package calculate-issue-price-0.1.3-py3-none-any.whl/0.1.0/calculate_issue_price-0.1.3-py3-none-any.whl-0.1.0.tar.gz

---
+++ packages/calculate-issue-price-0.1.3-py3-none-any.whl/calculate_issue_price-0.1.3-py3-none-any.whl-0.1.0.tar.gz/calculate_issue_price-0.1.3-py3-none-any.whl-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='calculate_issue_price-0.1.3-py3-none-any.whl',
+    version='0.1.0',
+    py_modules=['calculate_issue'],
+    install_requires=[
+        # List any dependencies here if needed
+    ],
+)
