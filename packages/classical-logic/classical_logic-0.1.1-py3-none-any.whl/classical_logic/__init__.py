2523fa1d85dbcffe5b07a7fc76b2763e0e216a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:34 +0000

    Add package classical-logic/0.1.1/classical_logic-0.1.1-py3-none-any.whl

---
+++ packages/classical-logic/classical_logic-0.1.1-py3-none-any.whl/classical_logic/__init__.py
@@ -0,0 +1,11 @@
+"""Python package for propositional logic."""
+from .core import (
+    And,
+    Predicate,
+    Not,
+    Proposition,
+    Or,
+    Implies,
+    Iff,
+)
+from .parsing import prop, props
