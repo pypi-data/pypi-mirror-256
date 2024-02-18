b2dbb82a31f264170d258fc61da3d74b1bb1e972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:13 +0000

    Add package edc-utils/0.3.31/edc_utils-0.3.31-py3-none-any.whl

---
+++ packages/edc-utils/edc_utils-0.3.31-py3-none-any.whl/edc_utils/context_processors_check.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.core.checks import Error
+from django.core.management import color_style
+
+style = color_style()
+
+
+def edc_context_processors_check(
+    app_configs, app_label=None, context_processor_name=None, error_code=None, **kwargs
+):
+    errors = []
+    for template_config in settings.TEMPLATES:
+        if context_processor_name not in template_config.get("OPTIONS").get(
+            "context_processors"
+        ):
+            errors.append(
+                Error(
+                    "Missing item in TEMPLATE.OPTIONS.context_processors. "
+                    f"Expected `{context_processor_name}`.",
+                    id=f"{app_label}.{error_code or '001'}",
+                )
+            )
+    return errors
