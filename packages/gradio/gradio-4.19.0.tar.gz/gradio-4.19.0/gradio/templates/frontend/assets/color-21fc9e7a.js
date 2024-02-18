b9d59bc80c24d39e6f438ba6285dc48b552facee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:03:58 +0000

    Add package gradio/4.19.0/gradio-4.19.0.tar.gz

---
+++ packages/gradio/gradio-4.19.0.tar.gz/gradio-4.19.0/gradio/templates/frontend/assets/color-21fc9e7a.js
@@ -0,0 +1,8 @@
+import { o as ordered_colors } from './Button-816a5c8f.js';
+
+const get_next_color = (index) => {
+  return ordered_colors[index % ordered_colors.length];
+};
+
+export { get_next_color as g };
+//# sourceMappingURL=color-21fc9e7a.js.map
---
+++ packages/gradio/gradio-4.19.0.tar.gz/gradio-4.19.0/gradio/templates/frontend/assets/index-34d305df.js
@@ -0,0 +1,6 @@
+export { L as Loader, S as StatusTracker, S as default } from './Index-a927ce67.js';
+export { T as Toast } from './Blocks-7026e92e.js';
+import './index-2e3ef8b2.js';
+import './svelte/svelte.js';
+import './Button-816a5c8f.js';
+//# sourceMappingURL=index-34d305df.js.map
