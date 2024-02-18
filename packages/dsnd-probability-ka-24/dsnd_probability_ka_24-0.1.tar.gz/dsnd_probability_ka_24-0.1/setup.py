8fa1d05560501342532ab541a3dd87497b2448f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:03:55 +0000

    Add package dsnd-probability-ka-24/0.1/dsnd_probability_ka_24-0.1.tar.gz

---
+++ packages/dsnd-probability-ka-24/dsnd_probability_ka_24-0.1.tar.gz/dsnd_probability_ka_24-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_ka_24',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_ka_24'],
+      author = 'Kaoutar Qanouni',
+      zip_safe=False)
