6dbe071d7c3bd2a9cb262ca3ef99658ebc58d4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:06 +0000

    Add package viewtext-curses/1.2.2/viewtext_curses-1.2.2.tar.gz

---
+++ packages/viewtext-curses/viewtext_curses-1.2.2.tar.gz/viewtext_curses-1.2.2/src/viewtext_c/globyl.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3.11
+# -*- coding: utf-8 -*-
+
+default_debug     = False
+default_delay     = 0
+default_msg_delay = 0.6
+default_tab_w     = 4
+
+class Global:
+	debug     = default_debug
+	delay     = default_delay
+	msg_delay = default_msg_delay
+	tab_w     = default_tab_w
