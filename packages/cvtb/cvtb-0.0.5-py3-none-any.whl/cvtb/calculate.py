046ef51a25f9c2b74973c07db2cd61f0035d0065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:47 +0000

    Add package cvtb/0.0.5/cvtb-0.0.5-py3-none-any.whl

---
+++ packages/cvtb/cvtb-0.0.5-py3-none-any.whl/cvtb/calculate.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+# lift image points into camera space 3d points
+def lift(k: np.ndarray, img: np.ndarray, dpt: np.ndarray, msk: np.ndarray) -> np.ndarray:
+    pass
+    
+
+if __name__ == '__main__':
+    k = np.array([[517.,   0., 320.],
+                  [  0., 517., 240.],
+                  [  0.,   0.,   1.]])
+    img = None
+
