587eb10bc2a06dc466f1ce6223ab4adfe176a7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:27 +0000

    Add package vectabass/0.1.17/vectabass-0.1.17.tar.gz

---
+++ packages/vectabass/vectabass-0.1.17.tar.gz/vectabass-0.1.17/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vectabass",
+    version="0.1.17",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "pydantic",
+        "requests",
+        "uvicorn",
+    ],
+    python_requires=">=3.7",
+)
