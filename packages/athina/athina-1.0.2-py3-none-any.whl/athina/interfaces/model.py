bbefd2d21ef3751a75041337b1008b8f8b61daca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:45 +0000

    Add package athina/1.0.2/athina-1.0.2-py3-none-any.whl

---
+++ packages/athina/athina-1.0.2-py3-none-any.whl/athina/interfaces/model.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class Model(Enum):
+    """
+    Supported models for evaluations.
+    """
+
+    GPT35_TURBO = "gpt-3.5-turbo"
+    GPT35_TURBO_1106 = "gpt-3.5-turbo-1106"
+    GPT4 = "gpt-4"
+    GPT4_1106_PREVIEW = "gpt-4-1106-preview"
+
+    @staticmethod
+    def is_supported(model_name: str) -> bool:
+        """
+        Checks if the model is supported.
+        """
+        return model_name in [model.value for model in Model]
+
+    @staticmethod
+    def supports_json_mode(model_name: str) -> bool:
+        """
+        Checks if the model supports json mode.
+        """
+        JSON_MODE_SUPPORTED_MODELS = [Model.GPT4_1106_PREVIEW, Model.GPT35_TURBO_1106]
+        return model_name in [model.value for model in JSON_MODE_SUPPORTED_MODELS]
