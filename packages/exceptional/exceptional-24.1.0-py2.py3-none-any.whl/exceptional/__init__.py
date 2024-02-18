e3aae5062c3094d671121a384509add4712628d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:43 +0000

    Add package exceptional/24.1.0/exceptional-24.1.0-py2.py3-none-any.whl

---
+++ packages/exceptional/exceptional-24.1.0-py2.py3-none-any.whl/exceptional/__init__.py
@@ -0,0 +1,12 @@
+"""
+exceptional: exception handling utilities
+"""
+
+from .exceptional import collect, raiser, suppress, wrap
+
+__all__ = [
+    "collect",
+    "raiser",
+    "suppress",
+    "wrap",
+]
