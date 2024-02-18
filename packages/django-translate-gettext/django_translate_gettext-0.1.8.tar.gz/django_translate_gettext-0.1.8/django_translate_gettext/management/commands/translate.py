c944ed78f3cd70b8c141fd1b76d75e01aa48e3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:26 +0000

    Add package django-translate-gettext/0.1.8/django_translate_gettext-0.1.8.tar.gz

---
+++ packages/django-translate-gettext/django_translate_gettext-0.1.8.tar.gz/django_translate_gettext-0.1.8/django_translate_gettext/management/commands/translate.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+from django.db.models import Model
+
+from django_translate_gettext.services import get_all_app_models, update_py_file
+
+
+class Command(BaseCommand):
+    help = "Add calling gettext for the model files"
+
+    def add_arguments(self, parser) -> None:
+        parser.add_argument(
+            "apps", nargs="+", type=str, help="Apps to add gettext for the model files.\nFor example: app1 app2 app3"
+        )
+        parser.add_argument(
+            "--format",
+            action="store_true",
+            help="Call Ruff formatting tool to format the code after generating new model files.",
+        )
+
+    def handle(self, **options) -> None:
+        for app_name in options["apps"]:
+            try:
+                app_models = get_all_app_models(app_label=app_name)
+            except LookupError as error:
+                self.stdout.write(self.style.ERROR(error))
+                continue
+
+            self.process_app_models(app_models=app_models, **options)
+
+        self.stdout.write(
+            self.style.WARNING(
+                "Please, check the files, and don't forget to call migrate command to apply the changes "
+                "to the database after adding the gettext."
+            )
+        )
+        self.stdout.write(
+            self.style.SUCCESS(
+                "Please, un the command 'python manage.py makemessages -l {lang code}' to create the .po files."
+            )
+        )
+
+    def process_app_models(self, *, app_models: set[Model], **options) -> None:
+        for model in app_models:
+            module: str = model._meta.concrete_model.__module__
+            module_path = module.replace(".", "/")
+
+            update_py_file(file_path=module_path, formatted=options["format"])
+
+            self.stdout.write(self.style.SUCCESS("Successfully added gettext for model files."))
