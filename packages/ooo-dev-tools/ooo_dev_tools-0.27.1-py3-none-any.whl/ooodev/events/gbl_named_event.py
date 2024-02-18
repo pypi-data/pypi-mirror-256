c361b59df8f27a5e69bc18eb8c4d341ac428243d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:16 +0000

    Add package ooo-dev-tools/0.27.1/ooo_dev_tools-0.27.1-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.27.1-py3-none-any.whl/ooodev/events/gbl_named_event.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+"""
+Global Named Events.
+"""
+from __future__ import annotations
+from typing import NamedTuple
+
+
+class GblNamedEvent(NamedTuple):
+    """
+    Global Named Events
+    """
+
+    PRINTING = "global_printing"
+    """Global Printing Event."""
+
+    INPUT_BOX_CREATING = "global_input_box_creating"
+    """Global Input Box Creating Event."""
+
+    MSG_BOX_CREATING = "global_msg_box_creating"
+    """Global Message Box Creating Event."""
+
+    EVENT_CANCELED = "global_event_canceled"
+    """Global Event Canceled Event."""
+
+    RANGE_OBJ_BEFORE_FROM_RANGE = "global_range_obj_before_from_range"
