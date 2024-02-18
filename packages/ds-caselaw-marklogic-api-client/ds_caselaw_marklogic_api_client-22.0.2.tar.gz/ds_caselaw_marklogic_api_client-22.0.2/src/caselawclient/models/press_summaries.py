0ed56d9f8f8984a0dc0b797d8ce4ac74c961a271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:05:32 +0000

    Add package ds-caselaw-marklogic-api-client/22.0.2/ds_caselaw_marklogic_api_client-22.0.2.tar.gz

---
+++ packages/ds-caselaw-marklogic-api-client/ds_caselaw_marklogic_api_client-22.0.2.tar.gz/ds_caselaw_marklogic_api_client-22.0.2/src/caselawclient/models/press_summaries.py
@@ -0,0 +1,33 @@
+from functools import cached_property
+from typing import Any
+
+from caselawclient.models.neutral_citation_mixin import NeutralCitationMixin
+from caselawclient.xml_helpers import get_xpath_match_string
+
+from .documents import Document
+
+
+class PressSummary(NeutralCitationMixin, Document):
+    """
+    Represents a press summary document.
+    """
+
+    document_noun = "press summary"
+    document_noun_plural = "press summaries"
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super(PressSummary, self).__init__(self.document_noun, *args, **kwargs)
+
+    @cached_property
+    def neutral_citation(self) -> str:
+        return get_xpath_match_string(
+            self.xml.xml_as_tree,
+            "/akn:akomaNtoso/akn:doc/akn:preface/akn:p/akn:neutralCitation/text()",
+            {
+                "akn": "http://docs.oasis-open.org/legaldocml/ns/akn/3.0",
+            },
+        )
+
+    @property
+    def best_human_identifier(self) -> str:
+        return self.neutral_citation
