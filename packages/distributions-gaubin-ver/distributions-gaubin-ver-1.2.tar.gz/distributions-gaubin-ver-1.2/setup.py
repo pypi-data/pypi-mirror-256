970410ae7f08e1b36c39ea975fc107ac050c088a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:36 +0000

    Add package distributions-gaubin-ver/1.2/distributions-gaubin-ver-1.2.tar.gz

---
+++ packages/distributions-gaubin-ver/distributions-gaubin-ver-1.2.tar.gz/distributions-gaubin-ver-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-gaubin-ver',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-gaubin-ver'],
+      author = 'Jose Vargas',
+      author_email = 'jose.rja@hotmail.com',
+      zip_safe=False)
