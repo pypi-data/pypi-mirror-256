740cb2a821b05a0bc6a1c84a07b2f3236640b24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:04:34 +0000

    Add package krakectl/0.0.0/krakectl-0.0.0.tar.gz

---
+++ packages/krakectl/krakectl-0.0.0.tar.gz/krakectl-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "PyYAML==5.*",
+    "python-dateutil==2.*",
+    "requests==2.*",
+    "texttable==1.*",
+]
+
+setup(
+    name="krakectl",
+    version="0.0.0",
+    description="",
+    url="https://gitlab.com/rak-n-rok/krake",
+    maintainer="Krake Development Team",
+    maintainer_email="krake@cloudandheat.com",
+    python_requires=">=3.8",
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={"console_scripts": ["rok=rok.__main__:main"]},
+)
