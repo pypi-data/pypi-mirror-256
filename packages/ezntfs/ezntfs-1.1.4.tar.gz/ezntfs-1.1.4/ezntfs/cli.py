01bdf9bad1b6ef28bff3df21c9e1c916a215d7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:30 +0000

    Add package ezntfs/1.1.4/ezntfs-1.1.4.tar.gz

---
+++ packages/ezntfs/ezntfs-1.1.4.tar.gz/ezntfs-1.1.4/ezntfs/cli.py
@@ -0,0 +1,103 @@
+import sys
+
+from . import ezntfs
+from . import __version__
+
+USAGE = f"""Usage: ezntfs <command>
+
+Commands:
+  list         List all NTFS volumes available for mounting
+  all          Mount all NTFS volumes via ntfs-3g
+  <disk id>    Mount a specific NTFS volume via ntfs-3g
+
+Version: {__version__}
+"""
+
+
+def main():
+    if len(sys.argv) < 2:
+        print(USAGE, end="")
+        sys.exit(1)
+
+    env = ezntfs.get_environment_info()
+    if env.fuse is None:
+        sys.exit("ERROR: Failed to detect macFUSE.")
+    if env.ntfs_3g is None:
+        sys.exit("ERROR: Failed to detect ntfs-3g.")
+
+    command = sys.argv[1]
+    volumes = ezntfs.get_all_ntfs_volumes()
+
+    if command == "list":
+        list_volumes(volumes)
+        sys.exit(0)
+
+    if command == "all":
+        if not env.can_mount:
+            sys.exit("ERROR: Need root privileges to mount via ntfs-3g.")
+
+        mount_all_volumes(volumes, version=env.ntfs_3g)
+        sys.exit(0)
+
+    if command in volumes:
+        if not env.can_mount:
+            sys.exit("ERROR: Need root privileges to mount via ntfs-3g.")
+
+        ok = mount_volume(volumes[command], version=env.ntfs_3g)
+        sys.exit(0 if ok else 1)
+
+    print("ezntfs: Invalid command or disk id.")
+    print()
+    print(USAGE, end="")
+    sys.exit(1)
+
+
+def list_volumes(volumes):
+    if len(volumes) == 0:
+        print("No NTFS volumes found.")
+
+    for id, volume in volumes.items():
+        name = f"{id}: {volume.name} [{volume.size}]"
+        is_read_only = volume.access is ezntfs.Access.READ_ONLY
+        details = (
+            "mounted: " + ("yes" if volume.mounted else "no")
+            + (" (read-only)" if is_read_only else "")
+        )
+
+        print(f"{name} -- {details}")
+
+
+def mount_all_volumes(volumes, version):
+    print(f"Found {len(volumes)} NTFS volume(s).")
+
+    for id, volume in volumes.items():
+        print()
+        mount_volume(volume, version)
+
+
+def mount_volume(volume, version):
+    print(f"Volume: {volume.name} [{volume.size}]")
+
+    if volume.access is ezntfs.Access.WRITABLE:
+        print(f"{volume.name} is already writable.")
+        return True
+
+    if volume.mounted:
+        print("Unmounting...")
+        ok = ezntfs.macos_unmount(volume)
+        if not ok:
+            return False
+
+    print("Mounting via ntfs-3g...")
+    ok = ezntfs.mount(volume, version=version, path=volume.mount_path)
+    if ok:
+        print(f"Successfully mounted {volume.name}.")
+        return True
+    else:
+        print(f"Failed to mount {volume.name}.")
+
+        if volume.mounted:
+            print("Remounting via macOS...")
+            ezntfs.macos_mount(volume)
+
+        return False
---
+++ packages/ezntfs/ezntfs-1.1.4.tar.gz/ezntfs-1.1.4/ezntfs/ezntfs.py
@@ -0,0 +1,178 @@
+from collections import namedtuple
+from enum import Enum
+import os
+import re
+import shutil
+import subprocess
+
+
+EnvironmentInfo = namedtuple("EnvironmentInfo", ["fuse", "ntfs_3g", "can_mount"])
+Volume = namedtuple("Volume", ["id", "node", "name", "mounted", "mount_path", "size", "access", "internal"])
+Access = Enum("Access", ["READ_ONLY", "WRITABLE", "NOT_APPLICABLE", "UNKNOWN"])
+
+NTFS_3G_PATH = os.getenv("NTFS_3G_PATH", shutil.which("ntfs-3g"))
+
+
+def get_environment_info():
+    fuse = (
+        "macfuse" if os.path.exists("/Library/Filesystems/macfuse.fs")
+        else "osxfuse" if os.path.exists("/Library/Filesystems/osxfuse.fs")
+        else None
+    )
+
+    ntfs_3g = get_ntfs_3g_version()
+
+    test_cmd = ["sudo", "--non-interactive", NTFS_3G_PATH, "--version"]
+    can_mount = (
+        fuse is not None
+        and ntfs_3g is not None
+        and subprocess.run(test_cmd, capture_output=True).returncode == 0
+    )
+
+    return EnvironmentInfo(fuse=fuse, ntfs_3g=ntfs_3g, can_mount=can_mount)
+
+
+def get_ntfs_3g_version():
+    if NTFS_3G_PATH is None:
+        return None
+
+    result = subprocess.run([NTFS_3G_PATH, "--version"], capture_output=True)
+    if result.returncode != 0:
+        return None
+
+    version = result.stderr.decode().strip()
+    m = re.match(r"ntfs-3g (\d+)\.(\d+)\.(\d+)(?:AR\.(\d+))? external FUSE (\d+)", version)
+    if m is None:
+        return None
+
+    year = int(m.group(1))
+    month = int(m.group(2))
+    day = int(m.group(3))
+    ar = int(m.group(4)) if m.group(4) is not None else 0
+
+    return (year, month, day, ar)
+
+
+def get_all_ntfs_volumes():
+    # NOTE: A "Windows_NTFS" partition type might actually be using the exFAT file system.
+    # The types listed by `diskutil list` refer to the partition type not the file system.
+    # "Windows_NTFS" is used for MBR partition tables and "Microsoft Basic Data" for GPT.
+    # To determine the actual file system used, we use `diskutil info` later on.
+    # Simpler volumes might not have a partition type set, so we always check those too.
+
+    list_out = run(["diskutil", "list"], capture_output=True)
+    lines = list_out.split("\n")
+
+    type_last_char_index = next(line for line in lines if re.match("\s*#:\s*TYPE", line)).index("E")
+
+    disk_ids = [
+        re.search(r"\S+$", line)[0]
+        for line in lines
+        if re.match(r"\s*\d+:\s*(Windows_NTFS|Microsoft Basic Data) ", line)
+        or re.match(r"\s*0:\s*", line) and line[type_last_char_index] == " "
+    ]
+
+    return { vol.id: vol for vol in map(get_ntfs_volume, disk_ids) if vol is not None }
+
+
+def get_ntfs_volume(idOrPath):
+    info_out = run(["diskutil", "info", idOrPath], capture_output=True)
+
+    info = {
+        line.split(":", 1)[0].strip(): line.split(":", 1)[1].strip()
+        for line in info_out.split("\n") if line != ""
+    }
+
+    if (
+        info["Type (Bundle)"] != "ntfs"
+        or info["File System Personality"] != "NTFS"
+        # Older versions of diskutil used the label "Read-Only Media"
+        or (info.get("Media Read-Only") or info.get("Read-Only Media")) == "Yes"
+    ):
+        return None
+
+    # Older versions of diskutil used the label "Read-Only Volume"
+    ro_value = info.get("Volume Read-Only") or info.get("Read-Only Volume")
+    # Remove trailing notes enclosed in parentheses, examples:
+    # - "Yes (read-only mount flag set)" => "Yes"
+    # - "Not applicable (not mounted)" => "Not applicable"
+    ro_value = re.sub(r"\s*\(.*\)$", "", ro_value)
+    access = (
+            Access.READ_ONLY if ro_value == "Yes"
+            else Access.WRITABLE if ro_value == "No"
+            else Access.NOT_APPLICABLE if ro_value == "Not applicable"
+            else Access.UNKNOWN
+    )
+
+    return Volume(
+        id=info["Device Identifier"],
+        node=info["Device Node"],
+        name=info["Volume Name"] if info["Volume Name"] != "" else "Untitled",
+        mounted=info["Mounted"] == "Yes",
+        mount_path=info.get("Mount Point"),
+        size=re.match(r"\d+(\.\d+)? \S+", info["Disk Size"])[0],
+        access=access,
+        internal=info["Device Location"] == "Internal"
+    )
+
+
+def mount(volume, version=None, path=None):
+    if path is None:
+        path = genrate_path(volume)
+
+    cmd = build_mount_command(
+        volume,
+        version=version,
+        user_id=os.getenv("SUDO_UID", os.getuid()),
+        group_id=os.getenv("SUDO_GID", os.getgid()),
+        path=path,
+    )
+
+    # User must run this command as root (via sudo)
+    # or ntfs-3g must have NOPASSWD set in sudoers
+    return run(["sudo", "--non-interactive"] + cmd)
+
+
+def build_mount_command(volume, *, version, user_id, group_id, path):
+    xattr_option = "user_xattr" if version is not None and version >= (2017, 3, 23, 6) else "auto_xattr"
+
+    return [
+        NTFS_3G_PATH,
+        "-o", f"volname={volume.name}",
+        "-o", "local",
+        "-o", "allow_other",
+        "-o", xattr_option,
+        "-o", f"uid={user_id}",
+        "-o", f"gid={group_id}",
+        "-o", "windows_names",
+        volume.node, path,
+    ]
+
+
+def genrate_path(volume):
+    path = f"/Volumes/{volume.name}"
+    if not os.path.exists(path):
+        return path
+
+    counter = 1
+    while os.path.exists(f"{path} {counter}"):
+        counter += 1
+
+    return f"{path} {counter}"
+
+
+def macos_mount(volume):
+    return run(["diskutil", "mount", volume.id])
+
+
+def macos_unmount(volume):
+    return run(["diskutil", "unmount", volume.id])
+
+
+def run(command, capture_output=False):
+    if capture_output:
+        result = subprocess.run(command, capture_output=True, check=True)
+        return result.stdout.decode()
+    else:
+        result = subprocess.run(command)
+        return result.returncode == 0
