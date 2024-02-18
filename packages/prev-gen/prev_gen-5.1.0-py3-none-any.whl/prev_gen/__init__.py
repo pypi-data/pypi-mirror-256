9375126c95b53b46f226d9412a46d9864301fefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:04:36 +0000

    Add package prev-gen/5.1.0/prev_gen-5.1.0-py3-none-any.whl

---
+++ packages/prev-gen/prev_gen-5.1.0-py3-none-any.whl/prev_gen/__init__.py
@@ -0,0 +1,11 @@
+from .literals import Literals
+from .color import Color
+from .settings import Settings
+from .table import Table
+from .preview import Preview
+from .reverse import Reverse
+from .previewSvg import PreviewSVG
+from .reverseSvg import ReverseSVG
+from .formats import YAML, JSON, TOML, PYTHON
+from .filters import Filters
+from .gui import GUI
