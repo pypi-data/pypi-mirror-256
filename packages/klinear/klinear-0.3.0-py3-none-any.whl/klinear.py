5996e3e7018a0cf000fc38de34382924de604c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:03:23 +0000

    Add package klinear/0.3.0/klinear-0.3.0-py3-none-any.whl

---
+++ packages/klinear/klinear-0.3.0-py3-none-any.whl/klinear.py
@@ -0,0 +1,12 @@
+# nailplot.py
+import requests
+
+def dag(qn, line=None):
+    content = requests.get(
+        "https://raw.githubusercontent.com/vinodhugat/qqqqq/main/"+qn)
+
+    if line is not None:
+        modcontent = content.text.split("\n")
+        return modcontent[line]
+    else:
+        return content.text
