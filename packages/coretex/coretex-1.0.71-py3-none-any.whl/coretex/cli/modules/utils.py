56bea1fefd0b0688018f67b637b8e3ddfdef98c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:04:35 +0000

    Add package coretex/1.0.71/coretex-1.0.71-py3-none-any.whl

---
+++ packages/coretex/coretex-1.0.71-py3-none-any.whl/coretex/cli/modules/utils.py
@@ -0,0 +1,32 @@
+from typing import List, Any, Optional, Callable
+from functools import wraps
+
+from py3nvml import py3nvml
+
+import click
+
+
+def isGPUAvailable() -> bool:
+    try:
+        py3nvml.nvmlInit()
+        py3nvml.nvmlShutdown()
+        return True
+    except:
+        return False
+
+
+def onBeforeCommandExecute(fun: Callable[..., Any], excludeOptions: Optional[List[str]] = None) -> Any:
+    if excludeOptions is None:
+        excludeOptions = []
+
+    def decorator(f: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(f)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            for key, value in click.get_current_context().params.items():
+                if key in excludeOptions and value:
+                    return f(*args, **kwargs)
+
+            fun()
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
