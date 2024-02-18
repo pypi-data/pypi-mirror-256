f205541b904a324311c60be010b21213ba282634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:37 +0000

    Add package gotranx/0.2.3/gotranx-0.2.3-py3-none-any.whl

---
+++ packages/gotranx/gotranx-0.2.3-py3-none-any.whl/gotranx/templates/c.py
@@ -0,0 +1,63 @@
+from textwrap import dedent, indent
+
+
+def init_state_values(name, state_names, state_values, code):
+    indented_code = indent(code, "    ")
+    indent_values = indent(", ".join(f"{n}={v}" for n, v in zip(state_names, state_values)), "    ")
+    return dedent(
+        f"""
+void init_state_values(double* {name}){{
+    /*
+{indent_values}
+    */
+{indented_code}
+}}
+""",
+    )
+
+
+def init_parameter_values(name, parameter_names, parameter_values, code):
+    indented_code = indent(code, "    ")
+    indent_values = indent(
+        ", ".join(f"{n}={v}" for n, v in zip(parameter_names, parameter_values)), "    "
+    )
+    return dedent(
+        f"""
+void init_parameter_values(double* {name}){{
+    /*
+{indent_values}
+    */
+{indented_code}
+}}
+""",
+    )
+
+
+def method(
+    name: str,
+    args: str,
+    states: str,
+    parameters: str,
+    values: str,
+    return_name: None = None,
+    num_return_values: int = 0,
+    **kwargs,
+):
+    indent_states = indent(states, "    ")
+    indent_parameters = indent(parameters, "    ")
+    indent_values = indent(values, "    ")
+    return dedent(
+        f"""
+void {name}({args}){{
+
+    // Assign states
+{indent_states}
+
+    // Assign parameters
+{indent_parameters}
+
+    // Assign expressions
+{indent_values}
+}}
+""",
+    )
