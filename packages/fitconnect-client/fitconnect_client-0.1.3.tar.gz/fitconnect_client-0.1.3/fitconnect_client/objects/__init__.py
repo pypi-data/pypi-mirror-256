3d1deda9073e5ca58d453ef0b14f0d4646ce54db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:11 +0000

    Add package fitconnect-client/0.1.3/fitconnect_client-0.1.3.tar.gz

---
+++ packages/fitconnect-client/fitconnect_client-0.1.3.tar.gz/fitconnect_client-0.1.3/fitconnect_client/objects/__init__.py
@@ -0,0 +1,3 @@
+from .exception import APIError
+from .attachment import Attachment
+from .submission import Submission
---
+++ packages/fitconnect-client/fitconnect_client-0.1.3.tar.gz/fitconnect_client-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="fitconnect_client",
+    packages=find_packages(include=["fitconnect_client", "fitconnect_client.objects"]),
+    version="0.1.3",
+    description="A python SDK to interact with the German Fitconnect system",
+    long_description="A python SDK to interact with the German Fitconnect system",
+    author="Jon Bulica",
+    author_email="jonbulica99@gmail.com",
+    install_requires=[
+        "urllib3",
+        "requests",
+        "jwcrypto"
+    ]
+)
