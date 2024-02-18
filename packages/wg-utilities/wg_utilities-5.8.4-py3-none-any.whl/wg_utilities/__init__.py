e88ced8ee9e11ba1815ea2cdcbbdd822cbe904e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:03 +0000

    Add package wg-utilities/5.8.4/wg_utilities-5.8.4-py3-none-any.whl

---
+++ packages/wg-utilities/wg_utilities-5.8.4-py3-none-any.whl/wg_utilities/__init__.py
@@ -0,0 +1,18 @@
+# pylint: disable=invalid-name
+"""Utilities package for my other projects."""
+from __future__ import annotations
+
+name = "wg_utilities"
+
+__version__ = "5.8.4"
+
+__all__ = [
+    "api",
+    "clients",
+    "decorators",
+    "devices",
+    "exceptions",
+    "functions",
+    "loggers",
+    "testing",
+]
