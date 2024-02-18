8ea811c8c41b69f17b02ce333f0bc295baa60b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:06:29 +0000

    Add package glenn-distributions/0.2/glenn_distributions-0.2.tar.gz

---
+++ packages/glenn-distributions/glenn_distributions-0.2.tar.gz/glenn_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='glenn_distributions',
+      version='0.2',
+      description='Gaussian & Binomial distributions',
+      packages=['glenn_distributions'],
+      author='Glenn Teh',
+      zip_safe=False)
