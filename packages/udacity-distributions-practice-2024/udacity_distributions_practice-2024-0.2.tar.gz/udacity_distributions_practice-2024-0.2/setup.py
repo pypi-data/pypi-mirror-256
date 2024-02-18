be9a6a035a433e9efb69416fedbd7a7c6b9f44ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:05:17 +0000

    Add package udacity-distributions-practice-2024/0.2/udacity_distributions_practice-2024-0.2.tar.gz

---
+++ packages/udacity-distributions-practice-2024/udacity_distributions_practice-2024-0.2.tar.gz/udacity_distributions_practice-2024-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_distributions_practice-2024',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Arjun Raghunandanan',
+      author_email = 'arjunregunandanan@gmail.com',
+      zip_safe=False)
