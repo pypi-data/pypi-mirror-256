2dd6944d359a3724e22700fcb94a2eca48aae68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:24 +0000

    Add package position-tools/0.2.2/position_tools-0.2.2.tar.gz

---
+++ packages/position-tools/position_tools-0.2.2.tar.gz/position_tools-0.2.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = ["numpy >= 1.11", "scipy", "tqdm"]
+TESTS_REQUIRE = ["pytest >= 2.7.1"]
+
+setup(
+    name="position_tools",
+    version="0.2.2",
+    license="MIT",
+    description=(""),
+    author="",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+)
