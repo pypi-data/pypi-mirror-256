3c53d11b16743fcd8a66c7c65076134a4ed4c6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:42 +0000

    Add package distributions-families/0.1/distributions_families-0.1.tar.gz

---
+++ packages/distributions-families/distributions_families-0.1.tar.gz/distributions_families-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_families',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      author='Asmaa Elbasioni',
+      author_email='a.elbasioni@gmail.com',
+      packages=['distributions_families'],
+      zip_safe=False)
