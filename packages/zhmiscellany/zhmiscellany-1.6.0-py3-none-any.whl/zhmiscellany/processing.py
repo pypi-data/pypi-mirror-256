83dadba8a3be10336733ec580846d33e5022eaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:57 +0000

    Add package zhmiscellany/1.6.0/zhmiscellany-1.6.0-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-1.6.0-py3-none-any.whl/zhmiscellany/processing.py
@@ -0,0 +1,24 @@
+import threading
+import traceback
+import zhmiscellany.string
+import concurrent.futures
+
+
+def start_daemon(**kwargs):
+    thread = threading.Thread(**kwargs)
+    thread.daemon = True
+    thread.start()
+    return thread
+
+
+def batch_threading(targets, max_threads):
+    def execute_target(target):
+        try:
+            target[0](*target[1])
+        except Exception as e:
+            print(traceback.format_exc())
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        futures = {executor.submit(execute_target, target): target for target in targets}
+
+        for future in concurrent.futures.as_completed(futures):
+            del futures[future]
