3b66359725ee1eeb73c44d9a4a821c3edf4ac95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:04:12 +0000

    Add package distribution-by-benson/0.1/distribution-by-benson-0.1.tar.gz

---
+++ packages/distribution-by-benson/distribution-by-benson-0.1.tar.gz/distribution-by-benson-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribution-by-benson',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Benson Sabu',
+      author_email = 'abcd@you.com',
+      zip_safe=False)
