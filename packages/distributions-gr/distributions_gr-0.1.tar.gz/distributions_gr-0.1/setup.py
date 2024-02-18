338c1ed018c040bb2184ce86524ee6e0bb6e4803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:03:32 +0000

    Add package distributions-gr/0.1/distributions_gr-0.1.tar.gz

---
+++ packages/distributions-gr/distributions_gr-0.1.tar.gz/distributions_gr-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_gr',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_gr'],
+      author="GunaSekhar Revuri",
+      author_email="gunarevuri@gmail.com",
+      zip_safe=False)
