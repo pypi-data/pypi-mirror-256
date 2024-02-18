e048cbe315f3412068e01e3ae33f29bce0d1fd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:13 +0000

    Add package custodian/2024.2.15/custodian-2024.2.15.tar.gz

---
+++ packages/custodian/custodian-2024.2.15.tar.gz/custodian-2024.2.15/custodian/__init__.py
@@ -0,0 +1,19 @@
+"""
+Custodian is a simple, robust and flexible just-in-time (JIT) job management
+framework written in Python.
+"""
+
+
+import os
+
+from .custodian import Custodian
+
+__author__ = (
+    "Shyue Ping Ong, William Davidson Richards, Stephen Dacek, Xiaohui Qu, Matthew Horton, "
+    "Samuel M. Blau, Janosh Riebesell"
+)
+__version__ = "2024.1.9"
+
+
+PKG_DIR = os.path.dirname(__file__)
+ROOT = os.path.dirname(PKG_DIR)
---
+++ packages/custodian/custodian-2024.2.15.tar.gz/custodian-2024.2.15/custodian/qchem/__init__.py
@@ -0,0 +1,5 @@
+"""This package implements various QChem Jobs and Error Handlers."""
+
+
+__author__ = "Samuel Blau, Brandon Wood, Shyam Dwaraknath"
+__credits__ = "Xiaohui Qu"
