f47e5dc7467f3fdbbd5af61e95bc707503ffd8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:03:49 +0000

    Add package sn0wyq-tinyman-py-sdk/2.2.0/sn0wyq-tinyman-py-sdk-2.2.0.tar.gz

---
+++ packages/sn0wyq-tinyman-py-sdk/sn0wyq-tinyman-py-sdk-2.2.0.tar.gz/sn0wyq-tinyman-py-sdk-2.2.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="sn0wyq-tinyman-py-sdk",
+    description="Tinyman Python SDK (forked)",
+    author="Tinyman",
+    author_email="hello@tinyman.org",
+    version="2.2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    project_urls={
+        "Source": "https://github.com/tinyman/tinyman-py-sdk",
+    },
+    install_requires=["py-algorand-sdk >= 1.10.0", "requests >= 2.0.0"],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+    package_data={
+        "tinyman.v1": ["asc.json"],
+        "tinyman.v2": ["amm_approval.map.json", "swap_router_approval.map.json"],
+    },
+    include_package_data=True,
+)
