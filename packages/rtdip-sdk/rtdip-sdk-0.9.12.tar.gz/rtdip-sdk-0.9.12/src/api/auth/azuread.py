2e2eef1b5235ebe4423ff420b5fb9fbf31bd766f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:05 +0000

    Add package rtdip-sdk/0.9.12/rtdip-sdk-0.9.12.tar.gz

---
+++ packages/rtdip-sdk/rtdip-sdk-0.9.12.tar.gz/rtdip-sdk-0.9.12/src/api/auth/azuread.py
@@ -0,0 +1,41 @@
+# Copyright 2022 RTDIP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from src.sdk.python.rtdip_sdk.authentication.azure import DefaultAuth
+import os
+
+tenant_id = os.environ.get("TENANT_ID")
+
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl="https://login.microsoftonline.com/{}/oauth2/v2.0/authorize".format(
+        tenant_id
+    ),
+    tokenUrl="https://login.microsoftonline.com/{}/oauth2/v2.0/token".format(tenant_id),
+    refreshUrl="https://login.microsoftonline.com/{}/oauth2/v2.0/refresh".format(
+        tenant_id
+    ),
+)
+
+
+def get_azure_ad_token(authorization=None):
+    if authorization is None or authorization == "No Token":
+        access_token = DefaultAuth().authenticate()
+        token = access_token.get_token(
+            "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default"
+        ).token
+    else:
+        token = authorization.replace("Bearer ", "")
+
+    return token
