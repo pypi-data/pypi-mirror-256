c35a70d2de496cb36f6988d03b7ba398b83c5883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:05 +0000

    Add package gridcreator/0.1.1/gridcreator-0.1.1.tar.gz

---
+++ packages/gridcreator/gridcreator-0.1.1.tar.gz/gridcreator-0.1.1/README.md
@@ -0,0 +1,20 @@
+# What does that library can do?
+
+This library is designed for creating grids to facilitate the decentralization of different products. It is currently being developed as a gridnet library with integrated network functions.
+
+# How to install?
+
+```
+pip install gridcreator
+```
+
+# How to use?
+
+```
+import grid
+
+your_grid = grid.Grid("example_grid", (3, 3, 3))
+
+your_grid.genesis()
+your_grid.add("example_element", "example")
+```
---
+++ packages/gridcreator/gridcreator-0.1.1.tar.gz/gridcreator-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+setup(
+    name='gridcreator',
+    version='0.1.1',
+    author='oblivisheee',
+    author_email='molniya213y@gmail.com',
+    description='That library provides utilities to create grids.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    license='GNU General Public License Version 3',
+    url='https://github.com/endprivate/grid'
+)
