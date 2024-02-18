4b58df4f7addc16e45a7728b4d282153f05c78a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:45 +0000

    Add package WordsPy/1.0.0/WordsPy-1.0.0.tar.gz

---
+++ packages/WordsPy/WordsPy-1.0.0.tar.gz/WordsPy-1.0.0/README.md
@@ -0,0 +1,2 @@
+# WordsPy
+A Python library allows developers to retrieve millions of words for the requested language with just one line of code.
---
+++ packages/WordsPy/WordsPy-1.0.0.tar.gz/WordsPy-1.0.0/WordsPy/Language.py
@@ -0,0 +1,536 @@
+from .Helpers import extract_array
+
+
+class Afrikaans:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Amharic:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Arabic:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Armenian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Azerbaijani:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Basque:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Belarusian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Bengali:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Bosnian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Bulgarian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Catalan:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Cebuano:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Chichewa:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Chinese:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Corsican:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Croatian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Czech:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Danish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Dutch:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class English:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Esperanto:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Estonian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Filipino:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Finnish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class French:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Frisian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Galician:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Georgian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class German:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Greek:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Gujarati:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class HaitianCreole:
+    def get_all_words(self):
+        return extract_array("Haitian%20Creole")
+
+
+class Hausa:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Hawaiian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Hebrew:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Hindi:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Hmong:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Hungarian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Icelandic:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Igbo:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Indonesian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Irish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Italian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Japanese:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Javanese:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Kannada:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Kazakh:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Khmer:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Kinyarwanda:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Korean:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Kurdish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Kyrgyz:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Lao:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Latin:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Latvian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Lithuanian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Luxembourgish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Macedonian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Malagasy:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Malay:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Malayalam:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Maltese:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Maori:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Marathi:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Mongolian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Myanmar:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Nepali:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Norwegian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Odia:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Pashto:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Persian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Polish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Portuguese:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Punjabi:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Romanian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Russian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Samoan:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class ScotsGaelic:
+    def get_all_words(self):
+        return extract_array("Scots%20Gaelic")
+
+
+class Serbian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Sesotho:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Shona:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Sindhi:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Sinhala:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Slovak:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Slovenian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Somali:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Spanish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Sundanese:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Swahili:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Swedish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Tajik:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Tamil:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Tatar:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Telugu:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Thai:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Turkish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Turkmen:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Ukranian:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Urdu:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Uyghur:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Uzbek:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Vietnamese:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Welsh:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Xhosa:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Yiddish:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Yoruba:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
+
+
+class Zulu:
+    def get_all_words(self):
+        return extract_array(type(self).__name__)
