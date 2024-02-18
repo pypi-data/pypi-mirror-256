016a8fd4824443c94e9cedf83608dea882991d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:58 +0000

    Add package grooveshop-django-api/0.68.0/grooveshop_django_api-0.68.0.tar.gz

---
+++ packages/grooveshop-django-api/grooveshop_django_api-0.68.0.tar.gz/grooveshop_django_api-0.68.0/blog/serializers/category.py
@@ -0,0 +1,34 @@
+from drf_spectacular.utils import extend_schema_field
+from parler_rest.fields import TranslatedFieldsField
+from parler_rest.serializers import TranslatableModelSerializer
+
+from blog.models.category import BlogCategory
+from core.api.schema import generate_schema_multi_lang
+
+
+@extend_schema_field(generate_schema_multi_lang(BlogCategory))
+class TranslatedFieldsFieldExtend(TranslatedFieldsField):
+    pass
+
+
+class BlogCategorySerializer(TranslatableModelSerializer):
+    translations = TranslatedFieldsFieldExtend(shared_model=BlogCategory)
+
+    class Meta:
+        model = BlogCategory
+        fields = (
+            "translations",
+            "id",
+            "slug",
+            "parent",
+            "level",
+            "tree_id",
+            "absolute_url",
+            "recursive_post_count",
+            "created_at",
+            "updated_at",
+            "sort_order",
+            "uuid",
+            "main_image_absolute_url",
+            "main_image_filename",
+        )
