576a984f350bad085666f14850c74c4f04aa8857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:17 +0000

    Add package lbconverter/0.0.18/lbconverter-0.0.18.tar.gz

---
+++ packages/lbconverter/lbconverter-0.0.18.tar.gz/lbconverter-0.0.18/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("app/readme.md") as f:
+    long_description = f.read()
+
+setup(
+name='lbconverter',
+version='0.0.18',
+author='Gabe',
+package_dir={"": "app"},
+author_email='gunderwood@labelbox.com',
+long_description=long_description,
+long_description_content_type= "text/markdown",
+description='module of classes to assist on getting model results to labelbox',
+packages=find_packages(where="app"),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
