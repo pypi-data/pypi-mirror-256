62440f9924a00bd85b865e4d50fcda50a312ae99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:30 +0000

    Add package sparcs-ptunnel/1.0.0rc13/sparcs_ptunnel-1.0.0rc13-py3-none-any.whl

---
+++ packages/sparcs-ptunnel/sparcs_ptunnel-1.0.0rc13-py3-none-any.whl/ptunnel/__main__.py
@@ -0,0 +1,40 @@
+import click
+from pathlib import Path
+
+import ptunnel
+import ptunnel.client
+import ptunnel.server
+
+__version__ = "1.0.0-pre13"
+ptunnel.__version__ = __version__
+
+@click.version_option(prog_name="ptunnel", version=__version__)
+@click.group()
+def main():
+    """
+    Simple port forwarding tool for SPARCS.
+    """
+    pass
+
+@main.command("server")
+@click.option("-c", "--config", default="config.json", help="Path to the config file.", type=Path)
+def crontab(config):
+    """
+    Server run.
+    """
+    config = ptunnel.server.load_config(config)
+    ptunnel.server.run()
+
+@main.command("config")
+def config():
+    """
+    Config file management.
+    """
+    ptunnel.server.save_config()
+
+@main.command("client")
+def client():
+    """
+    Client run.
+    """
+    ptunnel.client.run()
