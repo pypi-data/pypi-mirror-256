74cc51ae75254cb24986b37447c2a5cf09ff8a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:43 +0000

    Add package aioli-sdk/0.1.0/aioli_sdk-0.1.0-py3-none-any.whl

---
+++ packages/aioli-sdk/aioli_sdk-0.1.0-py3-none-any.whl/aioli/common/api/__init__.py
@@ -0,0 +1,15 @@
+from aioli.common.api import errors
+from aioli.common.api._util import PageOpts
+from aioli.common.api.request import (
+    WebSocket,
+    browser_open,
+    delete,
+    do_request,
+    get,
+    make_url,
+    parse_master_address,
+    patch,
+    post,
+    put,
+    ws,
+)
