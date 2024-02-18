0694ce40a8ba321dadb9f0be72c848182096fdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:40 +0000

    Add package pixano/0.5.0b4/pixano-0.5.0b4-py3-none-any.whl

---
+++ packages/pixano/pixano-0.5.0b4-py3-none-any.whl/pixano/app/dist/_app/immutable/chunks/datasetStores.ixD7r-qb.js
@@ -0,0 +1,14 @@
+import{w as e}from"./entry.IGbnXg9x.js";function c(s){return s?.length!==void 0?s:Array.from(s)}const a=20,t=1;/**
+ * @copyright CEA
+ * @author CEA
+ * @license CECILL
+ *
+ * This software is a collaborative computer program whose purpose is to
+ * generate and explore labeled data for computer vision applications.
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ *
+ * http://www.cecill.info
+ */const n={currentPage:t,pageSize:a},r=e(),S=e([]),l=e(!1),A=e(n),T=e({shouldSave:!1,canSave:!1});export{t as D,A as a,a as b,n as c,r as d,c as e,l as i,S as m,T as s};
