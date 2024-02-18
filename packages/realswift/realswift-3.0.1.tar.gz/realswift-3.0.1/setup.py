9749dfb565a99af6a4e8f6f2849e292ae6c22221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:43 +0000

    Add package realswift/3.0.1/realswift-3.0.1.tar.gz

---
+++ packages/realswift/realswift-3.0.1.tar.gz/realswift-3.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name="realswift",
+	version="3.0.1",
+	author="Jagadish Dabbiru",
+	author_email="jagadish.dabbiru@gmail.com",
+	packages=["realswift"],
+	description="AI automation tool leverages a cognitive approach inspired by the human brain's intuitive understanding of web interfaces",
+	long_description="This project performs user interactions with OpenCV2 by integrating the PyAutoGUI library alongside custom utility functions. The script offers a range of functionalities, including simulating user actions like typing, clicking, scrolling, and hovering. These actions are executed within a defined window or specific area, enhancing the automation of user interactions",
+	long_description_content_type="text/markdown",
+	url="https://github.com/jagadish165/realswift",
+	license='BSD-2-Clause',
+	python_requires='>=3.8',
+	install_requires=[]
+)
