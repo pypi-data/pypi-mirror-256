18e386194d1f5bf4e1d73ec6afea1ab493f2f4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:53 +0000

    Add package vec2d/0.2.3/vec2d-0.2.3.tar.gz

---
+++ packages/vec2d/vec2d-0.2.3.tar.gz/vec2d-0.2.3/vec2d/graph/__init__.py
@@ -0,0 +1,23 @@
+"""
+__init__.py for the vector2d.graph module which provides the graphic/drawing
+capabilities.
+"""
+from vec2d.graph.vector2d_graphics import (
+    Arrow,
+    Colors,
+    LineStyles,
+    Points,
+    Polygon,
+    Segment,
+    draw,
+)
+
+__all__ = [
+    "Arrow",
+    "Colors",
+    "LineStyles",
+    "Points",
+    "Polygon",
+    "Segment",
+    "draw",
+]
