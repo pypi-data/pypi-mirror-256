6ecee34b9fabad99331e72f86952441259abe503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:06 +0000

    Add package basepair/2.0.13/basepair-2.0.13-py3-none-any.whl

---
+++ packages/basepair/basepair-2.0.13-py3-none-any.whl/basepair/modules/storage/driver/minio.py
@@ -0,0 +1,22 @@
+'''Driver for AWS S3 compute'''
+
+# App import
+from .aws_s3 import Driver as S3Driver
+from .abstract import raise_no_implemented
+
+# constants
+
+class Driver(S3Driver):
+  '''Min IO Driver class'''
+
+  def __init__(self, cfg=None):
+    super().__init__(cfg)
+
+  def get_lifecycle(self, bucket=None):
+    raise_no_implemented("MinIO does not support lifecycle")
+
+  def restore_from_cold(self, uri, days):
+    raise_no_implemented("MinIO does not support restore from cold")
+
+  def set_lifecycle(self, **kwargs):
+    raise_no_implemented("MinIO does not support lifecycle")
