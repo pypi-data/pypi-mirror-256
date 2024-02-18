d53271092439607631c54eace0fcc7f49c7236e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:06:21 +0000

    Add package ast-probability/1.1/ast_probability-1.1.tar.gz

---
+++ packages/ast-probability/ast_probability-1.1.tar.gz/ast_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ast_probability',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ast_probability'],
+      author = 'Kyle_1',
+      author_email = 'joankorirchemutai@gmail.com',
+     zip_safe=False)
