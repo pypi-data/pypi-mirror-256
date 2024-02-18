93e336a5007558617a738e1dc2319d6f34a06634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:08 +0000

    Add package pyaskit/1.2.4/pyaskit-1.2.4.tar.gz

---
+++ packages/pyaskit/pyaskit-1.2.4.tar.gz/pyaskit-1.2.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyaskit",
+    version="1.2.4",
+    packages=find_packages(),
+    description="AskIt: Unified programming interface for programming with large language models (GPT-3.5, GPT-4, Llama 2)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Katsumi Okuda",
+    author_email="okuda@csail.mit.edu",
+    url="https://github.com/katsumiok/pyaskit",
+    python_requires=">=3.7",
+    install_requires=[
+        "openai>=1.12.0",
+        "unidecode",
+        "timeout-decorator",
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="MIT",
+    keywords="openai,gpt,gpt-3,gpt-4,llama,api,wrapper,framework,dsl,llm",
+)
