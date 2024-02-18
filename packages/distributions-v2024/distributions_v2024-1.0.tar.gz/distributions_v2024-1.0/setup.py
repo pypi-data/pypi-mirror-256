c77b608a8066293a3bcb612cb1324271bcff53b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:16 +0000

    Add package distributions-v2024/1.0/distributions_v2024-1.0.tar.gz

---
+++ packages/distributions-v2024/distributions_v2024-1.0.tar.gz/distributions_v2024-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='distributions_v2024',
+    version='1.0',
+    description='Probability distributions',
+    auther='A E',
+    packages=['distributions_v2024'],
+    zip_safe=False
+)
