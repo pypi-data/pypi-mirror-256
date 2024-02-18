cb89d14f73286bd7ba8e1b9ebfad16609ffe7d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:55 +0000

    Add package fatek-fbs-lib/0.1.10/fatek-fbs-lib-0.1.10.tar.gz

---
+++ packages/fatek-fbs-lib/fatek-fbs-lib-0.1.10.tar.gz/fatek-fbs-lib-0.1.10/README.md
@@ -0,0 +1,8 @@
+[![Build Status](https://travis-ci.org/rimek/fatek-fbs-lib.svg?branch=master)](https://travis-ci.org/rimek/fatek-fbs-lib)
+[![Coverage Status](https://coveralls.io/repos/github/rimek/fatek-fbs-lib/badge.svg?branch=master)](https://coveralls.io/github/rimek/fatek-fbs-lib?branch=master)
+
+# fatek-fbs-lib
+Library to read/write Fatek FBs PLC registers
+
+
+Branch 'py3' contains a version compatibile with python3 only
---
+++ packages/fatek-fbs-lib/fatek-fbs-lib-0.1.10.tar.gz/fatek-fbs-lib-0.1.10/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import find_packages, setup
+
+VERSION = '0.1.10'
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='fatek-fbs-lib',
+    version=VERSION,
+    description='Fatek FBs communication lib',
+    author='Marcin Rim',
+    author_email='rimek@poczta.fm',
+    url='https://github.com/rimek/fatek-fbs-lib',
+    packages=find_packages(),
+    test_suite='tests',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "pymodbus3; python_version >= '3.0'",
+        "pymodbus; python_version < '3.0'",
+        'pyserial',
+    ],
+    scripts=['bin/fatek-cli'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
