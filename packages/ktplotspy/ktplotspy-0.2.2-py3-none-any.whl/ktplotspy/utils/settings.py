aaad7327fbe457a35ae262847d7decba86165b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:53 +0000

    Add package ktplotspy/0.2.2/ktplotspy-0.2.2-py3-none-any.whl

---
+++ packages/ktplotspy/ktplotspy-0.2.2-py3-none-any.whl/ktplotspy/utils/settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution("ktplotspy").version
+except:  # pragma: no cover
+    __version__ = "dev"
+
+DEFAULT_SEP = ">@<"
+DEFAULT_SPEC_PAT = "/|:|\\?|\\*|\\+|\\|\\(|\\)|\\/"
+DEFAULT_CELLSIGN_ALPHA = 0.5
+DEFAULT_COLUMNS = ["interaction_group", "celltype_group"]
+DEFAULT_V5_COL_START = 13
+DEFAULT_COL_START = 11
+DEFAULT_CLASS_COL = 12
