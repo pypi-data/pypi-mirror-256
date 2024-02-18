db9c5caa1b52b1e1c3773aca33201dcd32c6a222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:11:43 +0000

    Add package distributions-memedi-2024/0.1/distributions_memedi_2024-0.1.tar.gz

---
+++ packages/distributions-memedi-2024/distributions_memedi_2024-0.1.tar.gz/distributions_memedi_2024-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_memedi_2024',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_memedi_2024'],
+      zip_safe=False)
