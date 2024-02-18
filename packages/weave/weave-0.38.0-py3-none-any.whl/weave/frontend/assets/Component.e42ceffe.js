32c600a4449cfd0b7d3d622e5cd0f406d251c69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:04:30 +0000

    Add package weave/0.38.0/weave-0.38.0-py3-none-any.whl

---
+++ packages/weave/weave-0.38.0-py3-none-any.whl/weave/frontend/assets/Component.e42ceffe.js
@@ -0,0 +1,2 @@
+import{ah as i,u as o,R as e}from"./index.663850a2.js";const n=l=>{const a=l.input,r=i({file:a}),t=o(r);return e.createElement("div",null,t.loading?e.createElement("div",null):e.createElement("img",{style:{maxWidth:"100%"},alt:"cool-alt",src:t.result}))};export{n as default};
+//# sourceMappingURL=Component.e42ceffe.js.map
