3062a01189501d5e3f6ea2b271825c649f16a4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:14 +0000

    Add package drexel-jupyter-logger/0.0.13/drexel_jupyter_logger-0.0.13.tar.gz

---
+++ packages/drexel-jupyter-logger/drexel_jupyter_logger-0.0.13.tar.gz/drexel_jupyter_logger-0.0.13/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name = "drexel_jupyter_logger",
+    version= "0.0.13",
+    author = "Joshua C. Agar",
+    description= "drexel jupyter logger package",
+    packages=["drexel_jupyter_logger"],
+    install_requires=["cryptography"],
+    entry_points={
+        'console_scripts': [
+            'student-info-from-nb=drexel_jupyter_logger.student_info_from_nb:main',
+            'grade-report=drexel_jupyter_logger.grade_report:main',
+            'build-quiz-for-jupyterbook=drexel_jupyter_logger.build_quiz_for_jupyterbook:main',
+            'read-log-file=drexel_jupyter_logger.read_log_file:main',
+            'remove-skip=drexel_jupyter_logger.remove_skip:main',
+            'tag-files=drexel_jupyter_logger.tag_files:main',
+            'activity=drexel_jupyter_logger.activity:main',
+            'fix_kernel=drexel_jupyter_logger.fix_kernel:main',
+        ],
+    }
+)
