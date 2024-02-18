030c451d6da3397ed6c843be7265072bec3e2c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:40 +0000

    Add package xscen/0.8.2/xscen-0.8.2.tar.gz

---
+++ packages/xscen/xscen-0.8.2.tar.gz/xscen-0.8.2/tests/conftest.py
@@ -0,0 +1,57 @@
+# noqa: D100
+import shutil
+from pathlib import Path
+
+import pytest
+
+import xscen as xs
+from xscen.testing import datablock_3d as _datablock_3d
+
+notebooks = Path(__file__).parent.parent / "docs" / "notebooks"
+SAMPLES_DIR = notebooks / "samples" / "tutorial"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cleanup_notebook_data_folder(request):
+    """Cleanup a testing file once we are finished.
+
+    This flag prevents remote data from being downloaded multiple times in the same pytest run.
+    """
+
+    def remove_data_folder():
+        data = notebooks / "_data"
+        if data.exists():
+            shutil.rmtree(data)
+
+    request.addfinalizer(remove_data_folder)
+
+
+@pytest.mark.requires_netcdf
+@pytest.fixture(scope="session")
+def samplecat():
+    """Generate a sample catalog with the tutorial netCDFs."""
+    df = xs.parse_directory(
+        directories=[SAMPLES_DIR],
+        patterns=[
+            "{activity}/{domain}/{institution}/{source}/{experiment}/{member}/{frequency}/{?:_}.nc"
+        ],
+        homogenous_info={
+            "mip_era": "CMIP6",
+            "type": "simulation",
+            "processing_level": "raw",
+        },
+        read_from_file=["variable", "date_start", "date_end"],
+        xr_open_kwargs={"engine": "h5netcdf"},
+    )
+    return xs.DataCatalog({"esmcat": xs.catalog.esm_col_data, "df": df})
+
+
+@pytest.fixture
+def datablock_3d():
+    """Create a generic timeseries object based on pre-defined dictionaries of existing variables.
+
+    See Also
+    --------
+    xscen.testing.datablock_3d
+    """
+    return _datablock_3d
