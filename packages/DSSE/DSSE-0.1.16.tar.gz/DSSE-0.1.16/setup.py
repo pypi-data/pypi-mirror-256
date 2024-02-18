09ed875b940a8f811281411fe4fef3c3fe618e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:23 +0000

    Add package DSSE/0.1.16/DSSE-0.1.16.tar.gz

---
+++ packages/DSSE/DSSE-0.1.16.tar.gz/DSSE-0.1.16/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+import sys
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+
+setup(name='DSSE',
+      version='0.1.16',
+      description='An environment to train drones to search and find a shipwrecked person lost in the ocean using reinforcement learning.',
+      url='https://github.com/pfeinsper/drone-swarm-search',
+      license='MIT',
+      packages=['DSSE'],
+      install_requires=[
+          'numpy',
+          'gymnasium',
+          'pygame',
+          'pettingzoo',
+          'matplotlib',
+      ],
+      zip_safe=False)
