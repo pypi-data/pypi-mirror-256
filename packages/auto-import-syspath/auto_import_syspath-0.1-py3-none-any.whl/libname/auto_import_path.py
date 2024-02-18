4e88bc62635a055abd8149801e79caf05b928561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:28 +0000

    Add package auto-import-syspath/0.1/auto_import_syspath-0.1-py3-none-any.whl

---
+++ packages/auto-import-syspath/auto_import_syspath-0.1-py3-none-any.whl/libname/auto_import_path.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+from os.path import join, abspath, basename
+
+
+def __found_before_path__(path_name: str, relative_path: str = None):
+	__before_path__ = None
+	__path__ = relative_path if relative_path else os.getcwd()
+
+	while True:
+		if basename(str(__path__).lower()) == path_name.lower():
+			return __path__
+
+		if __path__ in os.getcwd().split('\\')[0] + "\\":
+			raise Exception("Modulo não encontrado")
+
+		__before_path__ = abspath(join(__path__, '..'))
+
+		print(__path__)
+		__path__ = __before_path__
+
+
+def import_sys_path(path_name: str, relative_path: str = None):
+	path = __found_before_path__(path_name, relative_path)
+	sys.path.append(path)
+
