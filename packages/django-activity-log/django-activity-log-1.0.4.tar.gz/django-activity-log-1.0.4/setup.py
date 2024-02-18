2edaae087ec584569d599c818349062a4dd1e9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:43 +0000

    Add package django-activity-log/1.0.4/django-activity-log-1.0.4.tar.gz

---
+++ packages/django-activity-log/django-activity-log-1.0.4.tar.gz/django-activity-log-1.0.4/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+try:
+    import pypandoc
+    DESCRIPTION = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    DESCRIPTION = open('README.md').read()
+setup(
+    name='django-activity-log',
+    version='1.0.4',
+    description='HTTP queries logger with flexible filters.',
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author='Hossein SayyedMousavi',
+    author_email='hossein.sayyedmousavi@gmail.com',
+    keywords=[
+        "django",
+        "database",
+        "user",
+        "activity log"
+    ],
+    packages=[
+        'activity_log',
+        'activity_log.migrations',
+    ],
+    url='https://github.com/HosseinSayyedMousavi',
+    license='MIT License',
+    readme="README.md",
+    install_requires=[
+        'django',
+    ],
+
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
