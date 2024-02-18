778447b3c93800407db055092c6ca61bf642c3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:04:11 +0000

    Add package warchant_dc_schema/0.0.10/warchant_dc_schema-0.0.10-py3-none-any.whl

---
+++ packages/warchant_dc_schema/warchant_dc_schema-0.0.10-py3-none-any.whl/dc_schema/cli.py
@@ -0,0 +1,21 @@
+import argparse
+import json
+
+from dc_schema import get_schema
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "file_path", help="The path to the python file containing the dataclass"
+    )
+    arg_parser.add_argument(
+        "dataclass", help="The name of the dataclass to generate the schema"
+    )
+    args = arg_parser.parse_args()
+
+    with open(args.file_path) as r:
+        exec(r.read(), locals())
+
+    schema = get_schema(locals()[args.dataclass])
+    print(json.dumps(schema, indent=2), end=None)
