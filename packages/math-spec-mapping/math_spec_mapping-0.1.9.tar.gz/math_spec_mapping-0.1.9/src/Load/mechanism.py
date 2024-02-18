9ea87dcf2509d762ae87b054cd7cf47cf74527b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:06:19 +0000

    Add package math-spec-mapping/0.1.9/math_spec_mapping-0.1.9.tar.gz

---
+++ packages/math-spec-mapping/math_spec_mapping-0.1.9.tar.gz/math_spec_mapping-0.1.9/src/Load/mechanism.py
@@ -0,0 +1,45 @@
+from typing import Dict
+from ..Classes import Mechanism
+from .general import check_json_keys
+
+
+def convert_mechanism(data: Dict, ms: Dict) -> Mechanism:
+    """Function to convert dictionary to mechanism object
+
+    Args:
+        data (Dict): The data to convert
+
+    Returns:
+        Mechanism: Mechanism object
+    """
+
+    if "metadata" not in data:
+        data["metadata"] = {}
+    # Check the keys are correct
+    check_json_keys(data, "Mechanism")
+    assert type(data["domain"]) == tuple, "{} domain is not a tuple".format(
+        data["name"]
+    )
+    if len(data["domain"]) == 0:
+        data["domain"] = ("Empty Space",)
+
+    # Copy
+    data = data.copy()
+
+    data["domain"] = tuple(ms["Spaces"][x] for x in data["domain"])
+
+    # Build the action transmission channel object
+    return Mechanism(data)
+
+
+def load_mechanisms(ms: Dict, json: Dict) -> None:
+    """Function to load states into the new dictionary
+
+    Args:
+        ms (Dict): MathSpec dictionary
+        json (Dict): JSON version of MathSpec to load
+    """
+
+    ms["Mechanisms"] = {}
+    for key in json["Mechanisms"]:
+        ms["Mechanisms"][key] = convert_mechanism(json["Mechanisms"][key], ms)
---
+++ packages/math-spec-mapping/math_spec_mapping-0.1.9.tar.gz/math_spec_mapping-0.1.9/src/Load/parameters.py
@@ -0,0 +1,43 @@
+from typing import Dict
+from ..Classes import ParameterContainer, ParameterSet, Parameter
+from .general import check_json_keys
+
+
+def convert_parameter_set(data: Dict) -> ParameterSet:
+    if "metadata" not in data:
+        data["metadata"] = {}
+    # Check the keys are correct
+    check_json_keys(data, "Parameter Set")
+
+    # Copy
+    data = data.copy()
+
+    # Convert the parameters
+    new_parameters = []
+    for param in data["parameters"]:
+        if "metadata" not in param:
+            param["metadata"] = {}
+        check_json_keys(param, "Parameter")
+        new_parameters.append(Parameter(param))
+    data["parameters"] = new_parameters
+
+    # Build the state object
+    return ParameterSet(data)
+
+
+def load_parameters(ms: Dict, json: Dict) -> None:
+    """Function to load parameters into the new dictionary
+
+    Args:
+        ms (Dict): MathSpec dictionary
+        json (Dict): JSON version of MathSpec to load
+    """
+
+    ms["Parameters"] = {}
+
+    # Convert parameter sets
+    for key in json["Parameters"]:
+        ms["Parameters"][key] = convert_parameter_set(json["Parameters"][key])
+
+    # Placeholder for now
+    ms["Parameters"] = ParameterContainer(ms["Parameters"])
