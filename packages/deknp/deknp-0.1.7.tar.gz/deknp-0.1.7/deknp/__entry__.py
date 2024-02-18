ae8e3a7ced6945757ac3024b55f7fc3df81763da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:04 +0000

    Add package deknp/0.1.7/deknp-0.1.7.tar.gz

---
+++ packages/deknp/deknp-0.1.7.tar.gz/deknp-0.1.7/deknp/__entry__.py
@@ -0,0 +1,38 @@
+import typer
+from .core import execute_install, execute_package_sure, execute_server, run_plugin_yaml, clear_pkg_cache
+
+app = typer.Typer(add_completion=False)
+
+
+def main():
+    app()
+
+
+@app.command()
+def install():
+    execute_install()
+
+
+@app.command()
+def sure(force=False):
+    execute_package_sure(force)
+
+
+@app.command()
+def server():
+    execute_server(False)
+
+
+@app.command()
+def serverfull():
+    execute_server(True)
+
+
+@app.command()
+def clearcache():
+    clear_pkg_cache()
+
+
+@app.command()
+def yaml():
+    run_plugin_yaml()
