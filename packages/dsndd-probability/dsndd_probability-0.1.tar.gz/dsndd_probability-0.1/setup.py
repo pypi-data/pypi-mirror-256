131b3f0ba7d9d8edd35e988d8586b52a54358560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:46 +0000

    Add package dsndd-probability/0.1/dsndd_probability-0.1.tar.gz

---
+++ packages/dsndd-probability/dsndd_probability-0.1.tar.gz/dsndd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="dsndd_probability", 
+ version="0.1",
+ description="Gaussian and Binomial distributions", 
+ packages=['dsndd_probability'],
+ author = "Roushan Kumar",
+author_email = "kumarroushan.18@gmail.com",
+ zip_safe=False)
