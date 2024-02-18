398b2e5ca1d7cda78bdcb37880eab4b6daabc2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:17 +0000

    Add package securities-exchange/0.0.10/securities-exchange-0.0.10.tar.gz

---
+++ packages/securities-exchange/securities-exchange-0.0.10.tar.gz/securities-exchange-0.0.10/docs/index.md
@@ -0,0 +1,13 @@
+# Welcome to securities_exchange
+
+
+[![image](https://img.shields.io/pypi/v/securities_exchange.svg)](https://pypi.python.org/pypi/securities_exchange)
+
+
+**A toy example of a Security Exchange in python**
+
+-   Free software: MIT License
+
+## Features
+
+-   TODO
---
+++ packages/securities-exchange/securities-exchange-0.0.10.tar.gz/securities-exchange-0.0.10/securities_exchange/__init__.py
@@ -0,0 +1,29 @@
+"""Top-level package for securities-exchange."""
+
+__author__ = """Giulio Guerri"""
+__email__ = "giulio.guerri93@gmail.com"
+__version__ = "0.0.10"
+
+import logging
+
+# Set up a global logger for the package
+logging.basicConfig(level=logging.WARNING, format="[%(levelname)s][%(asctime)s]: %(message)s", force=True)
+logger = logging.getLogger(__name__)
+
+
+def set_logging_level(level):
+    """
+    Set the logging level for the entire package.
+
+    Args:
+        level (int): The logging level (e.g., logging.DEBUG, logging.INFO, logging.WARNING, etc.).
+    """
+    logging.getLogger(__name__).setLevel(level)
+
+
+from .securities_exchange import SecuritiesExchange
+from .enums import OrderType, OrderStatus, MarketSide
+from .order import Order
+from .orderbook import OrderBook
+from .bookside import BookSide
+
