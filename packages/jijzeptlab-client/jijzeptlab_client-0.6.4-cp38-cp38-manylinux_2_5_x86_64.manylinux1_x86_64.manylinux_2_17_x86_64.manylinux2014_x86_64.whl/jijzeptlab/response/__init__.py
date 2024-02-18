c72dac3a5d2babb0966925d2e0d125314aa927f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:28 +0000

    Add package jijzeptlab-client/0.6.4/jijzeptlab_client-0.6.4-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/jijzeptlab-client/jijzeptlab_client-0.6.4-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/jijzeptlab/response/__init__.py
@@ -0,0 +1,9 @@
+from jijzeptlab.response import response, util
+from jijzeptlab.response.response import JijZeptLabResponse, JijZeptLabResult
+
+__all__ = [
+    "response",
+    "util",
+    "JijZeptLabResponse",
+    "JijZeptLabResult",
+]
