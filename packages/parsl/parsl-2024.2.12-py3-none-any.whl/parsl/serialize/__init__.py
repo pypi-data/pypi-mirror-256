f6abcfb13f28e5255419e179fdb4920afbb43cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:11:19 +0000

    Add package parsl/2024.2.12/parsl-2024.2.12-py3-none-any.whl

---
+++ packages/parsl/parsl-2024.2.12-py3-none-any.whl/parsl/serialize/__init__.py
@@ -0,0 +1,6 @@
+from parsl.serialize.facade import serialize, deserialize, pack_apply_message, unpack_apply_message
+
+__all__ = ['serialize',
+           'deserialize',
+           'pack_apply_message',
+           'unpack_apply_message']
