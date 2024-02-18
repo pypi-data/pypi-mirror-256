2c88d0f18c7bbfa2c40506fa6283e7ac1dd52bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:20 +0000

    Add package chrpy/0.0.1/chrpy-0.0.1.tar.gz

---
+++ packages/chrpy/chrpy-0.0.1.tar.gz/chrpy-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chrpy',
+    packages=find_packages(include=['chrpy']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
