7e9753d1ab531f83b7d15de3d0fa274bc79eb02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:04:12 +0000

    Add package m9s-account-tax-rule-zone/7.0.1/m9s_account_tax_rule_zone-7.0.1.tar.gz

---
+++ packages/m9s-account-tax-rule-zone/m9s_account_tax_rule_zone-7.0.1.tar.gz/m9s_account_tax_rule_zone-7.0.1/__init__.py
@@ -0,0 +1,33 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+
+from . import account, country, purchase, purchase_request, sale
+
+__all__ = ['register']
+
+
+def register():
+    Pool.register(
+        country.Country,
+        account.TaxZone,
+        account.TaxRuleLineTemplate,
+        account.TaxRuleLine,
+        module='account_tax_rule_zone', type_='model')
+    Pool.register(
+        account.InvoiceLine,
+        depends=['account_invoice'],
+        module='account_tax_rule_zone', type_='model')
+    Pool.register(
+        sale.Sale,
+        sale.SaleLine,
+        depends=['sale'],
+        module='account_tax_rule_zone', type_='model')
+    Pool.register(
+        purchase.PurchaseLine,
+        depends=['purchase'],
+        module='account_tax_rule_zone', type_='model')
+    Pool.register(
+        purchase_request.CreatePurchase,
+        depends=['purchase_request'],
+        module='account_tax_rule_zone', type_='wizard')
