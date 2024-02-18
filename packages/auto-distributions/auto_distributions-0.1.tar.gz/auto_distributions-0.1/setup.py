9b50b8ce040bcc1c576c297b6ad17fe83ba34da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:11:25 +0000

    Add package auto-distributions/0.1/auto_distributions-0.1.tar.gz

---
+++ packages/auto-distributions/auto_distributions-0.1.tar.gz/auto_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='auto_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['auto_distributions'],
+      zip_safe=False)
