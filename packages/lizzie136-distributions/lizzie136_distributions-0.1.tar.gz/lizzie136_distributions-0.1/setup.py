3fdbe33179a06e7e63f62f63e8645ea6a6a9231d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:05 +0000

    Add package lizzie136-distributions/0.1/lizzie136_distributions-0.1.tar.gz

---
+++ packages/lizzie136-distributions/lizzie136_distributions-0.1.tar.gz/lizzie136_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lizzie136_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['l136_distributions'],
+      author='Elizabeth Margolis',
+      author_email="darknessover136@gmail.com",
+      zip_safe=False)
