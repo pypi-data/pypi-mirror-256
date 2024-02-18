c45447cb40afbf21d9f7b4d9bd78e40854d4014d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:18 +0000

    Add package wagtail-showables/1.0.9/wagtail_showables-1.0.9.tar.gz

---
+++ packages/wagtail-showables/wagtail_showables-1.0.9.tar.gz/wagtail_showables-1.0.9/wagtail_showables/middleware.py
@@ -0,0 +1,31 @@
+import warnings
+from .backends import (
+    get_showable_backend,
+    _performance_backend_data,
+    ShowablePerformanceCacheBackend,
+    ShowablePerformanceDBBackend,
+)
+
+class ShowablePerformanceDataMiddleware:
+    """Middleware that saves request in thread local storage."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        backend = get_showable_backend()
+
+        if not isinstance(backend, (ShowablePerformanceCacheBackend, ShowablePerformanceDBBackend)):
+            warnings.warn(
+                "You are using the ShowablePerformanceDataMiddleware without the ShowablePerformanceCacheBackend. "
+                "This middleware will have no effect."
+            )
+            return self.get_response(request)
+
+        _performance_backend_data.data = backend.fetch_registry_data()
+        try:
+            response = self.get_response(request)
+        finally:
+            if hasattr(_performance_backend_data, "data"):
+                del _performance_backend_data.data
+        return response
