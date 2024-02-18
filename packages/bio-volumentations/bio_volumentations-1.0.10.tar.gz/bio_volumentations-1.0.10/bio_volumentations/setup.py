7936525e5a6adf9b9ba49c676e1692487ea9f01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:00 +0000

    Add package bio-volumentations/1.0.10/bio_volumentations-1.0.10.tar.gz

---
+++ packages/bio-volumentations/bio_volumentations-1.0.10.tar.gz/bio_volumentations-1.0.10/bio_volumentations/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='bio_volumentations',
+    version='1.0.10',
+    author='Roman Sol (ZFTurbo), ashawkey, qubvel, muellerdo, Lucia Hradecka',
+    packages=find_packages(),
+    url='https://gitlab.fi.muni.cz/xdupkan/volumentations/',
+    description='Library for 3D augmentations for biomedical images',
+    long_description='Library for 3D augmentations for biomedical images. Inspired by albumentations.'
+                     'More details: https://gitlab.fi.muni.cz/xdupkan/volumentations/',
+    install_requires=[
+        'scikit-image',
+        'scipy',
+        'opencv-python',
+        "numpy",
+    ],
+    entry_points={
+        'console_scripts': [
+            'bioVolumentation=bio_volumentations:__init__'
+        ]
+    }
+)
