29425d1b422e69aeafe0974d25c4d8a9d0f30d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:03:57 +0000

    Add package credici/0.1.0/credici-0.1.0-py3-none-any.whl

---
+++ packages/credici/credici-0.1.0-py3-none-any.whl/credici/schema/storage_schema.py
@@ -0,0 +1,24 @@
+from uuid import UUID
+from datetime import datetime
+
+"""Base schema for the storage layer."""
+
+
+class StorageUser:
+    id: UUID
+
+
+class StorageUserCycle:
+    id: UUID
+    user_id: UUID
+    cycle_start_at: datetime
+    cycle_end_at: datetime
+
+
+class StorageLedger:
+    transaction_id: UUID
+    user_id: UUID
+    amount: int
+    timestamp: datetime
+    created_at: datetime
+    expire_at: datetime
