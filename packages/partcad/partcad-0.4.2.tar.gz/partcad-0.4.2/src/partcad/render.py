953b0211db17029c87281d7d5aee1ac3482307cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:13 +0000

    Add package partcad/0.4.2/partcad-0.4.2.tar.gz

---
+++ packages/partcad/partcad-0.4.2.tar.gz/partcad-0.4.2/src/partcad/render.py
@@ -0,0 +1,24 @@
+#
+# OpenVMP, 2023
+#
+# Author: Roman Kuzmenko
+# Created: 2023-12-22
+#
+# Licensed under Apache License, Version 2.0.
+#
+
+DEFAULT_RENDER_WIDTH = 1024
+DEFAULT_RENDER_HEIGHT = 1024
+
+DEFAULT_RENDER_SVG_OPTS = {
+    "width": DEFAULT_RENDER_WIDTH,
+    "height": DEFAULT_RENDER_HEIGHT,
+    "showAxes": False,
+    "projectionDir": [1, -1, 0.75],
+    "strokeWidth": 0.25,
+    "strokeColor": [64, 192, 64],
+    "hiddenColor": [32, 64, 32],
+    "showHidden": False,
+    # "marginLeft": 0,
+    # "marginTop": 0,
+}
