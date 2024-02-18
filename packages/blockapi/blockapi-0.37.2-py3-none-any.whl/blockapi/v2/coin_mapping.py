9a72788f1b5e00a232879e89fc9d3f930a9d9588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:13 +0000

    Add package blockapi/0.37.2/blockapi-0.37.2-py3-none-any.whl

---
+++ packages/blockapi/blockapi-0.37.2-py3-none-any.whl/blockapi/v2/coin_mapping.py
@@ -0,0 +1,14 @@
+from blockapi.v2.coins import COIN_DAI, COIN_ETH, COIN_PRIME, COIN_USDC, COIN_WETH
+from blockapi.v2.models import Blockchain, Coin
+
+OPENSEA_COINS: dict[str, Coin] = {
+    'ETH': COIN_ETH,
+    'USDC': COIN_USDC,
+    'PRIME': COIN_PRIME,
+    'DAI': COIN_DAI,
+}
+
+OPENSEA_CONTRACTS = {
+    ('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', Blockchain.ETHEREUM): COIN_WETH,
+    ('0x0000000000000000000000000000000000000000', Blockchain.ETHEREUM): COIN_ETH,
+}
