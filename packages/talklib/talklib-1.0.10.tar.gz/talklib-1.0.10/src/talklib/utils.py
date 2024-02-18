0548c2273ae914275c0db578b0dc886e9d3069c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:46 +0000

    Add package talklib/1.0.10/talklib-1.0.10.tar.gz

---
+++ packages/talklib/talklib-1.0.10.tar.gz/talklib-1.0.10/src/talklib/utils.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from email.message import EmailMessage
+import ffmpeg
+import logging
+from logging.handlers import SysLogHandler
+import os
+import smtplib
+
+from twilio.rest import Client
+
+from talklib.ev import EV
+
+def get_timestamp() -> str:
+    timestamp = datetime.now().strftime('%H:%M:%S on %d %b %Y')
+    return timestamp
+
+def clear_screen() -> None:
+    '''clears the terminal'''
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def print_to_screen_and_wait(message: str) -> None:
+    '''clear terminal and print message to screen.'''
+    clear_screen()
+    print(f'{message}\n')  # get user's attention!
+    input('(press enter to close this window)') # force user to acknowledge by closing window
+
+def today_is_weekday() -> bool:
+    '''crude mechanism for determining if today is a weekday.'''
+    today = datetime.now().strftime('%a')
+    weekend = ['Sat', 'Sun']
+    if today not in weekend:
+        return True
+    else:
+        return False
