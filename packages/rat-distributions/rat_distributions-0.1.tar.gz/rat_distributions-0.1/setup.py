2b7f345bd1c8ec4b230e825a028ccab2f72f9af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:15 +0000

    Add package rat-distributions/0.1/rat_distributions-0.1.tar.gz

---
+++ packages/rat-distributions/rat_distributions-0.1.tar.gz/rat_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rat_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['rat_distributions'],
+      author = 'Ratinesh Kumar',
+      author_email = 'a13888120@gmail.com',
+      zip_safe=False)
