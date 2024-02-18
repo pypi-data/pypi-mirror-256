8f6c1e1f30e9c51495919e5aa342682d9c133d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:00 +0000

    Add package cheminformatics-toolkit/0.1.7/cheminformatics_toolkit-0.1.7.tar.gz

---
+++ packages/cheminformatics-toolkit/cheminformatics_toolkit-0.1.7.tar.gz/cheminformatics_toolkit-0.1.7/cheminformatics_toolkit/global_data.py
@@ -0,0 +1,4 @@
+
+class global_data:
+    jobtypes = ['inptest', 'from_crest_to_pyadf', 'from_pyadf_to_rdkit', 'structural_analysis']
+    structural_parameters = ['distances', 'angles', 'dihedrals', 'rdfs']
---
+++ packages/cheminformatics-toolkit/cheminformatics_toolkit-0.1.7.tar.gz/cheminformatics_toolkit-0.1.7/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cheminformatics_toolkit']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cheminformatics-toolkit',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': '# About\n\n# Documentation\n\n# Setup\n\n# Testing\n\n# Quick start\n\n# Authors\n\n# Contributing\n',
+    'author': 'gosia olejniczak',
+    'author_email': 'gosia.olejniczak@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
