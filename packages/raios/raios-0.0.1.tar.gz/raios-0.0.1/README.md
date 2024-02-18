f9376dd2cb1d7ce83d8021bc283176964afa9932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:27:04 +0000

    Add package raios/0.0.1/raios-0.0.1.tar.gz

---
+++ packages/raios/raios-0.0.1.tar.gz/raios-0.0.1/README.md
@@ -0,0 +1,2 @@
+# RaiOS 
+### Robotic AI Operating System: a high-level Python framework focused on consumer-grade hardware.
---
+++ packages/raios/raios-0.0.1.tar.gz/raios-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='raios',
+    version='0.0.1',
+    packages=find_packages(),
+    license='BSD 2-Clause License',
+    long_description=open('README.md').read(),
+)
