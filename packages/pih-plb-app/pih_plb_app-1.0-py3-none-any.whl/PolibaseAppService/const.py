7078376ab1bf34e852e11f9bb49fa0b5e504677c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:00 +0000

    Add package pih-plb-app/1.0/pih_plb_app-1.0-py3-none-any.whl

---
+++ packages/pih-plb-app/pih_plb_app-1.0-py3-none-any.whl/PolibaseAppService/const.py
@@ -0,0 +1,21 @@
+import ipih
+
+from pih.collections.service import ServiceDescription
+from pih.consts.hosts import Hosts
+from pih.consts import UNKNOWN
+
+NAME: str = "PolibaseApp"
+
+VERSION: str = "1.0"
+
+HOST = Hosts.POLIBASE
+
+SD: ServiceDescription = ServiceDescription(
+    name=NAME,
+    description="Polibase Application service",
+    host=HOST.NAME,
+    pyton_executable_path=UNKNOWN,
+    run_from_system_account=True,
+    standalone_name="plb_app",
+    version=VERSION,
+)
