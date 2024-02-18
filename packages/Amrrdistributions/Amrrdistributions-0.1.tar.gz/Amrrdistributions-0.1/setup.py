5cf1506ec1f5621cd8edbd551ee33d33f235b26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:09:03 +0000

    Add package Amrrdistributions/0.1/Amrrdistributions-0.1.tar.gz

---
+++ packages/Amrrdistributions/Amrrdistributions-0.1.tar.gz/Amrrdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Amrrdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      long_description_content_type='text/markdown',
+
+      packages=['Amrrdistributions'],
+      zip_safe=False)
