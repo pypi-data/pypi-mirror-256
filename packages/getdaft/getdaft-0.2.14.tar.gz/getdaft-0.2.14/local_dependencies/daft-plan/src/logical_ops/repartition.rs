409cbe4f3c50563e516947b79f5329b268466ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:03:40 +0000

    Add package getdaft/0.2.14/getdaft-0.2.14.tar.gz

---
+++ packages/getdaft/getdaft-0.2.14.tar.gz/getdaft-0.2.14/local_dependencies/daft-plan/src/logical_ops/repartition.rs
@@ -0,0 +1,55 @@
+use std::sync::Arc;
+
+use common_error::{DaftError, DaftResult};
+use daft_dsl::Expr;
+use itertools::Itertools;
+
+use crate::{LogicalPlan, PartitionScheme};
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Repartition {
+    // Upstream node.
+    pub input: Arc<LogicalPlan>,
+    pub num_partitions: Option<usize>,
+    pub partition_by: Vec<Expr>,
+    pub scheme: PartitionScheme,
+}
+
+impl Repartition {
+    pub(crate) fn try_new(
+        input: Arc<LogicalPlan>,
+        num_partitions: Option<usize>,
+        partition_by: Vec<Expr>,
+        scheme: PartitionScheme,
+    ) -> DaftResult<Self> {
+        if matches!(scheme, PartitionScheme::Range) {
+            return Err(DaftError::ValueError(
+                "Repartitioning with the Range partition scheme is not supported.".to_string(),
+            ));
+        }
+        Ok(Self {
+            input,
+            num_partitions,
+            partition_by,
+            scheme,
+        })
+    }
+
+    pub fn multiline_display(&self) -> Vec<String> {
+        let mut res = vec![];
+        res.push(format!("Repartition: Scheme = {:?}", self.scheme));
+        res.push(format!(
+            "Number of partitions = {}",
+            self.num_partitions
+                .map(|n| n.to_string())
+                .unwrap_or("Unknown".to_string())
+        ));
+        if !self.partition_by.is_empty() {
+            res.push(format!(
+                "Partition by = {}",
+                self.partition_by.iter().map(|e| e.to_string()).join(", ")
+            ));
+        }
+        res
+    }
+}
---
+++ packages/getdaft/getdaft-0.2.14.tar.gz/getdaft-0.2.14/local_dependencies/daft-plan/src/partitioning.rs
@@ -0,0 +1,127 @@
+use std::fmt::{Display, Formatter};
+
+use daft_dsl::Expr;
+
+use daft_core::impl_bincode_py_state_serialization;
+use itertools::Itertools;
+use serde::{Deserialize, Serialize};
+
+#[cfg(feature = "python")]
+use {
+    daft_dsl::python::PyExpr,
+    pyo3::{
+        pyclass, pyclass::CompareOp, pymethods, types::PyBytes, PyObject, PyResult, PyTypeInfo,
+        Python, ToPyObject,
+    },
+};
+
+/// Partition scheme for Daft DataFrame.
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[cfg_attr(feature = "python", pyclass(module = "daft.daft"))]
+pub enum PartitionScheme {
+    Range,
+    Hash,
+    Random,
+    Unknown,
+}
+
+impl_bincode_py_state_serialization!(PartitionScheme);
+
+impl Display for PartitionScheme {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        // Leverage Debug trait implementation, which will already return the enum variant as a string.
+        write!(f, "{:?}", self)
+    }
+}
+
+/// Partition specification: scheme, number of partitions, partition column.
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[cfg_attr(feature = "python", pyclass(module = "daft.daft"))]
+pub struct PartitionSpec {
+    pub scheme: PartitionScheme,
+    pub num_partitions: usize,
+    pub by: Option<Vec<Expr>>,
+}
+
+impl PartitionSpec {
+    pub fn new_internal(
+        scheme: PartitionScheme,
+        num_partitions: usize,
+        by: Option<Vec<Expr>>,
+    ) -> Self {
+        Self {
+            scheme,
+            num_partitions,
+            by,
+        }
+    }
+
+    pub fn multiline_display(&self) -> Vec<String> {
+        let mut res = vec![];
+        res.push(format!("Scheme = {}", self.scheme));
+        res.push(format!("Num partitions = {}", self.num_partitions));
+        if let Some(ref by) = self.by {
+            res.push(format!(
+                "By = {}",
+                by.iter().map(|e| e.to_string()).join(", ")
+            ));
+        }
+        res
+    }
+}
+
+impl Default for PartitionSpec {
+    fn default() -> Self {
+        Self {
+            scheme: PartitionScheme::Unknown,
+            num_partitions: 1,
+            by: None,
+        }
+    }
+}
+
+#[cfg(feature = "python")]
+#[pymethods]
+impl PartitionSpec {
+    #[new]
+    #[pyo3(signature = (scheme=PartitionScheme::Unknown, num_partitions=0usize, by=None))]
+    pub fn new(scheme: PartitionScheme, num_partitions: usize, by: Option<Vec<PyExpr>>) -> Self {
+        Self::new_internal(
+            scheme,
+            num_partitions,
+            by.map(|v| v.iter().map(|e| e.clone().into()).collect()),
+        )
+    }
+
+    #[getter]
+    pub fn get_scheme(&self) -> PyResult<PartitionScheme> {
+        Ok(self.scheme.clone())
+    }
+
+    #[getter]
+    pub fn get_num_partitions(&self) -> PyResult<i64> {
+        Ok(self.num_partitions as i64)
+    }
+
+    #[getter]
+    pub fn get_by(&self) -> PyResult<Option<Vec<PyExpr>>> {
+        Ok(self
+            .by
+            .as_ref()
+            .map(|v| v.iter().map(|e| e.clone().into()).collect()))
+    }
+
+    fn __richcmp__(&self, other: &Self, op: CompareOp) -> bool {
+        match op {
+            CompareOp::Eq => self == other,
+            CompareOp::Ne => !self.__richcmp__(other, CompareOp::Eq),
+            _ => unimplemented!("not implemented"),
+        }
+    }
+
+    pub fn __str__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self))
+    }
+}
+
+impl_bincode_py_state_serialization!(PartitionSpec);
