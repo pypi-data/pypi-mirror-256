f964905b028c130ce926d0033fd610dcbc089075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:26 +0000

    Add package nguyenpanda/0.1.0/nguyenpanda-0.1.0-py3-none-any.whl

---
+++ packages/nguyenpanda/nguyenpanda-0.1.0-py3-none-any.whl/nguyenpanda/__init__.py
@@ -0,0 +1,27 @@
+from .butterfly import *
+from .owl import *
+from .swan import *
+
+__all__ = (
+    'butterfly',
+    'owl',
+    'swan',
+)
+
+__version__ = '0.1.0'
+
+__title__ = 'nguyenpanda'
+
+__description__ = '''
+\'nguyenpanda\' library contains several package named as animals 
+which symbolizes:
+    - butterfly: random packages
+    - owl: mathematical packages
+    - swan: beautiful packages
+'''
+
+__url__ = 'https://github.com/nguyenpanda'
+
+__author__ = 'Tuong Nguyen Ha'
+
+__email__ = 'nguyen.hatuong0107@hcmut.edu.vn'
