49c0c22da196374693692afc7bed5b1315d4e3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 09:03:51 +0000

    Add package hab/0.35.0/hab-0.35.0.tar.gz

---
+++ packages/hab/hab-0.35.0.tar.gz/hab-0.35.0/bin/hab.bat
@@ -0,0 +1,52 @@
+:: Hab needs to modify the existing terminal's environment in some cases.
+:: To do this we can't use a setuptools exe and must use a script the terminal
+:: supports. This calls the python module cli passing the arguments to it and
+:: ends up calling the script file that code generates if required.
+
+@ECHO OFF
+
+:: Generate a unique temp file name
+:uniqLoop
+set "temp_directory=%tmp%\hab~%RANDOM%"
+if exist "%temp_directory%" goto :uniqLoop
+
+:: Create the launch and config filenames we will end up using
+mkdir %temp_directory%
+set "temp_launch_file=%temp_directory%\hab_launch.bat"
+set "temp_config_file=%temp_directory%\hab_config.bat"
+
+:: Calculate the command to run python with
+SETLOCAL ENABLEEXTENSIONS
+IF DEFINED HAB_PYTHON (
+    :: If HAB_PYTHON is specified, use it explicitly
+    set py_exe=%HAB_PYTHON%
+) ELSE IF DEFINED VIRTUAL_ENV (
+    :: We are inside a virtualenv, so just use the python command
+    set py_exe=python
+) ELSE (
+    :: Use system defined generic python call
+    set "py_exe=py -3"
+)
+
+:: Call our worker python process that may write the temp filename
+%py_exe% -m hab --script-dir "%temp_directory%" --script-ext ".bat" %*
+ENDLOCAL
+
+:: Run the launch or config script if it was created on disk
+if exist %temp_launch_file% (
+    call %temp_launch_file%
+) else (
+    if exist %temp_config_file% (
+        call %temp_config_file%
+    )
+)
+
+:: the config scripts turn echo back on, Make sure to prefix future commands
+:: with @ to prevent printing them to the console
+
+:: Remove the temp directory and its contents
+@RMDIR /S /Q %temp_directory%
+
+:: Ensure the errorlevel is reported to the calling process. This is needed
+:: when calling hab via subprocess/QtCore.QProcess calls to receive errorlevel
+@exit /b %ERRORLEVEL%
