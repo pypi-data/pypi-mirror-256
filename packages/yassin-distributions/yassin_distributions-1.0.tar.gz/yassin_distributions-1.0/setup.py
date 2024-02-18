94598bd6160ad43ba5b94e87f41fa7cb7a798137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:20 +0000

    Add package yassin-distributions/1.0/yassin_distributions-1.0.tar.gz

---
+++ packages/yassin-distributions/yassin_distributions-1.0.tar.gz/yassin_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='yassin_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['yassin_distributions'],
+      author = 'Yassin elsaman',
+      author_email = 'yassinelsaman025@gmail.com',
+      zip_safe=False)
