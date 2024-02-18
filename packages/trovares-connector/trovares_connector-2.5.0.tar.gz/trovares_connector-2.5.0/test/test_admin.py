482f57cac715dd546ae8e3891854db6203e7371f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:20 +0000

    Add package trovares-connector/2.5.0/trovares_connector-2.5.0.tar.gz

---
+++ packages/trovares-connector/trovares_connector-2.5.0.tar.gz/trovares_connector-2.5.0/test/test_admin.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*- --------------------------------------------------===#
+#
+#  Copyright 2020-2022 Trovares Inc.
+#
+#===----------------------------------------------------------------------===#
+
+import unittest
+
+class TestLabelset(unittest.TestCase):
+  def setup_method(self, method):
+    pass
+
+  def teardown_method(self, method):
+    pass
+
+  def test_compute_label_set(self):
+      pass
