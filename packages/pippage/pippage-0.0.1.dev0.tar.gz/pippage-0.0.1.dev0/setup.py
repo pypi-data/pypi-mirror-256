03dec1fe6fdfd7b653e0f769fc527267bc528307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:49 +0000

    Add package pippage/0.0.1.dev0/pippage-0.0.1.dev0.tar.gz

---
+++ packages/pippage/pippage-0.0.1.dev0.tar.gz/pippage-0.0.1.dev0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="pippage",
+    version="0.0.1.dev0",
+    author="Joshua Cannon",
+    author_email="joshdcannon@gmail.com",
+    description="(Coming Soon!)",
+    url="https://github.com/thejcannon/pippage",
+    packages=["pippage"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
