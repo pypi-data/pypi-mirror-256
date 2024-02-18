26a0603670abba7b743712d4f96a257ef8014b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:17 +0000

    Add package dkyLIB/1.0/dkyLIB-1.0.tar.gz

---
+++ packages/dkyLIB/dkyLIB-1.0.tar.gz/dkyLIB-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='dkyLIB',
+    version='1.0',
+    description='dky lib 1.0',
+    author='dky',
+    author_email='v2.0@qq.com',
+    packages=['dkyLIB']
+)
