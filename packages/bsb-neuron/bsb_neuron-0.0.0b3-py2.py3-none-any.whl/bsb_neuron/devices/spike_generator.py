ef4cb1a928821c98ec26fac0c477160b969c7ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:18 +0000

    Add package bsb-neuron/0.0.0b3/bsb_neuron-0.0.0b3-py2.py3-none-any.whl

---
+++ packages/bsb-neuron/bsb_neuron-0.0.0b3-py2.py3-none-any.whl/bsb_neuron/devices/spike_generator.py
@@ -0,0 +1,22 @@
+from bsb import config
+from bsb.config import types
+from bsb.simulation.targetting import LocationTargetting
+
+from ..device import NeuronDevice
+
+
+@config.node
+class SpikeGenerator(NeuronDevice, classmap_entry="spike_generator"):
+    locations = config.attr(type=LocationTargetting, default={"strategy": "soma"})
+    synapses = config.list()
+    parameters = config.catch_all(type=types.any_())
+
+    def implement(self, adapter, simulation, simdata):
+        for model, pop in self.targetting.get_targets(
+            adapter, simulation, simdata
+        ).items():
+            for target in pop:
+                for location in self.locations.get_locations(target):
+                    for synapse in location.section.synapses:
+                        if not self.synapses or synapse.synapse_name in self.synapses:
+                            synapse.stimulate(**self.parameters)
