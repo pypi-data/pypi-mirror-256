d1b5c03e021494953a8c18d6b1284aeb446481f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:13 +0000

    Add package ipih/1.52/ipih-1.52.tar.gz

---
+++ packages/ipih/ipih-1.52.tar.gz/ipih-1.52/ipih_build.py
@@ -0,0 +1,72 @@
+import ipih
+
+from ipih import NAME, VERSION
+from build_tools import build
+from pih import A
+
+# for facade
+# setuptools
+# prettytable
+# colorama
+# protobuf
+# grpcio (six)
+# pyperclip
+# win32com -> pywin32 (!!!)
+
+# MC
+# pywhatkit
+
+# for orion
+# myssql
+
+# for log
+# telegram_send
+
+# for dc2
+# docs:
+# mailmerge (pip install docx-mailmerge)
+# xlsxwriter
+# xlrd
+# python-barcode
+# Pillow
+# ad:
+# pyad
+# pywin32 (!!!)
+# wmi
+# transliterate
+
+# for data storage
+# pysos
+# lmdbm
+
+# for printer (dc2)
+# pysnmp
+
+# for polibase
+# cx_Oracle
+
+# for mobile helper
+# paramiko
+
+#########################################################################################################
+
+def build_main() -> None:
+    build(
+        NAME,
+        "Smart import for PIH module",
+        VERSION,
+        None,
+        [
+            "prettytable",
+            "colorama",
+            "grpcio",
+            "protobuf",
+            "requests",
+            "transliterate",
+            "psutil",
+            "setuptools",
+        ],
+    )
+
+if __name__ == "__main__":
+    build_main()
