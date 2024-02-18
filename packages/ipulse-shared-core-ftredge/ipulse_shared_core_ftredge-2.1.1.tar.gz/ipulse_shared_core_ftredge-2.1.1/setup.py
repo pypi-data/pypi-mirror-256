0bc64480f29d493b324d8002ba26e472594e55dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:04:29 +0000

    Add package ipulse-shared-core-ftredge/2.1.1/ipulse_shared_core_ftredge-2.1.1.tar.gz

---
+++ packages/ipulse-shared-core-ftredge/ipulse_shared_core_ftredge-2.1.1.tar.gz/ipulse_shared_core_ftredge-2.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipulse_shared_core_ftredge',
+    version='2.1.1',
+    package_dir={'': 'src'},  # Specify the source directory
+    packages=find_packages(where='src'),  # Look for packages in 'src'
+    install_requires=[
+        # List your dependencies here
+        'pydantic[email]',
+        'uuid',
+        'python-dateutil',
+        'pytest',
+    ],
+    author='Russlan Ramdowar',
+    description='Shared models for the Pulse platform project. Using AI for financial advisory and investment management.',
+    url='https://github.com/TheFutureEdge/ipulse_shared_core',
+)
---
+++ packages/ipulse-shared-core-ftredge/ipulse_shared_core_ftredge-2.1.1.tar.gz/ipulse_shared_core_ftredge-2.1.1/src/ipulse_shared_core_ftredge/models/__init__.py
@@ -0,0 +1,8 @@
+from .user_profile import UserProfile
+from .user_profile_update import UserProfileUpdate
+from .organisation import Organisation
+from .user_auth import UserAuth
+
+
+
+
