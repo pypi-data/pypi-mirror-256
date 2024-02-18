0c420b24c6b26f159314088db49fa8eb0887687c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:11 +0000

    Add package target365-sdk/1.8.4/target365_sdk-1.8.4-py3-none-any.whl

---
+++ packages/target365-sdk/target365_sdk-1.8.4-py3-none-any.whl/target365_sdk/models/strex_transaction.py
@@ -0,0 +1,37 @@
+from .model import Model
+
+class StrexTransaction(Model):
+
+    def _accepted_params(self):
+        return [
+            'transactionId',
+            'sessionId',
+            'correlationId',
+            'shortNumber',
+            'recipient',
+            'content',
+            'oneTimePassword',
+            'deliveryMode',
+            'statusCode',
+            'detailedStatusCode',
+            'statusDescription',
+            'smscTransactionId',
+            'created',
+            'lastModified',
+            'merchantId',
+            'serviceCode',
+            'businessModel',
+            'preAuthServiceId',
+            'preAuthServiceDescription',
+            'age',
+            'isRestricted',
+            'smsConfirmation',
+            'invoiceText',
+            'price',
+            'timeout',
+            'tags',
+            'properties',
+            'billed',
+            'resultCode',
+            'resultDescription',
+        ]
