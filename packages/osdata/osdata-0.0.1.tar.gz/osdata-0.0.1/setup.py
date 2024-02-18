8a208b79f77b00566cdb74cd91650b62497e6a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:03:58 +0000

    Add package osdata/0.0.1/osdata-0.0.1.tar.gz

---
+++ packages/osdata/osdata-0.0.1.tar.gz/osdata-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="osdata",
+    version="0.0.1",
+    author='Osyris Technologies, Inc.',
+    packages=['osdata'],
+    install_requires=[
+        # List your project dependencies here.
+    ],
+    # Additional metadata about your package.
+    author_email="dev@osyris.io",
+    description="A Universal API for discovering and managing research datasets",
+    keywords=["osyris", "data", "dataset", "ai"],
+)
