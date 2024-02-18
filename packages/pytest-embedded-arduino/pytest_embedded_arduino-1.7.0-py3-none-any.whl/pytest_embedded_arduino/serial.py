b7f84b4d94d83e40f970c425b14ff949c8d60d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:47 +0000

    Add package pytest-embedded-arduino/1.7.0/pytest_embedded_arduino-1.7.0-py3-none-any.whl

---
+++ packages/pytest-embedded-arduino/pytest_embedded_arduino-1.7.0-py3-none-any.whl/pytest_embedded_arduino/serial.py
@@ -0,0 +1,72 @@
+import logging
+from typing import Optional
+
+import esptool
+from pytest_embedded_serial_esp.serial import EspSerial, EsptoolArgs
+
+from .app import ArduinoApp
+
+
+class ArduinoSerial(EspSerial):
+    """
+    Arduino serial Dut class
+
+    Auto flash the app while starting test.
+    """
+
+    SUGGEST_FLASH_BAUDRATE = 921600
+
+    def __init__(
+        self,
+        app: ArduinoApp,
+        target: Optional[str] = None,
+        **kwargs,
+    ) -> None:
+        self.app = app
+        super().__init__(
+            target=target or self.app.target,
+            **kwargs,
+        )
+
+    def _start(self):
+        if self.skip_autoflash:
+            logging.info('Skipping auto flash...')
+            super()._start()
+        else:
+            self.flash()
+
+    @EspSerial.use_esptool()
+    def flash(self) -> None:
+        """
+        Flash the binary files to the board.
+        """
+        flash_files = [
+            (offset, open(path, 'rb')) for (offset, path, encrypted) in self.app.flash_files if not encrypted
+        ]
+
+        default_kwargs = {
+            'addr_filename': flash_files,
+            'encrypt_files': None,
+            'no_stub': False,
+            'compress': True,
+            'verify': False,
+            'ignore_flash_encryption_efuse_setting': False,
+            'erase_all': False,
+            'encrypt': False,
+            'force': False,
+            'chip': self.app.target,
+        }
+
+        default_kwargs.update(self.app.flash_settings[self.app.target])
+        flash_args = EsptoolArgs(**default_kwargs)
+
+        try:
+            self.stub.change_baud(self.esptool_baud)
+            esptool.detect_flash_size(self.stub, flash_args)
+            esptool.write_flash(self.stub, flash_args)
+            self.stub.change_baud(self.baud)
+        except Exception:
+            raise
+        finally:
+            for _, f in flash_files:
+                f.close()
