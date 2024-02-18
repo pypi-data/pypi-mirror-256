319445e486b1e9d6d0a53eea68626e8c135db721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:42 +0000

    Add package py3dep/0.16.2/py3dep-0.16.2.tar.gz

---
+++ packages/py3dep/py3dep-0.16.2.tar.gz/py3dep-0.16.2/conftest.py
@@ -0,0 +1,18 @@
+"""Configuration for pytest."""
+
+import pytest
+from click.testing import CliRunner
+
+
+@pytest.fixture()
+def runner():
+    """Return a CliRunner."""
+    return CliRunner()
+
+
+@pytest.fixture(autouse=True)
+def _add_standard_imports(doctest_namespace):
+    """Add py3dep namespace for doctest."""
+    import py3dep
+
+    doctest_namespace["py3dep"] = py3dep
