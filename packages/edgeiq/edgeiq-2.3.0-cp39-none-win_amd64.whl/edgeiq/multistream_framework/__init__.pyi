7cec2b4da9a77e258d58ee51b09d56e3dd66b01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:48 +0000

    Add package edgeiq/2.3.0/edgeiq-2.3.0-cp39-none-win_amd64.whl

---
+++ packages/edgeiq/edgeiq-2.3.0-cp39-none-win_amd64.whl/edgeiq/multistream_framework/__init__.pyi
@@ -0,0 +1,4 @@
+from .framework import MultiprocessFramework as MultiprocessFramework
+from .utils import MultiprocessAppInterface as MultiprocessAppInterface
+
+__all__ = ['MultiprocessFramework', 'MultiprocessAppInterface']
