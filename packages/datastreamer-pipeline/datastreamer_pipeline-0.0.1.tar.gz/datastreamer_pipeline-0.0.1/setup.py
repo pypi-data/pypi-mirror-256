2924753d440f9bc754659617d8c91f3b1a7e8220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:52 +0000

    Add package datastreamer-pipeline/0.0.1/datastreamer_pipeline-0.0.1.tar.gz

---
+++ packages/datastreamer-pipeline/datastreamer_pipeline-0.0.1.tar.gz/datastreamer_pipeline-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='datastreamer_pipeline',
+   version='0.0.1',
+   author='Datastreamer',
+   author_email='security@datastreamer.io',
+   url='https://datastreamer.io',
+   readme="README.md",
+   long_description="""This is a security placeholder package.
+If you want to claim this name for legitimate purposes,
+please contact us at security@datastreamer.io""",
+   long_description_content_type='text/markdown',
+   description='A package to prevent Dependency Confusion attacks',
+   )
