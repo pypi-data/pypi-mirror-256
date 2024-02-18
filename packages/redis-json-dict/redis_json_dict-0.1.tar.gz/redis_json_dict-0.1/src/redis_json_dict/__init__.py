eb3dcc5742d7417632377fce7de698ab22f4a3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:08 +0000

    Add package redis-json-dict/0.1/redis_json_dict-0.1.tar.gz

---
+++ packages/redis-json-dict/redis_json_dict-0.1.tar.gz/redis_json_dict-0.1/src/redis_json_dict/__init__.py
@@ -0,0 +1,13 @@
+"""
+Copyright (c) 2024 Brookhaven National Laboratory. All rights reserved.
+
+redis-json-dict: A Redis-backed persistent Python dictionary
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
+
+from redis_json_dict.redis_json_dict import RedisJSONDict  # noqa: F401
