ac9263170a1fc0a70a0dbc3e2b0717b2000aad6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:32 +0000

    Add package tsshapelet/2.0.3/tsshapelet-2.0.3.tar.gz

---
+++ packages/tsshapelet/tsshapelet-2.0.3.tar.gz/tsshapelet-2.0.3/src/tsshapelet/__init__.py
@@ -0,0 +1,5 @@
+from .shapelet import Shapelet
+from .utils import barycenters, utils, metrics
+from .features import statistical_features, time_series_features
+from .comparator import query, pairwise_argmin
+from .shapelet_dtw import dtw, dtw_matrix
