97c325c9142d24af5c5d4bd4214bc303750f9914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:05:06 +0000

    Add package mo-sqlite/2.545.24046/mo_sqlite-2.545.24046-py3-none-any.whl

---
+++ packages/mo-sqlite/mo_sqlite-2.545.24046-py3-none-any.whl/mo_sqlite/expressions/__init__.py
@@ -0,0 +1,22 @@
+from mo_sqlite.expressions._utils import SQLang
+from mo_sqlite.expressions.coalesce_op import CoalesceOp
+from mo_sqlite.expressions.sql_alias_op import SqlAliasOp
+from mo_sqlite.expressions.sql_case_op import CaseOp
+from mo_sqlite.expressions.sql_eq_op import SqlEqOp
+from mo_sqlite.expressions.sql_group_by_op import SqlGroupByOp
+from mo_sqlite.expressions.sql_gt_op import SqlGtOp
+from mo_sqlite.expressions.sql_gte_op import SqlGteOp
+from mo_sqlite.expressions.sql_in_op import SqlInOp
+from mo_sqlite.expressions.sql_inner_join_op import SqlInnerJoinOp
+from mo_sqlite.expressions.sql_is_null_op import SqlIsNullOp
+from mo_sqlite.expressions.sql_limit_op import SqlLimitOp
+from mo_sqlite.expressions.sql_literal import SqlLiteral
+from mo_sqlite.expressions.sql_lt_op import SqlLtOp
+from mo_sqlite.expressions.sql_lte_op import SqlLteOp
+from mo_sqlite.expressions.sql_order_by_op import SqlOrderByOp
+from mo_sqlite.expressions.sql_script import SqlScript
+from mo_sqlite.expressions.sql_select_op import SqlSelectOp
+from mo_sqlite.expressions.sql_select_op import SqlSelectOp
+from mo_sqlite.expressions.sql_variable import SqlVariable
+
+SQLang.register_ops(vars())
