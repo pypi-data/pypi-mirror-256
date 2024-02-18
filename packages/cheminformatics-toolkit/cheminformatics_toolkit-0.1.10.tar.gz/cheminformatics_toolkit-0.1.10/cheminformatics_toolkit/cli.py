ed40fd1b1f48c242749764e1148fb886e221f379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:34 +0000

    Add package cheminformatics-toolkit/0.1.10/cheminformatics_toolkit-0.1.10.tar.gz

---
+++ packages/cheminformatics-toolkit/cheminformatics_toolkit-0.1.10.tar.gz/cheminformatics_toolkit-0.1.10/cheminformatics_toolkit/cli.py
@@ -0,0 +1,321 @@
+import os
+import sys
+import argparse
+import pprint
+from pathlib import Path
+from .global_data import *
+
+class input_data:
+
+    def __init__(self, args_list):
+
+        self.args_list = args_list[1:]
+        self.options   = {}
+
+        if args_list[0]:
+            self.runinp_dir = Path(args_list[0]).resolve().parent 
+        else:
+            self.runinp_dir = os.getcwd()
+
+        self.jobtypes = global_data.jobtypes
+        self.structural_parameters = global_data.structural_parameters
+
+
+    def parse_options(self):
+
+        parser = argparse.ArgumentParser()
+
+        required_args = parser.add_argument_group('required arguments')
+
+        required_args.add_argument('--job_type',
+                                   dest='job_type',
+                                   action='store',
+                                   metavar='writeme',
+                                   choices=self.jobtypes,
+                                   required=True,
+                                   help='''
+                                        help msg
+                                        ''')
+
+        required_args.add_argument('--work_pathdir',
+                                   dest='work_pathdir',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=True,
+                                   help='''
+                                        help msg
+                                        ''')
+
+        optional_args = parser.add_argument_group('optional arguments')
+
+
+        optional_args.add_argument('--structural_parameters',
+                                   dest='structural_parameters',
+                                   action='append',
+                                   metavar='writeme',
+                                   choices=self.structural_parameters,
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--username',
+                                   dest='username',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--work_pathsubdir',
+                                   dest='work_pathsubdir',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+ 
+        optional_args.add_argument('--inp_xyz_fullpath',
+                                   dest='inp_xyz_fullpath',
+                                   action='append',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+ 
+        optional_args.add_argument('--inp_sdf_dirpath',
+                                   dest='inp_sdf_dirpath',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--out_filename',
+                                   dest='out_filename',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--molname',
+                                   dest='molname',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--molcharge',
+                                   dest='molcharge',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--qm_runscript_template_fullpath',
+                                   dest='qm_runscript_template_fullpath',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--qm_input_template_fullpath',
+                                   dest='qm_input_template_fullpath',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+        optional_args.add_argument('--cluster_ntasks',
+                                   dest='cluster_ntasks',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--cluster_timeh',
+                                   dest='cluster_timeh',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--cluster_part',
+                                   dest='cluster_part',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--hamiltonians',
+                                   dest='hamiltonians',
+                                   action='append',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--dftfuns',
+                                   dest='dftfuns',
+                                   action='append',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--basis_sets',
+                                   dest='basis_sets',
+                                   action='append',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        optional_args.add_argument('--nr_atoms_in_active_sub',
+                                   dest='nr_atoms_in_active_sub',
+                                   action='store',
+                                   metavar='writeme',
+                                   required=False,
+                                   help='''
+                                        help msg
+                                        ''')
+
+
+        args = parser.parse_args(self.args_list)
+        self.options = vars(args)
+
+
+    def print_options(self):
+
+        if self.options == {}:
+            self.parse_options()
+
+        print("Input options:")
+        print(self.options)
+
+
+    def process_options(self):
+
+        if self.options == {}:
+            self.parse_options()
+
+        # resolve paths:
+
+        runinp_dir = Path(self.runinp_dir).resolve().absolute()   # where the ctk job input is 
+        self.options['runinp_dir']   = runinp_dir
+
+        work_dir   = Path(runinp_dir, Path(self.options['work_pathdir'])).resolve().absolute() # workdir (e.g., scratch); can be relative to runinp_dir
+        self.options['work_pathdir'] = work_dir
+
+        if self.options['qm_input_template_fullpath'] is not None:
+            qm_tmpl_inp = Path(runinp_dir, Path(self.options['qm_input_template_fullpath'])).resolve().absolute()
+            self.options['qm_input_template_fullpath'] = qm_tmpl_inp
+        if self.options['qm_runscript_template_fullpath'] is not None:
+            qm_tmpl_run = Path(runinp_dir, Path(self.options['qm_runscript_template_fullpath'])).resolve().absolute()
+            self.options['qm_runscript_template_fullpath'] = qm_tmpl_run
+
+        if self.options['out_filename'] is not None:
+            outfile = Path(work_dir, Path(self.options['out_filename']).resolve()).absolute()
+        else:
+            outfile = Path(work_dir, 'output.txt').absolute()
+        self.options['outfile'] = outfile
+
+        logfile = Path(work_dir, 'log').absolute()
+        self.options['logfile'] = logfile
+
+        if  self.options['inp_xyz_fullpath'] is not None:
+            inpxyzf = [Path(runinp_dir, Path(x)).resolve().absolute() for x in self.options['inp_xyz_fullpath']]
+            self.options['inpxyzf'] = inpxyzf
+        else:
+            self.options['inpxyzf'] = None
+
+        if  self.options['inp_sdf_dirpath'] is not None:
+            inpsdfdir = Path(runinp_dir, Path(self.options['inp_sdf_dirpath'])).resolve().absolute()
+            self.options['inpsdfdir'] = inpsdfdir
+        else:
+            self.options['inpsdfdir'] = None
+
+
+
+
+def read_input(finp=None, verbose=False):
+
+    """
+    read input options, either from a command line or from a file
+    """
+
+    args = []
+
+    if finp is None:
+        try:
+            args = sys.argv[1:]
+        except:
+            sys.exit(1)
+        if verbose:
+            msg1 = 'Arguments for {} are read from command line'.format(sys.argv[0])
+            msg2 = '\n'.join(x for x in args[1:])
+            print(msg1+'\n'+msg2)
+        finp_path = None
+    else:
+        finp_path = Path(finp)
+        with open(finp_path, 'r') as f:
+            args = [line.strip() for line in f if line[0] != '#' and line != '\n']
+        if verbose:
+            msg1 = 'Arguments for {} are read from file {}'.format(sys.argv[0], finp_path)
+            msg2 = '\n'.join(x for x in args[1:])
+            print(msg1+'\n'+msg2)
+
+    args = [finp_path] + args
+            
+    return args
+
+
+
+if __name__ == '__main__':
+    args = read_input()
+    data = input_data(args)
+    data.parse_options()
+    data.process_options()
+
+
+
+
---
+++ packages/cheminformatics-toolkit/cheminformatics_toolkit-0.1.10.tar.gz/cheminformatics_toolkit-0.1.10/cheminformatics_toolkit/utils_crest.py
@@ -0,0 +1,227 @@
+import os
+from pathlib import Path
+import glob
+import shutil
+import argparse
+import numpy as np
+import pandas as pd
+
+
+class crest_analysis():
+
+    def __init__(self, options):
+
+        self.options = options
+
+        self.rundir      = self.options['runinp_dir']
+        self.workdir     = self.options['work_pathdir']
+        if self.options['work_pathsubdir'] is None:
+            if 'pyadf' in self.options['job_type']:
+                self.worksubdir     = 'qm_calcs/pyadf'
+            else:
+                self.worksubdir     = 'qm_calcs'
+        else:
+            self.worksubdir     = self.options['work_pathsubdir']
+
+        self.out_filepath  = self.options['outfile']
+        self.log_filepath  = self.options['logfile']
+
+        self.inpxyzf  = self.options['inpxyzf']
+        self.inpmoln  = self.options['molname']
+        self.nr_parsed_structures = 0
+
+        self.qm_tmpl_inp = self.options['qm_input_template_fullpath']
+        self.qm_tmpl_run = self.options['qm_runscript_template_fullpath']
+         
+        if self.options['nr_atoms_in_active_sub'] is not None:
+            self.nr_atoms_in_active = int(self.options['nr_atoms_in_active_sub'])
+        else:
+            self.nr_atoms_in_active = 0
+
+    def ignore_files(self, dirname, files):
+        return [f for f in files if os.path.isfile(os.path.join(dirname, f))]
+
+
+    def get_single_struc(self, finp):
+        data = {}
+        with open(finp, 'r') as f:
+            lines = f.readlines()
+            nr_struct = 0
+            new_struct_line = 0
+            nr_at = int(lines[0])
+            for i, line in enumerate(lines):
+                if new_struct_line < len(lines):
+                    #nr_at = int(lines[new_struct_line])
+                    energy = lines[new_struct_line+1].strip()
+                    coords = [x.strip() for x in lines[new_struct_line+2:new_struct_line+2+nr_at]]
+                    nr_struct += 1
+                    data[nr_struct] = [str(nr_at)] + [energy] + [x for x in coords]
+                    new_struct_line = new_struct_line + (2 + nr_at)
+        return data
+
+
+
+    def parse_structures(self, to_subsystems=False):
+        fileout_dir= Path(self.workdir, 'tmpdir').absolute()
+        fileout_dir.mkdir(parents=True, exist_ok=True)
+        output_xyz_files = []
+        for fxyz in self.inpxyzf:
+            data = self.get_single_struc(str(Path(fxyz).resolve()))
+            for k, v in data.items():
+                mol_name = self.inpmoln+'_struc_'+str(k)
+                fileout  = Path(fileout_dir, mol_name+'.xyz').absolute()
+                output_xyz_files.append(fileout)
+                with open(str(fileout), 'w') as fout:
+                    for l in v:
+                        fout.write('{}\n'.format(l))
+                if to_subsystems == True:
+                    #warning - error-prone:
+                    sub1f = Path(fileout_dir, mol_name+'_sub1.xyz').absolute()
+                    sub2f = Path(fileout_dir, mol_name+'_sub2.xyz').absolute()
+                    with open(str(sub1f), 'w') as fout:
+                        fout.write('{}\n'.format(self.nr_atoms_in_active))
+                        fout.write('sub1\n')
+                        for l in v[2:self.nr_atoms_in_active-1+3]:
+                            fout.write('{}\n'.format(l))
+                    with open(str(sub2f), 'w') as fout:
+                        fout.write('{}\n'.format(int(v[0])-self.nr_atoms_in_active))
+                        fout.write('sub2\n')
+                        for l in v[2+self.nr_atoms_in_active:]:
+                            fout.write('{}\n'.format(l))
+        return output_xyz_files
+
+
+    def cleanup(self, f):
+        print('warning: removing dir: ', f)
+        shutil.rmtree(f) 
+
+    def prep_for_pyadf(self, xyzfiles=None):
+        if xyzfiles is None:
+            if self.nr_atoms_in_active == 0:
+                xyzfiles = self.parse_structures(to_subsystems=False)
+            else:
+                xyzfiles = self.parse_structures(to_subsystems=True)
+        self.setup_qm_dirstructure(xyzfiles)
+        self.cleanup(Path(self.workdir, 'tmpdir').absolute())
+
+
+    def setup_qm_dirstructure(self, xyzfiles):
+        hs = self.options['hamiltonians']
+        ds = self.options['dftfuns']
+        bs = self.options['basis_sets']
+        for mf in xyzfiles:
+            molname = mf.stem
+            for h in hs:
+                h = h.replace(" ", "")
+                for d in ds:
+                    d = d.replace(" ", "")
+                    for b in bs:
+                        b = b.replace(" ", "")
+                        if 'pyadf' in self.options['job_type']:
+                            subdirname=self.options['username']+'-storage/'+self.worksubdir+'/' + molname + '/' + h + '/' + d + '/' + b
+                            qm_dir = Path(self.workdir, self.worksubdir, molname, h, d, b).absolute()
+                            coor_dir = Path(self.workdir, self.worksubdir, molname, h, d, b, 'coordinates').absolute()
+                            Path(qm_dir).mkdir(parents=True, exist_ok=True)
+                            Path(coor_dir).mkdir(parents=True, exist_ok=True)
+                            shutil.copy(mf, coor_dir)
+                            sub1f=str(mf).replace('.xyz', '_sub1.xyz')
+                            sub2f=str(mf).replace('.xyz', '_sub2.xyz')
+                            if Path(sub1f).exists(): 
+                                shutil.copy(Path(sub1f), coor_dir)
+                                molfilename=Path(sub1f).name
+                            else:
+                                molfilename=mf.name
+                            if Path(sub2f).exists(): 
+                                shutil.copy(Path(sub2f), coor_dir)
+                                envfilename=Path(sub2f).name
+                            else:
+                                envfilename=None
+                            if self.options["molcharge"] is not None:
+                                totalmolcharge = self.options["molcharge"]
+                            else:
+                                totalmolcharge = None
+                            shutil.copy(self.qm_tmpl_inp, qm_dir)
+                            shutil.copy(self.qm_tmpl_run, qm_dir)
+                            # now, string replacement:
+                            final_runf = Path(qm_dir, self.qm_tmpl_run.name).absolute()
+                            final_inpf = Path(qm_dir, self.qm_tmpl_inp.name).absolute()
+                            with open(self.qm_tmpl_run, "r") as f:
+                                lines = f.readlines()
+                                with open(final_runf, "w") as g:
+                                    self.modify_lines(g, lines, \
+                                                      data_dir_in_scratch=subdirname, data_dir_in_storage=subdirname, \
+                                                      project_name=self.qm_tmpl_inp.stem, \
+                                                      molfilename=molfilename, envfilename=envfilename, molcharge=totalmolcharge, \
+                                                      cluster_ntasks=self.options['cluster_ntasks'], \
+                                                      cluster_timeh=self.options['cluster_timeh'], \
+                                                      cluster_part=self.options['cluster_part'], \
+                                                      basis=b, hamiltonian=h, dftfun=d)
+                            with open(self.qm_tmpl_inp, "r") as f:
+                                lines = f.readlines()
+                                with open(final_inpf, "w") as g:
+                                    self.modify_lines(g, lines, \
+                                                      data_dir_in_scratch=subdirname, data_dir_in_storage=subdirname, \
+                                                      project_name=self.qm_tmpl_inp.stem, \
+                                                      molfilename=molfilename, envfilename=envfilename, molcharge=totalmolcharge, \
+                                                      cluster_ntasks=self.options['cluster_ntasks'], \
+                                                      cluster_timeh=self.options['cluster_timeh'], \
+                                                      cluster_part=self.options['cluster_part'], \
+                                                      basis=b, hamiltonian=h, dftfun=d)
+
+
+
+
+    def modify_lines(self, g, lines, \
+                     data_dir_in_scratch, data_dir_in_storage, project_name, \
+                     molfilename, envfilename=None, molcharge=None, \
+                     cluster_ntasks=None, cluster_timeh=None, cluster_part=None, \
+                     basis=None, hamiltonian=None, dftfun=None):
+    
+        patterns={}
+    
+        patterns["cluster_ntasks"] = str(cluster_ntasks)
+        patterns["cluster_timeh"] = str(cluster_timeh)
+        patterns["cluster_part"] = cluster_part
+
+        patterns["data_dir_in_scratch"] = data_dir_in_scratch
+        patterns["data_dir_in_storage"] = data_dir_in_storage
+        patterns["project_name"] = project_name
+    
+        patterns["molfilename"] = molfilename
+        if envfilename is not None:
+            patterns["envfilename"] = envfilename
+        else:
+            patterns["envfilename"] = 'remove'
+        if molcharge is not None:
+            patterns["molcharge"] = int(molcharge)
+        else:
+            patterns["molcharge"] = 'remove'
+    
+        patterns["choice_of_basis"] = basis
+        patterns["choice_of_dftfun"] = dftfun
+
+        # TODO
+        #patterns["myname-space"] = molfilename.stem
+
+
+        if 'ZORA' in hamiltonian:
+            if 'spinorbit' in hamiltonian:
+                patterns["choice_of_hamiltonian"] = 'ZORA=True, SpinOrbit=True'
+                patterns["choice_of_unrestricted"] = 'remove'
+                patterns["choice_of_noncollinear"] = 'True'
+            else:
+                patterns["choice_of_hamiltonian"] = 'ZORA=True, SpinOrbit=False'
+                patterns["choice_of_unrestricted"] = 'False'
+                patterns["choice_of_noncollinear"] = 'remove'
+        else:
+            patterns["choice_of_hamiltonian"] = 'remove'
+            patterns["choice_of_unrestricted"] = 'remove'
+
+    
+        for l in lines:
+            for k, v in patterns.items():
+                if v:
+                    l = l.replace(k, v)
+            if 'remove' not in l:
+                g.write(l)
+    
