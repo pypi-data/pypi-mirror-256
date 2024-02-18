0908cce55e89423a71fa7aea740172fcfa9b003d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:40 +0000

    Add package DjangoIntellitextFormField/1.5/DjangoIntellitextFormField-1.5.tar.gz

---
+++ packages/DjangoIntellitextFormField/DjangoIntellitextFormField-1.5.tar.gz/DjangoIntellitextFormField-1.5/DjangoIntellitextFormField/__init__.py
@@ -0,0 +1,2 @@
+from DjangoIntellitextFormField.IntellitextFormField import (IntellitextBaseForm, IntellitextModelForm,
+                                                             IntellitextField, get_intellitext_choices, PrePopulatedForm)
---
+++ packages/DjangoIntellitextFormField/DjangoIntellitextFormField-1.5.tar.gz/DjangoIntellitextFormField-1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='DjangoIntellitextFormField',
+    version='1.5',
+    packages=['DjangoIntellitextFormField'],
+    url='https://github.com/amcsparron2793-Water/DjangoIntellitextFormField',
+    download_url='https://github.com/amcsparron2793-Water/DjangoIntellitextFormField/archive/refs/tags/1.5.tar.gz',
+    keywords=['Django', 'Intellitext', 'PickList'],
+    license='MIT License',
+    author='Amcsparron',
+    author_email='amcsparron@albanyny.gov',
+    description='adds intellitext forms and fields to a django project'
+)
