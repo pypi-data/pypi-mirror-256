df2b16a7431189f44409dcc4c1852847c6515ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:37 +0000

    Add package likeness/1.0.2/likeness-1.0.2.tar.gz

---
+++ packages/likeness/likeness-1.0.2.tar.gz/likeness-1.0.2/likeness/__init__.py
@@ -0,0 +1,6 @@
+from .discount import discount
+from .ignore import ignore
+from .likeness import likeness
+from .likeness_mixin import LikenessMixin
+
+__all__ = ["LikenessMixin", "discount", "ignore", "likeness"]
