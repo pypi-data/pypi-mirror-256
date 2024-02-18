d49d467b317269665df946391d3d9e16191ce469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:06:03 +0000

    Add package raknet/0.1.0/raknet-0.1.0.tar.gz

---
+++ packages/raknet/raknet-0.1.0.tar.gz/raknet-0.1.0/src/rak_peer.cpp
@@ -0,0 +1,23 @@
+#include <pybind11/pybind11.h>
+
+#include "rak_peer.h"
+
+#include <raknet/RakNetTypes.h>
+#include <raknet/RakPeer.h>
+#include <raknet/RakPeerInterface.h>
+
+namespace py = pybind11;
+
+void def_rak_peer(pybind11::module &m) {
+    py::class_<RakNet::RakPeer>(m, "RakPeer")
+        .def(py::init<>())
+        .def(
+            "start",
+            [](RakNet::RakPeerInterface &a, const char *host, int port, int max_connections) {
+                auto local_addr = RakNet::SocketDescriptor(port, host);
+                a.Startup(max_connections, &local_addr, 1);
+            },
+            py::arg("host") = py::none(),
+            py::arg("port") = 0,
+            py::arg("max_connections") = 1);
+}
