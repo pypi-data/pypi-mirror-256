a98c0b7cb5e91914be97b0c9493038cbd955d508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:23 +0000

    Add package airfold-cli/0.8.2/airfold_cli-0.8.2-py3-none-any.whl

---
+++ packages/airfold-cli/airfold_cli-0.8.2-py3-none-any.whl/airfold_cli/error.py
@@ -0,0 +1,25 @@
+from airfold_common.error import AirfoldError
+
+
+class APIError(AirfoldError):
+    pass
+
+
+class ForbiddenError(APIError):
+    pass
+
+
+class UnauthorizedError(APIError):
+    pass
+
+
+class ProjectNotFoundError(APIError):
+    pass
+
+
+class ConflictError(APIError):
+    pass
+
+
+class InternalServerError(APIError):
+    pass
