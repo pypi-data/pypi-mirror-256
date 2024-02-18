5be806cafc995094a40c56ee09fea23a594d73f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:47 +0000

    Add package wardleymap/0.1.8/wardleymap-0.1.8.tar.gz

---
+++ packages/wardleymap/wardleymap-0.1.8.tar.gz/wardleymap-0.1.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wardleymap',
+    version='0.1.8',
+    author='Mark Craddock',
+    author_email='python@firstliot.uk',
+    description='A Python package to create and visualize Wardley Maps',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://wardleymaps.ai/',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'streamlit',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/wardleymap/wardleymap-0.1.8.tar.gz/wardleymap-0.1.8/wardleymap/__init__.py
@@ -0,0 +1,3 @@
+from .wardleymap import wardley
+from .wardleymap import create_svg_map
+from .wardleymap import get_owm_map
