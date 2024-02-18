99a20db9b57343954b43976c3ab646bac30d3c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:08 +0000

    Add package gs-quant/1.0.59/gs_quant-1.0.59.tar.gz

---
+++ packages/gs-quant/gs_quant-1.0.59.tar.gz/gs_quant-1.0.59/gs_quant/backtests/actions.py
@@ -0,0 +1,288 @@
+"""
+Copyright 2019 Goldman Sachs.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+"""
+
+from collections import namedtuple
+from typing import TypeVar
+
+from gs_quant.backtests.backtest_utils import *
+from gs_quant.backtests.backtest_objects import ConstantTransactionModel, TransactionModel
+from gs_quant.risk.transform import Transformer
+from gs_quant.base import Priceable
+from gs_quant.markets.securities import *
+from gs_quant.markets.portfolio import Portfolio
+from gs_quant.target.backtests import BacktestTradingQuantityType
+
+action_count = 1
+
+
+class Action(object):
+    def __init__(self, name: str = None):
+        self._needs_scaling = False
+        self._calc_type = CalcType.simple
+        self._risk = None
+        global action_count
+        if name is None:
+            self._name = 'Action{}'.format(action_count)
+            action_count += 1
+        else:
+            self._name = name
+        self._transaction_cost = ConstantTransactionModel(0)
+
+    @property
+    def calc_type(self):
+        return self._calc_type
+
+    @property
+    def risk(self):
+        return self._risk
+
+    @property
+    def transaction_cost(self):
+        return self._transaction_cost
+
+
+TAction = TypeVar('TAction', bound='Action')
+
+
+class AddTradeAction(Action):
+    def __init__(self,
+                 priceables: Union[Priceable, Iterable[Priceable]],
+                 trade_duration: Union[str, dt.date, dt.timedelta] = None,
+                 name: str = None,
+                 transaction_cost: TransactionModel = ConstantTransactionModel(0)):
+        """
+        create an action which adds a trade when triggered.  The trades are resolved on the trigger date (state) and
+        last until the trade_duration if specified or for all future dates if not.
+        :param priceables: a priceable or a list of pricables.
+        :param trade_duration: an instrument attribute eg. 'expiration_date' or a date or a tenor or timedelta
+                               if left as None the
+                               trade will be added for all future dates
+        :param name: optional additional name to the priceable name
+        :param transaction_cost: optional a cash amount paid for each transaction, paid on both enter and exit
+        """
+        super().__init__(name)
+
+        self._priceables = []
+        self._dated_priceables = {}
+        self._trade_duration = trade_duration
+        self._transaction_cost = transaction_cost
+        for i, p in enumerate(make_list(priceables)):
+            if p.name is None:
+                self._priceables.append(p.clone(name=f'{self._name}_Priceable{i}'))
+            else:
+                self._priceables.append(p.clone(name=f'{self._name}_{p.name}'))
+
+    @property
+    def priceables(self):
+        return self._priceables
+
+    @property
+    def trade_duration(self):
+        return self._trade_duration
+
+    def set_dated_priceables(self, state, priceables):
+        self._dated_priceables[state] = make_list(priceables)
+
+    @property
+    def dated_priceables(self):
+        return self._dated_priceables
+
+    @property
+    def transaction_cost(self):
+        return self._transaction_cost
+
+
+AddTradeActionInfo = namedtuple('AddTradeActionInfo', 'scaling')
+EnterPositionQuantityScaledActionInfo = namedtuple('EnterPositionQuantityScaledActionInfo', 'not_applicable')
+HedgeActionInfo = namedtuple('HedgeActionInfo', 'not_applicable')
+ExitTradeActionInfo = namedtuple('ExitTradeActionInfo', 'not_applicable')
+RebalanceActionInfo = namedtuple('RebalanceActionInfo', 'not_applicable')
+
+
+class EnterPositionQuantityScaledAction(Action):
+    def __init__(self,
+                 priceables: Union[Priceable, Iterable[Priceable]],
+                 trade_duration: Union[str, dt.date, dt.timedelta] = None,
+                 name: str = None,
+                 trade_quantity: float = 1,
+                 trade_quantity_type: Union[BacktestTradingQuantityType, str] = BacktestTradingQuantityType.quantity):
+        """
+        create an action which enters trades when triggered.  The trades are executed with specified quantity and
+        last until the trade_duration if specified, or for all future dates if not.
+        :param priceables: a priceable or a list of pricables.
+        :param trade_duration: an instrument attribute eg. 'expiration_date' or a date or a tenor if left as None the
+                               trade will be added for all future dates
+        :param name: optional additional name to the priceable name
+        :param trade_quantity: the amount, in units of trade_quantity_type to be traded
+        :param trade_quantity_type: the quantity type used to scale trade. eg. quantity for units, notional for
+                                    underlier notional
+        """
+        super().__init__(name)
+        self._priceables = []
+        self._trade_duration = trade_duration
+        for i, p in enumerate(make_list(priceables)):
+            if p.name is None:
+                self._priceables.append(p.clone(name=f'{self._name}_Priceable{i}'))
+            else:
+                self._priceables.append(p.clone(name=f'{self._name}_{p.name}'))
+        self._trade_quantity = trade_quantity
+        self._trade_quantity_type = trade_quantity_type
+
+    @property
+    def priceables(self):
+        return self._priceables
+
+    @property
+    def trade_duration(self):
+        return self._trade_duration
+
+    @property
+    def trade_quantity(self):
+        return self._trade_quantity
+
+    @property
+    def trade_quantity_type(self):
+        return self._trade_quantity_type
+
+
+class ExitPositionAction(Action):
+    def __init__(self, name: str = None):
+        """
+        Fully exit all held positions
+        :param name: optional name of the action
+        """
+        super().__init__(name)
+
+
+class ExitTradeAction(Action):
+    def __init__(self, priceable_names: Union[str, Iterable[str]] = None, name: str = None):
+        """
+        Fully exit all held positions
+        :param priceable_names: optional string or list of strings of priceable names
+        :param name: optional name of the action
+        """
+        super().__init__(name)
+        self._priceables_names = make_list(priceable_names)
+
+    @property
+    def priceable_names(self):
+        return self._priceables_names
+
+
+class ExitAllPositionsAction(ExitTradeAction):
+    def __init__(self, name: str = None):
+        """
+        Fully exit all held positions
+        :param name: optional name of the action
+        """
+        super().__init__([], name)
+
+        self._calc_type = CalcType.path_dependent
+
+
+class HedgeAction(Action):
+    def __init__(self, risk, priceables: Priceable = None, trade_duration: str = None, name: str = None,
+                 csa_term: str = None, scaling_parameter: str = 'notional_amount',
+                 transaction_cost: TransactionModel = ConstantTransactionModel(0),
+                 risk_transformation: Transformer = None):
+        super().__init__(name)
+        self._calc_type = CalcType.semi_path_dependent
+        self._priceable = priceables
+        self._risk = risk
+        self._trade_duration = trade_duration
+        self._csa_term = csa_term
+        self._scaling_parameter = scaling_parameter
+        self._transaction_cost = transaction_cost
+        self._risk_transformation = risk_transformation
+        if isinstance(priceables, Portfolio):
+            trades = []
+            for i, priceable in enumerate(priceables):
+                if priceable.name is None:
+                    trades.append(priceable.clone(name=f'{self._name}_Priceable{i}'))
+                else:
+                    trades.append(priceable.clone(name=f'{self._name}_{priceable.name}'))
+            self._priceable = Portfolio(trades)
+        else:
+            if priceables is not None:
+                if priceables.name is None:
+                    self._priceable = priceables.clone(name=f'{self._name}_Priceable0')
+                else:
+                    self._priceable = priceables.clone(name=f'{self._name}_{priceables.name}')
+
+    @property
+    def trade_duration(self):
+        return self._trade_duration
+
+    @property
+    def priceable(self):
+        return self._priceable
+
+    @property
+    def risk(self):
+        return self._risk
+
+    @property
+    def csa_term(self):
+        return self._csa_term
+
+    @property
+    def scaling_parameter(self):
+        return self._scaling_parameter
+
+    @property
+    def transaction_cost(self):
+        return self._transaction_cost
+
+    @property
+    def risk_transformation(self):
+        return self._risk_transformation
+
+
+class RebalanceAction(Action):
+    def __init__(self, priceable: Priceable, size_parameter, method,
+                 transaction_cost: TransactionModel = ConstantTransactionModel(0)):
+        super().__init__()
+        self._calc_type = CalcType.path_dependent
+        self._size_parameter = size_parameter
+        self._method = method
+        self._transaction_cost = transaction_cost
+        if priceable.unresolved is None:
+            raise ValueError("Please specify a resolved priceable to rebalance.")
+        if priceable is not None:
+            if priceable.name is None:
+                self._priceable = priceable.clone(name=f'{self._name}_Priceable0')
+            else:
+                self._priceable = priceable.clone(name=f'{self._name}_{priceable.name}')
+
+    @property
+    def priceable(self):
+        return self._priceable
+
+    @property
+    def size_parameter(self):
+        return self._size_parameter
+
+    @property
+    def method(self):
+        return self._method
+
+    @property
+    def args(self):
+        return self._args
+
+    @property
+    def transaction_cost(self):
+        return self._transaction_cost
---
+++ packages/gs-quant/gs_quant-1.0.59.tar.gz/gs_quant-1.0.59/gs_quant/backtests/data_sources.py
@@ -0,0 +1,148 @@
+"""
+Copyright 2019 Goldman Sachs.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+"""
+
+import datetime as dt
+from enum import Enum
+import numpy as np
+import pandas as pd
+import pytz
+from typing import Union, Iterable
+
+from gs_quant.backtests.core import ValuationFixingType
+from gs_quant.data import DataFrequency, Dataset
+from gs_quant.instrument import Instrument
+
+
+class MissingDataStrategy(Enum):
+    fill_forward = 'fill_forward'
+    interpolate = 'interpolate'
+    fail = 'fail'
+
+
+class DataSource:
+    def get_data(self, state):
+        raise RuntimeError("Implemented by subclass")
+
+
+class GsDataSource(DataSource):
+    def __init__(self, data_set: str, asset_id: str, min_date: dt.date = None, max_date: dt.date = None,
+                 value_header: str = 'rate'):
+        self._data_set = data_set
+        self._asset_id = asset_id
+        self._min_date = min_date
+        self._max_date = max_date
+        self._value_header = value_header
+        self._loaded_data = None
+
+    def get_data(self, state: Union[dt.date, dt.datetime] = None):
+        if self._loaded_data is None:
+            ds = Dataset(self._data_set)
+            if self._min_date:
+                self._loaded_data = ds.get_data(self._min_date, self._max_date, assetId=(self._asset_id,))
+            else:
+                return ds.get_data(state, state, assetId=(self._asset_id,))[self._value_header]
+        return self._loaded_data[self._value_header].at[pd.to_datetime(state)]
+
+
+class GenericDataSource(DataSource):
+    def __init__(self, data_set: pd.Series, missing_data_strategy: MissingDataStrategy = MissingDataStrategy.fail):
+        """
+        A data source which holds a pandas series indexed by date or datetime
+        :param data_set: a pandas dataframe indexed by date or datetime
+        :param missing_data_strategy: MissingDataStrategy which defines behaviour if data is missing, will only take
+                                      effect if using get_data, gat_data_range has no expectations of the number of
+                                      expected data points.
+        """
+        self._data_set = data_set
+        self._missing_data_strategy = missing_data_strategy
+        self._tz_aware = isinstance(self._data_set.index[0],
+                                    dt.datetime) and self._data_set.index[0].tzinfo is not None
+        if self._missing_data_strategy == MissingDataStrategy.interpolate:
+            self._data_set.interpolate()
+        elif self._missing_data_strategy == MissingDataStrategy.fill_forward:
+            self._data_set.ffill()
+
+    def get_data(self, state: Union[dt.date, dt.datetime, Iterable]):
+        """
+        Get the value of the dataset at a time or date.  If a list of dates or times is provided return the avg value
+        :param state: a date, datetime or a list of dates or datetimes
+        :return: float value
+        """
+        if isinstance(state, Iterable):
+            return [self.get_data(i) for i in state]
+
+        if self._tz_aware and (state.tzinfo is None or state.tzinfo.utcoffset(state) is None):
+            state = pytz.utc.localize(state)
+        if pd.Timestamp(state) in self._data_set:
+            return self._data_set[pd.Timestamp(state)]
+        elif state in self._data_set or self._missing_data_strategy == MissingDataStrategy.fail:
+            return self._data_set[state]
+        else:
+            if isinstance(self._data_set.index, pd.DatetimeIndex):
+                self._data_set.at[pd.to_datetime(state)] = np.nan
+                self._data_set.sort_index(inplace=True)
+            else:
+                self._data_set.at[state] = np.nan
+            self._data_set.sort_index()
+            if self._missing_data_strategy == MissingDataStrategy.interpolate:
+                self._data_set = self._data_set.interpolate()
+            elif self._missing_data_strategy == MissingDataStrategy.fill_forward:
+                self._data_set = self._data_set.ffill()
+            else:
+                raise RuntimeError(f'unrecognised missing data strategy: {str(self._missing_data_strategy)}')
+            return self._data_set[pd.to_datetime(state)] if isinstance(self._data_set.index,
+                                                                       pd.DatetimeIndex) else self._data_set[state]
+
+    def get_data_range(self, start: Union[dt.date, dt.datetime],
+                       end: Union[dt.date, dt.datetime, int]):
+        """
+        get a range of values from the dataset.
+        :param start: a date or datetime
+        :param end: a date, datetime or an int.  If an int is provided we return that many data points back from the
+                    start date
+        :return: pd.Series
+        """
+        if isinstance(end, int):
+            return self._data_set.loc[self._data_set.index < start].tail(end)
+        return self._data_set.loc[(start < self._data_set.index) & (self._data_set.index <= end)]
+
+
+class DataManager:
+    def __init__(self):
+        self._data_sources = {}
+
+    def add_data_source(self, series: Union[pd.Series, DataSource], data_freq: DataFrequency,
+                        instrument: Instrument, valuation_type: ValuationFixingType):
+        if not isinstance(series, DataSource) and not len(series):
+            return
+        if instrument.name is None:
+            raise RuntimeError('Please add a name identify your instrument')
+        key = (data_freq, instrument.name, valuation_type)
+        if key in self._data_sources:
+            raise RuntimeError('A dataset with this frequency instrument name and valuation type already added to '
+                               'Data Manager')
+        self._data_sources[key] = GenericDataSource(series) if isinstance(series, pd.Series) else series
+
+    def get_data(self, state: Union[dt.date, dt.datetime], instrument: Instrument, valuation_type: ValuationFixingType):
+        key = (DataFrequency.REAL_TIME if isinstance(state, dt.datetime) else DataFrequency.DAILY,
+               instrument.name.split('_')[-1], valuation_type)
+        return self._data_sources[key].get_data(state)
+
+    def get_data_range(self, start: Union[dt.date, dt.datetime],
+                       end: Union[dt.date, dt.datetime], instrument: Instrument, valuation_type: ValuationFixingType):
+        key = (DataFrequency.REAL_TIME if isinstance(start, dt.datetime) else DataFrequency.DAILY,
+               instrument.name.split('_')[-1], valuation_type)
+        return self._data_sources[key].get_data_range(start, end)
