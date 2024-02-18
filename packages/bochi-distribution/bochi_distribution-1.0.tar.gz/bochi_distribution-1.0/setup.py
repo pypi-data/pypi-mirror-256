30c77d0feab0bc2fe5d999ee4e4ac9da43d43cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:18 +0000

    Add package bochi-distribution/1.0/bochi_distribution-1.0.tar.gz

---
+++ packages/bochi-distribution/bochi_distribution-1.0.tar.gz/bochi_distribution-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bochi_distribution',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['bochi_distribution'],
+      author = 'Tanya Singh-Mishra',
+      zip_safe=False)
