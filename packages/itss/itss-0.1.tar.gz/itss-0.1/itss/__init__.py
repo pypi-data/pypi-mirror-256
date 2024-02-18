aa5dd35a53ffc459c5b0292cb2c35d01f9bc0c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:18 +0000

    Add package itss/0.1/itss-0.1.tar.gz

---
+++ packages/itss/itss-0.1.tar.gz/itss-0.1/itss/__init__.py
@@ -0,0 +1,2 @@
+from fun1 import*
+from fun2 import itssgroup
---
+++ packages/itss/itss-0.1.tar.gz/itss-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='itss',
+ version='0.1',
+ author="Tamilselvan Pandurangan",
+ author_email="nithingowdaam0123@gmail.com",
+ description="Simple itss demo",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
