d8792dcdb20a874537880e2a21505f5a18aa1891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:58 +0000

    Add package cathodedataextractor/0.0.3/cathodedataextractor-0.0.3.tar.gz

---
+++ packages/cathodedataextractor/cathodedataextractor-0.0.3.tar.gz/cathodedataextractor-0.0.3/cathodedataextractor/cathodetext2chem/__init__.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+from .parser_pipeline import CathodeParserPipelineBuilder
+from .regex_parser import CathodeRegExParser
+from .postprocessing_tools import CathodeStoichiometricVariablesProcessing
