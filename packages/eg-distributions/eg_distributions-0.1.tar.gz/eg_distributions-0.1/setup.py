e76eb64fcb54966e1ba6be951aaf69aaa0d3da6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:06:12 +0000

    Add package eg-distributions/0.1/eg_distributions-0.1.tar.gz

---
+++ packages/eg-distributions/eg_distributions-0.1.tar.gz/eg_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='eg_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['eg_distributions'],
+      zip_safe=False)
