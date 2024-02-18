bf892cf536626669050c3650e8c9696921a6a878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:35 +0000

    Add package prob-normal-binomial-dist-bee001/0.1/prob_normal_binomial_dist_bee001-0.1.tar.gz

---
+++ packages/prob-normal-binomial-dist-bee001/prob_normal_binomial_dist_bee001-0.1.tar.gz/prob_normal_binomial_dist_bee001-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='prob_normal_binomial_dist_bee001',
+      version='0.1',
+      description='Gaussian distributions and Binomial Distributions',
+      author = 'Chukwuezi Precious',
+      packages=['prob_normal_binomial_dist_bee001'],
+      zip_safe=False)
