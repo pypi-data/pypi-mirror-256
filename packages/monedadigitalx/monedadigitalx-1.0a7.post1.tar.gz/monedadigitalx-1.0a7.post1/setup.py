a56dc902710586b312b313477995a92afb8275c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:19 +0000

    Add package monedadigitalx/1.0a7.post1/monedadigitalx-1.0a7.post1.tar.gz

---
+++ packages/monedadigitalx/monedadigitalx-1.0a7.post1.tar.gz/monedadigitalx-1.0a7.post1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+version = '1.0a7r1'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="monedadigitalx",
+    version=version,
+
+    author="federico123579",
+    author_email="federico123579@gmail.com",
+
+    url="https://github.com/AmadoRamos/XTBApi.git",
+    download_url=f"https://github.com/AmadoRamos/XTBApi/archive/refs/tags/v{version}.zip",
+
+    description="A python based API for XTB trading using websocket_client.",
+
+    packages=['XTBApi'],
+    install_requires=['requests', 'websocket-client'],
+
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+    ],
+    include_package_data=True, # for MANIFEST.in
+    python_requires='>=3.6.0',
+
+    package_data={package: ["py.typed", "*.pyi", "**/*.pyi"] for package in find_packages()},
+    zip_safe=False,
+)
