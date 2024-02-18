b0cdfb6fc99a635ce6ad60474900edc7f27e5341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:03:44 +0000

    Add package keylogger-thread/0.1/keylogger-thread-0.1.tar.gz

---
+++ packages/keylogger-thread/keylogger-thread-0.1.tar.gz/keylogger-thread-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="keylogger-thread",
+    description="A Python package for easily logging keys.",
+    long_description=open("long-description.txt").read(),
+    version="0.1",
+    packages=find_packages()
+)
