739861e653ad61f2127eb16bbd22053a4e845ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:30 +0000

    Add package PyPlumIO/0.5.14/PyPlumIO-0.5.14-py3-none-any.whl

---
+++ packages/PyPlumIO/PyPlumIO-0.5.14-py3-none-any.whl/pyplumio/helpers/typing.py
@@ -0,0 +1,28 @@
+"""Contains type aliases."""
+from __future__ import annotations
+
+from typing import Any, Literal, Protocol, TypeVar, Union, runtime_checkable
+
+T = TypeVar("T")
+
+ParameterValueType = Union[int, float, bool, Literal["off"], Literal["on"]]
+
+
+@runtime_checkable
+class Subtractable(Protocol[T]):
+    """Supports subtraction operation."""
+
+    __slots__ = ()
+
+    def __sub__(self, other: T) -> T:
+        """Subtract a value."""
+
+
+@runtime_checkable
+class Comparable(Protocol):
+    """Supports comparison."""
+
+    __slots__ = ()
+
+    def __eq__(self, other: Any) -> bool:
+        """Compare a value."""
