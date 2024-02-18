9b97f18c59047f4271ca51a1eb27d983ac263c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:23 +0000

    Add package wagtail-showables/1.0.6/wagtail_showables-1.0.6.tar.gz

---
+++ packages/wagtail-showables/wagtail_showables-1.0.6.tar.gz/wagtail_showables-1.0.6/wagtail_showables/options.py
@@ -0,0 +1,42 @@
+from functools import cache
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+LEVEL_NONE = 0
+LEVEL_SHOW = 1
+LEVEL_EDIT = 2
+
+ADMIN_INTERACTION_LEVEL = getattr(
+    settings, "WAGTAIL_SHOWABLES_ADMIN_INTERACTION_LEVEL", LEVEL_NONE
+)
+
+DISABLED_DISPLAY_TEXT = getattr(
+    settings, "WAGTAIL_SHOWABLES_DISABLED_DISPLAY_TEXT", _("This block is disabled.")
+)
+
+SHOWABLES_DEFAULT_BACKEND = getattr(
+    settings, "WAGTAIL_SHOWABLES_DEFAULT_BACKEND", "default"
+)
+
+SHOWABLES_BACKEND = getattr(
+    settings, "WAGTAIL_SHOWABLES_BACKEND", {
+        "default": {
+            "CLASS": "wagtail_showables.backends.ShowableCacheBackend",
+            "OPTIONS": {
+                "cache_key": "wagtail_showables_registry",
+                "cache_backend": "default",
+            },
+        },
+        "performance_cache": {
+            "CLASS": "wagtail_showables.backends.ShowablePerformanceCacheBackend",
+            "OPTIONS": {
+                "cache_key": "wagtail_showables_registry",
+                "cache_backend": "cache",
+            },
+        },
+        "performance_db": {
+            "CLASS": "wagtail_showables.backends.ShowablePerformanceDBBackend",
+            "OPTIONS": {},
+        },
+    }
+)
