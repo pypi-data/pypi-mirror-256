fd8f57f70291a90d0af8908be7968d92b9dfd36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:46 +0000

    Add package distributions-es/4.0/distributions_es-4.0.tar.gz

---
+++ packages/distributions-es/distributions_es-4.0.tar.gz/distributions_es-4.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_es',
+      version='4.0',
+      description='Gaussian distributions',
+      packages=['distributions_es'],
+      author = "esra ai",
+      author_email = "esra.al@gmail.com",
+      zip_safe=False)
