910beba5f4aeffed96ac872ba85d0e4b5d034558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:09:05 +0000

    Add package dist-prob-123/0.1/dist_prob_123-0.1.tar.gz

---
+++ packages/dist-prob-123/dist_prob_123-0.1.tar.gz/dist_prob_123-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_prob_123',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_prob_123'],
+      zip_safe=False)
