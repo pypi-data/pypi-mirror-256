e4a501042712ccbe5b26fff7aeb75e709b013d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:52 +0000

    Add package program-distributions/0.1/program_distributions-0.1.tar.gz

---
+++ packages/program-distributions/program_distributions-0.1.tar.gz/program_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='program_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['program_distributions'],
+      zip_safe=False)
