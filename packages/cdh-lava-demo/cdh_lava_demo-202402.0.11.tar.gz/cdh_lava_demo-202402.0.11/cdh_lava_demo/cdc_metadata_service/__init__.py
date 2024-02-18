59b3038ca5eb102179235b12dc2317c410edcadb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:05 +0000

    Add package cdh-lava-demo/202402.0.11/cdh_lava_demo-202402.0.11.tar.gz

---
+++ packages/cdh-lava-demo/cdh_lava_demo-202402.0.11.tar.gz/cdh_lava_demo-202402.0.11/cdh_lava_demo/cdc_metadata_service/__init__.py
@@ -0,0 +1,39 @@
+"""Initialize the cdc_metadata_service subpackage of cdh_lava_demo package"""
+
+# allow absolute import from the root folder
+# whatever its name is.
+from cdh_lava_demo.cdc_metadata_service import workflow_metadata
+from cdh_lava_demo.cdc_metadata_service import logging_metadata
+from cdh_lava_demo.cdc_metadata_service import job_metadata
+from cdh_lava_demo.cdc_metadata_service import environment_metadata
+from cdh_lava_demo.cdc_metadata_service import dataset_metadata
+
+import sys  # don't remove required for error handling
+import os
+
+# Import from sibling directory ..\databricks_service
+OS_NAME = os.name
+
+sys.path.append("..")
+if OS_NAME.lower() == "nt":
+    print("cdc_metadata_service: windows")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..\\..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..\\..\\..")))
+else:
+    print("cdc_metadata_service: non windows")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../../..")))
+
+
+import cdh_lava_demo.cdc_log_service.environment_logging
+import cdh_lava_demo.cdc_log_service.environment_tracing
+import cdh_lava_demo.databricks_service.notebook
+
+__all__ = [
+    "environment_metadata",
+    "job_metadata",
+    "logging_metadata",
+    "workflow_metadata",
+]
