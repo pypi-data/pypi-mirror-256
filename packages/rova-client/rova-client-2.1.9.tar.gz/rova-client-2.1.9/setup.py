eda4b4e00c85ad50d2c2a57521c0c509b999d6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:34 +0000

    Add package rova-client/2.1.9/rova-client-2.1.9.tar.gz

---
+++ packages/rova-client/rova-client-2.1.9.tar.gz/rova-client-2.1.9/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rova-client',
+      version='2.1.9',
+      description='Event tracking with rova AI',
+      packages=['rova_client'],
+      author_email='sam@rovaai.com',
+      zip_safe=False)
