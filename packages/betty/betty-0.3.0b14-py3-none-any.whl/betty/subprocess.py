5b822f8b275c8422daacf22a536b734ce6b23a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:11 +0000

    Add package betty/0.3.0b14/betty-0.3.0b14-py3-none-any.whl

---
+++ packages/betty/betty-0.3.0b14-py3-none-any.whl/betty/subprocess.py
@@ -0,0 +1,68 @@
+"""
+Provide a subprocess API.
+"""
+import logging
+import os
+import subprocess as stdsubprocess
+from asyncio import subprocess
+from textwrap import indent
+from typing import Sequence, Any, Callable, Awaitable, ParamSpec
+
+
+P = ParamSpec('P')
+
+
+async def run_exec(runnee: Sequence[str], **kwargs: Any) -> subprocess.Process:
+    """
+    Run a command.
+    """
+    return await _run(
+        subprocess.create_subprocess_exec,
+        runnee,
+        runnee[0],
+        *runnee[1:],
+        **kwargs,
+    )
+
+
+async def run_shell(runnee: Sequence[str], **kwargs: Any) -> subprocess.Process:
+    """
+    Run a command in a shell.
+    """
+    return await _run(
+        subprocess.create_subprocess_shell,
+        runnee,
+        ' '.join(runnee),
+        **kwargs,
+    )
+
+
+async def _run(
+    runner: Callable[P, Awaitable[subprocess.Process]],
+    runnee: Sequence[str],
+    *args: P.args,
+    **kwargs: P.kwargs,
+) -> subprocess.Process:
+    kwargs['stdout'] = subprocess.PIPE
+    kwargs['stderr'] = subprocess.PIPE
+    process = await runner(*args, **kwargs)
+    await process.wait()
+
+    if process.returncode == 0:
+        return process
+
+    stdout = process.stdout
+    stdout_str = '' if stdout is None else '\n'.join((await stdout.read()).decode().split(os.linesep))
+    stderr = process.stderr
+    stderr_str = '' if stderr is None else '\n'.join((await stderr.read()).decode().split(os.linesep))
+
+    error = stdsubprocess.CalledProcessError(
+        process.returncode,  # type: ignore[arg-type]
+        ' '.join(runnee),
+        stdout_str,
+        stderr_str,
+    )
+    logging.getLogger(__name__).warning(
+        f'{str(error)}\nSTDOUT:\n{indent(stdout_str, "    ")}\nSTDERR:{indent(stderr_str, "   ")}',
+    )
+    raise error
