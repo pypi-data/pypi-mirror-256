b646669c96978c3236e1aba52dccf6a993fc262d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:46 +0000

    Add package azol/0.1.1a11/azol-0.1.1a11-py3-none-any.whl

---
+++ packages/azol/azol-0.1.1a11-py3-none-any.whl/azol/resources/__init__.py
@@ -0,0 +1,5 @@
+"""Module containing useful resources that are used, for example, for data annotation in azol"""
+from .directory_builtin_roles import builtin_aad_roles
+from .graph_delegated_permissions import delegated_permissions
+from .graph_permissions_map import graph_permission_map
+from .rbac_roles import rbac_roles
