63eb5dcc1d0913eee7b3bbb3809fc1d9e451bcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:03:57 +0000

    Add package Binom-Gauss-distributions-bySawsan/1.0/Binom_Gauss_distributions_bySawsan-1.0.tar.gz

---
+++ packages/Binom-Gauss-distributions-bySawsan/Binom_Gauss_distributions_bySawsan-1.0.tar.gz/Binom_Gauss_distributions_bySawsan-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Binom_Gauss_distributions_bySawsan',
+      version='1.0',
+      description='Binomial and Gaussian distributions',
+      packages=['Binom_Gauss_distributions_bySawsan'],
+      author = 'Sawsan Omira',
+      author_email = 'sawsanemail@example.com',
+      zip_safe=False)
