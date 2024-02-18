cca2ad8cce865dabf846f054f91dba8e5210c332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:03:28 +0000

    Add package alfred3-interact/0.2.5.dev2/alfred3_interact-0.2.5.dev2.tar.gz

---
+++ packages/alfred3-interact/alfred3_interact-0.2.5.dev2.tar.gz/alfred3_interact-0.2.5.dev2/src/alfred3_interact/__init__.py
@@ -0,0 +1,15 @@
+from ._util import NoMatch
+from .element import Chat, ToggleMatchMakerActivation, ViewMembers
+from .match import MatchMaker
+from .page import (
+    MatchingPage,
+    MatchMakerActivation,
+    MatchMakerMonitoring,
+    MatchTestPage,
+    WaitingPage,
+)
+from .spec import IndividualSpec, ParallelSpec, SequentialSpec
+
+# from .randomizer import GroupRandomizer
+
+# from .quota import GroupQuota
