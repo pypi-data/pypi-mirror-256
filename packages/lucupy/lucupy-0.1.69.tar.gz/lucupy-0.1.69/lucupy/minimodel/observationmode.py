da754740151fc6827a368e6f38c2c5fd7f2f56dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:07 +0000

    Add package lucupy/0.1.69/lucupy-0.1.69.tar.gz

---
+++ packages/lucupy/lucupy-0.1.69.tar.gz/lucupy-0.1.69/lucupy/minimodel/observationmode.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+from __future__ import annotations
+
+# This has to be outside of Observation to avoid circular imports.
+from enum import Enum
+from typing import FrozenSet
+
+
+class ObservationMode(str, Enum):
+    # TODO: This is not stored anywhere and is only used temporarily in the atom code in the
+    # TODO: OcsProgramExtractor. Should it be stored anywhere or is it only used in intermediate
+    # TODO: calculations? It seems to depend on the instrument and FPU.
+    UNKNOWN = 'unknown'
+    IMAGING = 'imaging'
+    LONGSLIT = 'longslit'
+    IFU = 'ifu'
+    MOS = 'mos'
+    XD = 'xd'
+    CORON = 'coron'
+    NRM = 'nrm'
+
+
+ObservationModes = FrozenSet[ObservationMode]
---
+++ packages/lucupy/lucupy-0.1.69.tar.gz/lucupy-0.1.69/lucupy/observatory/gemini/geminiobservation.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+from datetime import timedelta
+from typing import Final
+
+from lucupy.minimodel import Observation
+from lucupy.observatory.gemini.geminiproperties import GeminiProperties
+from lucupy.types import ZeroTime
+
+_IGRINS_CAL_TIME: Final[timedelta] = timedelta(minutes=10)
+
+
+def with_igrins_cal(func):
+    def add_calibration(self):
+        if (GeminiProperties.Instruments.IGRINS in self.required_resources()
+                and self.total_used - self.program_time() > ZeroTime):
+            return func(self) + _IGRINS_CAL_TIME
+        return func(self)
+
+    return add_calibration
+
+
+class GeminiObservation(Observation):
+    """A Gemini-specific extension of the Observation class.
+    """
+
+    @with_igrins_cal
+    def total_used(self) -> timedelta:
+        """Override total_used method from Observation.
+
+           Adds IGRINS calibration time using a decorator.
+
+        Returns:
+            timedelta: Total used time.
+        """
+        return super().total_used()
