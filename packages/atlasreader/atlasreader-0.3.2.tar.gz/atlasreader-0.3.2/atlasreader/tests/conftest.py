cc4bcb0da59e473451dd3862f89db8058476613b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:07:51 +0000

    Add package atlasreader/0.3.2/atlasreader-0.3.2.tar.gz

---
+++ packages/atlasreader/atlasreader-0.3.2.tar.gz/atlasreader-0.3.2/atlasreader/tests/conftest.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def data_dir():
+    return Path(__file__).parent / "data"
+
+
+@pytest.fixture
+def stat_img(data_dir):
+    return data_dir / "collection_658" / "image_10426.nii.gz"
