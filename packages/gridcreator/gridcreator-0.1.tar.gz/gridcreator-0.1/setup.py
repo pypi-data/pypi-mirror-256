cd9ce3d6d9fb959e49f792d1bba9bcfdf1e08a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:00 +0000

    Add package gridcreator/0.1/gridcreator-0.1.tar.gz

---
+++ packages/gridcreator/gridcreator-0.1.tar.gz/gridcreator-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gridcreator',
+    version='0.1',
+    author='oblivisheee',
+    author_email='molniya213y@gmail.com',
+    description='That library provides utilities to create grids.',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    license='GNU General Public License Version 3'
+)
