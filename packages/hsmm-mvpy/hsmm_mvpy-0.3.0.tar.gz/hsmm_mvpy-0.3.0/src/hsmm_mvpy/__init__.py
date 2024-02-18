1f8b5d4fa2b2a559a827c94278fa3684e6616c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:56 +0000

    Add package hsmm-mvpy/0.3.0/hsmm_mvpy-0.3.0.tar.gz

---
+++ packages/hsmm-mvpy/hsmm_mvpy-0.3.0.tar.gz/hsmm_mvpy-0.3.0/src/hsmm_mvpy/__init__.py
@@ -0,0 +1,10 @@
+"""Software for fitting HsMM-MVPA nodels on EEG data."""
+
+
+from . import models
+from . import utils
+from . import visu
+from . import resample
+from . import clusters
+
+__all__ = ["models", "simulations", "utils","visu", "clusters"]
