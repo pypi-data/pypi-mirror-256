cbb99da1773403ab7896ed0347529956e1864447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:03:56 +0000

    Add package pih-printer/1.1/pih_printer-1.1-py3-none-any.whl

---
+++ packages/pih-printer/pih_printer-1.1-py3-none-any.whl/PrinterService/const.py
@@ -0,0 +1,34 @@
+import ipih
+
+from pih.collections.service import ServiceDescription
+from pih.consts.hosts import Hosts
+
+from enum import StrEnum
+
+NAME: str = "Printer"
+
+HOST = Hosts.WS255
+
+VERSION: str = "1.1"
+
+MODULES: tuple[str, ...] = (
+    "pyasn1==0.4.8",
+    "pycryptodomex==3.15.0",
+    "pysmi==0.3.4",
+    "pysnmp",
+)
+
+SD: ServiceDescription = ServiceDescription(
+    name=NAME,
+    description="Printer service",
+    host=HOST.NAME,
+    commands=("printers_report", "printer_snmp_call"),
+    use_standalone=True,
+    standalone_name="printer",
+    version=VERSION,
+)
+
+
+class PrinterCommands(StrEnum):
+    REPORT = "report"
+    STATUS = "status"
