d15b14f827983c089b282a049a0d06031dcbd2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:03:51 +0000

    Add package soda-core-pandas-dask/3.2.1/soda-core-pandas-dask-3.2.1.tar.gz

---
+++ packages/soda-core-pandas-dask/soda-core-pandas-dask-3.2.1.tar.gz/soda-core-pandas-dask-3.2.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-pandas-dask"
+package_version = "3.2.1"
+description = "Soda Core Dask Package"
+
+requires = [f"soda-core=={package_version}", "dask>=2022.10.0", "dask-sql>=2022.12.0,<2023.6.0"]
+
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
