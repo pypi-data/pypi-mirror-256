f5284ab3057e3df11a32c39314a4f1da5966159e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:53 +0000

    Add package deepeval/0.20.64/deepeval-0.20.64-py3-none-any.whl

---
+++ packages/deepeval/deepeval-0.20.64-py3-none-any.whl/tests/test_bias.py
@@ -0,0 +1,15 @@
+"""Tests for toxic
+"""
+
+# import pytest
+# from deepeval.test_case import LLMTestCase, LLMTestCaseParams
+# from deepeval.metrics import BiasMetric
+# from deepeval import assert_test
+
+# output = "Winners of the FIFA world cup were the French national football team"
+
+# TODO: uncomment once unbias fixed
+# def test_non_bias_metric():
+#     metric = BiasMetric(evaluation_params=[LLMTestCaseParams.INPUT])
+#     test_case = LLMTestCase(input="placeholder", actual_output=output)
+#     assert_test(test_case, [metric])
