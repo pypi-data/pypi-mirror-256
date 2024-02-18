629f99bfb3dbaf701756a6f77094c78cd3f9361b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:04 +0000

    Add package pyBrainGenixClient/2024.2.14.2/pyBrainGenixClient-2024.2.14.2-py3-none-any.whl

---
+++ packages/pyBrainGenixClient/pyBrainGenixClient-2024.2.14.2-py3-none-any.whl/BrainGenix/NES/VSDA/Calcium/Configuration.py
@@ -0,0 +1,19 @@
+# BrainGenix-NES
+# AGPLv3
+
+
+class Configuration():
+
+    def __init__(self):
+        
+        # Create Attributes
+        self.PixelResolution_nm:float = None
+        self.ImageWidth_px:int = None
+        self.ImageHeight_px:int = None
+        self.SliceThickness_nm:float = None
+        self.ScanRegionOverlap_percent:float = None 
+        self.MicroscopeFOV_deg:float = None
+        self.NumPixelsPerVoxel_px:int = None
+        self.FlourescingNeuronTags:list = None
+        self.ImagingInterval_ms:float = None
+    
