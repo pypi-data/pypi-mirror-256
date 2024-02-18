58abf6c49946a17347ddc40fb425932c23a4d709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:05:02 +0000

    Add package depscanner/0.4/depscanner-0.4-py3-none-any.whl

---
+++ packages/depscanner/depscanner-0.4-py3-none-any.whl/depscanner/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : nickdecodes
+@Email   : nickdecodes@163.com
+@Usage   :
+@FileName: __init__.py
+@DateTime: 2024/1/29 16:48
+@SoftWare:
+"""
+
+from .depscanner import DependencyScanner
+
+__all__ = ['DependencyScanner']
