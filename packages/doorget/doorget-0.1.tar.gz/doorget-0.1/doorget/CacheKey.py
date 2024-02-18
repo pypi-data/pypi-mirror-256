1e51b6b9acc58f3a9cbec8318fa616137eeb9de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:04 +0000

    Add package doorget/0.1/doorget-0.1.tar.gz

---
+++ packages/doorget/doorget-0.1.tar.gz/doorget-0.1/doorget/CacheKey.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+
+class CacheKey:
+    kwargs: Tuple
+
+    def __init__(self, kwargs) -> None:
+        self.kwargs = kwargs
+    def __eq__(self, another: 'CacheKey') -> bool:
+        return hasattr(another, 'kwargs') and self.kwargs == another.kwargs
+    def __hash__(self) -> int:
+        return hash(self.kwargs)
+    def __str__(self) -> str:
+        return self.kwargs.__str__()
+    def __repr__(self) -> str:
+        return self.kwargs.__repr__()
