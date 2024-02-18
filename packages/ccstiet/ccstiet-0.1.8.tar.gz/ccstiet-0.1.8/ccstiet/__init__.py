c5238a38b102bfbec9b1a60e8385e0ad6b290b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:38 +0000

    Add package ccstiet/0.1.8/ccstiet-0.1.8.tar.gz

---
+++ packages/ccstiet/ccstiet-0.1.8.tar.gz/ccstiet-0.1.8/ccstiet/__init__.py
@@ -0,0 +1,4 @@
+__title__ = 'ccstiet'
+__version__ = '0.1.8'
+
+from .ccstiet import *
---
+++ packages/ccstiet/ccstiet-0.1.8.tar.gz/ccstiet-0.1.8/ccstiet/ccstiet.py
@@ -0,0 +1,70 @@
+
+class exbo(object):
+  # constructor
+  def __init__(self):
+    pass
+
+
+def abhinav():
+    print("Abhinav Jain")
+
+def adrija():
+    print("Adrija Sengupta")
+
+def akarsh():
+    print("Akarsh Shrivastava")
+
+def armaan():
+    print("Armaan Mittal")
+
+def dev():
+    print("Dev Goyal")
+
+def dinesh():
+    print("Dinesh Agrawal")
+
+def gopal():
+    print("Gopal Agarwal")
+
+def gurman():
+    print("Gurman Kaur")
+
+def hushraj():
+    print("Hushraj Singh")
+
+def jahanvi():
+    print("Jahanvi Srivastava")
+
+def jasdeep():
+    print("Jasdeep Singh")
+
+def kanishk():
+    print("Kanishk Jagya")
+
+def pancham():
+    print("Pancham Agarwal")
+
+def pariansh():
+    print("Pariansh Mahajan")
+
+def reyan():
+    print("Reyan Singh")
+
+def saaransh():
+    print("Saaransh Gupta")
+
+def sharath():
+    print("Sharath Chandra Chinnarigari")
+
+def shaurya():
+    print("Shaurya Verma")
+
+def sparsh():
+    print("Sparsh Rastogi")
+
+def swapnil():
+    print("Swapnil Chhibber")
+
+
+if __name__ == '__main__':
+  pass
