21f943a3c43e9839d9441dc9a7a6932d86afcd8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:04:39 +0000

    Add package kg77-distributions/0.1/kg77-distributions-0.1.tar.gz

---
+++ packages/kg77-distributions/kg77-distributions-0.1.tar.gz/kg77-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kg77-distributions',
+      version='0.1',
+      description='Gaussian, Normal & Binomial distributions',
+      packages=['kg77-distributions'],
+      author='Khalid ElGazzar',
+      author_email='khalid.elgazzar@example.com',
+      zip_safe=False)
