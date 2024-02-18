a92cbb7ab63e27063e53364bda38f9587ab4f90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:13 +0000

    Add package edc-utils/0.3.31/edc-utils-0.3.31.tar.gz

---
+++ packages/edc-utils/edc-utils-0.3.31.tar.gz/edc-utils-0.3.31/edc_utils/dashboard_middleware_check.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.core.checks import Error
+from django.core.management import color_style
+
+style = color_style()
+
+
+def edc_middleware_check(
+    app_configs, app_label=None, middleware_name=None, error_code=None, **kwargs
+):
+    errors = []
+    if middleware_name not in settings.MIDDLEWARE:
+        errors.append(
+            Error(
+                "Missing MIDDLEWARE. " f"Expected `{middleware_name}`.",
+                id=f"{app_label}.{error_code or '001'}",
+            )
+        )
+    return errors
