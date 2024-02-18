443c37e54b255363f8dc4a93986be5c0e8881f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:52 +0000

    Add package Abdurrahman/1.2/Abdurrahman-1.2.tar.gz

---
+++ packages/Abdurrahman/Abdurrahman-1.2.tar.gz/Abdurrahman-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Abdurrahman',
+      version='1.02',
+      description='Gaussian distributions',
+      packages=['Abdurrahman'],
+      auther = "Abdurrahman",
+      auther_emai = "abdurrahmanmorsi@gmail.com",
+      zip_safe=False)
