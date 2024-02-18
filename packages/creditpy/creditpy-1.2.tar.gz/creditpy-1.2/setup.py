265a9291addfd5cbe7f69c9a51a4b6c4971468e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:05:04 +0000

    Add package creditpy/1.2/creditpy-1.2.tar.gz

---
+++ packages/creditpy/creditpy-1.2.tar.gz/creditpy-1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='creditpy',
+    version='1.2',
+    packages=find_packages(),
+    author='Ayhan Dis',
+    author_email='disayhan@gmail.com',
+    description='A package for credit scoring analysis',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ayhandis/creditpy',
+    license='MIT',
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'statsmodels',
+        'scipy',
+        'gap_stat',
+        'scikit-learn-extra'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
