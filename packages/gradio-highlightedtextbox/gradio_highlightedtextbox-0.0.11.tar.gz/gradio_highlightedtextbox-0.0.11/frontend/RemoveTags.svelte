b2e620e94aec1f87b11da75b91b397208b98980c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:06:05 +0000

    Add package gradio-highlightedtextbox/0.0.11/gradio_highlightedtextbox-0.0.11.tar.gz

---
+++ packages/gradio-highlightedtextbox/gradio_highlightedtextbox-0.0.11.tar.gz/gradio_highlightedtextbox-0.0.11/frontend/RemoveTags.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Check } from "@gradio/icons";
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher()
+</script>
+ 
+<button
+    on:click={() => dispatch('clear')}
+    title="Remove highlights"
+    aria-roledescription="Remove highlights"
+    aria-label="Remove highlights"
+>
+    <Check />
+</button>
+
+<style>
+	button {
+		position: relative;
+		cursor: pointer;
+		padding: 5px;
+		width: 22px;
+		height: 22px;
+        background-color: green;
+	}
+</style>
