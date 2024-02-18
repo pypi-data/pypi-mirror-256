a264ce5cfa70f15ed2ccd0f4b08284ab5d91247a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:13 +0000

    Add package TilesetParser/1.0.0/TilesetParser-1.0.0.tar.gz

---
+++ packages/TilesetParser/TilesetParser-1.0.0.tar.gz/TilesetParser-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TilesetParser',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.26.4',
+        'opencv-python==4.9.0.80'
+    ],
+    entry_points={
+        'console_scripts': [
+            'tilesetparser=TilesetParser.main:main',
+        ],
+    },
+)
