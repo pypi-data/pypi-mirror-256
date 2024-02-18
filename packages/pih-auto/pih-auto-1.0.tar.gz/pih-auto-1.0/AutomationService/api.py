af7ab1d3e126acb60db4a641e799ab0ebb32e79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:03:45 +0000

    Add package pih-auto/1.0/pih-auto-1.0.tar.gz

---
+++ packages/pih-auto/pih-auto-1.0.tar.gz/pih-auto-1.0/AutomationService/api.py
@@ -0,0 +1,11 @@
+from typing import Any, Callable
+from dataclasses import dataclass
+
+@dataclass
+class TimeTrackingReportConfiguration:
+    division_id: int | None = None
+    folder_name: str | None = None
+    tab_number_list: list[str] | Callable[[None], list[str]] | None = None
+    tab_number_filter_function: Callable[[Mark], bool] | None = None
+    addition_tab_number_list: list[str] | Callable[[None], list[str]] | None = None
+    additional_division_id: int | None = None
