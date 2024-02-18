246c04f6b57b2c7b3bc94bf2a2194427b73ce62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:44 +0000

    Add package yaxis-largecount-tanmay/0.1/yaxis_largecount_tanmay-0.1.tar.gz

---
+++ packages/yaxis-largecount-tanmay/yaxis_largecount_tanmay-0.1.tar.gz/yaxis_largecount_tanmay-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yaxis_largecount_tanmay',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='Y axis for thousands and millions',
+    author='Tanmay Kadam',
+    author_email='tanmay.kdm@gmail.com',
+    url='https://github.com/muffin-head',  # Optional
+    keywords='y axis ease use',
+)
+
