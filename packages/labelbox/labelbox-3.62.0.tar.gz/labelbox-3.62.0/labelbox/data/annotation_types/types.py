29d90bf6e4fdc3ae997814269755454f700deced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:11 +0000

    Add package labelbox/3.62.0/labelbox-3.62.0.tar.gz

---
+++ packages/labelbox/labelbox-3.62.0.tar.gz/labelbox-3.62.0/labelbox/data/annotation_types/types.py
@@ -0,0 +1,50 @@
+import sys
+from typing import Generic, TypeVar, Any
+
+from typing_extensions import Annotated
+from packaging import version
+from pydantic import Field
+from pydantic.fields import ModelField
+import numpy as np
+
+Cuid = Annotated[str, Field(min_length=25, max_length=25)]
+
+DType = TypeVar('DType')
+DShape = TypeVar('DShape')
+
+
+class _TypedArray(np.ndarray, Generic[DType, DShape]):
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, val, field: ModelField):
+        if not isinstance(val, np.ndarray):
+            raise TypeError(f"Expected numpy array. Found {type(val)}")
+
+        if sys.version_info.minor > 6:
+            actual_dtype = field.sub_fields[-1].type_.__args__[0]
+        else:
+            actual_dtype = field.sub_fields[-1].type_.__values__[0]
+
+        if val.dtype != actual_dtype:
+            raise TypeError(
+                f"Expected numpy array have type {actual_dtype}. Found {val.dtype}"
+            )
+        return val
+
+
+if version.parse(np.__version__) >= version.parse('1.25.0'):
+    from typing import GenericAlias
+    TypedArray = GenericAlias(_TypedArray, (Any, DType))
+elif version.parse(np.__version__) >= version.parse('1.23.0'):
+    from numpy._typing import _GenericAlias
+    TypedArray = _GenericAlias(_TypedArray, (Any, DType))
+elif version.parse('1.22.0') <= version.parse(
+        np.__version__) < version.parse('1.23.0'):
+    from numpy.typing import _GenericAlias
+    TypedArray = _GenericAlias(_TypedArray, (Any, DType))
+else:
+    TypedArray = _TypedArray[Any, DType]
---
+++ packages/labelbox/labelbox-3.62.0.tar.gz/labelbox-3.62.0/labelbox/data/serialization/ndjson/base.py
@@ -0,0 +1,58 @@
+from typing import Optional
+from uuid import uuid4
+from pydantic import root_validator, validator
+
+from labelbox.utils import _CamelCaseMixin, is_exactly_one_set
+from ...annotation_types.types import Cuid
+
+
+class DataRow(_CamelCaseMixin):
+    id: str = None
+    global_key: str = None
+
+    @root_validator()
+    def must_set_one(cls, values):
+        if not is_exactly_one_set(values.get('id'), values.get('global_key')):
+            raise ValueError("Must set either id or global_key")
+        return values
+
+
+class NDJsonBase(_CamelCaseMixin):
+    uuid: str = None
+    data_row: DataRow
+
+    @validator('uuid', pre=True, always=True)
+    def set_id(cls, v):
+        return v or str(uuid4())
+
+    def dict(self, *args, **kwargs):
+        """ Pop missing id or missing globalKey from dataRow """
+        res = super().dict(*args, **kwargs)
+        if not self.data_row.id:
+            res['dataRow'].pop('id')
+        if not self.data_row.global_key:
+            res['dataRow'].pop('globalKey')
+        return res
+
+
+class NDAnnotation(NDJsonBase):
+    name: Optional[str] = None
+    schema_id: Optional[Cuid] = None
+    message_id: Optional[str] = None
+    page: Optional[int] = None
+    unit: Optional[str] = None
+
+    @root_validator()
+    def must_set_one(cls, values):
+        if ('schema_id' not in values or values['schema_id']
+                is None) and ('name' not in values or values['name'] is None):
+            raise ValueError("Schema id or name are not set. Set either one.")
+        return values
+
+    def dict(self, *args, **kwargs):
+        res = super().dict(*args, **kwargs)
+        if 'name' in res and res['name'] is None:
+            res.pop('name')
+        if 'schemaId' in res and res['schemaId'] is None:
+            res.pop('schemaId')
+        return res
