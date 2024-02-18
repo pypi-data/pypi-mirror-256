166cf42489a66061bf914faf67b55dfd5166f9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 20:04:17 +0000

    Add package kservehelper/1.1.8/kservehelper-1.1.8-py3-none-any.whl

---
+++ packages/kservehelper/kservehelper-1.1.8-py3-none-any.whl/tests/test_upload.py
@@ -0,0 +1,45 @@
+import os
+import pytest
+import unittest
+from typing import List
+from kservehelper.model import KServeModel
+from kservehelper.types import Input, Path
+
+
+class CustomModel:
+
+    def load(self):
+        pass
+
+    def predict(
+            self,
+            angle: int = Input(
+                description="Rotation angle",
+                default=90
+            )
+    ) -> List[Path]:
+        from PIL import Image
+
+        paths = []
+        folder = os.path.dirname(os.path.abspath(__file__))
+        input_image = Image.open(os.path.join(folder, "data/dog.jpg"))
+        for i in range(4):
+            image = input_image.rotate(angle + i * 10)
+            output_path = f"/tmp/dog_{i}.jpg"
+            image.save(output_path)
+            paths.append(Path(output_path))
+        return paths
+
+
+class TestKServeModel(unittest.TestCase):
+
+    @pytest.mark.skip
+    def test_model(self):
+        payload = {"angle": 45, "upload_webhook": "http://localhost:12000/upload"}
+        model = KServeModel("test", CustomModel)
+        outputs = model.predict(payload)
+        print(outputs)
+
+
+if __name__ == "__main__":
+    unittest.main()
