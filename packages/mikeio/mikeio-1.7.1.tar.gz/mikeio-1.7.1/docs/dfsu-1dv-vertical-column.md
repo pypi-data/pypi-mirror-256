b932706795b60b5a1c98be4092cea3c1bdb184be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:04:10 +0000

    Add package mikeio/1.7.1/mikeio-1.7.1.tar.gz

---
+++ packages/mikeio/mikeio-1.7.1.tar.gz/mikeio-1.7.1/docs/dfsu-1dv-vertical-column.md
@@ -0,0 +1,14 @@
+# Dfsu 1DV Vertical Column
+
+
+
+
+## FM Geometry Vertical Column API
+
+```{eval-rst}
+.. autoclass:: mikeio.spatial._FM_geometry_layered.GeometryFMVerticalColumn
+	:members:
+	:inherited-members:
+```
+
+
---
+++ packages/mikeio/mikeio-1.7.1.tar.gz/mikeio-1.7.1/mikeio/spatial/__init__.py
@@ -0,0 +1,32 @@
+from ._geometry import GeometryPoint3D, GeometryPoint2D, GeometryUndefined
+from ._FM_geometry import (
+    GeometryFM2D,
+    GeometryFMPointSpectrum,
+    GeometryFMLineSpectrum,
+    GeometryFMAreaSpectrum,
+)
+
+from ._FM_geometry_layered import (
+    GeometryFMVerticalColumn,
+    GeometryFMVerticalProfile,
+    GeometryFM3D,
+)
+
+from ._grid_geometry import Grid1D, Grid2D, Grid3D
+
+
+__all__ = [
+    "GeometryPoint3D",
+    "GeometryPoint2D",
+    "GeometryUndefined",
+    "GeometryFM2D",
+    "GeometryFM3D",
+    "GeometryFMPointSpectrum",
+    "GeometryFMLineSpectrum",
+    "GeometryFMAreaSpectrum",
+    "GeometryFMVerticalColumn",
+    "GeometryFMVerticalProfile",
+    "Grid1D",
+    "Grid2D",
+    "Grid3D",
+]
