d3bb91b6c6faf7245a3c62109cabcecba602ad79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:05:36 +0000

    Add package nasa-pace-data-reader/0.0.3.12/nasa_pace_data_reader-0.0.3.12.tar.gz

---
+++ packages/nasa-pace-data-reader/nasa_pace_data_reader-0.0.3.12.tar.gz/nasa_pace_data_reader-0.0.3.12/README.md
@@ -0,0 +1,64 @@
+# NASA-PACE-Data-Reader
+
+This repository hosts a Python package designed to read L1C files from NASA PACE instruments, including HARP2, SPEXone, and OCI. Future development plans include the addition of readers for L2 aerosol and surface products.
+
+## Building and Uploading the Package:
+
+To build and upload the package, you can either run the `sh Install.sh` script (ensure to specify the correct version), or follow the steps outlined below:
+
+---
+
+## Example Usage:
+
+Here is a simple example of how to use the package:
+
+```Python
+from nasa_pace_data_reader import L1
+
+# Location of the file
+fileName = '/Users/aputhukkudy/Downloads/PACE_HARP2.20220321T101844.L1C.5.2KM.V03.SIM2.1_.nc'
+
+# Read the file
+l1c = L1.L1C()
+l1c_dict = l1c.read(fileName)
+
+# Print the keys and the shape of the data
+[print('{:>24}: {}'.format(key, l1c_dict[key].shape)) for key in l1c_dict.keys()][0]
+
+pixel = [250,300]
+
+# Load the plot class
+plt_ = plot.Plot(l1c_dict)
+
+# Set the dpi
+plt_.setDPI(256)
+
+# set which band to plot
+band = 'NIR'
+plt_.setBand(band)
+
+# Plot the pixel
+plt_.plotPixel(pixel[0], pixel[1])
+
+# define the wavelengths and variables to plot
+plt_.setInstrument()
+
+# plot all vars and bands
+plt_.plotPixelVars(pixel[0], pixel[1])
+
+# plot only specific bands and vars
+plt_.vars2plot = ['i', 'q', 'u']    # Order in the list is the order of plotting
+plt_.bands2plot = ['NIR', 'blue']   # Order in the list is the order of plotting
+
+# plot 
+plt_.plotPixelVars(pixel[0], pixel[1], bands= plt_.bands2plot, alpha=0.5, linewidth=0.5) # you can pass any other arguments to the plot function
+```
+
+---
+
+## Change Log:
+
+---
+
+## Key Improvements:
+
---
+++ packages/nasa-pace-data-reader/nasa_pace_data_reader-0.0.3.12.tar.gz/nasa_pace_data_reader-0.0.3.12/src/nasa_pace_data_reader/L1.py
@@ -0,0 +1,121 @@
+import os
+from netCDF4 import Dataset
+
+
+
+class L1C:
+    """Class for reading
+        NASA PACE Level 1C data files.
+        
+    """
+
+    def __init__(self):
+        """Initializes the class."""
+        self.instrument = 'HARP2'   # Default instrument
+        self.product = 'L1C'        # Default product
+        self.projectRGB = True      # Default project to RGB
+
+        # viewing angle to plot
+        self.viewing_angle = 'nadir'    # Default viewing angle options are 'nadir', 'aft' and 'forward'
+    
+    def __instrument__(self, instrument):
+        """Sets the instrument."""
+        self.instrument = instrument
+
+    def __projectRGB__(self, projectRGB):
+        """Sets the projectRGB."""
+        self.projectRGB = projectRGB
+    
+    def __viewing_angle__(self, viewing_angle):
+        """Sets the viewing angle."""
+        self.viewing_angle = viewing_angle   
+
+    def read(self, filename):
+        """Reads the data from the file.
+        
+        Args:
+            filename (str): The name of the file to read.
+
+        Returns:
+            dict: A dictionary containing the data.
+
+        """
+        print(f'Reading {self.instrument} data from {filename}')
+
+        dataNC = Dataset(filename, 'r')
+        data = {}
+
+        try:
+
+            # Access the 'observation_data' & 'geolocation_data' group
+            obs_data = dataNC.groups['observation_data']
+            geo_data = dataNC.groups['geolocation_data']
+            
+            # Define the variable names
+            geo_names = ['latitude', 'longitude', 'scattering_angle', 'solar_zenith_angle', 
+                        'solar_azimuth_angle', 'sensor_zenith_angle', 'sensor_azimuth_angle']
+
+            # Read the variables
+            for var in geo_names:
+                data[var] = geo_data.variables[var][:]
+
+            # Read the data
+            obs_names = ['i', 'q', 'u', 'dolp']
+            for var in obs_names:
+                data[var] = obs_data.variables[var][:]
+
+            # close the netCDF file
+            dataNC.close()
+
+            return data
+
+        except KeyError as e:
+            print(f'Error: {filename} does not contain the required variables.')
+            print('Error:', e)
+
+    
+            # close the netCDF file
+            dataNC.close()
+
+        
+class L1B:
+    """Class for reading
+        NASA PACE Level 1B data files.
+        
+    """
+
+    def __init__(self):
+        """Initializes the class."""
+        self.instrument = 'HARP2'   # Default instrument
+        self.product = 'L1B'        # Default product
+        self.projectRGB = True      # Default project to RGB
+
+        # viewing angle to plot
+        self.viewing_angle = 'nadir'    # Default viewing angle options are 'nadir', 'aft' and 'forward'
+        
+
+    def read(self, filename):
+        """Reads the data from the file."""
+        print(f'Reading {self.instrument} data from {filename}')
+
+        dataNC = Dataset(filename, 'r')
+
+        try:
+            # FIXME: This is just a place holder, needs to be updated
+            # Read latitude and longitude
+            lat = dataNC.variables['latitude'][:]
+            lon = dataNC.variables['longitude'][:]
+
+            # Read the data
+            data = dataNC.variables['radiance'][:]
+            
+            # close the netCDF file
+            dataNC.close()
+
+        except KeyError:
+            print(f'Error: {filename} does not contain the required variables.')
+    
+            # close the netCDF file
+            dataNC.close()
+
+
