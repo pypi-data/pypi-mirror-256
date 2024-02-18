b03d987ec33e6c2ac2ccd296010f6af0038fbc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:42 +0000

    Add package nasa-pace-data-reader/0.0.3.7/nasa_pace_data_reader-0.0.3.7-py3-none-any.whl

---
+++ packages/nasa-pace-data-reader/nasa_pace_data_reader-0.0.3.7-py3-none-any.whl/nasa_pace_data_reader/L1.py
@@ -0,0 +1,18 @@
+class L1C:
+    """Class for reading
+        NASA PACE Level 1C data files.
+        
+    """
+
+    def __init__(self):
+        """Initializes the class."""
+        self.instrument = 'HARP2'   # Default instrument
+
+    def add(self, x, y):
+        """Adds two numbers."""
+        return x + y
+
+    def multiply(self, x, y):
+        """Multiplies two numbers."""
+        return x * y
+
