24b42119c1e9f91f2d0bd35cd2d5d1443b054d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:02 +0000

    Add package structifyai/0.0.1/structifyai-0.0.1-py3-none-any.whl

---
+++ packages/structifyai/structifyai-0.0.1-py3-none-any.whl/structifyai/models/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+# flake8: noqa
+"""
+    Structify
+
+    Structify's goal is to take every source of knowledge and represent it on one place, deduplicated in the format that you specify.
+
+    Version: 0.1.0
+
+    Contact: team@structify.ai
+"""
+
+
+# import models into model package
