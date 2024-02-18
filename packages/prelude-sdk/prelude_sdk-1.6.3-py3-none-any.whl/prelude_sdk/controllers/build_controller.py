37a1b81630ad7a85b440d4cf5d52a41dccdc9bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:27 +0000

    Add package prelude-sdk/1.6.3/prelude_sdk-1.6.3-py3-none-any.whl

---
+++ packages/prelude-sdk/prelude_sdk-1.6.3-py3-none-any.whl/prelude_sdk/controllers/build_controller.py
@@ -0,0 +1,82 @@
+import requests
+
+from prelude_sdk.models.account import verify_credentials
+
+
+class BuildController:
+
+    def __init__(self, account):
+        self.account = account
+
+    @verify_credentials
+    def create_test(self, name, unit, technique=None, advisory=None, test_id=None):
+        """ Create or update a test """
+        body = dict(name=name, unit=unit)
+        if technique:
+            body['technique'] = technique
+        if advisory:
+            body['advisory'] = advisory
+        if test_id:
+            body['id'] = test_id
+
+        res = requests.post(
+            f'{self.account.hq}/build/tests',
+            json=body,
+            headers=self.account.headers,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def update_test(self, test_id, name=None, unit=None, technique=None, advisory=None):
+        """ Update a test """
+        body = dict()
+        if name:
+            body['name'] = name
+        if unit:
+            body['unit'] = unit
+        if technique is not None:
+            body['technique'] = technique
+        if advisory is not None:
+            body['advisory'] = advisory
+
+        res = requests.post(
+            f'{self.account.hq}/build/tests/{test_id}',
+            json=body,
+            headers=self.account.headers,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def delete_test(self, test_id):
+        """ Delete an existing test """
+        res = requests.delete(
+            f'{self.account.hq}/build/tests/{test_id}',
+            headers=self.account.headers,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def upload(self, test_id, filename, data):
+        """ Upload a test or attachment """
+        if len(data) > 1000000:
+            raise ValueError(f'File size must be under 1MB ({filename})')
+
+        h = self.account.headers | {'Content-Type': 'application/octet-stream'}
+        res = requests.post(
+            f'{self.account.hq}/build/tests/{test_id}/{filename}',
+            data=data,
+            headers=h,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
---
+++ packages/prelude-sdk/prelude_sdk-1.6.3-py3-none-any.whl/prelude_sdk/controllers/detect_controller.py
@@ -0,0 +1,161 @@
+import requests
+
+from prelude_sdk.models.codes import RunCode
+from prelude_sdk.models.account import verify_credentials
+
+
+class DetectController:
+
+    def __init__(self, account):
+        self.account = account
+
+    @verify_credentials
+    def register_endpoint(self, host, serial_num, tags=None):
+        """ Register (or re-register) an endpoint to your account """
+        body = dict(id=f'{host}:{serial_num}')
+        if tags:
+            body['tags'] = tags
+
+        res = requests.post(
+            f'{self.account.hq}/detect/endpoint',
+            headers=self.account.headers,
+            json=body,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.text
+        raise Exception(res.text)
+
+    @verify_credentials
+    def update_endpoint(self, endpoint_id, tags=None):
+        """ Update an endpoint in your account """
+        body = dict()
+        if tags is not None:
+            body['tags'] = tags
+
+        res = requests.post(
+            f'{self.account.hq}/detect/endpoint/{endpoint_id}',
+            headers=self.account.headers,
+            json=body,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def delete_endpoint(self, ident: str):
+        """ Delete an endpoint from your account """
+        params = dict(id=ident)
+        res = requests.delete(
+            f'{self.account.hq}/detect/endpoint',
+            headers=self.account.headers,
+            json=params,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def list_endpoints(self, days: int = 90):
+        """ List all endpoints on your account """
+        params = dict(days=days)
+        res = requests.get(
+            f'{self.account.hq}/detect/endpoint',
+            headers=self.account.headers,
+            params=params,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def list_advisories(self):
+        """ List advisories """
+        res = requests.get(
+            f'{self.account.hq}/detect/advisories',
+            headers=self.account.headers,
+            params={},
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def describe_activity(self, filters: dict, view: str = 'protected'):
+        """ Get report for an Account """
+        params = dict(view=view, **filters)
+        res = requests.get(
+            f'{self.account.hq}/detect/activity',
+            headers=self.account.headers,
+            params=params,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def list_tests(self):
+        """ List all tests available to an account """
+        res = requests.get(
+            f'{self.account.hq}/detect/tests',
+            headers=self.account.headers,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def get_test(self, test_id):
+        """ Get properties of an existing test """
+        res = requests.get(
+            f'{self.account.hq}/detect/tests/{test_id}',
+            headers=self.account.headers,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def download(self, test_id, filename):
+        """ Clone a test file or attachment"""
+        res = requests.get(
+            f'{self.account.hq}/detect/tests/{test_id}/{filename}',
+            headers=self.account.headers,
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.content
+        raise Exception(res.text)
+
+    @verify_credentials
+    def enable_test(self, ident: str, run_code: RunCode, tags: str):
+        """ Enable a test so endpoints will start running it """
+        res = requests.post(
+            url=f'{self.account.hq}/detect/queue/{ident}',
+            headers=self.account.headers,
+            json=dict(code=run_code.name, tags=tags),
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
+
+    @verify_credentials
+    def disable_test(self, ident: str, tags: str):
+        """ Disable a test so endpoints will stop running it """
+        res = requests.delete(
+            f'{self.account.hq}/detect/queue/{ident}',
+            headers=self.account.headers,
+            params=dict(tags=tags),
+            timeout=10
+        )
+        if res.status_code == 200:
+            return res.json()
+        raise Exception(res.text)
