7e523287bb27a4305e52b372aeb55bf1d6f9baa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:03:50 +0000

    Add package splinepy/0.0.52/splinepy-0.0.52-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/splinepy/splinepy-0.0.52-cp311-cp311-macosx_11_0_arm64.whl/splinepy/helpme/check.py
@@ -0,0 +1,83 @@
+import numpy as _np
+
+
+def valid_queries(spline, queries):
+    """Check queries
+
+    Check if queries are valid for a specific spline, so not to exceed
+    parametric bounds for requests
+
+    Parameters
+    ----------
+    spline : Spline-Type
+      spline that sets the bounds
+    queries : np.array
+      Requested queries
+
+    Returns
+    -------
+    valid : true
+    """
+    bounds = spline.parametric_bounds
+    # Check dimensions
+    if bounds.shape[1] != queries.shape[1]:
+        raise ValueError(
+            f"Dimension mismatch between parametric dimension of "
+            f"spline ({spline.para_dim}), and query-request "
+            f"({queries.shape[1]})."
+        )
+
+    # Check minimum value
+    min_query = _np.min(queries, axis=0)
+    if _np.any(bounds[0, :] > min_query):
+        error_dim = _np.where(bounds[1, :] > min_query)[0][0]
+        error_query = _np.argmin(queries, axis=0)[error_dim]
+        raise ValueError(
+            f"Query request out of bounds in parametric dimension "
+            f"{error_dim}. Detected query {queries[error_query,:]} at "
+            f"positions {error_query}, which is out of bounds with "
+            f"minimum values {bounds[1,:]}."
+        )
+
+    # Check maximum value
+    max_query = _np.max(queries, axis=0)
+    if _np.any(bounds[1, :] < max_query):
+        error_dim = _np.where(bounds[1, :] < max_query)[0][0]
+        error_query = _np.argmax(queries, axis=0)[error_dim]
+        raise ValueError(
+            f"Query request out of bounds in parametric dimension "
+            f"{error_dim}. Detected query {queries[error_query,:]} at "
+            f"positions {error_query}, which is out of bounds with "
+            f"maximum values {bounds[1,:]}."
+        )
+    return True
+
+
+class Checker:
+    """Helper class to allow direct extraction from spline obj (BSpline or
+    NURBS). Internal use only.
+
+    Examples
+    ---------
+
+    .. code-block :: python
+
+      spline.check.valid_queries(queries)
+
+    Parameters
+    ----------
+    spline : Spline
+      Parent spline, that is to be checked
+    """
+
+    __slots__ = ("_helpee",)
+
+    def __init__(self, spl):
+        self._helpee = spl
+
+    def valid_queries(self, queries):
+        return valid_queries(self._helpee, queries)
+
+
+# Use function docstrings in Checker functions
+Checker.valid_queries.__doc__ = valid_queries.__doc__
