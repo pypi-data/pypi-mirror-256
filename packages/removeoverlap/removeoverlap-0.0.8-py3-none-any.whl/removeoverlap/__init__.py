2db0544a1c3bce7474fcb4ce765b8ff3b1aba978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 01:26:32 +0000

    Add package removeoverlap/0.0.8/removeoverlap-0.0.8-py3-none-any.whl

---
+++ packages/removeoverlap/removeoverlap-0.0.8-py3-none-any.whl/removeoverlap/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+{{docstring}}
+"""
+
+from removeoverlap.chan import measure
+from removeoverlap.decompose import remove_overlap
+from removeoverlap.model.edge import Edge
+from removeoverlap.model.hrectangle import HRectangle
