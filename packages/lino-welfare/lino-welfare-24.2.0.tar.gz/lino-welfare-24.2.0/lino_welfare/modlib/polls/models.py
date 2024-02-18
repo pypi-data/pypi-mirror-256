b3eb438fb81294dae8d2d1d3e975ee8a59c8b5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:30 +0000

    Add package lino-welfare/24.2.0/lino-welfare-24.2.0.tar.gz

---
+++ packages/lino-welfare/lino-welfare-24.2.0.tar.gz/lino-welfare-24.2.0/lino_welfare/modlib/polls/models.py
@@ -0,0 +1,8 @@
+# Copyright 2014-2015 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+
+"""Database models for `lino_welfare.modlib.polls`.
+
+"""
+
+from lino_xl.lib.polls.models import *
