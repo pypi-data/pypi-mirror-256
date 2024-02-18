116598fdd1c3bfd93dea823f9b22ccf7974d7319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:55 +0000

    Add package livestt/1.0.1/livestt-1.0.1.tar.gz

---
+++ packages/livestt/livestt-1.0.1.tar.gz/livestt-1.0.1/livestt/wake_word/wake_word.py
@@ -0,0 +1,45 @@
+from transformers.pipelines.audio_utils import ffmpeg_microphone_live
+from typing import Callable
+from transformers import pipeline
+import torch
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+classifier = pipeline(
+    "audio-classification", model="MIT/ast-finetuned-speech-commands-v2", device=device
+)
+
+
+def wait(
+    callback: Callable,
+    wake_word="sheila",
+    prob_threshold=0.5,
+    chunk_length_s=2.0,
+    stream_chunk_s=0.25,
+    debug=True,
+):
+    if wake_word not in classifier.model.config.label2id.keys():
+        raise ValueError(
+            f"Wake word {wake_word} not in set of valid class labels, pick a wake word in the set {classifier.model.config.label2id.keys()}."
+        )
+
+    sampling_rate = classifier.feature_extractor.sampling_rate
+
+    mic = ffmpeg_microphone_live(
+        sampling_rate=sampling_rate,
+        chunk_length_s=chunk_length_s,
+        stream_chunk_s=stream_chunk_s,
+    )
+
+    print("Listening for wake word...")
+    for prediction in classifier(mic):
+        prediction = prediction[0]
+        if debug:
+            print(prediction)
+        if prediction["label"] == wake_word:
+            if prediction["score"] > prob_threshold:
+                return True
+
+
+if __name__ == "__main__":
+    wait()
---
+++ packages/livestt/livestt-1.0.1.tar.gz/livestt-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.1'
+DESCRIPTION = "Simple and easy to use realtime speech to text"
+# Setting up
+setup(
+    name="livestt",
+    version=VERSION,
+    author="a3l6",
+    author_email="<emen3998@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
