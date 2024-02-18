457bc79892d04767f0d8893186d0099d6960314f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:36 +0000

    Add package vidsrc-search/0.0.6/vidsrc-search-0.0.6.tar.gz

---
+++ packages/vidsrc-search/vidsrc-search-0.0.6.tar.gz/vidsrc-search-0.0.6/src/vidsrc_search/__main__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import warnings
+
+warnings.filterwarnings("ignore")
+
+from . import utils
+from . import argparsing
+
+
+def main():
+    try:
+        utils.bootstrap()
+        argparsing.parse_args()
+        utils.cleanup()
+    except KeyboardInterrupt:
+        print()
+        print(" [Warning] Vidsrc-search received KeyboardInterrupt")
+        print(" [Warning] Program force quitting with os._exit()")
+        print(" [Warning] Performing program cleanup process")
+        utils.cleanup()
+        os._exit(0)
+    except Exception as e:
+        print()
+        print(f" [Fatal] An unknown error was encountered during the execution of vidsrc-search")
+        print(f" [Fatal] Error message from program: {e}")
+        print(f" [Fatal] Performing program cleanup process")
+        utils.cleanup()
+        sys.exit(129)
+
+
+if __name__ == "__main__":
+    main()
