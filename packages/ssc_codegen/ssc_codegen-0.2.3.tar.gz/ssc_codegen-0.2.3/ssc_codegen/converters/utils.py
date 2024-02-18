195c2cc949e37c113fea346b72526ae39bb07d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:03 +0000

    Add package ssc_codegen/0.2.3/ssc_codegen-0.2.3.tar.gz

---
+++ packages/ssc_codegen/ssc_codegen-0.2.3.tar.gz/ssc_codegen-0.2.3/ssc_codegen/converters/utils.py
@@ -0,0 +1,16 @@
+import re
+
+
+def sanitize_regex(s: str, escape_ch: str = "\\") -> str:
+    """sanitize matching characters. Used, for trim, l_trim, r_trim realisations by regex"""
+
+    def _repl(ch: re.Match[str]) -> str:
+        return escape_ch + ch[0]
+
+    return re.sub(r"[\-.^$*+?{}\[\]\\|()]", _repl, s)
+
+
+def wrap_regex_pattern(s: str, prefix: str = "r", escape_ch: str = "\\") -> str:
+    if '"' in s:
+        s = s.replace('"', escape_ch + '"')
+    return prefix + '"' + s + '"'
