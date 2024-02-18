b0e4edbaa3804f19427e20bfa3d01bac1cc9877a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 11:04:14 +0000

    Add package plcrex/2.0.0/plcrex-2.0.0-py3-none-any.whl

---
+++ packages/plcrex/plcrex-2.0.0-py3-none-any.whl/plcrex/__init__.py
@@ -0,0 +1,22 @@
+#
+# This file is part of PLCreX (https://github.com/marwern/PLCreX).
+#
+# Copyright (c) 2022-2024 Marcel C. Werner.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+__app_name__ = "plcrex"
+__version__ = "2.0.0"
+__owner__ = "Marcel C. Werner"
+__copyright__ = "2022-2024, " + __owner__
