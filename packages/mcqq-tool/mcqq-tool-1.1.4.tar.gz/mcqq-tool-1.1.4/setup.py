a602979a26096d8da7b8f2f283ef7ab78ff4995c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:05 +0000

    Add package mcqq-tool/1.1.4/mcqq-tool-1.1.4.tar.gz

---
+++ packages/mcqq-tool/mcqq-tool-1.1.4.tar.gz/mcqq-tool-1.1.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="mcqq-tool",
+    version="1.1.4",
+    author="17TheWord",
+    author_email="17theword@gmail.com",
+    description="MC_QQ 工具包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MineGraphCN/mcqq-tool",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent"
+    ]
+)
