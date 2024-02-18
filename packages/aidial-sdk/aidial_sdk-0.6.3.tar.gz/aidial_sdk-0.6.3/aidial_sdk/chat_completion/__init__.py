34c66d54534ea0fe44fd21289c0c3b7b193ef28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:04:20 +0000

    Add package aidial-sdk/0.6.3/aidial_sdk-0.6.3.tar.gz

---
+++ packages/aidial-sdk/aidial_sdk-0.6.3.tar.gz/aidial_sdk-0.6.3/aidial_sdk/chat_completion/__init__.py
@@ -0,0 +1,19 @@
+from aidial_sdk.chat_completion.base import ChatCompletion
+from aidial_sdk.chat_completion.choice import Choice
+from aidial_sdk.chat_completion.enums import FinishReason, Status
+from aidial_sdk.chat_completion.request import (
+    Addon,
+    Attachment,
+    CustomContent,
+    Function,
+    FunctionCall,
+    FunctionChoice,
+    Message,
+    Request,
+    Role,
+    Tool,
+    ToolCall,
+    ToolChoice,
+)
+from aidial_sdk.chat_completion.response import Response
+from aidial_sdk.chat_completion.stage import Stage
