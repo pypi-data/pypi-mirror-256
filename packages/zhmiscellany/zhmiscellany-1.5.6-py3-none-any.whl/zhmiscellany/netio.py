fb52dc78c1820c2b70164fad115b8930d49b8cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:58 +0000

    Add package zhmiscellany/1.5.6/zhmiscellany-1.5.6-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-1.5.6-py3-none-any.whl/zhmiscellany/netio.py
@@ -0,0 +1,33 @@
+import os, requests
+import zhmiscellany.string
+import urllib.parse
+
+
+def resolve_file(url, destination_folder="."):
+    file_name = urllib.parse.unquote(url.split("/")[-1])
+    destination_path = f"{destination_folder}/{file_name}"
+    if len(destination_path) > 250:
+        destination_path = f'{destination_path[:(250 - len(os.path.splitext(destination_path)[1]))]}{os.path.splitext(destination_path)[1]}'
+    return destination_path
+
+
+def download_file(url, destination_folder=".", just_return_path=False):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        # Get the file name from the URL
+        destination_path = resolve_file(url, destination_folder)
+
+        if os.path.exists(destination_path):
+            if just_return_path:
+                return destination_path
+            return False
+
+        # Save the file
+        with open(destination_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=128):
+                file.write(chunk)
+
+        return destination_path
+
+    else:
+        raise Exception(f"Failed to download the file. Status code: {response.status_code}")
