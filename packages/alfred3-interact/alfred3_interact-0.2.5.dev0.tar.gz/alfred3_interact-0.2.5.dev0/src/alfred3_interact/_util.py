bfc87fb208e70b99c8f101b3fc09f3a63de7607b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:04:46 +0000

    Add package alfred3-interact/0.2.5.dev0/alfred3_interact-0.2.5.dev0.tar.gz

---
+++ packages/alfred3-interact/alfred3_interact-0.2.5.dev0.tar.gz/alfred3_interact-0.2.5.dev0/src/alfred3_interact/_util.py
@@ -0,0 +1,29 @@
+"""
+Soome basic utilities.
+"""
+
+
+def saving_method(exp) -> str:
+    if not exp.secrets.getboolean("mongo_saving_agent", "use"):
+        if exp.config.getboolean("local_saving_agent", "use"):
+            return "local"
+    elif exp.secrets.getboolean("mongo_saving_agent", "use"):
+        return "mongo"
+    else:
+        return None
+
+
+class MatchingTimeout(Exception):
+    pass
+
+
+class MatchingError(Exception):
+    pass
+
+
+class BusyGroup(Exception):
+    pass
+
+
+class NoMatch(Exception):
+    pass
