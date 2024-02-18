a482056f04ed0f3554fdd39c3d87074364c0c829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:13 +0000

    Add package scos-distributions-beta/0.1/scos_distributions_beta-0.1.tar.gz

---
+++ packages/scos-distributions-beta/scos_distributions_beta-0.1.tar.gz/scos_distributions_beta-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='scos_distributions_beta',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['scos_distributions_beta'],
+      author = 'scoscot',
+      zip_safe=False)
