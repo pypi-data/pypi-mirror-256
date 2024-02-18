cf70cdf0cad5b68b0b1200655078aac38c42a288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:06:48 +0000

    Add package deciphon/0.11.7/deciphon-0.11.7.tar.gz

---
+++ packages/deciphon/deciphon-0.11.7.tar.gz/deciphon-0.11.7/deciphon/progress.py
@@ -0,0 +1,42 @@
+from time import sleep
+import rich.progress
+from threading import Event, Thread
+from deciphon_core.scan import Scan
+
+
+class Progress:
+    def __init__(self, scan: Scan, disabled=False):
+        self._continue = Event()
+        self._scan = scan
+        self._thread = Thread(target=self.progress_entry)
+        self._disabled = disabled
+
+    def start(self):
+        if not self._disabled:
+            self._thread.start()
+
+    def progress_entry(self):
+        with rich.progress.Progress() as progress:
+            task = progress.add_task("Scanning", total=100)
+            while not self._continue.is_set():
+                progress.update(task, completed=self._scan.progress())
+                if not progress.console.is_interactive:
+                    progress.print(progress)
+                    sleep(1.15)
+                else:
+                    sleep(0.35)
+            progress.update(task, completed=self._scan.progress())
+            if not progress.console.is_interactive:
+                progress.print(progress)
+
+    def stop(self):
+        if not self._disabled:
+            self._continue.set()
+            self._thread.join()
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *_):
+        self.stop()
