f2b5dba64657194e25e4960f65cc39347ff515c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:08:35 +0000

    Add package lexisnexisapi/3.0.3.13/lexisnexisapi-3.0.3.13.tar.gz

---
+++ packages/lexisnexisapi/lexisnexisapi-3.0.3.13.tar.gz/lexisnexisapi-3.0.3.13/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = '3.0.3.13'
+
+#I think I can get rid of this, but I'll test next time.
+#def read(fname):
+#    return open(os.path.join(os.getcwd(), fname)).read()
+
+setup(
+    name="lexisnexisapi",
+    version= VERSION,
+    license='MIT',
+    author="Robert Cuffney",
+    author_email='Robert Cuffney <robert.cuffney@lexisnexis.com>',
+    description = 'a module to support lexisnexis api(s)',
+    long_description = open('README.md').read(),
+    long_description_content_type='text/markdown', 
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    url='https://pypi.org/project/lexisnexisapi/',
+    keywords='metabase webservices lexisnexis',
+    install_requires=['pandas','requests','xmltodict',],
+)
