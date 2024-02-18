878847621c2747f5aeaa3a3a04bd567337ab4d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 04:03:30 +0000

    Add package resumake/2.0.0/resumake-2.0.0.tar.gz

---
+++ packages/resumake/resumake-2.0.0.tar.gz/resumake-2.0.0/src/resumake/components/project.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass(kw_only=True)
+class Project:
+    name: str
+    url: str
+    desc: str
+    skills: list[str]
+
+    def __str__(self) -> str:
+        return f"**{self.name}** ([{self.url}](https://{self.url})) {self.desc} **[{', '.join(self.skills)}]**"
