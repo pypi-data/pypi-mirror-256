3b5b85c61599f63f3ae10b299e9930b3c0071869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:03:54 +0000

    Add package stestvtestdistributions/2.0/stestvtestdistributions-2.0.tar.gz

---
+++ packages/stestvtestdistributions/stestvtestdistributions-2.0.tar.gz/stestvtestdistributions-2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='stestvtestdistributions',
+      version='2.0',
+      description='Gaussian and Binomial distributions',
+      packages=['stestvtestdistributions'],
+      author='Swathi',
+      zip_safe=False)
