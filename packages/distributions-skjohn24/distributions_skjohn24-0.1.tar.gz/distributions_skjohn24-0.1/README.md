53e6fe49ee23131186850fc96173263171b26f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:07 +0000

    Add package distributions-skjohn24/0.1/distributions_skjohn24-0.1.tar.gz

---
+++ packages/distributions-skjohn24/distributions_skjohn24-0.1.tar.gz/distributions_skjohn24-0.1/README.md
@@ -0,0 +1,9 @@
+# distributions_sjohn24 package
+
+ Summary of the package
+
+# Files
+
+Explanation of the package
+
+# Installation
---
+++ packages/distributions-skjohn24/distributions_skjohn24-0.1.tar.gz/distributions_skjohn24-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_skjohn24',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_skjohn24'],
+      author='skjohn',
+      email='testing@testing.com',
+      zip_safe=False)
