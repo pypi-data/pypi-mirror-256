5df22d3b21e03ddcf2ec4d5932774db1a015c8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 16:04:16 +0000

    Add package ai-assistant-by-timurkarev/0.1.0/ai_assistant_by_timurkarev-0.1.0.tar.gz

---
+++ packages/ai-assistant-by-timurkarev/ai_assistant_by_timurkarev-0.1.0.tar.gz/ai_assistant_by_timurkarev-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ai_assistant_by_timurkarev',
+    version='0.1.0',
+    description='AI Assistant',
+    author='Timur Karev',
+    long_description='it is my own package, nothing interesting'
+    
+    )
