0aa332bb284f1a9629b168c75fb613c17357418b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:10:40 +0000

    Add package logiclayer-complexity/0.4.1/logiclayer_complexity-0.4.1.tar.gz

---
+++ packages/logiclayer-complexity/logiclayer_complexity-0.4.1.tar.gz/logiclayer_complexity-0.4.1/logiclayer_complexity/__init__.py
@@ -0,0 +1,9 @@
+from .module import EconomicComplexityModule
+from .__version__ import __description__, __title__, __version__
+
+__all__ = (
+    "__title__",
+    "__description__",
+    "__version__",
+    "EconomicComplexityModule",
+)
