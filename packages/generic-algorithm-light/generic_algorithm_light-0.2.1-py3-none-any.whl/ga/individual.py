159d7e0f22b18d515177d79da16792d609a2753f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 02:12:01 +0000

    Add package generic-algorithm-light/0.2.1/generic_algorithm_light-0.2.1-py3-none-any.whl

---
+++ packages/generic-algorithm-light/generic_algorithm_light-0.2.1-py3-none-any.whl/ga/individual.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Dict, List
+
+import numpy as np
+
+
+class IndividualBase(ABC):
+    @abstractmethod
+    def get_values(self):
+        pass
+
+
+class Individual:
+    def __init__(self, params: Dict, value: List[np.array] = None):
+        if value is None:
+            self.value = [
+                np.random.uniform(params["lower_bound"], params["upper_bound"], 1)[0]
+                for _ in range(params["number_of_genes"])
+            ]
+        else:
+            self.value = value
+
+    def get_values(self) -> List[np.array]:
+        return self.value
