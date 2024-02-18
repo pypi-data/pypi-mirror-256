25c8d5673edf6f0100c6bc4ad29fa832618e244e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:40 +0000

    Add package pyiso4/0.1.6/pyiso4-0.1.6-py3-none-any.whl

---
+++ packages/pyiso4/pyiso4-0.1.6-py3-none-any.whl/pyiso4/__init__.py
@@ -0,0 +1,9 @@
+"""
+Abbreviate a scientific journal title following the ISO-4 rules
+"""
+
+__version__ = '0.1.6'
+__author__ = 'Pierre Beaujean'
+__maintainer__ = 'Pierre Beaujean'
+__email__ = 'pierre.beaujean@unamur.be'
+__status__ = 'Development'
