c9859af51a9bb82ce80068959a423e479a213ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:01 +0000

    Add package pih-event/1.25/pih_event-1.25-py3-none-any.whl

---
+++ packages/pih-event/pih_event-1.25-py3-none-any.whl/EventService/const.py
@@ -0,0 +1,24 @@
+import ipih
+
+from pih.collections.service import ServiceDescription
+from pih.consts.hosts import Hosts
+
+NAME: str = "Event"
+
+HOST = Hosts.BACKUP_WORKER
+
+VERSION: str = "1.25"
+
+CONFIG_LOCATION: str = "telegram_send_config"
+
+MODULES: tuple[str, ...] = ("telegram-send",)
+
+SD: ServiceDescription = ServiceDescription(
+    name=NAME,
+    description="Log and Event service",
+    host=HOST.NAME,
+    commands=("send_log_message", "send_event"),
+    standalone_name="event",
+    use_standalone=True,
+    version=VERSION,
+)
