411da5a60d910bd2a7a8ef67dfec7f92cf5e4f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:03:39 +0000

    Add package python-aiconfig-test/1.1.39/python_aiconfig_test-1.1.39-py3-none-any.whl

---
+++ packages/python-aiconfig-test/python_aiconfig_test-1.1.39-py3-none-any.whl/aiconfig/eval/api/__init__.py
@@ -0,0 +1,27 @@
+"""This file is officially the eval API.    
+
+Usage:
+
+from aiconfig.eval.api import (
+    brevity,
+    substring_match,
+    run_test_suite_with_inputs,
+    TestSuiteWithInputsSettings,
+)
+"""
+from .. import common, metrics
+
+# pyright: reportWildcardImportFromLibrary=false
+from ..lib import TestSuiteWithInputsSettings, run_test_suite_outputs_only, run_test_suite_with_inputs
+from ..metrics import Metric, brevity, substring_match
+
+__all__ = [
+    "Metric",
+    "common",
+    "metrics",
+    "brevity",
+    "substring_match",
+    "run_test_suite_with_inputs",
+    "run_test_suite_outputs_only",
+    "TestSuiteWithInputsSettings",
+]
