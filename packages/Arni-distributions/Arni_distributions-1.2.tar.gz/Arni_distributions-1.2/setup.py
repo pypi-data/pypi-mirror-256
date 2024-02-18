4ed183803032f7237606132f51e9dec2e2f97dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:13 +0000

    Add package Arni-distributions/1.2/Arni_distributions-1.2.tar.gz

---
+++ packages/Arni-distributions/Arni_distributions-1.2.tar.gz/Arni_distributions-1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='Arni_distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['Arni_distributions'],
+      author="Arni khede",
+      author_email="arnikhede03@gmail.com",
+      zip_safe=False)
