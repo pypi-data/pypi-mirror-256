b977f8450db33931bf0a19cce490b204797d2ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:03:52 +0000

    Add package pyntload/0.1.9/pyntload-0.1.9.tar.gz

---
+++ packages/pyntload/pyntload-0.1.9.tar.gz/pyntload-0.1.9/pyntload/helper.py
@@ -0,0 +1,35 @@
+import requests
+import base64
+import json
+import sys,imp
+
+#headers to use
+headers = {
+        "Accept": "application/json,text/javascript,*/*",
+        'Authorization': 'Bearer dapi03fa57be956e3d0a07a050177327334d-2',
+        "Content-Type": "application/json"
+}
+
+#base url
+base_url = "https://adb-7118152657858843.3.azuredatabricks.net/"
+
+def read_databricks_notebook(notebook_path):
+    print("----- read_databricks_notebook -----")
+
+    #get notebook export
+    response = requests.request(
+        "GET",
+        base_url + "api/2.0/workspace/export",
+        headers =headers,
+        params= {
+            'path': notebook_path,
+            'format': 'JUPYTER'
+        }
+    )
+    
+    #parse python code (json to dict)
+    jupyter_code = json.loads(base64.b64decode(response.json()["content"]))
+    jupyter_code_cells = [cell for cell in jupyter_code["cells"] if cell["cell_type"]=="code"]
+    #jupyter_code_cells = [''.join(cell['source']) for cell in jupyter_code_cells]
+        
+    return jupyter_code_cells
