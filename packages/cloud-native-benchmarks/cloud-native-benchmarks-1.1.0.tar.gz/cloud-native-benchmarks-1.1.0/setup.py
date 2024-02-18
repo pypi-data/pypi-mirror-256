c0394d0f52637fe70d6e8773642bb7a8e28820f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:04:43 +0000

    Add package cloud-native-benchmarks/1.1.0/cloud-native-benchmarks-1.1.0.tar.gz

---
+++ packages/cloud-native-benchmarks/cloud-native-benchmarks-1.1.0.tar.gz/cloud-native-benchmarks-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+
+
+setup(name="cloud-native-benchmarks",
+      version='1.1.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False
+)
+
