40f1e49a41bed637f55f395cd792d88fc624f0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:09:06 +0000

    Add package horsetalk/0.22.4/horsetalk-0.22.4-py3-none-any.whl

---
+++ packages/horsetalk/horsetalk-0.22.4-py3-none-any.whl/out/horsetalk/horse.pyi
@@ -0,0 +1,9 @@
+from .horse_age import HorseAge as HorseAge
+from _typeshed import Incomplete
+
+class Horse:
+    REGEX: Incomplete
+    name: Incomplete
+    country: Incomplete
+    age: Incomplete
+    def __init__(self, name, country: Incomplete | None = None, age_or_yob: Incomplete | None = None, *, context_date: Incomplete | None = None) -> None: ...
