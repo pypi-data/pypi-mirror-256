2370666aab7f067b2a301dd593232586d08b23a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 18:04:15 +0000

    Add package distributions-gl-cs/1.1/distributions_gl_cs-1.1.tar.gz

---
+++ packages/distributions-gl-cs/distributions_gl_cs-1.1.tar.gz/distributions_gl_cs-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_gl_cs',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_gl_cs'],
+      author = "Gabriel Cantanhede",
+      author_email = "gabriel.lcsilva@hotmail.com",
+      zip_safe=False)
