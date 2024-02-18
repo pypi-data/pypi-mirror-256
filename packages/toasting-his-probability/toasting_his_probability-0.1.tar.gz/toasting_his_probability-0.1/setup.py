7cb2f412d0a5fd19023db6e6bd1ff332b14f854d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:03:51 +0000

    Add package toasting-his-probability/0.1/toasting_his_probability-0.1.tar.gz

---
+++ packages/toasting-his-probability/toasting_his_probability-0.1.tar.gz/toasting_his_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='toasting_his_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['toasting_his_probability'],
+      author = 'James Jose',
+      author_email = 'jamesjose107@gmail.com',
+      zip_safe=False)
