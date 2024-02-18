53e21ca99577f0fcc8357acf95011ecf10f4fc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:21 +0000

    Add package PyEngine-3D-v3/3.1/PyEngine_3D_v3-3.1.tar.gz

---
+++ packages/PyEngine-3D-v3/PyEngine_3D_v3-3.1.tar.gz/PyEngine_3D_v3-3.1/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+
+setup(python_requires='>=3.10', name='PyEngine_3D_v3', version='3.1', packages=find_packages(),
+      author_email='v6577295@gmail.com', description='3D-engine for Python.')
