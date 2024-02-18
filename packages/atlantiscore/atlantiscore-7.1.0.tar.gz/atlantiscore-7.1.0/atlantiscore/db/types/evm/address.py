3c6ea245ba67f69415740f397b277cf356c137d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:05:00 +0000

    Add package atlantiscore/7.1.0/atlantiscore-7.1.0.tar.gz

---
+++ packages/atlantiscore/atlantiscore-7.1.0.tar.gz/atlantiscore-7.1.0/atlantiscore/db/types/evm/address.py
@@ -0,0 +1,14 @@
+from sqlalchemy.types import LargeBinary
+
+from atlantiscore.db.types.evm.base import ByteEncoding
+from atlantiscore.types.evm import EVMAddress as PythonEVMAddress, LiteralByteEncoding
+
+BYTE_COUNT = 20
+
+
+class EVMAddress(ByteEncoding):
+    _default_type: LargeBinary = LargeBinary(BYTE_COUNT)
+
+    @staticmethod
+    def _parse(value: PythonEVMAddress | LiteralByteEncoding) -> PythonEVMAddress:
+        return PythonEVMAddress(value)
---
+++ packages/atlantiscore/atlantiscore-7.1.0.tar.gz/atlantiscore-7.1.0/atlantiscore/db/types/evm/transaction.py
@@ -0,0 +1,19 @@
+from sqlalchemy.types import LargeBinary
+
+from atlantiscore.db.types.evm.base import ByteEncoding
+from atlantiscore.types.evm import (
+    EVMTransactionHash as PythonTransactionHash,
+    LiteralByteEncoding,
+)
+
+BYTE_COUNT = 32
+
+
+class EVMTransactionHash(ByteEncoding):
+    _default_type: LargeBinary = LargeBinary(BYTE_COUNT)
+
+    @staticmethod
+    def _parse(
+        value: PythonTransactionHash | LiteralByteEncoding,
+    ) -> PythonTransactionHash:
+        return PythonTransactionHash(value)
