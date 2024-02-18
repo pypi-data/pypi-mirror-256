e5992cde02c58673736d3511cf0050313a156056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 21:04:55 +0000

    Add package idtrackerai/5.2.7/idtrackerai-5.2.7-py3-none-any.whl

---
+++ packages/idtrackerai/idtrackerai-5.2.7-py3-none-any.whl/idtrackerai/GUI_tools/__init__.py
@@ -0,0 +1,46 @@
+from functools import cache
+from pathlib import Path
+
+import numpy as np
+
+from .GUI_main_base import GUIBase
+from .widgets_utils.canvas import Canvas, CanvasMouseEvent, CanvasPainter
+from .widgets_utils.custom_list import CustomList
+from .widgets_utils.other_utils import (
+    LightPopUp,
+    QHLine,
+    WrappedLabel,
+    build_ROI_patches_from_list,
+    get_path_from_points,
+    key_event_modifier,
+)
+from .widgets_utils.sliders import InvertibleSlider, LabelRangeSlider
+from .widgets_utils.video_paths_holder import VideoPathHolder
+from .widgets_utils.video_player import VideoPlayer
+
+
+@cache
+def get_cmap():
+    parent_dir = Path(__file__).parent
+    for file in parent_dir.glob("cmap_*"):
+        return np.loadtxt(parent_dir / file, dtype=np.uint8)
+    raise FileNotFoundError(parent_dir)
+
+
+__all__ = [
+    "LabelRangeSlider",
+    "CustomList",
+    "WrappedLabel",
+    "Canvas",
+    "CanvasPainter",
+    "GUIBase",
+    "VideoPlayer",
+    "VideoPathHolder",
+    "key_event_modifier",
+    "build_ROI_patches_from_list",
+    "QHLine",
+    "CanvasMouseEvent",
+    "get_path_from_points",
+    "LightPopUp",
+    "InvertibleSlider",
+]
---
+++ packages/idtrackerai/idtrackerai-5.2.7-py3-none-any.whl/idtrackerai/validator/widgets/__init__.py
@@ -0,0 +1,21 @@
+from .additional_info import AdditionalInfo
+from .errors_explorer import ErrorsExplorer
+from .id_groups import IdGroups
+from .id_labels import IdLabels
+from .interpolator import Interpolator
+from .mark_properties import MarkBlobs
+from .paint_blobs import find_selected_blob, paintBlobs, paintTrails
+from .setup_points import SetupPoints
+
+__all__ = [
+    "MarkBlobs",
+    "paintBlobs",
+    "IdGroups",
+    "find_selected_blob",
+    "IdLabels",
+    "paintTrails",
+    "ErrorsExplorer",
+    "SetupPoints",
+    "Interpolator",
+    "AdditionalInfo",
+]
