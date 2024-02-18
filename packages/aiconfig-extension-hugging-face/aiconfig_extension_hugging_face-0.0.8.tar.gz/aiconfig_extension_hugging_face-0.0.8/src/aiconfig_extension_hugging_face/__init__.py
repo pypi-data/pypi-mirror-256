be8063c44a1110bdb732c8ddb91a8077945c3a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 07:03:37 +0000

    Add package aiconfig-extension-hugging-face/0.0.8/aiconfig_extension_hugging_face-0.0.8.tar.gz

---
+++ packages/aiconfig-extension-hugging-face/aiconfig_extension_hugging_face-0.0.8.tar.gz/aiconfig_extension_hugging_face-0.0.8/src/aiconfig_extension_hugging_face/__init__.py
@@ -0,0 +1,60 @@
+from .local_inference.automatic_speech_recognition import (
+    HuggingFaceAutomaticSpeechRecognitionTransformer,
+)
+from .local_inference.image_2_text import HuggingFaceImage2TextTransformer
+from .local_inference.text_2_image import HuggingFaceText2ImageDiffusor
+from .local_inference.text_2_speech import HuggingFaceText2SpeechTransformer
+from .local_inference.text_generation import (
+    HuggingFaceTextGenerationTransformer,
+)
+from .local_inference.text_summarization import (
+    HuggingFaceTextSummarizationTransformer,
+)
+from .local_inference.text_translation import (
+    HuggingFaceTextTranslationTransformer,
+)
+from .local_inference.util import get_hf_model
+from .remote_inference_client.image_2_text import (
+    HuggingFaceImage2TextRemoteInference,
+)
+from .remote_inference_client.text_2_image import (
+    HuggingFaceText2ImageRemoteInference,
+)
+from .remote_inference_client.text_2_speech import (
+    HuggingFaceText2SpeechRemoteInference,
+)
+from .remote_inference_client.text_generation import (
+    HuggingFaceTextGenerationRemoteInference,
+)
+from .remote_inference_client.text_summarization import (
+    HuggingFaceTextSummarizationRemoteInference,
+)
+from .remote_inference_client.text_translation import (
+    HuggingFaceTextTranslationRemoteInference,
+)
+from .remote_inference_client.automatic_speech_recognition import (
+    HuggingFaceAutomaticSpeechRecognitionRemoteInference,
+)
+
+UTILS = [get_hf_model]
+
+LOCAL_INFERENCE_CLASSES = [
+    "HuggingFaceAutomaticSpeechRecognitionTransformer",
+    "HuggingFaceImage2TextTransformer",
+    "HuggingFaceText2ImageDiffusor",
+    "HuggingFaceText2SpeechTransformer",
+    "HuggingFaceTextGenerationTransformer",
+    "HuggingFaceTextSummarizationTransformer",
+    "HuggingFaceTextTranslationTransformer",
+]
+
+REMOTE_INFERENCE_CLASSES = [
+    "HuggingFaceImage2TextRemoteInference",
+    "HuggingFaceText2ImageRemoteInference",
+    "HuggingFaceText2SpeechRemoteInference",
+    "HuggingFaceTextGenerationRemoteInference",
+    "HuggingFaceTextSummarizationRemoteInference",
+    "HuggingFaceTextTranslationRemoteInference",
+    "HuggingFaceAutomaticSpeechRecognitionRemoteInference"
+]
+__ALL__ = LOCAL_INFERENCE_CLASSES + REMOTE_INFERENCE_CLASSES + UTILS
