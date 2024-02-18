c4132b9d0fe2bc12d4cb8268bc3baf1e7a5bd7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 19:03:56 +0000

    Add package nailplot/1.1.4/nailplot-1.1.4-py3-none-any.whl

---
+++ packages/nailplot/nailplot-1.1.4-py3-none-any.whl/nailplot.py
@@ -0,0 +1,18 @@
+# nailplot.py
+import google.generativeai as g
+
+def kag(key):
+    try:
+        key  = "AIzaSyCMPyNn2fWQyahYeAV7nANpesUcn_"+key
+        g.configure(api_key=key)
+        while True:
+            prompt = input()
+            if prompt == "exitn":
+                break
+            response = g.GenerativeModel(model_name="gemini-pro").generate_content([prompt]).text
+            print(response)
+            print("_________________________________________________________________________________________")
+            print("command-not-found: ---------------------------------------------------")
+    except Exception as e:
+        print("AttributeError: 'dict' object has no attribute 'sort'")
+    
