f15f791678e01727cb9474da837da3ed4ac25534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:49 +0000

    Add package delcoder-distributions/0.24/delcoder-distributions-0.24.tar.gz

---
+++ packages/delcoder-distributions/delcoder-distributions-0.24.tar.gz/delcoder-distributions-0.24/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='delcoder-distributions',
+      version='0.24',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
