f64576515176ecf08a0b5c98ecf10ef9f38ffc3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 23:06:27 +0000

    Add package reflex/0.4.0a2/reflex-0.4.0a2-py3-none-any.whl

---
+++ packages/reflex/reflex-0.4.0a2-py3-none-any.whl/reflex/components/radix/themes/layout/stack.py
@@ -0,0 +1,53 @@
+"""Stack components."""
+from __future__ import annotations
+
+from typing import Literal
+
+from reflex.components.component import Component
+
+from ..base import LiteralSize
+from .flex import Flex
+
+LiteralJustify = Literal["start", "center", "end"]
+LiteralAlign = Literal["start", "center", "end", "stretch"]
+
+
+class Stack(Flex):
+    """A stack component."""
+
+    @classmethod
+    def create(
+        cls,
+        *children,
+        spacing: LiteralSize = "2",
+        **props,
+    ) -> Component:
+        """Create a new instance of the component.
+
+        Args:
+            *children: The children of the stack.
+            spacing: The spacing between each stack item.
+            **props: The properties of the stack.
+
+        Returns:
+            The stack component.
+        """
+        return super().create(
+            *children,
+            spacing=spacing,
+            **props,
+        )
+
+
+class VStack(Stack):
+    """A vertical stack component."""
+
+    def _apply_theme(self, theme: Component):
+        self.style.update({"flex_direction": "column"})
+
+
+class HStack(Stack):
+    """A horizontal stack component."""
+
+    def _apply_theme(self, theme: Component):
+        self.style.update({"flex_direction": "row"})
