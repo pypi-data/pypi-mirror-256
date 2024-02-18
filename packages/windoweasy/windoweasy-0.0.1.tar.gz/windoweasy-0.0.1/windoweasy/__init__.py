259e23977f9d6b094d3f555b8ffe740263ad064a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:03:55 +0000

    Add package windoweasy/0.0.1/windoweasy-0.0.1.tar.gz

---
+++ packages/windoweasy/windoweasy-0.0.1.tar.gz/windoweasy-0.0.1/windoweasy/__init__.py
@@ -0,0 +1,10 @@
+###############################
+#                             #
+#          MIT License        #
+#                             #
+#    Copyright 2024 INTERJL   #
+#                             #
+###############################
+
+from .windoweasy import WindowEasy
+from .palette import Palette
