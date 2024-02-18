bfc1447e4728ce09ea5e6cbde2a7dc16e0914c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:17 +0000

    Add package result-analytics/1.6.0/result-analytics-1.6.0.tar.gz

---
+++ packages/result-analytics/result-analytics-1.6.0.tar.gz/result-analytics-1.6.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name="result-analytics",
+    packages=find_packages(exclude=["*.tests"]),
+    include_package_data=True,
+    version="v1.6.0",
+    license="MIT",
+    description="A simple project to extract data from PDFs and analyse them.",
+    long_description="A simple project to extract data from PDFs and analyse them.",
+    author="Mateo & Tom Jeannesson",
+    author_email="tomjeannesson@gmail.com",
+    url="https://github.com/tomjeannesson/result-analytics",
+    download_url="https://github.com/tomjeannesson/result-analytics/archive/refs/tags/v1.6.0.tar.gz",
+    keywords=["Sport", "Ski", "Result", "Analytics", "Moguls"],
+    install_requires=["PyPDF2>=3.0.0", "pandas", "requests", "beautifulsoup4>=4.12.2"],
+    classifiers=[
+        "Development Status :: 4 - Beta",  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
