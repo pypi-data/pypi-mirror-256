5c9080e3bc8af67b07e873feab978ca09fb5122d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 17:05:02 +0000

    Add package yandex-capacity-planning-data-app/66.0.2/yandex-capacity-planning-data-app-66.0.2.tar.gz

---
+++ packages/yandex-capacity-planning-data-app/yandex-capacity-planning-data-app-66.0.2.tar.gz/yandex-capacity-planning-data-app-66.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='yandex-capacity-planning-data-app',
+   version='66.0.2',
+   author='Yandex',
+   author_email='security@yandex-team.ru',
+   url='https://ya.ru',
+   readme="README.md",
+   long_description="""This is a security placeholder package.""",
+   long_description_content_type='text/markdown',
+   description='A package to prevent Dependency Confusion attacks against Yandex.',
+   )
