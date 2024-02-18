56c8f8aace6faeaca2518d7fbf23b781d182a545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:12 +0000

    Add package nguyenpanda/0.1.1/nguyenpanda-0.1.1-py3-none-any.whl

---
+++ packages/nguyenpanda/nguyenpanda-0.1.1-py3-none-any.whl/nguyenpanda/owl/__init__.py
@@ -0,0 +1,13 @@
+# from .color import Color
+
+__all__ = [
+    # 'color',
+]
+
+__version__ = '0.1.0'
+
+__title__ = 'owl'
+
+__description__ = '''
+\'owl\' package provides things related to mathematics
+'''
