355aae778e7bebb9cc433c3b549508bb32e369ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:04:18 +0000

    Add package magicapp-api/0.0.3/magicapp_api-0.0.3-py3-none-any.whl

---
+++ packages/magicapp-api/magicapp_api-0.0.3-py3-none-any.whl/magicapp_api/data_models/data_classes.py
@@ -0,0 +1,57 @@
+from typing import List, Tuple
+
+from pydantic import BaseModel
+
+import magicapp_api.data_models.api_dtos as api_dtos
+
+
+class AllGuidelineDTOs(BaseModel):
+    guideline_dto: api_dtos.Guideline
+    section_dtos: List[api_dtos.Section]
+    recommendation_dtos: List[api_dtos.Recommendation]
+    pico_dtos: List[api_dtos.Pico]
+    dichotomous_outcome_dtos: List[api_dtos.DichotomousOutcome]
+    continuous_outcome_dtos: List[api_dtos.ContinuousOutcome]
+    non_poolable_outcome_dtos: List[api_dtos.NonPoolableOutcome]
+
+
+class AllSectionDTOs(BaseModel):
+    section_dtos: List[api_dtos.Section]
+    recommendation_dtos: List[api_dtos.Recommendation]
+    pico_dtos: List[api_dtos.Pico]
+    dichotomous_outcome_dtos: List[api_dtos.DichotomousOutcome]
+    continuous_outcome_dtos: List[api_dtos.ContinuousOutcome]
+    non_poolable_outcome_dtos: List[api_dtos.NonPoolableOutcome]
+
+
+class AllRecommendationDTOs(BaseModel):
+    recommendation_dtos: List[api_dtos.Recommendation]
+    pico_dtos: List[api_dtos.Pico]
+    dichotomous_outcome_dtos: List[api_dtos.DichotomousOutcome]
+    continuous_outcome_dtos: List[api_dtos.ContinuousOutcome]
+    non_poolable_outcome_dtos: List[api_dtos.NonPoolableOutcome]
+
+
+class AllPicoDTOs(BaseModel):
+    pico_dtos: List[api_dtos.Pico]
+    dichotomous_outcome_dtos: List[api_dtos.DichotomousOutcome]
+    continuous_outcome_dtos: List[api_dtos.ContinuousOutcome]
+    non_poolable_outcome_dtos: List[api_dtos.NonPoolableOutcome]
+
+
+class AllOutcomeDTOs(BaseModel):
+    dichotomous_outcome_dtos: List[api_dtos.DichotomousOutcome]
+    continuous_outcome_dtos: List[api_dtos.ContinuousOutcome]
+    non_poolable_outcome_dtos: List[api_dtos.NonPoolableOutcome]
+
+
+class OutcomeIDs(BaseModel):
+    di_outcome_ids: List[Tuple[int, int, int]]
+    co_outcome_ids: List[Tuple[int, int, int]]
+    np_outcome_ids: List[Tuple[int, int, int]]
+
+
+class Outcomes(BaseModel):
+    di_outcome_dtos: List[api_dtos.DichotomousOutcome]
+    co_outcome_dtos: List[api_dtos.ContinuousOutcome]
+    np_outcome_dtos: List[api_dtos.NonPoolableOutcome]
