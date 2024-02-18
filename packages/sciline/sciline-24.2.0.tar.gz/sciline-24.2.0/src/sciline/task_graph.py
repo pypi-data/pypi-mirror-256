e8c379f4130dad7ecf416363666edebbc6866ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:17 +0000

    Add package sciline/24.2.0/sciline-24.2.0.tar.gz

---
+++ packages/sciline/sciline-24.2.0.tar.gz/sciline-24.2.0/src/sciline/task_graph.py
@@ -0,0 +1,165 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+from __future__ import annotations
+
+from html import escape
+from typing import Any, Optional, Sequence, Tuple, TypeVar, Union
+
+from .scheduler import DaskScheduler, NaiveScheduler, Scheduler
+from .typing import Graph, Item
+from .utils import keyname
+
+T = TypeVar("T")
+
+
+def _list_items(items: Sequence[str]) -> str:
+    return '\n'.join(
+        (
+            '<ul>',
+            ('\n'.join((f'<li>{escape(it)}</li>' for it in items))),
+            '</ul>',
+        )
+    )
+
+
+def _list_max_n_then_hide(items: Sequence[str], n: int = 5, header: str = '') -> str:
+    def wrap(s: str) -> str:
+        return '\n'.join(
+            (
+                '<div class="task-graph-detail-list">'
+                '<style> .task-graph-detail-list ul { margin-top: 0; } </style>',
+                s,
+                '</div>',
+            )
+        )
+
+    return wrap(
+        '\n'.join(
+            (
+                header,
+                _list_items(items),
+            )
+        )
+        if len(items) <= n
+        else '\n'.join(
+            (
+                '<details>',
+                '<style>',
+                'details[open] .task-graph-summary ul { display: none; }',
+                '</style>',
+                '<summary class="task-graph-summary">',
+                header,
+                _list_items((*items[:n], '...')),
+                '</summary>',
+                _list_items(items),
+                '</details>',
+            )
+        )
+    )
+
+
+class TaskGraph:
+    """
+    Holds a concrete task graph and keys to compute.
+
+    Task graphs are typically created by :py:class:`sciline.Pipeline.build`. They allow
+    for computing all or a subset of the results in the graph.
+    """
+
+    def __init__(
+        self,
+        *,
+        graph: Graph,
+        keys: Union[type, Tuple[type, ...], Item[T], Tuple[Item[T], ...]],
+        scheduler: Optional[Scheduler] = None,
+    ) -> None:
+        self._graph = graph
+        self._keys = keys
+        if scheduler is None:
+            try:
+                scheduler = DaskScheduler()
+            except ImportError:
+                scheduler = NaiveScheduler()
+        self._scheduler = scheduler
+
+    def compute(
+        self,
+        keys: Optional[
+            Union[type, Tuple[type, ...], Item[T], Tuple[Item[T], ...]]
+        ] = None,
+    ) -> Any:
+        """
+        Compute the result of the graph.
+
+        Parameters
+        ----------
+        keys:
+            Optional list of keys to compute. This can be used to override the keys
+            stored in the graph instance. Note that the keys must be present in the
+            graph as intermediate results, otherwise KeyError is raised.
+
+        Returns
+        -------
+        If ``keys`` is a single type, returns the single result that was computed.
+        If ``keys`` is a tuple of types, returns a dictionary with type as keys
+        and the corresponding results as values.
+
+        """
+        if keys is None:
+            keys = self._keys
+        if isinstance(keys, tuple):
+            results = self._scheduler.get(self._graph, list(keys))
+            return dict(zip(keys, results))
+        else:
+            return self._scheduler.get(self._graph, [keys])[0]
+
+    def visualize(
+        self, **kwargs: Any
+    ) -> graphviz.Digraph:  # type: ignore[name-defined] # noqa: F821
+        """
+        Return a graphviz Digraph object representing the graph.
+
+        Parameters
+        ----------
+        kwargs:
+            Keyword arguments passed to :py:class:`graphviz.Digraph`.
+        """
+        from .visualize import to_graphviz
+
+        return to_graphviz(self._graph, **kwargs)
+
+    def _repr_html_(self) -> str:
+        leafs = sorted(
+            [
+                escape(keyname(key))
+                for key in (
+                    self._keys if isinstance(self._keys, tuple) else [self._keys]
+                )
+            ]
+        )
+        roots = sorted(
+            {
+                escape(keyname(key))
+                for key, provider in self._graph.items()
+                if provider.kind != 'function'
+            }
+        )
+        scheduler = escape(str(self._scheduler))
+
+        def head(word: str) -> str:
+            return f'<h5>{word}</h5>'
+
+        return '\n'.join(
+            (
+                '<style>.task-graph-repr h5 { display: inline; }</style>',
+                '<div class="task-graph-repr">',
+                head('Output keys: '),
+                ','.join(leafs),
+                '<br>',
+                head('Scheduler: '),
+                scheduler,
+                '<br>',
+                _list_max_n_then_hide(roots, header=head('Input keys:')),
+                '</div>',
+            )
+        )
