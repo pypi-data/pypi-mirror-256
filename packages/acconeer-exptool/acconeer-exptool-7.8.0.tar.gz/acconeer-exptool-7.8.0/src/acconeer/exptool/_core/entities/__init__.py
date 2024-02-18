aa1cffc72b5fe34057951bb0fe805f7742c4bb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:57 +0000

    Add package acconeer-exptool/7.8.0/acconeer-exptool-7.8.0.tar.gz

---
+++ packages/acconeer-exptool/acconeer-exptool-7.8.0.tar.gz/acconeer-exptool-7.8.0/src/acconeer/exptool/_core/entities/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Acconeer AB, 2023
+# All rights reserved
+
+from .client_info import (
+    ClientInfo,
+    ClientInfoCreationError,
+    MockInfo,
+    SerialInfo,
+    SocketInfo,
+    USBInfo,
+)
