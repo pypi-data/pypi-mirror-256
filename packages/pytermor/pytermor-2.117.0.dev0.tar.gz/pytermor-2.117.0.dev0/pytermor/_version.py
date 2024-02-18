0a3b13c055ccb9bd9f730b4c469f17c501311285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 22:04:39 +0000

    Add package pytermor/2.117.0.dev0/pytermor-2.117.0.dev0.tar.gz

---
+++ packages/pytermor/pytermor-2.117.0.dev0.tar.gz/pytermor-2.117.0.dev0/pytermor/_version.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+#  pytermor [ANSI formatted terminal output toolset]
+#  (c) 2022-2023. A. Shavykin <0.delameter@gmail.com>
+#  Licensed under GNU Lesser General Public License v3.0
+# -----------------------------------------------------------------------------
+__version__ = '2.117.0.dev0'
+__updated__ = "2024-02-15 17:38:59+03:00"
