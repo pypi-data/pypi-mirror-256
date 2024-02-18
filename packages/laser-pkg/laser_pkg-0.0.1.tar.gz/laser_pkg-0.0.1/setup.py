e2716d4e6374b5513ccdf63ab8591ce77cb938bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 12:04:46 +0000

    Add package laser-pkg/0.0.1/laser_pkg-0.0.1.tar.gz

---
+++ packages/laser-pkg/laser_pkg-0.0.1.tar.gz/laser_pkg-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="laser_pkg",
+    version="0.0.1",
+    author="JiyunLee",
+    author_email="easyun@lgensol.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.10",
+)
