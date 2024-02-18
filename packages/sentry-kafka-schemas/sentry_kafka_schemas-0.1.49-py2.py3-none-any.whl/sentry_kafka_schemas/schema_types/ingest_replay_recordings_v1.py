146ab0750ebc7f5a0451a6d0daaf3342816d3df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 08:03:53 +0000

    Add package sentry-kafka-schemas/0.1.49/sentry_kafka_schemas-0.1.49-py2.py3-none-any.whl

---
+++ packages/sentry-kafka-schemas/sentry_kafka_schemas-0.1.49-py2.py3-none-any.whl/sentry_kafka_schemas/schema_types/ingest_replay_recordings_v1.py
@@ -0,0 +1,59 @@
+from typing import Union, TypedDict, Literal, Any
+from typing_extensions import Required
+
+
+class ReplayRecording(TypedDict, total=False):
+    """
+    replay_recording.
+
+    A replay recording, or a chunk thereof
+    """
+
+    type: Required[Literal["replay_recording_not_chunked"]]
+    """ Required property """
+
+    replay_id: Required[str]
+    """ Required property """
+
+    key_id: Union[None, int]
+    org_id: Required[int]
+    """
+    minimum: 0
+
+    Required property
+    """
+
+    project_id: Required[int]
+    """
+    minimum: 0
+
+    Required property
+    """
+
+    received: Required[int]
+    """
+    minimum: 0
+
+    Required property
+    """
+
+    retention_days: Required[int]
+    """ Required property """
+
+    payload: Required[Any]
+    """
+    msgpack bytes
+
+    WARNING: we get an schema without any type
+
+    Required property
+    """
+
+    version: int
+    """ default: 0 """
+
+
+
+_REPLAY_RECORDING_VERSION_DEFAULT = 0
+""" Default value of the field path 'replay_recording version' """
+
