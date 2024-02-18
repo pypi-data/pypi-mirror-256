06ba349d19bc3e470f3b6faa050b07b4b6c9f008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 06:05:13 +0000

    Add package uglychain/0.1.6/uglychain-0.1.6.tar.gz

---
+++ packages/uglychain/uglychain-0.1.6.tar.gz/uglychain-0.1.6/uglychain/llm/llama_index.py
@@ -0,0 +1,44 @@
+from typing import Any
+
+try:
+    from llama_index.core.llms import (
+        CompletionResponse,
+        CompletionResponseGen,
+        CustomLLM,
+        LLMMetadata,
+    )
+    from llama_index.core.llms.callbacks import llm_completion_callback
+except ImportError as err:
+    raise ImportError("Please install the `llama-index-core` package to use this LLM.") from err
+
+from .base import BaseLanguageModel
+
+
+class LlamaIndexLLM(CustomLLM):
+    llm: BaseLanguageModel
+
+    @property
+    def metadata(self) -> LLMMetadata:
+        """Get LLM metadata."""
+        if hasattr(self.llm, "MAX_TOKENS"):
+            context_window = self.llm.MAX_TOKENS  # type: ignore
+        else:
+            context_window = 4096
+        return LLMMetadata(
+            context_window=context_window,
+            is_chat_model=True,
+            model_name=self.llm.model,
+        )
+
+    @llm_completion_callback()
+    def complete(self, prompt: str, **kwargs: Any) -> CompletionResponse:
+        response = self.llm.generate(prompt)
+        return CompletionResponse(text=response)
+
+    @llm_completion_callback()
+    def stream_complete(self, prompt: str, **kwargs: Any) -> CompletionResponseGen:
+        response = ""
+        dummy_response = self.llm.generate(prompt)
+        for token in dummy_response:
+            response += token
+            yield CompletionResponse(text=response, delta=token)
