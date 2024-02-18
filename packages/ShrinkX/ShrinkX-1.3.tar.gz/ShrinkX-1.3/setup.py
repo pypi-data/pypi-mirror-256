8b7d4fc67f5eb74fd3e6d22b53beaa930445dd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 05:04:24 +0000

    Add package ShrinkX/1.3/ShrinkX-1.3.tar.gz

---
+++ packages/ShrinkX/ShrinkX-1.3.tar.gz/ShrinkX-1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ShrinkX',
+    version='1.3',
+    packages=find_packages(),
+    description='A Python tool for effortlessly compressing media files for sharing online.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Joshua Foroughi',
+    author_email='stormedxx@icloud.com',
+    url='https://github.com/stormedx/shrinkx',
+    install_requires=[
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'shrinkx=shrinkx.shrinkx:main',
+        ],
+    },
+)
