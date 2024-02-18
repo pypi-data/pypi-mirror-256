c620be43ce5318b9b4f0f2692db6323eeba32fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 13:04:46 +0000

    Add package binomial-package-Scripts/1.0/binomial_package_Scripts-1.0.tar.gz

---
+++ packages/binomial-package-Scripts/binomial_package_Scripts-1.0.tar.gz/binomial_package_Scripts-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binomial_package_Scripts',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['binomial_package_Scripts'],
+      author = 'Andre Muth',
+      author_email = 'andremuth@gmx.de',
+      zip_safe=False)
