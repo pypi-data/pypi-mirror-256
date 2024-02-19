b967c3a3de365cb6fb5fe6ce91ded3dd2683642c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:23 +0000

    Add package landborn/0.1.3/landborn-0.1.3-py3-none-any.whl

---
+++ packages/landborn/landborn-0.1.3-py3-none-any.whl/landborn/__init__.py
@@ -0,0 +1,8 @@
+from .barplot import barplot
+from .lineplot import lineplot
+from .scatterplot import scatterplot
+
+__version__ = '0.1.3'
+__author__ = 'Molly Nelson'
+
+print("Package Landborn Successfully Imported")
