9518abe582c1ca336587c9ee962ae3753fb64e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:06:51 +0000

    Add package odoo11-addon-letsencrypt/11.0.2.0.0.99.dev18/odoo11_addon_letsencrypt-11.0.2.0.0.99.dev18-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-letsencrypt/odoo11_addon_letsencrypt-11.0.2.0.0.99.dev18-py2.py3-none-any.whl/odoo/addons/letsencrypt/__manifest__.py
@@ -0,0 +1,35 @@
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Let's Encrypt",
+    "version": "11.0.2.0.0",
+    "author": "Therp BV,"
+              "Tecnativa,"
+              "Acysos S.L,"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-tools",
+    "category": "Hidden/Dependency",
+    "summary": "Request SSL certificates from letsencrypt.org",
+    "depends": [
+        "base_setup",
+    ],
+    "data": [
+        "data/ir_config_parameter.xml",
+        "data/ir_cron.xml",
+        "views/res_config_settings.xml",
+    ],
+    "demo": [
+        "demo/ir_cron.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "external_dependencies": {
+        "python": [
+            "acme",
+            "cryptography",
+            "dns",
+            "josepy",
+        ],
+    },
+}
