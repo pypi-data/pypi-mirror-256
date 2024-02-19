8bdf5570c48eb4d892f7cb21d93d501e4a148474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:11:51 +0000

    Add package galmoss/20240216.6/galmoss-20240216.6.tar.gz

---
+++ packages/galmoss/galmoss-20240216.6.tar.gz/galmoss-20240216.6/galmoss/__init__.py
@@ -0,0 +1,7 @@
+from .Parameter.basic import Parameters as p
+from .data import Profiles, DataSet
+from .profile.basic import LightProfile
+from .fitting import Fitting
+from .profile import light_profile as lp
+
+__version__ = '20240216.6'
