029160be4cc47ee43ce48dd541e1e30e84591b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:06:42 +0000

    Add package fnug/0.0.18/fnug-0.0.18-py3-none-any.whl

---
+++ packages/fnug/fnug-0.0.18-py3-none-any.whl/fnug/cli.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import click
+
+from fnug import FnugApp
+from fnug.config import load_config
+
+DEFAULT_FILE_NAMES = [".fnug.json", ".fnug.yaml", ".fnug.yml"]
+
+
+@click.command()
+@click.option("--config", "-c", type=click.Path(), help="Config file")
+def cli(config: str | None = None):
+    """Entrypoint for the fnug CLI."""
+    if config is None:
+        for file_name in DEFAULT_FILE_NAMES:
+            if Path(file_name).exists():
+                config = file_name
+                break
+        else:
+            raise click.ClickException(f"Could not find a config file. Tried: {', '.join(DEFAULT_FILE_NAMES)}")
+
+    file_path = Path(config)
+    cwd = file_path.parent
+    cfg = load_config(file_path)
+    FnugApp(cfg, cwd=cwd).run()
