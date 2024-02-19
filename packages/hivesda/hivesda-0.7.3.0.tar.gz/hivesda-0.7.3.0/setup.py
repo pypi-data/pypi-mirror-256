4690e6beefb208055d757dd87070ac2257739287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:18 +0000

    Add package hivesda/0.7.3.0/hivesda-0.7.3.0.tar.gz

---
+++ packages/hivesda/hivesda-0.7.3.0.tar.gz/hivesda-0.7.3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='hivesda', # 패키지 명
+
+version='0.7.3.0',
+
+description='Test Package',
+
+author='Jae',
+
+author_email='jjkjy@nate.com',
+
+url='',
+
+license='MIT', # MIT에서 정한 표준 라이센스 따른다
+
+python_requires='>=3',
+
+install_requires=[], # 패키지 사용을 위해 필요한 추가 설치 패키지
+
+packages=['hivesda'] # 패키지가 들어있는 폴더들
+
+)
