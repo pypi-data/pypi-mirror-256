44c94eef6ef75b29d33e764cf0938dda58b63739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:39 +0000

    Add package uv/0.1.1/uv-0.1.1.tar.gz

---
+++ packages/uv/uv-0.1.1.tar.gz/uv-0.1.1/crates/pypi-types/src/base_url.rs
@@ -0,0 +1,86 @@
+use serde::{Deserialize, Serialize};
+use url::Url;
+
+/// Join a possibly relative URL to a base URL.
+///
+/// When `maybe_relative` is not relative, then it is parsed and returned with
+/// `base` being ignored.
+///
+/// This is useful for parsing URLs that may be absolute or relative, with a
+/// known base URL, and that doesn't require having already parsed a `BaseUrl`.
+pub fn base_url_join_relative(base: &str, maybe_relative: &str) -> Result<Url, JoinRelativeError> {
+    match Url::parse(maybe_relative) {
+        Ok(absolute) => Ok(absolute),
+        Err(err) => {
+            if err == url::ParseError::RelativeUrlWithoutBase {
+                let base = Url::parse(base).map_err(|err| JoinRelativeError {
+                    original: base.to_string(),
+                    source: err,
+                })?;
+                base.join(maybe_relative).map_err(|err| JoinRelativeError {
+                    original: format!("{base}/{maybe_relative}"),
+                    source: err,
+                })
+            } else {
+                Err(JoinRelativeError {
+                    original: maybe_relative.to_string(),
+                    source: err,
+                })
+            }
+        }
+    }
+}
+
+/// An error that occurs when `base_url_join_relative` fails.
+///
+/// The error message includes the URL (`base` or `maybe_relative`) passed to
+/// `base_url_join_relative` that provoked the error.
+#[derive(Clone, Debug, thiserror::Error)]
+#[error("Failed to parse URL: `{original}`")]
+pub struct JoinRelativeError {
+    original: String,
+    source: url::ParseError,
+}
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub struct BaseUrl(
+    #[serde(
+        serialize_with = "Url::serialize_internal",
+        deserialize_with = "Url::deserialize_internal"
+    )]
+    Url,
+);
+
+impl BaseUrl {
+    /// Parse the given URL. If it's relative, join it to the current [`BaseUrl`]. Allows for
+    /// parsing URLs that may be absolute or relative, with a known base URL.
+    pub fn join_relative(&self, url: &str) -> Result<Url, url::ParseError> {
+        match Url::parse(url) {
+            Ok(url) => Ok(url),
+            Err(err) => {
+                if err == url::ParseError::RelativeUrlWithoutBase {
+                    self.0.join(url)
+                } else {
+                    Err(err)
+                }
+            }
+        }
+    }
+
+    /// Return the underlying [`Url`].
+    pub fn as_url(&self) -> &Url {
+        &self.0
+    }
+}
+
+impl From<Url> for BaseUrl {
+    fn from(url: Url) -> Self {
+        Self(url)
+    }
+}
+
+impl std::fmt::Display for BaseUrl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
