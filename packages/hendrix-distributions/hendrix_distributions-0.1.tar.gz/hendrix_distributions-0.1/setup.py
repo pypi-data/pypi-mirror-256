cbadb3a941d539502896f141736105a532adb01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:16 +0000

    Add package hendrix-distributions/0.1/hendrix_distributions-0.1.tar.gz

---
+++ packages/hendrix-distributions/hendrix_distributions-0.1.tar.gz/hendrix_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hendrix_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['hendrix_distributions'],
+      author= 'Hendrix Ebrudu',
+      author_email = 'hendrixobuks@gmail.com',
+      zip_safe=False)
