67431c748bf53de2b6d2ca59960ea01a3930d0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:39 +0000

    Add package ku-distributions/0.1/ku_distributions-0.1.tar.gz

---
+++ packages/ku-distributions/ku_distributions-0.1.tar.gz/ku_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ku_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ku_distributions'],
+      author = 'Kishan Udas',
+      zip_safe=False)
