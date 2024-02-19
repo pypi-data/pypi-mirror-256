a7a3d0785215772fdc6baabe38c3b304f50e49d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:00 +0000

    Add package fedpredict/0.0.4.4/fedpredict-0.0.4.4.tar.gz

---
+++ packages/fedpredict/fedpredict-0.0.4.4.tar.gz/fedpredict-0.0.4.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+setup(
+    name='fedpredict',
+    version='v0.0.4.4',
+    packages=['fedpredict', 'fedpredict.utils', 'fedpredict.utils.compression_methods'],
+    url='https://github.com/claudiocapanema/fedpredict',
+    license='',
+    author='claudio',
+    author_email='claudiogs.capanema@gmail.com',
+    description='FedPredict is a personalization plugin for Federated Learning methods'
+)
