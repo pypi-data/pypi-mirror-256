0e88afac28672672d9e5e0056382b6724a65b160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:55 +0000

    Add package mcMqttComms/0.0.3/mcMqttComms-0.0.3.tar.gz

---
+++ packages/mcMqttComms/mcMqttComms-0.0.3.tar.gz/mcMqttComms-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="mcMqttComms",
+    version="0.0.3",
+    description="Module for handling mission control messages to be sent to a Drone via MQTT.",
+    packages= [],
+    author="Jonathan Thai",
+    author_email="thaijonathan53@gmail.com",      
+    install_requires = ["boto3", "paho-mqtt"]
+      
+      
+      )
