4a384dc388cc216e4aa2a614b6eb2a84e8ed7cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:23 +0000

    Add package thoughtful-common-packages/0.0.13/thoughtful-common-packages-0.0.13.tar.gz

---
+++ packages/thoughtful-common-packages/thoughtful-common-packages-0.0.13.tar.gz/thoughtful-common-packages-0.0.13/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+install_requirements = open("requirements.txt").readlines()
+
+setup(
+    name="thoughtful-common-packages",
+    packages=find_packages(),
+    install_requires=install_requirements,
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    description="The package contains all frequently used packages in Thoughtful",
+    keywords="thoughtful-common-packages",
+    url="https://www.thoughtful.ai/",
+    version="0.0.13",
+)
