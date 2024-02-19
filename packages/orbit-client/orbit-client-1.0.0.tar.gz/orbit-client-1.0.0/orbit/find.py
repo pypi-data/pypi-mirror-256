d02fe50e2a257408c631fa9a1f26e89338309bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:04 +0000

    Add package orbit-client/1.0.0/orbit-client-1.0.0.tar.gz

---
+++ packages/orbit-client/orbit-client-1.0.0.tar.gz/orbit-client-1.0.0/orbit/find.py
@@ -0,0 +1,28 @@
+"""Functions for finding Orbit templates and other components."""
+import logging
+import os
+from pathlib import Path
+
+from orbit.exceptions import NonTemplatedInputDirException
+
+
+logger = logging.getLogger(__name__)
+
+
+def find_template(repo_dir: "os.PathLike[str]") -> Path:
+    """Determine which child directory of ``repo_dir`` is the project template.
+
+    :param repo_dir: Local directory of newly cloned repo.
+    :return: Relative path to project template.
+    """
+    logger.debug("Searching %s for the project template.", repo_dir)
+
+    for str_path in os.listdir(repo_dir):
+        if "orbit" in str_path and "{{" in str_path and "}}" in str_path:
+            project_template = Path(repo_dir, str_path)
+            break
+    else:
+        raise NonTemplatedInputDirException
+
+    logger.debug("The project template appears to be %s", project_template)
+    return project_template
---
+++ packages/orbit-client/orbit-client-1.0.0.tar.gz/orbit-client-1.0.0/orbit/version.py
@@ -0,0 +1,5 @@
+__version__ = 'v1.0.0'
+
+
+def get_comet_version():
+    return __version__
