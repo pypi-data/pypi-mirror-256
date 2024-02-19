6d291c4b81cc1175f653108d1842762df38d94be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:19 +0000

    Add package pih-tls/1.1/pih_tls-1.1.tar.gz

---
+++ packages/pih-tls/pih_tls-1.1.tar.gz/pih_tls-1.1/pih_tls_build.py
@@ -0,0 +1,16 @@
+import ipih
+
+from pih_tls import NAME, VERSION
+from build_tools import build
+
+build(
+    NAME,
+    "Shared tools for PIH module",
+    VERSION,
+    None,
+    [
+        "pillow",
+        "numpy",
+        "pdf2image",
+    ],
+)
