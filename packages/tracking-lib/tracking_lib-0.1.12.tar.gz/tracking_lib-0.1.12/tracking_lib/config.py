4bba118d15fdef7adf70e439f3d69ab644ed98c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:51 +0000

    Add package tracking-lib/0.1.12/tracking_lib-0.1.12.tar.gz

---
+++ packages/tracking-lib/tracking_lib-0.1.12.tar.gz/tracking_lib-0.1.12/tracking_lib/config.py
@@ -0,0 +1,8 @@
+
+class Config:
+
+    def __init__(self, control_channel_project_id=None, control_channel_topic_id=None):
+        self.control_channel_project_id = control_channel_project_id
+        self.control_channel_topic_id = control_channel_topic_id
+
+    
