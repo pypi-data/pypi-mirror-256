9001a12b917beac779944f83bfc3c3f6116babb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:04 +0000

    Add package zaber-motion-bindings-linux/5.1.1/zaber_motion_bindings_linux-5.1.1.tar.gz

---
+++ packages/zaber-motion-bindings-linux/zaber_motion_bindings_linux-5.1.1.tar.gz/zaber_motion_bindings_linux-5.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+print(find_packages(exclude=["test*", "test_*", "*_test*"]))
+
+setup(
+    name='zaber_motion_bindings_linux',
+    version='5.1.1',
+    packages=find_packages(exclude=["test*", "test_*", "*_test*"]),
+    package_data={
+        '': ['*.so']
+    },
+    description='Linux bindings for Zaber Motion Library',
+    long_description='Linux bindings for Zaber Motion Library',
+    url='https://gitlab.com/ZaberTech/zaber-motion-lib',
+    author='Zaber Technologies Inc.',
+    author_email='contact@zaber.com',
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+    ],
+    keywords='',
+    install_requires=[],
+    extras_require={'build':['invoke']}
+)
---
+++ packages/zaber-motion-bindings-linux/zaber_motion_bindings_linux-5.1.1.tar.gz/zaber_motion_bindings_linux-5.1.1/zaber_motion_bindings_linux/__init__.py
@@ -0,0 +1,2 @@
+from .bindings import load_library as load_library
+from .version import __version__ as __version__
