71b2ad233c9592d90fc09b4c463ae4307798ecf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:06:14 +0000

    Add package CoolBar/0.5/CoolBar-0.5.tar.gz

---
+++ packages/CoolBar/CoolBar-0.5.tar.gz/CoolBar-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CoolBar',
+    packages=find_packages(),
+    version='0.5',
+    license='Apache-2.0',
+    description='Dynamic Python Progress Bar',
+    author='ReloadingBee',
+    author_email='reloadingbee@gmail.com',
+    url='https://github.com/ReloadingBee/CoolBar',
+    download_url='https://github.com/ReloadingBee/CoolBar/archive/v_0.5.tar.gz',
+    keywords=['progress', 'loading', 'bar', 'dynamic', 'python'],
+    install_requires=[],    
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+    ],
+)
