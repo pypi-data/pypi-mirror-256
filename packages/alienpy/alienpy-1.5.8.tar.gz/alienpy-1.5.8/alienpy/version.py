b3bb025d8a0ecd940a633ce1f97be6b389c057e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:53 +0000

    Add package alienpy/1.5.8/alienpy-1.5.8.tar.gz

---
+++ packages/alienpy/alienpy-1.5.8.tar.gz/alienpy-1.5.8/alienpy/version.py
@@ -0,0 +1,11 @@
+"""alienpy:: Definitions of version strings"""
+
+import sys
+
+ALIENPY_VERSION_HASH = '3c454f2'
+ALIENPY_VERSION_DATE = '20240215_205013'
+ALIENPY_VERSION_STR = '1.5.8'
+
+if __name__ == '__main__':
+    print('This file should not be executed!', file = sys.stderr, flush = True)
+    sys.exit(95)
