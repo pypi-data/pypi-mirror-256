96a5117fa23be75f955bfcce380b536df060760a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:06:31 +0000

    Add package akash-package/0.1/akash_package-0.1.tar.gz

---
+++ packages/akash-package/akash_package-0.1.tar.gz/akash_package-0.1/akash_package/__init__.py
@@ -0,0 +1,2 @@
+from .main import testing_package
+from .main import dot_prod_array
---
+++ packages/akash-package/akash_package-0.1.tar.gz/akash_package-0.1/akash_package/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+def testing_package():
+    print("Checking how to convert python file into package")
+
+def dot_prod_array(a,b):
+    print('Array1:',a)
+    print('Array2',b)
+    print('Dot product of Array1 and Array2',np.dot(a,b))
