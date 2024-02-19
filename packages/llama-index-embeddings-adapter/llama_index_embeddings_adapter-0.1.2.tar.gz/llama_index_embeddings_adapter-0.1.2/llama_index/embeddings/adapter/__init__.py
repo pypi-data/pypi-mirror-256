528f02ccb5bec2a7d56eb2682e51ed425a18b4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:11 +0000

    Add package llama-index-embeddings-adapter/0.1.2/llama_index_embeddings_adapter-0.1.2.tar.gz

---
+++ packages/llama-index-embeddings-adapter/llama_index_embeddings_adapter-0.1.2.tar.gz/llama_index_embeddings_adapter-0.1.2/llama_index/embeddings/adapter/__init__.py
@@ -0,0 +1,9 @@
+from llama_index.embeddings.adapter.base import AdapterEmbeddingModel, LinearAdapterEmbeddingModel
+from llama_index.embeddings.adapter.utils import BaseAdapter, LinearLayer
+
+__all__ = [
+    "AdapterEmbeddingModel",
+    "LinearAdapterEmbeddingModel",
+    "BaseAdapter",
+    "LinearLayer",
+]
