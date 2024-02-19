3f10d52250a11bddb289f0457e9f75e1f144cd84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:44 +0000

    Add package ds-my-multitool/1.0.5/ds_my_multitool-1.0.5.tar.gz

---
+++ packages/ds-my-multitool/ds_my_multitool-1.0.5.tar.gz/ds_my_multitool-1.0.5/src/my_multitool/exceptions.py
@@ -0,0 +1,32 @@
+"""Module with exceptions.
+
+Module that contains all the exceptions for the My Multitool tool.
+"""
+
+
+class MyMultitoolException(Exception):
+    """Base exception for My Multutool."""
+
+
+class ConfigFileNotFoundException(MyMultitoolException):
+    """Exception for when the configfile doesn't exist."""
+
+
+class ConfigFileNotValidException(MyMultitoolException):
+    """Exception for when the configfile is invalid."""
+
+
+class NoConfigToSaveException(MyMultitoolException):
+    """Exception for a config is saved before loading it."""
+
+
+class GenericCLIException(MyMultitoolException):
+    """Exception for a generic error in the CLI options."""
+
+
+class NoConfirmationException(MyMultitoolException):
+    """Exception when the user presses N at a confirmation."""
+
+
+class SQLError(MyMultitoolException):
+    """Exception for a SQL error."""
