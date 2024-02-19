88686837024a0ff1a937d3f3504bb9ed58d104dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:16 +0000

    Add package FinRecipes/0.0.3/FinRecipes-0.0.3.tar.gz

---
+++ packages/FinRecipes/FinRecipes-0.0.3.tar.gz/FinRecipes-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md','r') as f:
+    description = f.read()
+
+setup(
+    name='FinRecipes',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[ 
+        # Add dependencies here.
+        'pandas',
+        'progressbar',
+        'yfinance',
+        'stockstats',
+        'datetime',
+        'pytz',
+        'numpy',
+        'tables'
+    ],
+
+    long_description=description,
+    long_description_content_type='text/markdown'
+)
