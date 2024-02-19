1066360d2896825a4e31a94a3abcfaf0669e84b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:34 +0000

    Add package msds-distributions/0.1/msds_distributions-0.1.tar.gz

---
+++ packages/msds-distributions/msds_distributions-0.1.tar.gz/msds_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='msds_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['msds_distributions'],
+      author = 'Mauro Sobreira',
+      author_email = 'msobreira27@yahoo.com',
+      zip_safe=False)
