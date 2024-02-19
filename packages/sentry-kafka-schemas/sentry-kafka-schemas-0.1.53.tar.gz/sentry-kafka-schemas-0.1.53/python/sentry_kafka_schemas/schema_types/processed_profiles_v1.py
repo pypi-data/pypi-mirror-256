46f6020582642a72ddfd0b0b2ccbcd4ffac4d04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:07:11 +0000

    Add package sentry-kafka-schemas/0.1.53/sentry-kafka-schemas-0.1.53.tar.gz

---
+++ packages/sentry-kafka-schemas/sentry-kafka-schemas-0.1.53.tar.gz/sentry-kafka-schemas-0.1.53/python/sentry_kafka_schemas/schema_types/processed_profiles_v1.py
@@ -0,0 +1,66 @@
+from typing import TypedDict
+from typing_extensions import Required
+
+
+class _Root(TypedDict, total=False):
+    android_api_level: int
+    architecture: str
+    device_classification: str
+    device_locale: Required[str]
+    """ Required property """
+
+    device_manufacturer: Required[str]
+    """ Required property """
+
+    device_model: Required[str]
+    """ Required property """
+
+    device_os_build_number: str
+    device_os_name: Required[str]
+    """ Required property """
+
+    device_os_version: Required[str]
+    """ Required property """
+
+    duration_ns: Required["_Uint"]
+    """ Required property """
+
+    environment: str
+    profile_id: Required[str]
+    """ Required property """
+
+    organization_id: Required["_Uint"]
+    """ Required property """
+
+    platform: Required[str]
+    """ Required property """
+
+    project_id: Required["_Uint"]
+    """ Required property """
+
+    received: Required[int]
+    """ Required property """
+
+    retention_days: Required[int]
+    """ Required property """
+
+    trace_id: Required[str]
+    """ Required property """
+
+    transaction_id: Required[str]
+    """ Required property """
+
+    transaction_name: Required[str]
+    """ Required property """
+
+    version_code: Required[str]
+    """ Required property """
+
+    version_name: Required[str]
+    """ Required property """
+
+
+
+_Uint = int
+""" minimum: 0 """
+
