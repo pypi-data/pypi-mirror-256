4ae05fc541945b69b3c6481372083580247ede6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:05 +0000

    Add package fake-bpy-module-latest/20240216/fake-bpy-module-latest-20240216.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20240216.tar.gz/fake-bpy-module-latest-20240216/bpy/__init__.pyi
@@ -0,0 +1,18 @@
+import sys
+import typing
+import bpy.types
+
+from . import types
+from . import ops
+from . import path
+from . import utils
+from . import msgbus
+from . import app
+from . import props
+
+GenericType = typing.TypeVar("GenericType")
+context: "bpy.types.Context"
+
+data: "bpy.types.BlendData"
+""" Access to Blender's internal data
+"""
