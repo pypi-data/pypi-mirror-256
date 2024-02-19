1caea733322686b05748f299a01b726dc40fc676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:11:54 +0000

    Add package itss/0.2/itss-0.2-py3-none-any.whl

---
+++ packages/itss/itss-0.2-py3-none-any.whl/itss/fun2.py
@@ -0,0 +1,14 @@
+class itssgroup:
+
+	def __init__(self):
+
+		self.topic = "ITSS EF"
+		self.place = "ITSS CBE"
+
+	def cbe(self):
+
+		print("Topic:", self.topic)
+		print("Place:", self.place)
+		
+
+
