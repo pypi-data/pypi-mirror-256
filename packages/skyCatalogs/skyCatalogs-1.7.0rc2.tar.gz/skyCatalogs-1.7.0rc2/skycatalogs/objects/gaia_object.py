6daf94d09b6d7d68577f11ca2bc678a9f8f321d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:11 +0000

    Add package skyCatalogs/1.7.0rc2/skyCatalogs-1.7.0rc2.tar.gz

---
+++ packages/skyCatalogs/skyCatalogs-1.7.0rc2.tar.gz/skyCatalogs-1.7.0rc2/skycatalogs/objects/gaia_object.py
@@ -0,0 +1,255 @@
+import os
+import warnings
+from functools import wraps
+import itertools
+from collections.abc import Iterable
+from pathlib import PurePath
+import numpy as np
+import erfa
+import astropy.modeling
+from astropy import units as u
+import galsim
+import lsst.daf.butler as daf_butler
+import lsst.geom
+from lsst.meas.algorithms import ReferenceObjectLoader
+from skycatalogs.utils.shapes import Disk, PolygonalRegion
+from skycatalogs.objects.base_object import BaseObject, ObjectCollection
+
+
+__all__ = ['GaiaObject', 'GaiaCollection']
+
+
+def ignore_erfa_warnings(func):
+    @wraps(func)
+    def call_func(*args, **kwargs):
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', 'ERFA', erfa.ErfaWarning)
+            return func(*args, **kwargs)
+    return call_func
+
+
+_FILE_PATH = str(PurePath(__file__))
+_SKYCATALOGS_DIR = _FILE_PATH[:_FILE_PATH.rindex('/skycatalogs')]
+
+
+# Read in function for stellar temperature estimation given bp and rp
+# fluxes
+_TEMP_EST_DATA_FILE = os.path.join(_SKYCATALOGS_DIR, 'skycatalogs',
+                                   'data', 'gaia_dr2',
+                                   'gaia_dr2_temp_from_bp-rp_ratio.txt')
+_TEMP_FUNC = galsim.LookupTable(*np.genfromtxt(_TEMP_EST_DATA_FILE))
+
+# Read in the Gaia DR2 passband for the bp flux to use for setting the
+# SED normalizations.
+_GAIA_PB_FILE = os.path.join(_SKYCATALOGS_DIR, 'skycatalogs',
+                             'data', 'gaia_dr2',
+                             'GaiaDR2_RevisedPassbands.dat')
+_GAIA_PBS = np.genfromtxt(_GAIA_PB_FILE, names=['wl', 'g', 'g_err', 'bp',
+                                                'bp_err', 'rp', 'rp_err'])
+_index = np.where(_GAIA_PBS['bp'] < 99)  # Omit sentinel values.
+# Create the Bandpass object for the bp passband, using the 'AB' zero point.
+_GAIA_BP = galsim.Bandpass(
+    galsim.LookupTable(_GAIA_PBS['wl'][_index], _GAIA_PBS['bp'][_index]),
+    wave_type='nm').withZeropoint('AB').thin()
+
+
+class GaiaObject(BaseObject):
+    """
+    Object class for Gaia DR2 data read from the LSST reference catalog files.
+    """
+    _stellar_temperature = _TEMP_FUNC
+    _gaia_bp_bandpass = _GAIA_BP
+    _wavelengths = np.arange(250, 1250, 5, dtype=float)
+
+    def __init__(self, obj_pars, parent_collection, index):
+        """
+        Parameters
+        ----------
+        obj_pars: dict-like
+            Dictionary of object parameters as read directly from the
+            reference catalog.
+        parent_collection: ObjectCollection
+            Parent collection of this object.
+        index: int
+            Index of this object in the parent collection
+        """
+        ra = np.degrees(obj_pars['coord_ra'])
+        dec = np.degrees(obj_pars['coord_dec'])
+        # Form the object id from the GAIA catalog id with the string
+        # 'gaia_dr2_' prepended.
+        obj_id = f"gaia_dr2_{obj_pars['id']}"
+        super().__init__(ra, dec, obj_id, 'gaia_star',
+                         belongs_to=parent_collection, belongs_index=index)
+        self.use_lut = self._belongs_to._use_lut
+        bp_flux = obj_pars['phot_bp_mean_flux']
+        rp_flux = obj_pars['phot_rp_mean_flux']
+        if rp_flux == 0.0 or bp_flux == 0.0:
+            self.stellar_temp = None
+        else:
+            try:
+                self.stellar_temp = self._stellar_temperature(bp_flux/rp_flux)
+                # Convert from flux units of nJy to AB mag for the bp passband,
+                # which we will use to normalize the SED.
+                self.bp_mag = -2.5*np.log10(bp_flux*1e-9) + 8.90
+            except galsim.errors.GalSimRangeError:
+                self.stellar_temp = None
+            except RuntimeError:
+                self.stellar_temp = None
+
+    def blambda(self, wl):
+        clight = astropy.constants.c.value*1e2  # cm/s
+        nu = clight/(wl*1e-7)  # Hz
+        Bnu = astropy.modeling.physical_models.BlackBody(
+            temperature=self.stellar_temp*u.K)
+        return Bnu(nu*u.Hz).value*nu**2/clight/1e7  # erg/nm/cm^2/s
+
+    def get_observer_sed_component(self, component, mjd=None):
+        if component != 'this_object':
+            raise RuntimeError("Unknown SED component: %s", component)
+        if self.stellar_temp is None:
+            return None
+        if self.use_lut:
+            flambda = self.blambda(self._wavelengths)
+            lut = galsim.LookupTable(self._wavelengths, flambda,
+                                     interpolant='linear')
+            sed = galsim.SED(lut, wave_type='nm', flux_type='flambda')
+        else:
+            sed = galsim.SED(self.blambda, wave_type='nm', flux_type='flambda')
+        return sed.withMagnitude(self.bp_mag, self._gaia_bp_bandpass)
+
+    def get_gsobject_components(self, gsparams=None, rng=None):
+        if gsparams is not None:
+            gsparams = galsim.GSParams(**gsparams)
+        return {'this_object': galsim.DeltaFunction(gsparams=gsparams)}
+
+    def set_use_lut(self, use_lut):
+        self.use_lut = use_lut
+
+
+class GaiaCollection(ObjectCollection):
+    # Class methods
+    _gaia_config = None
+
+    @classmethod
+    def set_config(cls, config):
+        GaiaCollection._gaia_config = config
+
+    @classmethod
+    def get_config(cls):
+        return GaiaCollection._gaia_config
+
+    @ignore_erfa_warnings
+    @staticmethod
+    def load_collection(region, skycatalog, mjd=None):
+        '''
+        region      One of Disk, PolygonalRegion from skyCatalogs.utils.shapes.
+                    Box is not currently supported
+        skycatalog  An instance of the SkyCatalog class
+        mjd         Time at which objects are to be assembled. Ignored for
+                    Gaia stars
+        '''
+        if not skycatalog:
+            raise ValueError(f'GaiaCollection.load_collection: skycatalog cannot be None')
+        if isinstance(region, Disk):
+            ra = lsst.geom.Angle(region.ra, lsst.geom.degrees)
+            dec = lsst.geom.Angle(region.dec, lsst.geom.degrees)
+            center = lsst.geom.SpherePoint(ra, dec)
+            radius = lsst.geom.Angle(region.radius_as, lsst.geom.arcseconds)
+            refcat_region = lsst.sphgeom.Circle(center.getVector(), radius)
+        elif isinstance(region, PolygonalRegion):
+            refcat_region = region._convex_polygon
+        else:
+            raise TypeError(f'GaiaCollection.load_collection: {region} not supported')
+
+        source_type = 'gaia_star'
+
+        if GaiaCollection.get_config() is None:
+            gaia_section = skycatalog.raw_config['object_types']['gaia_star']
+            GaiaCollection.set_config(gaia_section)
+
+        butler_params = GaiaCollection.get_config()['butler_parameters']
+        butler = daf_butler.Butler(butler_params['repo'],
+                                   collections=butler_params['collections'])
+        refs = set(butler.registry.queryDatasets(butler_params['dstype']))
+        refCats = [daf_butler.DeferredDatasetHandle(butler, _, {})
+                   for _ in refs]
+        dataIds = [butler.registry.expandDataId(_.dataId) for _ in refs]
+        config = ReferenceObjectLoader.ConfigClass()
+        config.filterMap = {f'{_}': f'phot_{_}_mean' for _ in ('g', 'bp', 'rp')}
+        ref_obj_loader = ReferenceObjectLoader(dataIds=dataIds,
+                                               refCats=refCats,
+                                               config=config)
+
+        sed_method = GaiaCollection.get_config().get('sed_method', 'use_lut')
+        use_lut = (sed_method.strip().lower() == 'use_lut')
+        band = 'bp'
+        cat = ref_obj_loader.loadRegion(refcat_region, band).refCat
+        df = cat.asAstropy().to_pandas().sort_values('id')
+
+        if mjd is None:
+            raise RuntimeError("MJD needs to be provided for Gaia "
+                               "proper motion calculations.")
+        # The erfa.pmsafe code
+        # (https://pyerfa.readthedocs.io/en/latest/api/erfa.pmsafe.html)
+        # expects ra, dec in units of radians and pm_ra, pm_dec
+        # (proper motion) in units of radians/year.  These are the
+        # units used in the refcats files.  However, erfa.pmsafe
+        # expects parallax in arcseconds so convert from the refcats
+        # units of radians:
+        arcsec_per_radian = (1.0*u.radian).to(u.arcsec).value
+        df['parallax'] *= arcsec_per_radian
+        # radial velocity is missing from the Gaia DR2 refcats, so pass
+        # an array of zeros.
+        rv1 = np.zeros(len(df))
+        epNa = 2400000.5  # "part A" of starting and ending epochs for MJDs.
+        ep2b = mjd
+        pm_outputs = erfa.pmsafe(df['coord_ra'], df['coord_dec'],
+                                 df['pm_ra'], df['pm_dec'], df['parallax'],
+                                 rv1, epNa, df['epoch'], epNa, ep2b)
+        # Update ra, dec values.
+        df['coord_ra'] = pm_outputs[0]
+        df['coord_dec'] = pm_outputs[1]
+
+        return GaiaCollection(df, skycatalog, source_type, use_lut, mjd)
+
+    def __init__(self, df, sky_catalog, source_type, use_lut, mjd):
+        self.df = df
+        self._sky_catalog = sky_catalog
+        self._partition_id = None
+        self._id = np.array([f"gaia_dr2_{df.iloc[key]['id']}" for key in range(len(df))])
+        self._mask = None
+        self._object_type_unique = source_type
+        self._use_lut = use_lut
+        self._rdrs = []
+        self._object_class = GaiaObject
+        self._use_lut = use_lut
+        self._mjd = mjd
+
+    @property
+    def native_columns(self):
+        return set()
+
+    @property
+    def use_lut(self):
+        return self._use_lut
+
+    @property
+    def mjd(self):
+        return self._mjd
+
+    def __getitem__(self, key):
+        if isinstance(key, int) or isinstance(key, np.int64):
+            return GaiaObject(self.df.iloc[key], self, key)
+
+        elif type(key) == slice:
+            ixdata = [i for i in range(min(key.stop, len(self._id)))]
+            ixes = itertools.islice(ixdata, key.start, key.stop, key.step)
+            return [self._object_class(self.df.iloc[i], self, i) for i in ixes]
+
+        elif type(key) == tuple and isinstance(key[0], Iterable):
+            #  check it's a list of int-like?
+            return [self._object_class(self.df.iloc[i], self,
+                                       i) for i in key[0]]
+
+    def __len__(self):
+        return len(self.df)
---
+++ packages/skyCatalogs/skyCatalogs-1.7.0rc2.tar.gz/skyCatalogs-1.7.0rc2/skycatalogs/utils/config_utils.py
@@ -0,0 +1,311 @@
+import os
+import yaml
+import git
+import logging
+from .exceptions import ConfigDuplicateKeyError
+# import jsonschema
+
+from collections import namedtuple
+
+__all__ = ['Config', 'open_config_file', 'Tophat', 'create_config',
+           'assemble_SED_models', 'assemble_MW_extinction',
+           'assemble_cosmology', 'assemble_object_types',
+           'assemble_provenance', 'assemble_variability_models', 'write_yaml',
+           'CURRENT_SCHEMA_VERSION']
+
+CURRENT_SCHEMA_VERSION = '1.2.0'
+
+
+def open_config_file(config_file):
+    '''
+    Given path to config file, return a Config object
+    '''
+    with open(config_file) as f:
+        return Config(yaml.safe_load(f))
+
+
+Tophat = namedtuple('Tophat', ['start', 'width'])
+
+
+def _custom_dir(c, add):
+    '''
+    dir should return
+       * functions defined for c's class
+       * instance variables belonging to c
+       * public stuff from the contained object (some table-like thing)
+    '''
+    return dir(type(c)) + list(c.__dict__.keys()) + add
+
+
+class DelegatorBase:
+    '''
+    This class allows derived classes to delegate operations to a
+    member.   Here it is used to delegate dict operations to
+    the member self._cfg
+    '''
+
+    @property
+    def _delegate(self):
+        pub = [o for o in dir(self.default) if not o.startswith('_')]
+        return pub
+
+    def __getattr__(self, k):
+        if k in self._delegate:
+            return getattr(self.default, k)
+        raise AttributeError(k)
+
+    def __dir__(self):
+        return _custom_dir(self, self._delegate)
+
+    def __init__(self):
+        pass
+
+
+class Config(DelegatorBase):
+    '''
+    A wrapper around the dict which is the contents of a Sky Catalog
+    config file (a dict) which understands some of the semantics
+
+    '''
+    def __init__(self, cfg, logname=None):
+        '''
+        Parameters
+        ----------
+        cfg  is normally a dict, but it can also itself be a Config.
+        In this case, store its dict to delegate to.
+        '''
+        if isinstance(cfg, Config):
+            cfg = cfg._cfg
+
+        self._cfg = cfg
+        self._logname = logname
+
+        super().__init__()
+
+        self.default = cfg
+
+    def __getitem__(self, k):
+        '''
+        Specific override of __getitem__, delegating to dict member
+        so that [ ] syntax will be handled properly
+        '''
+        return self._cfg.__getitem__(k)
+
+    def list_sed_models(self):
+        return self._cfg['SED_models'].keys()
+
+    def list_object_types(self):
+        return self._cfg['object_types'].keys()
+
+    def get_sed_model(self, modelname):
+        return self._cfg['SED_models'][modelname]
+
+    def object_is_composite(self, objectname):
+        return 'composite' in self._cfg['object_types'][objectname]
+
+    def get_object_parent(self, objectname):
+        if 'parent' in self._cfg['object_types'][objectname]:
+            return self._cfg['object_types'][objectname]['parent']
+        else:
+            return None
+
+    def get_object_sedmodel(self, objectname):
+        if 'sed_model' in self._cfg['object_types'][objectname]:
+            return self._cfg['object_types'][objectname]['sed_model']
+        else:
+            return None
+
+    def get_tophat_parameters(self):
+        '''
+        Return list of named tuples
+        Should maybe be part of Sky Catalogs API
+        '''
+        if not self.get_config_value('SED_models/tophat', silent=True):
+            return None
+        raw_bins = self._cfg['SED_models']['tophat']['bins']
+
+        return [Tophat(b[0], b[1]) for b in raw_bins]
+
+    def get_config_value(self, key_path, silent=False):
+        '''
+        Find value belonging to key in a config.
+        Parameters
+        ----------
+        key_path    string   of form 'a/b/c/../q' where all but last
+                             component must be a dict
+        silent      boolean  If False (default) complain when key is not
+                             found.  Else return None
+
+        Returns
+        -------
+        Value associated with key_path if config contains such a path
+        '''
+        path_items = key_path.split('/')
+        d = self._cfg
+        for i in path_items[:-1]:
+            if i not in d.keys():
+                if silent:
+                    return None
+                raise ValueError(f'Item {i} not found')
+            d = d[i]
+            if not isinstance(d, dict):
+                raise ValueError(f'intermediate {d} is not a dict')
+        return d[path_items[-1]]
+
+    def add_key(self, k, v):
+        '''
+        Parameters
+        ----------
+        k        Top-level key belonging to the schema
+        v        value for the key
+        '''
+
+        if k in self._cfg:
+            raise ConfigDuplicateKeyError(k)
+        self._cfg[k] = v
+
+    def write_config(self, dirpath, filename=None, overwrite=False):
+        '''
+        Export self to yaml document and write to specified directory.
+        If filename is None the file will be named after catalog_name
+
+        Parameters
+        ----------
+        dirpath        Directory to which file will be written
+        filename       If supplied, use for filename
+        overwrite      By default do not overwrite existing file with same path
+
+        Return
+        ------
+        Full path of output config
+        '''
+        # ##self.validate()   skip for now
+
+        if not filename:
+            filename = self._cfg['catalog_name'] + '.yaml'
+
+        return write_yaml(self._cfg, os.path.join(dirpath, filename),
+                          overwrite=overwrite, logname=self._logname)
+
+
+def write_yaml(input_dict, outpath, overwrite=False, logname=None):
+    if not overwrite:
+        try:
+            with open(outpath, mode='x') as f:
+                yaml.dump(input_dict, f)
+        except FileExistsError:
+            txt = 'Config.write_yaml: Will not overwrite pre-existing config '
+            if logname:
+                logger = logging.getLogger(logname)
+                logger.warning(txt + outpath)
+            else:
+                print(txt + outpath)
+            return
+    else:
+        with open(outpath, mode='w') as f:
+            yaml.dump(input_dict, f)
+
+    return outpath
+
+
+def create_config(catalog_name, logname=None):
+    return Config({'catalog_name': catalog_name}, logname)
+#                  'schema_version': schema_version,
+#                  'code_version': desc.skycatalogs.__version__}, logname)
+
+
+def assemble_cosmology(cosmology):
+    d = {k: cosmology.__getattribute__(k) for k in ('Om0', 'Ob0', 'sigma8',
+                                                    'n_s')}
+    d['H0'] = float(cosmology.H0.value)
+    return d
+
+
+def assemble_MW_extinction():
+    av = {'mode': 'data'}
+    rv = {'mode': 'constant', 'value': 3.1}
+    return {'r_v': rv, 'a_v': av}
+
+
+def assemble_object_types(pkg_root, galaxy_nside=32):
+    '''
+    Include all supported object types even though a particular catalog
+    might not use them all
+    '''
+    t_path = os.path.join(pkg_root, 'cfg', 'object_types.yaml')
+    with open(t_path) as f:
+        d = yaml.safe_load(f)
+        d['object_types']['galaxy']['area_partition']['nside'] = galaxy_nside
+        return d['object_types']
+
+
+def assemble_SED_models(bins):
+    tophat_d = {'units': 'angstrom', 'bin_parameters': ['start', 'width']}
+    tophat_d['bins'] = bins
+    file_nm_d = {'units': 'nm'}
+    return {'tophat': tophat_d, 'file_nm': file_nm_d}
+
+
+def assemble_provenance(pkg_root, inputs={}, schema_version=None):
+
+    if not schema_version:
+        schema_version = CURRENT_SCHEMA_VERSION
+    import skycatalogs
+    version_d = {'schema_version': schema_version}
+    if '__version__' in dir(skycatalogs):
+        code_version = skycatalogs.__version__
+    else:
+        code_version = 'unknown'
+    version_d['code_version'] = code_version
+
+    repo = git.Repo(pkg_root)
+    has_uncommited = repo.is_dirty()
+    has_untracked = (len(repo.untracked_files) > 0)
+
+    git_d = {}
+    git_d['git_hash'] = repo.commit().hexsha
+    git_d['git_branch'] = repo.active_branch.name
+    status = []
+    if has_uncommited:
+        status.append('UNCOMMITTED_FILES')
+    if has_untracked:
+        status.append('UNTRACKED_FILES')
+    if len(status) == 0:
+        status.append('CLEAN')
+    git_d['git_status'] = status
+
+    if inputs:
+        return {'versioning': version_d, 'skyCatalogs_repo': git_d,
+                'inputs': inputs}
+    else:
+        return{'versioning': version_d, 'skyCatalogs_repo': git_d}
+
+
+# In config just keep track of models by object type. Information
+# about the parameters they require is internal to the code.
+_AGN_MODELS = ['agn_random_walk']
+_SNCOSMO_MODELS = ['sn_salt2_extended']
+
+
+def assemble_variability_models(object_types):
+    '''
+    Add information about all known variability models for supplied object
+    types.
+    Parameters
+    ----------
+    object_types: iterable of (pointsource) object type names
+
+    Returns
+    -------
+    A dict with object type names for keys.  Values are also dicts with
+    keys = model name and values defining struct of parameters for that
+    model, e.g. ordered dict with parameter names for keys and parameter
+    data types for values
+    '''
+    models = dict()
+    if 'agn' in object_types:
+        models['agn'] = _AGN_MODELS
+    if 'sncosmo' in object_types:
+        models['sncosmo'] = _SNCOSMO_MODELS
+
+    return models
