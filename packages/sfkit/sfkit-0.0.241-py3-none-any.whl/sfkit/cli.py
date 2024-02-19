149b936436eba0c8911c22690ac577515f1e3116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 00:05:54 +0000

    Add package sfkit/0.0.241/sfkit-0.0.241-py3-none-any.whl

---
+++ packages/sfkit/sfkit-0.0.241-py3-none-any.whl/sfkit/cli.py
@@ -0,0 +1,35 @@
+from sfkit.auth.auth import auth
+from sfkit.auth.setup_networking import setup_networking
+from sfkit.encryption.generate_personal_keys import generate_personal_keys
+from sfkit.parser import get_parser
+from sfkit.protocol.register_data import register_data
+from sfkit.protocol.run_protocol import run_protocol
+
+
+def main() -> None:
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if args.command == "auth":
+        study_id: str = args.study_id or ""
+        auth(study_id)
+    elif args.command == "networking":
+        ports = args.ports or ""
+        ip_address = args.ip_address or ""
+        setup_networking(ports, ip_address)
+    elif args.command == "generate_keys":
+        generate_personal_keys()
+    elif args.command == "register_data":
+        geno_binary_file_prefix = args.geno_binary_file_prefix or ""
+        data_path = args.data_path or ""
+        register_data(geno_binary_file_prefix, data_path)
+    elif args.command == "run_protocol":
+        phase: str = ""  # args.phase or ""
+        demo: bool = args.demo or False
+        visualize_results: str = args.visualize_results or ""
+        results_path: str = args.results_path or ""
+        retry: bool = args.retry or False
+        skip_cp0: bool = args.skip_cp0 or False
+        run_protocol(phase, demo, visualize_results, results_path, retry, skip_cp0)
+    else:
+        parser.print_help()
---
+++ packages/sfkit/sfkit-0.0.241-py3-none-any.whl/sfkit/utils/constants.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+
+def is_installed(binary: str) -> bool:
+    return shutil.which(binary) is not None
+
+
+SFKIT_API_URL = os.environ.get("SFKIT_API_URL", "https://sfkit-website-bhj5a4wkqa-uc.a.run.app/api")
+print(f"SFKIT_API_URL: {SFKIT_API_URL}")
+METADATA_VM_IDENTITY_URL = (
+    "http://metadata.google.internal/computeMetadata/v1/"
+    "instance/service-accounts/default/identity?"
+    "audience={}&format={}&licenses={}"
+)
+BLOCKS_MODE = "usingblocks-"
+SFKIT_DIR = os.environ.get("SFKIT_DIR", os.path.join(os.path.expanduser("~"), ".config", "sfkit"))
+AUTH_FILE = os.path.join(SFKIT_DIR, "auth.txt")
+AUTH_KEY = os.path.join(SFKIT_DIR, "auth_key.txt")
+IS_DOCKER = os.path.exists("/.dockerenv")
+IS_INSTALLED_VIA_SCRIPT = is_installed("sfgwas") and is_installed("plink2") and is_installed("GwasClient")
+EXECUTABLES_PREFIX = os.path.expanduser("~") + "/.local/" if IS_INSTALLED_VIA_SCRIPT else ""
+SFKIT_PREFIX = "sfkit: "
+OUT_FOLDER = os.path.join(os.environ.get("SFKIT_DIR", ""), "out")
+ENCRYPTED_DATA_FOLDER = os.path.join(os.environ.get("SFKIT_DIR", ""), "encrypted_data")
+SFKIT_PROXY_ON: bool = os.getenv("SFKIT_PROXY_ON", "false").lower() == "true"
