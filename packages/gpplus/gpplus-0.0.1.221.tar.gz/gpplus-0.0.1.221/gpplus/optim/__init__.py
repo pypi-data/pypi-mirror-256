885c0d6088ea15f7009259cc9a96020c65ee0e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:47 +0000

    Add package gpplus/0.0.1.221/gpplus-0.0.1.221.tar.gz

---
+++ packages/gpplus/gpplus-0.0.1.221.tar.gz/gpplus-0.0.1.221/gpplus/optim/__init__.py
@@ -0,0 +1,5 @@
+from .mll_scipy import fit_model_scipy
+from .mll_torch import fit_model_torch
+from .mll_noise_tune import noise_tune
+from .mll_noise_tune2 import noise_tune2
+from .mll_noise_tune3 import noise_tune3_fixed_noise
