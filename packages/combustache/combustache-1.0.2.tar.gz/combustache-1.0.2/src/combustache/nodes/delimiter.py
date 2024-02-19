668390b713943da09d41a163a9b8fd031b081bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:25:47 +0000

    Add package combustache/1.0.2/combustache-1.0.2.tar.gz

---
+++ packages/combustache/combustache-1.0.2.tar.gz/combustache-1.0.2/src/combustache/nodes/delimiter.py
@@ -0,0 +1,38 @@
+from combustache.exceptions import DelimiterError
+from combustache.nodes.node import Node
+
+
+class Delimiter(Node):
+    left = '='
+    right = '='
+    ignorable = True
+
+    def __init__(
+        self,
+        content: str,
+        tag_start: int,
+        tag_end: int,
+        template: str,
+        template_start: int,
+        template_end: int,
+        left_delimiter: str,
+        right_delimiter: str,
+    ) -> None:
+        super().__init__(
+            content,
+            tag_start,
+            tag_end,
+            template,
+            template_start,
+            template_end,
+            left_delimiter,
+            right_delimiter,
+        )
+        split = self.content.split()
+        if len(split) != 2:
+            raise DelimiterError(
+                'Impossible delimiter tag found '
+                f'{self.presentable} at {self.start}.'
+            )
+        self.left_delimiter = split[0]
+        self.right_delimiter = split[1]
