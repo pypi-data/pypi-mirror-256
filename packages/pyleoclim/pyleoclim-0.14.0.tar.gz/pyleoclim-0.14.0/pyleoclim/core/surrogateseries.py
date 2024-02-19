be730e24dfa9ea47d6891b7d429370f8f8a1ffab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:34 +0000

    Add package pyleoclim/0.14.0/pyleoclim-0.14.0.tar.gz

---
+++ packages/pyleoclim/pyleoclim-0.14.0.tar.gz/pyleoclim-0.14.0/pyleoclim/core/surrogateseries.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SurrogateSeries is a child of MultipleSeries, designed for Monte Carlo tests 
+"""
+
+from ..core.multipleseries import MultipleSeries
+
+class SurrogateSeries(MultipleSeries):
+    ''' Object containing surrogate timeseries, usually obtained through recursive modeling (e.g., AR(1))
+
+    Surrogate Series is a child of MultipleSeries. All methods available for MultipleSeries are available for surrogate series.
+    '''
+    def __init__(self, series_list, surrogate_method=None, surrogate_args=None): 
+        self.series_list = series_list
+        self.surrogate_method = surrogate_method
+        self.surrogate_args = surrogate_args
