a58396d10b3854dd1f02a843479201406f60a1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 01:26:34 +0000

    Add package trace-attributes/1.0.11/trace-attributes-1.0.11.tar.gz

---
+++ packages/trace-attributes/trace-attributes-1.0.11.tar.gz/trace-attributes-1.0.11/src/python/langtrace/trace_attributes/__init__.py
@@ -0,0 +1,20 @@
+# __init__.py
+from enum import Enum
+
+from .models.langtrace_span_attributes import LangTraceSpanAttributes
+from .models.openai_span_attributes import OpenAISpanAttributes
+
+
+class Event(Enum):
+    STREAM_START = "stream.start"
+    STREAM_OUTPUT = "stream.output"
+    STREAM_END = "stream.end"
+class LlamaIndexMethods(Enum):
+    TASK_LLAMAINDEX_BASEEXTRACTOR_EXTRACT = "task.llamaindex.BaseExtractor.extract"
+    TASK_LLAMAINDEX_SIMPLEPROMPT_CALL = "task.llamaindex.SimplePrompt.call"
+    TASK_LLAMAINDEX_CHATENGINE_EXTRACT = "task.llamaindex.ChatEngine.chat"
+    TASK_LLAMAINDEX_RETRIEVER_RETRIEVE = "task.llamaindex.Retriever.retrieve"
+    TASK_LLAMAINDEX_QUERYENGINE_QUERY = "task.llamaindex.QueryEngine.query"
+
+# Export only what you want to be accessible directly through `import my_package`
+__all__ = ['OpenAISpanAttributes', 'LangTraceSpanAttributes', 'Event', 'LlamaIndexMethods']
