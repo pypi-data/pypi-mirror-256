6dbafeebc2181bfb75bb401192421c6d6449a6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:12:37 +0000

    Add package akshey-dsnd-probability/0.1/akshey_dsnd_probability-0.1.tar.gz

---
+++ packages/akshey-dsnd-probability/akshey_dsnd_probability-0.1.tar.gz/akshey_dsnd_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='akshey_dsnd_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['akshey_dsnd_probability'],
+      author = 'Akshey Nama',
+      zip_safe=False)
