c5b09ba64aca13238f1bf2081d0b8dbcd6a0c53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 02:11:54 +0000

    Add package pih-med-auto/1.0/pih_med_auto-1.0-py3-none-any.whl

---
+++ packages/pih-med-auto/pih_med_auto-1.0-py3-none-any.whl/MedicalAutomationService/const.py
@@ -0,0 +1,23 @@
+import ipih
+
+from pih import A
+from pih.collections.service import ServiceDescription
+
+NAME: str = "MedicalAutomation"
+
+HOST = A.CT_H.BACKUP_WORKER
+
+VERSION: str = "1.0"
+
+MODULES: tuple[str, ...] = (
+    A.PTH_FCD_DIST.NAME(A.CT_SR.MOBILE_HELPER.standalone_name),  # type: ignore
+)
+
+SD: ServiceDescription = ServiceDescription(
+    name=NAME,
+    description="Medical Automation service",
+    host=HOST.NAME,
+    version=VERSION,
+    standalone_name="med_auto",
+    use_standalone=True,
+)
